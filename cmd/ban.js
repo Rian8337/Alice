80cfb587c328dfb075126aa6a4cef949d6cdc6e5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-25 19:53:47 +0700

    Add files via upload

---
+++ cmd/ban.js
@@ -0,0 +1,53 @@
+let Discord = require('discord.js');
+let config = require('../config.json');
+
+module.exports.run = (client, message, args) => {
+    if (message.channel instanceof Discord.DMChannel || message.author.id != '386742340968120321') return;
+    let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
+    if (!logchannel) {
+        message.channel.send(`Please create ${config.management_channel} first!`);
+        return;
+    }
+    let userid = args[0];
+    if (!userid) {
+        message.channel.send("Please specify the correct user to ban!");
+        return;
+    }
+    userid = userid.replace('<@!','');
+    userid = userid.replace('<@','');
+    userid = userid.replace('>','');
+    if (userid == message.author.id) {
+        message.channel.send("You cannot ban yourself!");
+        return;
+    }
+    userid = message.guild.members.get(userid);
+    if (!userid.bannable) {
+        message.channel.send("This user cannot be banned!");
+        return;
+    }
+    let reason = args.slice(1).join(" ");
+    if (!reason) {
+        message.channel.send("Please enter your reason.");
+        return;
+    }
+    message.guild.ban(userid, {reason: reason}).then (() => {
+        message.author.lastMessage.delete();
+
+        const embed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setFooter("Alice Synthesis Thirty", "https://i.imgur.com/S5yspQs.jpg")
+            .setTimestamp(new Date())
+            .setColor(message.member.highestRole.hexColor)
+            .setDescription("**Ban executed**")
+            .addField("Banned user: " + userid.user.username, "User ID: " + userid.id)
+            .addField("=================", "Reason:\n" + reason);
+
+        logchannel.send({embed})
+    }).catch(() => {
+        message.channel.send("User is already banned!")
+    })
+};
+
+module.exports.help = {
+    name: "ban"
+};
---
+++ cmd/unban.js
@@ -0,0 +1,45 @@
+let Discord = require('discord.js');
+let config = require('../config.json');
+
+module.exports.run = async (client, message, args) => {
+    if (message.channel instanceof Discord.DMChannel || message.author.id != '386742340968120321') return;
+    let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
+    if (!logchannel) {
+        message.channel.send(`Please create ${config.management_channel} first!`);
+        return;
+    }
+    let userid = await client.fetchUser(args[0]);
+    if (!userid) {
+        message.channel.send("Please specify the correct user ID to unban!");
+        return;
+    }
+    if (userid.id == message.author.id) {
+        message.channel.send("You cannot unban yourself!");
+        return;
+    }
+    let reason = args.slice(1).join(" ");
+    if (!reason) {
+        message.channel.send("Please enter your reason.");
+        return;
+    }
+    message.guild.unban(userid, reason).then (() => {
+        message.author.lastMessage.delete().catch();
+
+        const embed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setFooter("Alice Synthesis Thirty", "https://i.imgur.com/S5yspQs.jpg")
+            .setTimestamp(new Date())
+            .setColor(message.member.highestRole.hexColor)
+            .setDescription("**Unban executed**")
+            .addField("Unbanned user: " + userid.username, "User ID: " + userid.id)
+            .addField("=================", "Reason:\n" + reason);
+
+        logchannel.send({embed})
+    }).catch(() => {
+        message.channel.send("User is not banned!")
+    })
+};
+
+module.exports.help = {
+    name: "unban"
+};
