44f1f97b4042fb8b207c5808f7283d001cc004a0
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-25 13:14:26 +0700

    Update say.js

--- cmd/say.js
+++ cmd/say.js
@@ -1,10 +1,10 @@
 module.exports.run = (client, message, args) => {
 	let sayMessage = args.join(" ");
 	if (sayMessage.search("@everyone") != -1 || sayMessage.search("@here") != -1) {
-		message.channel.send("No no, you dummy");
+		message.channel.send("**S**: No no, you dummy");
 		return;
 	}
-	message.channel.send(sayMessage);
+	message.channel.send("**S**: " + sayMessage);
 };
 
 module.exports.help = {
