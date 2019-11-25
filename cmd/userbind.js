b8a6ed9dca657355810bed1b0ade323ccf95291b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-25 12:00:41 +0700

    Update userbind.js

--- cmd/userbind.js
+++ cmd/userbind.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 module.exports.run = (client, message, args, maindb) => {
 	let uid = Number(args[0]);
-	if (!Number.isInteger(args[0])) {message.channel.send("Invalid uid")}
+	if (!Number.isInteger(uid)) {message.channel.send("Invalid uid")}
 	else {
 		let name="";
 		var options = {
@@ -12,7 +12,7 @@ module.exports.run = (client, message, args, maindb) => {
 			path: "/profile.php?uid="+uid+".html"
 		};
 
-		var content = "";   
+		var content = "";
 
 		var req = http.request(options, function(res) {
 			res.setEncoding("utf8");
@@ -21,49 +21,49 @@ module.exports.run = (client, message, args, maindb) => {
 			});
 
 			res.on("end", function () {
-			const a = content;
-			let b = a.split('\n'), c = []; 
-			for (x = 0; x < b.length; x++) {
-				if (b[x].includes('h3 m-t-xs m-b-xs')) {
-					b[x]=b[x].replace('<div class="h3 m-t-xs m-b-xs">',"");
-					b[x]=b[x].replace('<\/div>',"");
-					b[x]=b[x].trim();
-					name = b[x]
+				const a = content;
+				let b = a.split('\n'), c = [];
+				for (x = 0; x < b.length; x++) {
+					if (b[x].includes('h3 m-t-xs m-b-xs')) {
+						b[x]=b[x].replace('<div class="h3 m-t-xs m-b-xs">',"");
+						b[x]=b[x].replace('<\/div>',"");
+						b[x]=b[x].trim();
+						name = b[x]
+					}
 				}
-			}
-			let binddb = maindb.collection("userbind");
-			let query = { discordid: message.author.id };
-			var bind = {
-				discordid: message.author.id,
-				uid: uid,
-				username: name,
-				pptotal: 0,
-				pp: []
-			};
-			var updatebind = {
-				$set: {
+				let binddb = maindb.collection("userbind");
+				let query = { discordid: message.author.id };
+				var bind = {
 					discordid: message.author.id,
 					uid: uid,
-					username: name 
-				}
-			};
-			binddb.find(query).toArray(function(err, res) {
-				if (err) throw err;
-				if (!res[0]) {
-					binddb.insertOne(bind, function(err, res) {
-						if (err) throw err;
-						console.log("bind added");
-						message.channel.send("Haii <3, binded <@"+message.author.id+"> to uid \n" +
-							"\t\t\t\t\t\tmessage.channel.send(\"Haii <3, binded <@\"+message.author.id+\"> to uid \"+uid);\n" +
-							"\t\t\t\t\t});\n" +
-							"\t\t\t\t}\n" +
-							"\t\t\t\telse {\n" +
-							"\t\t\t\t\tbinddb.updateOne(query, updatebind, function(err, res) {\n" +
-							"\t\t\t\t\t\tif (err) throw err;\n" +
-							"\t\t\t\t\t\tconsole.log(\"bind updated\");"+uid);
-					});
-				}
-			});
+					username: name,
+					pptotal: 0,
+					pp: []
+				};
+				var updatebind = {
+					$set: {
+						discordid: message.author.id,
+						uid: uid,
+						username: name
+					}
+				};
+				binddb.find(query).toArray(function(err, res) {
+					if (err) throw err;
+					if (!res[0]) {
+						binddb.insertOne(bind, function(err, res) {
+							if (err) throw err;
+							console.log("bind added");
+							message.channel.send("Haii <3, binded <@"+message.author.id+"> to uid "+uid);
+						});
+					}
+					else {
+						binddb.updateOne(query, updatebind, function(err, res) {
+							if (err) throw err;
+							console.log("bind updated");
+							message.channel.send("Haii <3, binded <@"+message.author.id+"> to uid "+uid);
+						});
+					}
+				});
 			});
 		});
 		req.end();
@@ -73,5 +73,3 @@ module.exports.run = (client, message, args, maindb) => {
 module.exports.help = {
 	name: "userbind"
 };
-
-
