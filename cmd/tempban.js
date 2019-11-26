099c16efeae269f56ed7d85b417a7d012f16e929
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-26 12:42:59 +0700

    Update tempban.js

--- cmd/tempban.js
+++ cmd/tempban.js
@@ -1,7 +1,7 @@
 let Discord = require('discord.js');
 let config = require('../config.json');
 
-module.exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
     if (message.channel instanceof Discord.DMChannel || message.author.id != '386742340968120321') return;
     let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
     if (!logchannel) {
@@ -21,7 +21,7 @@ module.exports.run = (client, message, args) => {
         return;
     }
     let toban = message.guild.members.get(userid);
-    if (!toban) toban = userid;
+    if (!toban) toban = await client.fetchUser(userid);
 
 
 
