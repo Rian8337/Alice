d65bd873782e9998f0113e43e8570a3e744e76e5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-26 13:17:46 +0700

    Update tempban.js

--- cmd/tempban.js
+++ cmd/tempban.js
@@ -67,7 +67,7 @@ module.exports.run = async (client, message, args) => {
             .setTimestamp(new Date())
             .setColor(message.member.highestRole.hexColor)
             .setDescription("**Temporary ban executed**")
-            .addField("Banned user: " + userid.user.username + "\nUser ID: " + userid.id, "Time: " + (bantime * 24) + "hours")
+            .addField("Banned user: " + toban.user.username + "\nUser ID: " + userid, "Time: " + (bantime * 24) + "hours")
             .addField("=================", "Reason:\n" + reason);
 
         logchannel.send({embed})
