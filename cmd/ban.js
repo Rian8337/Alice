399de3ca04abb1d38efefaa3846512935968535e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-26 15:22:53 +0700

    Update ban.js

--- cmd/ban.js
+++ cmd/ban.js
@@ -3,20 +3,22 @@ let config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
     if (message.channel instanceof Discord.DMChannel || message.author.id != '386742340968120321') return;
+
     let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
     if (!logchannel) {
         message.channel.send(`Please create ${config.management_channel} first!`);
         return;
     }
+
     let userid = args[0];
     if (!userid) {
         message.channel.send("Please specify the correct user to ban!");
         return;
     }
-
     userid = userid.replace('<@!','');
     userid = userid.replace('<@','');
     userid = userid.replace('>','');
+
     if (isNaN(userid)) {
         message.channel.send("Please specify the correct user to ban!");
         return;
@@ -25,16 +27,25 @@ module.exports.run = async (client, message, args) => {
         message.channel.send("You cannot ban yourself!");
         return;
     }
+
+    let banned = message.guild.fetchBans().find(user => user.id === userid);
+    if (banned) {
+        message.channel.send("User is already banned!");
+        return;
+    }
+
     let toban = await client.fetchUser(userid);
     if (!toban) {
         message.channel.send("User not found!");
         return;
     }
+    
     let reason = args.slice(1).join(" ");
     if (!reason) {
         message.channel.send("Please enter your reason.");
         return;
     }
+    
     message.guild.ban(toban, {reason: reason}).then (() => {
         message.author.lastMessage.delete();
 
@@ -45,14 +56,10 @@ module.exports.run = async (client, message, args) => {
             .setColor(message.member.highestRole.hexColor)
             .setDescription("**Ban executed**")
             .addField("Banned user: " + toban.username, "User ID: " + userid)
-            .addField("=================", "Reason:\n" + reason);
+            .addField("====================", "Reason:\n" + reason);
 
         logchannel.send({embed})
-    }).catch(() => {
-        message.channel.send("User is already banned!")
     })
-
-   
 };
 
 module.exports.help = {
