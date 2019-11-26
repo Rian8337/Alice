437841768b535c4309542944d23178f455a60064
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-26 11:59:44 +0700

    Create tempban.js

---
+++ cmd/tempban.js
@@ -0,0 +1,68 @@
+let Discord = require('discord.js');
+let config = require('../config.json');
+
+module.exports.run = (client, message, args) => {
+    if (message.channel instanceof Discord.DMChannel || message.author.id != '386742340968120321') return;
+    let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
+    if (!logchannel) {
+        message.channel.send(`Please create ${config.management_channel} first!`);
+        return;
+    }
+    let userid = args[0];
+    if (!userid) {
+        message.channel.send("Please specify the correct user to ban!");
+        return;
+    }
+    userid = userid.replace('<@!','');
+    userid = userid.replace('<@','');
+    userid = userid.replace('>','');
+    if (userid == message.author.id) {
+        message.channel.send("You cannot ban yourself!");
+        return;
+    }
+    userid = message.guild.members.get(userid);
+    if (!userid.bannable) {
+        message.channel.send("This user cannot be banned!");
+        return;
+    }
+    let bantime = args[1];
+    if (bantime.content.endsWith("h")) bantime = parseFloat(bantime) / 24;
+    else if (bantime.content.endsWith("d")) bantime = parseFloat(bantime);
+    else {
+        message.channel.send("Please specify if time is in hours or days!");
+        return;
+    }
+    if (isNaN(bantime) || bantime <= 0) {
+        message.channel.send("Invalid ban time");
+        return;
+    }
+    if (bantime > 7) {
+        message.channel.send("Maximum ban time is 7 days");
+        return;
+    }
+    let reason = args.slice(2).join(" ");
+    if (!reason) {
+        message.channel.send("Please enter your reason.");
+        return;
+    }
+    message.guild.ban(userid, {days: bantime, reason: reason}).then (() => {
+        message.author.lastMessage.delete();
+
+        const embed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setFooter("Alice Synthesis Thirty", "https://i.imgur.com/S5yspQs.jpg")
+            .setTimestamp(new Date())
+            .setColor(message.member.highestRole.hexColor)
+            .setDescription("**Temporary ban executed**")
+            .addField("Banned user: " + userid.user.username + "\nUser ID: " + userid.id, "Time: " + (bantime * 24) + "hours")
+            .addField("=================", "Reason:\n" + reason);
+
+        logchannel.send({embed})
+    }).catch(() => {
+        message.channel.send("User is already banned!")
+    })
+};
+
+module.exports.help = {
+    name: "tempban"
+};
