dbb6666855a2a5893d5f4796334c0db6c56a8e92
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-01 10:33:21 +0700

    Update togglestatus.js

--- cmd/togglestatus.js
+++ cmd/togglestatus.js
@@ -3,17 +3,11 @@ let config = require('../config.json');
 module.exports.run = (client, message, args) => {
     if (message.author.id != '386742340968120321') return;
     let x = args[0];
-    if (!x || x < 0 || isNaN(x)) {
-        message.channel.send("Invalid list");
-        return
-    }
+    if (!x || x < 0 || isNaN(x)) return;
     let activity = config.activity_list;
     if (x == 1) {
         let episode = args[1];
-        if (episode <= 0 || isNaN(episode)) {
-            message.channel.send("Invalid episode");
-            return
-        }
+        if (episode <= 0 || isNaN(episode)) return;
         client.user.setActivity(activity[x][0] + episode, {type: activity[x][1], url: "https://github.com/Rian8337/Alice"}).catch();
         message.channel.send("Status changed to " + activity[x][0] + episode + ", activity type: " + activity[x][1])
     }
