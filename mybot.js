d81f3e227bbd21a991647bab58380130e8b59849
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-01 14:19:00 +0700

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -42,12 +42,12 @@ client.on("ready", () => {
 });
 
 client.on("message", message => {
+	if (message.author.bot) return;
 	let owner = message.guild.members.get('386742340968120321');
 	if (!owner) {
 		message.channel.send("Bot owner needs to be in the server for the bot to function!").catch(e => console.log(e));
 		return
 	}
-	if (message.author.bot) return;
 	let msgArray = message.content.split(/\s+/g);
 	let command = msgArray[0];
 	let args = msgArray.slice(1);
