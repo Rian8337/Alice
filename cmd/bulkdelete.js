2fe5d6ee755a2bd30ff5c0e0d2c26298b5307830
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-04 22:43:00 +0700

    Update bulkdelete.js

--- cmd/bulkdelete.js
+++ cmd/bulkdelete.js
@@ -1,20 +1,12 @@
 let Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-    if (message.author.id != '386742340968120321') return;
+    if (message.author.id != '386742340968120321') return message.channel.send("You don't have permission to do this");
     let todelete = parseInt(args[0]);
-    if (!todelete) {
-        message.channel.send("Please specify number of messages to delete");
-        return;
-    }
-    if (isNaN(todelete)) {
-        message.channel.send("Invalid number of messages to delete");
-        return;
-    }
-    if (todelete < 2 || todelete > 100) {
-        message.channel.send("Invalid number of messages to delete, must be in range of 2-100");
-        return;
-    }
+    if (!todelete) return message.channel.send("Please specify number of messages to delete");
+    if (isNaN(todelete)) return message.channel.send("Invalid number of messages to delete");
+    if (todelete < 2 || todelete > 100) return message.channel.send("Invalid number of messages to delete, must be in range of 2-100");
+    
     message.author.lastMessage.delete().catch(e => console.log(e));
     message.channel.bulkDelete(todelete).then (() => {
         const embed = new Discord.RichEmbed()
