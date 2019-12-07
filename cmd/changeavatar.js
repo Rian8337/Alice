10eaabf4b79df2f5bd8e7d6bd937831026b9417d
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-07 15:47:16 +0700

    Update changeavatar.js

--- cmd/changeavatar.js
+++ cmd/changeavatar.js
@@ -1,13 +1,19 @@
 let config = require('../config.json');
+let cd = new Set();
 
 module.exports.run = (client, message, args) => {
     if (message.author.id != '386742340968120321') return message.channel.send("You don't have permission to do this");
     let x = args[0];
+    if (cd.has(message.author.id)) return message.channel.send("Please wait for a bit before using this command again!");
     if (isNaN(x)) return message.channel.send("Invalid input");
     let avatar = config.avatar_list;
     if (!avatar[x]) return message.channel.send("There is no avatar!");
     client.user.setAvatar(avatar[x]).catch();
     message.channel.send(`Changed avatar to ${avatar[x]}`)
+    cd.add(message.author.id);
+    setTimeout(() => {
+        cd.delete(message.author.id)
+    }, 5000)
 };
 
 module.exports.help = {
