e01367ce9728bbf401bf50945a15cd61f48e8287
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-07 15:34:43 +0700

    Update togglestatus.js

--- cmd/togglestatus.js
+++ cmd/togglestatus.js
@@ -1,7 +1,7 @@
 let config = require('../config.json');
 
 module.exports.run = (client, message, args) => {
-    if (message.author.id != '386742340968120321') return;
+    if (message.author.id != '386742340968120321') return message.channel.send("You don't have permission to do this");
     let x = args[0];
     if (!x || x < 0 || isNaN(x)) return message.channel.send("Invalid input");
     let activity = config.activity_list;
