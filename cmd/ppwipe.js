dd063385bd6f9b4a89f2ac7b90a3e6eb8dc0434e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-08 17:57:52 +0700

    Update ppwipe.js

--- cmd/ppwipe.js
+++ cmd/ppwipe.js
@@ -1,4 +1,5 @@
 var Discord = require('discord.js');
+let config = require('../config.json');
 
 module.exports.run = (client, message, args, maindb) => {
 	if (message.author.id != '386742340968120321') return message.channel.send("You don't have permission to do this");
@@ -18,7 +19,7 @@ module.exports.run = (client, message, args, maindb) => {
 	binddb.find(query).toArray(function (err, userres) {
 		if (err) {
 			console.log(err);
-			return message.channel.send("Error: Unable to retrieve user data from database! Please try again")
+			return message.channel.send("Error: Empty database response. Please try again!")
 		}
 		if (userres[0]) {
 			let uid = userres[0].uid;
@@ -34,10 +35,13 @@ module.exports.run = (client, message, args, maindb) => {
 			pplist = [];
 			message.channel.send("User pp data has been wiped");
 
+			let footer = config.avatar_list;
+			const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+
 			const embed = new Discord.RichEmbed()
 				.setTitle("__PP data wipe performed__")
 				.setColor("#188c1f")
-				.setFooter("Alice Synthesis Thirty", "https://i.imgur.com/S5yspQs.jpg")
+				.setFooter("Alice Synthesis Thirty", footer[index])
 				.setTimestamp(new Date())
 				.addField("**User stats**", `Discord User: <@${discordid}>\nUsername: ${username}\nUid: ${uid}`)
 				.addField("**PP stats**", `PP count: ${parseFloat(pre_pptotal).toFixed(2)} pp\nPlay count: ${playc}`);
@@ -58,7 +62,7 @@ module.exports.run = (client, message, args, maindb) => {
 				addcount = 0;
 			})
 		} else message.channel.send("The account is not binded, he/she/you need to use `&userbind <uid>` first. To get uid, use `&profilesearch <username>`")
-	});
+	})
 };
 
 module.exports.help = {
