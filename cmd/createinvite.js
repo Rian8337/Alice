e4d922bf403a2c0629241fee734172d8291b0a77
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-08 17:52:08 +0700

    Update createinvite.js

--- cmd/createinvite.js
+++ cmd/createinvite.js
@@ -2,10 +2,7 @@ let Discord = require('discord.js');
 let config = require("../config.json");
 
 module.exports.run = (client, message, args) => {
-    if (!message.member.roles.find(r => r.name === "Manager")) {
-        message.channel.send("You don't have permission to do this");
-        return
-    }
+    if (message.author.id != '386742340968120321') return;
     let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
     if (!logchannel) {
         message.channel.send(`Please create ${config.management_channel} first!`);
@@ -13,21 +10,21 @@ module.exports.run = (client, message, args) => {
     }
     let maxage = args[0];
     if (!maxage) {
-      message.channel.send("Please enter time (seconds) until invite link expires!");
-      return
+        message.channel.send("Please enter time (seconds) until invite link expires!");
+        return
     }
     if (isNaN(maxage) || maxage < 0 || (maxage > 0 && maxage < 1)) {
-      message.channel.send("Invalid time");
-      return
+        message.channel.send("Invalid time");
+        return
     }
     let maxuses = args[1];
     if (!maxuses) {
-      message.channel.send("Please enter maximum invite link usage!");
-      return
+        message.channel.send("Please enter maximum invite link usage!");
+        return
     }
     if (isNaN(maxuses) || maxuses < 0) {
-      message.channel.send("Invalid maximum usage");
-      return
+        message.channel.send("Invalid maximum usage");
+        return
     }
     let reason = args.slice(2).join(" ");
     if (!reason) {
@@ -43,9 +40,12 @@ module.exports.run = (client, message, args) => {
         if (maxage == 0) time = 'Never';
         if (maxuses == 0) maxuses = 'Infinite';
 
+        let footer = config.avatar_list;
+        const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+
         const embed = new Discord.RichEmbed()
             .setAuthor(message.author.tag, message.author.avatarURL)
-            .setFooter("Alice Synthesis Thirty", "https://i.imgur.com/8Fkb6Us.jpg")
+            .setFooter("Alice Synthesis Thirty", footer[index])
             .setTimestamp(new Date())
             .setColor(message.member.highestRole.hexColor)
             .setTitle("Invite link created")
