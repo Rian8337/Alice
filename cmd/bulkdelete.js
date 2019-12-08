aeeea6fde4a91c2f0be340b040de76a2634c399f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-08 17:51:11 +0700

    Update bulkdelete.js

--- cmd/bulkdelete.js
+++ cmd/bulkdelete.js
@@ -1,19 +1,32 @@
 let Discord = require('discord.js');
+let config = require('../config.json');
 
 module.exports.run = (client, message, args) => {
-    if (message.author.id != '386742340968120321') return message.channel.send("You don't have permission to do this");
+    if (message.author.id != '386742340968120321') return;
     let todelete = parseInt(args[0]);
-    if (!todelete) return message.channel.send("Please specify number of messages to delete");
-    if (isNaN(todelete)) return message.channel.send("Invalid number of messages to delete");
-    if (todelete < 2 || todelete > 100) return message.channel.send("Invalid number of messages to delete, must be in range of 2-100");
-    
+    if (!todelete) {
+        message.channel.send("Please specify number of messages to delete");
+        return;
+    }
+    if (isNaN(todelete)) {
+        message.channel.send("Invalid number of messages to delete");
+        return;
+    }
+    if (todelete < 2 || todelete > 100) {
+        message.channel.send("Invalid number of messages to delete, must be in range of 2-100");
+        return;
+    }
+
     message.channel.bulkDelete(todelete + 1).then (() => {
+        let footer = config.avatar_list;
+        const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+
         const embed = new Discord.RichEmbed()
           .setAuthor(message.author.tag, message.author.avatarURL)
           .setDescription("**Bulk delete executed**")
           .setColor(message.member.highestRole.hexColor)
           .setTimestamp(new Date())
-          .setFooter("Alice Synthesis Thirty", "https://i.imgur.com/S5yspQs.jpg")
+          .setFooter("Alice Synthesis Thirty", footer[index])
           .addField("Amount of messages", todelete);
 
       message.channel.send({embed})
