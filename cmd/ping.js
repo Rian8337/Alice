9d8027826063e160df9408153b3f8e25799a05d3
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-08 17:54:31 +0700

    Update ping.js

--- cmd/ping.js
+++ cmd/ping.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
+let config = require('../config.json');
 
 module.exports.run = (client, message) => {
 	console.log("Pong!");
 	let time = (Date.now() - message.createdTimestamp)/10;
+	let footer = config.avatar_list;
+	const index = Math.floor(Math.random() * (footer.length - 1) + 1);
 	let embed = new Discord.RichEmbed()
 		.setAuthor("Pong!")
 		.setColor(message.member.highestRole.hexColor)
-		.setFooter("Alice Synthesis Thirty", "https://i.imgur.com/S5yspQs.jpg")
+		.setFooter("Alice Synthesis Thirty", footer[index])
 		.addField("Response time: ", `${time} ms`);
 	message.channel.send(embed);
 };
