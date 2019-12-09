6cee79428dba4528a3e7de11477092c58b741d41
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-09 15:01:49 +0700

    Update recent5.js

--- cmd/recent5.js
+++ cmd/recent5.js
@@ -33,6 +33,23 @@ function rankEmote(input) {
 	}
 }
 
+function editpp(client, rplay, name, page, footer, index) {
+	let embed = new Discord.RichEmbed()
+		.setDescription("Recent play for **" + name + " (Page " + page + "/10)**")
+		.setColor(8102199)
+		.setFooter("Alice Synthesis Thirty", footer[index]);
+
+	for (var i = 5 * (page - 1); i < 5 + 5 * (page - 1); i++) {
+		if (!rplay[i]) break;
+		var date = new Date(rplay[i].date*1000);
+		date.setUTCHours(date.getUTCHours() + 8);
+		var play = client.emojis.get(rankEmote(rplay[i].mark)).toString() + " | " + rplay[i].filename + " " + modread(rplay[i].mode);
+		var score = rplay[i].score.toLocaleString() + ' / ' + rplay[i].combo + 'x / ' + parseFloat(rplay[i].accuracy)/1000 + '% / ' + rplay[i].miss + ' miss(es) \n `' + date.toUTCString() + '`';
+		embed.addField(play, score)
+	}
+	return embed
+}
+
 module.exports.run = (client, message, args) => {
 	if (cd.has(message.author.id)) return message.channel.send("Please wait for a bit before using this command again!");
 	let uid = parseInt(args[0]);
@@ -53,7 +70,7 @@ module.exports.run = (client, message, args) => {
 			if (!content) return message.channel.send("Error: Empty API response. Please try again!");
 			var resarr = content.split('<br>');
 			var headerres = resarr[0].split(' ');
-			if (headerres[0] == 'FAILED') {message.channel.send("User doesn't exist"); return;}
+			if (headerres[0] == 'FAILED') return message.channel.send("User doesn't exist");
 			resarr.shift();
 			content = resarr.join("");
 			var obj = JSON.parse(content);
@@ -61,44 +78,35 @@ module.exports.run = (client, message, args) => {
 			var rplay = obj.recent;
 			let footer = config.avatar_list;
 			const index = Math.floor(Math.random() * (footer.length - 1) + 1);
-			let embed = new Discord.RichEmbed()
-				.setDescription("Recent play for **" + name + " (Page " + page + "/10)**")
-				.setColor(8102199)
-				.setFooter("Alice Synthesis Thirty", footer[index]);
+			let embed = editpp(client, rplay, name, page, footer, index);
 
-			for (var i = 5 * (page - 1); i < 5 + 5 * (page - 1); i++) {
-				if (!rplay[i]) break;
-				var date = new Date(rplay[i].date*1000);
-				date.setUTCHours(date.getUTCHours() + 8);
-				var play = client.emojis.get(rankEmote(rplay[i].mark)).toString() + " | " + rplay[i].filename + " " + modread(rplay[i].mode);
-				var score = rplay[i].score.toLocaleString() + ' / ' + rplay[i].combo + 'x / ' + parseFloat(rplay[i].accuracy)/1000 + '% / ' + rplay[i].miss + ' miss(es) \n `' + date.toUTCString() + '`';
-				embed.addField(play, score)
-			}
 			if (!rplay[0]) {message.channel.send("This player haven't submitted any play"); return;}
 			
 			message.channel.send({embed}).then (msg => {
-				msg.react("⬅️").then(() => {
-					msg.react("➡️").catch(e => console.log(e));
+				msg.react("⏮️").then(() => {
+					msg.react("⬅️").then(() => {
+						msg.react("➡️").then(() => {
+							msg.react("⏭️").catch(e => console.log(e))
+						})
+					})
 				});
+
+				let backward = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '⏮️' && user.id === message.author.id, {time: 60000});
 				let back = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '⬅️' && user.id === message.author.id, {time: 60000});
 				let next = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '➡️' && user.id === message.author.id, {time: 60000});
+				let forward = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '⏭️' && user.id === message.author.id, {time: 60000});
+
+				backward.on('collect', () => {
+					page = 1;
+					embed = editpp(client, rplay, name, page, footer, index);
+					msg.edit(embed).catch(e => console.log(e));
+					msg.reactions.forEach(reaction => reaction.remove(message.author.id).catch(e => console.log(e)))
+				});
 
 				back.on('collect', () => {
 					if (page === 1) page = 10;
 					else page--;
-					embed = new Discord.RichEmbed()
-						.setDescription("Recent play for **" + name + " (Page " + page + "/10)**")
-						.setColor(message.member.highestRole.hexColor)
-						.setFooter("Alice Synthesis Thirty", footer[index]);
-
-					for (var i = 5 * (page - 1); i < 5 + 5 * (page - 1); i++) {
-						if (!rplay[i]) break;
-						var date = new Date(rplay[i].date*1000);
-						date.setUTCHours(date.getUTCHours() + 8);
-						var play = client.emojis.get(rankEmote(rplay[i].mark)).toString() + " | " + rplay[i].filename + " " + modread(rplay[i].mode);
-						var score = rplay[i].score.toLocaleString() + ' / ' + rplay[i].combo + 'x / ' + parseFloat(rplay[i].accuracy)/1000 + '% / ' + rplay[i].miss + ' miss(es) \n `' + date.toUTCString() + '`';
-						embed.addField(play, score)
-					}
+					embed = editpp(client, rplay, name, page, footer, index);
 					msg.edit(embed).catch(e => console.log(e));
 					msg.reactions.forEach(reaction => reaction.remove(message.author.id).catch(e => console.log(e)))
 				});
@@ -106,19 +114,14 @@ module.exports.run = (client, message, args) => {
 				next.on('collect', () => {
 					if (page === 10) page = 1;
 					else page++;
-					embed = new Discord.RichEmbed()
-						.setDescription("Recent play for **" + name + " (Page " + page + "/10)**")
-						.setColor(message.member.highestRole.hexColor)
-						.setFooter("Alice Synthesis Thirty", footer[index]);
+					embed = editpp(client, rplay, name, page, footer, index);
+					msg.edit(embed).catch(e => console.log(e));
+					msg.reactions.forEach(reaction => reaction.remove(message.author.id).catch(e => console.log(e)))
+				});
 
-					for (var i = 5 * (page - 1); i < 5 + 5 * (page - 1); i++) {
-						if (!rplay[i]) break;
-						var date = new Date(rplay[i].date*1000);
-						date.setUTCHours(date.getUTCHours() + 8);
-						var play = client.emojis.get(rankEmote(rplay[i].mark)).toString() + " | " + rplay[i].filename + " " + modread(rplay[i].mode);
-						var score = rplay[i].score.toLocaleString() + ' / ' + rplay[i].combo + 'x / ' + parseFloat(rplay[i].accuracy)/1000 + '% / ' + rplay[i].miss + ' miss(es) \n `' + date.toUTCString() + '`';
-						embed.addField(play, score)
-					}
+				forward.on('collect', () => {
+					page = 10;
+					embed = editpp(client, rplay, name, page, footer, index);
 					msg.edit(embed).catch(e => console.log(e));
 					msg.reactions.forEach(reaction => reaction.remove(message.author.id).catch(e => console.log(e)))
 				})
