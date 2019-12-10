dde2dfc777c8be0a5fd192844d346584e7a80be7
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-10 16:09:15 +0700

    Update fetchmessage.js

--- cmd/fetchmessage.js
+++ cmd/fetchmessage.js
@@ -1,14 +1,12 @@
 let Discord = require('discord.js');
 
-async function filterMessage(message, channel, filter, i, count, embed, startid) {
-    let final = await channel.fetchMessages({limit: 100, after: startid});
-
-    let lastid = final.first();
-    if (!lastid) {
+async function filterMessage(message, filter, i, count, embed, startid) {
+    let final = await message.channel.fetchMessages({limit: 100, after: startid});
+    if (!final) {
         console.log("Complete!");
-        return message.channel.send(embed);
+        return message.channel.send(embed)
     }
-    lastid = lastid.id;
+    let lastid = final.first().id;
     console.log("Start ID: " + startid);
     console.log("Last ID: " + lastid + "\n");
 
@@ -30,14 +28,11 @@ async function filterMessage(message, channel, filter, i, count, embed, startid)
                 .setColor(message.member.highestRole.hexColor);
         }
     });
-    return filterMessage(message, channel, filter, i, count, embed, lastid)
+    return filterMessage(message, filter, i, count, embed, lastid)
 }
 
 module.exports.run = async (client, message, args) => {
     if (message.author.id != '386742340968120321') return message.channel.send("You don't have permission to do this");
-    let guild = client.guilds.get("316545691545501706");
-    let channel = guild.channels.find(c => c.name === 'voting');
-    if (!channel) return message.channel.send("Channel is not available");
 
     let startid = args[0];
     if (isNaN(startid)) return message.channel.send("Please enter valid message ID!");
@@ -51,7 +46,7 @@ module.exports.run = async (client, message, args) => {
     let i = 1;
     let count = 1;
 
-    await filterMessage(message, channel, filter, i, count, embed, startid);
+    await filterMessage(message, filter, i, count, embed, startid);
 };
 
 module.exports.help = {
