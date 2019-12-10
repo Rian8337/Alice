bac873089974c0fe590cb0fb7986f291ab62eb04
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-10 12:56:35 +0700

    Update fetchmessage.js

--- cmd/fetchmessage.js
+++ cmd/fetchmessage.js
@@ -1,8 +1,7 @@
-module.exports.run = (client, message, args) => {
+let Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
     if (message.author.id !== '386742340968120321') return message.channel.send("You don't have permission to do this");
-    let guild = client.guilds.get("316545691545501706");
-    let channel = guild.channels.find(c => c.name === 'voting');
-    if (!channel) return;
 
     let startid = args[0];
     if (isNaN(startid)) return message.channel.send("Please enter valid message ID!");
@@ -13,9 +12,18 @@ module.exports.run = (client, message, args) => {
     let filter = args.slice(2).join(" ");
     if (!filter) return message.channel.send("Please insert filter!");
 
-    channel.fetchMessages({limit: limit, after: startid}).then (msg => {
-        console.log(msg.filter(m => m.content === filter && !m.author.bot));
-    }).catch(() => message.channel.send("Message not found!"))
+    let final = await message.channel.fetchMessages({limit: limit, after: startid});
+    final = final.filter(m => m.content == filter && !m.author.bot);
+    let embed = new Discord.RichEmbed()
+        .setTitle("Users who sent `" + filter + "`:")
+        .setColor(message.member.highestRole.hexColor);
+
+    let i = 1;
+    final.forEach(msg => {
+        embed.addField(`${i}. ${msg.author.tag}`, msg.content);
+        i++
+    });
+    await message.channel.send({embed})
 };
 
 module.exports.help = {
