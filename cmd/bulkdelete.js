1d6812c562915173b2d0468f2614502b99511415
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-11 01:16:01 +0700

    Update bulkdelete.js

--- cmd/bulkdelete.js
+++ cmd/bulkdelete.js
@@ -2,20 +2,12 @@ let Discord = require('discord.js');
 let config = require('../config.json');
 
 module.exports.run = (client, message, args) => {
-    if (message.author.id != '386742340968120321') return;
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do this");
+
     let todelete = parseInt(args[0]);
-    if (!todelete) {
-        message.channel.send("Please specify number of messages to delete");
-        return;
-    }
-    if (isNaN(todelete)) {
-        message.channel.send("Invalid number of messages to delete");
-        return;
-    }
-    if (todelete < 2 || todelete > 100) {
-        message.channel.send("Invalid number of messages to delete, must be in range of 2-100");
-        return;
-    }
+    if (!todelete) return message.channel.send("Please specify number of messages to delete");
+    if (isNaN(todelete)) return message.channel.send("Invalid number of messages to delete");
+    if (todelete < 2 || todelete > 100) return message.channel.send("Invalid number of messages to delete, must be in range of 2-100");
 
     message.channel.bulkDelete(todelete + 1).then (() => {
         let footer = config.avatar_list;
@@ -29,7 +21,9 @@ module.exports.run = (client, message, args) => {
           .setFooter("Alice Synthesis Thirty", footer[index])
           .addField("Amount of messages", todelete);
 
-      message.channel.send({embed})
+      message.channel.send({embed}).then(msg => {
+          msg.delete(10000)
+      }).catch(e => console.log(e))
   })
 };
 
