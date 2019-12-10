df3555b2b8055a54a960514692e1e4aa074aa088
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-11 01:15:00 +0700

    Add files via upload

---
+++ cmd/modhelp.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+
+module.exports.run = (client, message) => {
+	fs.readFile("modhelp.txt", function(err, data1) {
+		if (err) {
+			console.log(err);
+			return message.channel.send("Error retrieving data. Please try again!")
+		}
+		message.channel.send("Complete command list can be found at https://github.com/Rian8337/Alice\n```"+data1+"```");
+	});
+};
+
+module.exports.help = {
+	name: "modhelp"
+};
