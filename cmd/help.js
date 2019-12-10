191ca049b19cd4a5e45703ce36f9678c3e8d4a7b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-11 01:20:44 +0700

    Update help.js

--- cmd/help.js
+++ cmd/help.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
 
-module.exports.run = (client, message, args) => {
+module.exports.run = (client, message) => {
 	fs.readFile("help.txt", function(err, data1) {
-        if (err) throw err;
-        else message.channel.send("```"+data1+"```");
+		if (err) {
+			console.log(err);
+			return message.channel.send("Error retrieving data. Please try again!")
+		}
+		message.channel.send("Complete command list can be found at https://github.com/Rian8337/Alice\n```"+data1+"```");
 	});
 };
 
