579b9d8fa2798708be1c1467487c745dc71f6f7a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-12 23:05:39 +0700

    Update userbind.js

--- cmd/userbind.js
+++ cmd/userbind.js
@@ -1,21 +1,21 @@
-const mongodb = require('mongodb');
 const http = require('http');
+const droidapikey = process.env.DROID_API_KEY;
 
 module.exports.run = (client, message, args, maindb) => {
 	let uid = args[0];
 	if (!uid) {message.channel.send("Your uid please!"); return;}
 	if (isNaN(uid)) {message.channel.send("Invalid uid")}
 	else {
-		let name="";
+		let name = "";
 		var options = {
 			host: "ops.dgsrz.com",
 			port: 80,
-			path: "/profile.php?uid="+uid+".html"
+			path: "/api/getuserinfo.php?apiKey=" + droidapikey + "&uid=" + uid
 		};
 
 		var content = "";
 
-		var req = http.request(options, function(res) {
+		var req = http.request(options, function (res) {
 			res.setEncoding("utf8");
 			res.on("data", function (chunk) {
 				content += chunk;
@@ -25,61 +25,53 @@ module.exports.run = (client, message, args, maindb) => {
 				return message.channel.send("Error: Unable to retrieve user data. Please try again!")
 			});
 			res.on("end", function () {
-				const a = content;
-				let b = a.split('\n'), c = [];
-				for (x = 0; x < b.length; x++) {
-					if (b[x].includes('h3 m-t-xs m-b-xs')) {
-						b[x]=b[x].replace('<div class="h3 m-t-xs m-b-xs">',"");
-						b[x]=b[x].replace('<\/div>',"");
-						b[x]=b[x].trim();
-						name = b[x]
-					}
-				}
-				let binddb = maindb.collection("userbind");
-				let query = { discordid: message.author.id };
-				var bind = {
+				var headerres = content.split('<br>')[0].split(" ");
+				if (headerres[0] == 'FAILED') return message.channel.send("User not found!");
+				name = headerres[2]
+			});
+			let binddb = maindb.collection("userbind");
+			let query = {discordid: message.author.id};
+			var bind = {
+				discordid: message.author.id,
+				uid: uid,
+				username: name,
+				pptotal: 0,
+				pp: []
+			};
+			var updatebind = {
+				$set: {
 					discordid: message.author.id,
 					uid: uid,
-					username: name,
-					pptotal: 0,
-					pp: []
-				};
-				var updatebind = {
-					$set: {
-						discordid: message.author.id,
-						uid: uid,
-						username: name
-					}
-				};
-				binddb.find(query).toArray(function(err, res) {
-					if (err) {
-						console.log(err);
-						return message.channel.send("Error: Empty database response. Please try again!")
-					}
-					if (!res[0]) {
-						binddb.insertOne(bind, function(err, res) {
-							if (err) {
-								console.log(err);
-								return message.channel.send("Error: Empty database response. Please try again!")
-							}
-							console.log("bind added");
-							message.channel.send("Haii <3, binded <@"+message.author.id+"> to uid "+uid);
-						})
-					}
-					else {
-						binddb.updateOne(query, updatebind, function(err, res) {
-							if (err) {
-								console.log(err);
-								return message.channel.send("Error: Empty database response. Please try again!")
-							}
-							console.log("bind updated");
-							message.channel.send("Haii <3, binded <@"+message.author.id+"> to uid "+uid);
-						})
-					}
-				})
+					username: name
+				}
+			};
+			binddb.find(query).toArray(function (err, res) {
+				if (err) {
+					console.log(err);
+					return message.channel.send("Error: Empty database response. Please try again!")
+				}
+				if (!res[0]) {
+					binddb.insertOne(bind, function (err, res) {
+						if (err) {
+							console.log(err);
+							return message.channel.send("Error: Empty database response. Please try again!")
+						}
+						console.log("bind added");
+						message.channel.send("Haii <3, binded <@" + message.author.id + "> to uid " + uid);
+					})
+				} else {
+					binddb.updateOne(query, updatebind, function (err, res) {
+						if (err) {
+							console.log(err);
+							return message.channel.send("Error: Empty database response. Please try again!")
+						}
+						console.log("bind updated");
+						message.channel.send("Haii <3, binded <@" + message.author.id + "> to uid " + uid);
+					})
+				}
 			})
 		});
-		req.end();
+		req.end()
 	}
 };
 
