4667af4ebfc73bd41af100e694f84cb8681d24b1
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-14 16:37:50 +0700

    Update skin.js

--- cmd/skin.js
+++ cmd/skin.js
@@ -1,28 +1,68 @@
-module.exports.run = async (client, message, args, maindb) => {
+module.exports.run = (client, message, args, maindb) => {
     let user = message.author.id;
-    if (args[0]) {
-        user = args[0];
-        user = user.replace("<@", "");
-        user = user.replace(">", "")
-    }
     let skindb = maindb.collection("skin");
     let query = {discordid: user};
-    skindb.find(query).toArray(async (err, res) => {
-        if (err) {
-            console.log(err);
-            return message.channel.send("Error: Empty database response. Please try again!")
-        }
-        if (res[0]) {
-            let name = await client.fetchUser(user);
-            name = name.username;
-            let skinlink = res[0].skin;
-            message.channel.send(`✅  **| ${name}'s skin: ${skinlink}.**\nFor a collection of skins, visit https://tsukushi.site/`)
-        }
-        else {
-            if (args[0]) message.channel.send("❎  **| The user hasn't set their skin yet! He/she must use `a!setskin <skin link>` first.**\nFor a collection of skins, visit https://tsukushi.site/");
-            else message.channel.send("❎  **| You haven't set your skin yet! You must use `a!setskin <skin link>` first.**\nFor a collection of skins, visit https://tsukushi.site/")
-        }
-    })
+    if (args[0] == 'set') {
+        let skinlink = args.join(" ");
+        if (!skinlink) return message.channel.send("❎  **| Please enter skin link!**");
+        skindb.find(query).toArray((err, res) => {
+            if (err) {
+                console.log(err);
+                return message.channel.send("Error: Empty database response. Please try again!")
+            }
+            if (!res[0]) {
+                var skinval = {
+                    discordid: user,
+                    skin: skinlink
+                };
+                skindb.insertOne(skinval, err => {
+                    if (err) {
+                        console.log(err);
+                        return message.channel.send("Error: Empty database response. Please try again!")
+                    }
+                    console.log("Skin added for " + user);
+                    message.channel.send(`✅  **| ${message.author.username}'s skin has been set to ${skinlink}.**`)
+                })
+            } else {
+                var updateval = {
+                    $set: {
+                        discordid: user,
+                        skin: skinlink
+                    }
+                };
+                skindb.updateOne(query, updateval, err => {
+                    if (err) {
+                        console.log(err);
+                        return message.channel.send("Error: Empty database response. Please try again!")
+                    }
+                    console.log("Skin updated for " + user);
+                    message.channel.send(`✅  **| ${message.author.username}'s skin has been set to ${skinlink}.**`)
+                })
+            }
+        })
+    }
+    else {
+        user = args[0];
+        user = user.replace("<@", "");
+        user = user.replace(">", "");
+        query = {discordid: user};
+        skindb.find(query).toArray(async (err, res) => {
+            if (err) {
+                console.log(err);
+                return message.channel.send("Error: Empty database response. Please try again!")
+            }
+            if (res[0]) {
+                let name = await client.fetchUser(user);
+                name = name.username;
+                let skinlink = res[0].skin;
+                message.channel.send(`✅  **| ${name}'s skin: ${skinlink}.**\nFor a collection of skins, visit https://tsukushi.site/`)
+            }
+            else {
+                if (args[0]) message.channel.send("❎  **| The user hasn't set their skin yet! He/she must use `a!skin set <skin link>` first.**\nFor a collection of skins, visit https://tsukushi.site/");
+                else message.channel.send("❎  **| You haven't set your skin yet! You must use `a!skin set <skin link>` first.**\nFor a collection of skins, visit https://tsukushi.site/")
+            }
+        })
+    }
 };
 
 module.exports.help = {
