4b3ec89db731e0599bbe37a7b0e6c2e92c63ef2e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-15 15:13:06 +0700

    Update recentme.js

--- cmd/recentme.js
+++ cmd/recentme.js
@@ -258,7 +258,7 @@ module.exports.run = (client, message, args, maindb) => {
 				let combo = play.combo;
 				let rank = rankread(play.mark);
 				let ptime = new Date(play.date * 1000);
-				ptime.setUTCHours(ptime.getUTCHours() + 7);
+				ptime.setUTCHours(ptime.getUTCHours() + 8);
 				let acc = (play.accuracy / 1000).toFixed(2);
 				let miss = play.miss;
 				let mod = play.mode;
