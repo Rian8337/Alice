1a5be73708f572768a5903b15d4227e41f233382
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-16 17:06:17 +0700

    Update changeavatar.js

--- cmd/changeavatar.js
+++ cmd/changeavatar.js
@@ -3,16 +3,17 @@ let cd = new Set();
 
 module.exports.run = (client, message, args) => {
     if (message.author.id != '386742340968120321') return message.channel.send("You don't have permission to do this");
+    let ufind = message.author.id;
+    if (cd.has(ufind)) return message.channel.send("Please wait for a bit before using this command again!");
     let x = args[0];
-    if (cd.has(message.author.id)) return message.channel.send("Please wait for a bit before using this command again!");
     if (isNaN(x)) return message.channel.send("Invalid input");
     let avatar = config.avatar_list;
     if (!avatar[x]) return message.channel.send("There is no avatar!");
     client.user.setAvatar(avatar[x]).then (() => {
-        message.channel.send(`Changed avatar to ${avatar[x]}`);
-        cd.add(message.author.id);
+        message.channel.send(`Changed avatar`, {file: avatar[x]});
+        cd.add(ufind);
         setTimeout(() => {
-            cd.delete(message.author.id)
+            cd.delete(ufind)
         }, 5000)
     }).catch(() => message.channel.send("You are changing avatar too fast!"));
 };
