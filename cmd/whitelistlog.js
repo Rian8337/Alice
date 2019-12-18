36338b237b50976d193b95f2dfdef5e5962e230e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-18 08:09:03 +0700

    Update whitelistlog.js

--- cmd/whitelistlog.js
+++ cmd/whitelistlog.js
@@ -3,6 +3,10 @@ let https = require("https");
 let apikey = process.env.OSU_API_KEY;
 let config = require('../config.json');
 
+function time(second) {
+    return [Math.floor(second / 60), (second - Math.floor(second / 60) * 60).toString().padStart(2, "0")].join(":")
+}
+
 module.exports.run = (client, message, args) => {
     if (!args[0]) return;
     let guild = client.guilds.get('528941000555757598');
@@ -42,7 +46,7 @@ module.exports.run = (client, message, args) => {
                 .setTimestamp(new Date())
                 .setThumbnail(`https://b.ppy.sh/thumb/${mapinfo.beatmapset_id}.jpg`)
                 .setDescription(`**${mapstring}**\n[Map Link](https://osu.ppy.sh/beatmapsets/${mapinfo.beatmapset_id}) | [Go to Message](${message.url})`)
-                .addField(`CS: ${mapinfo.diff_size} - AR: ${mapinfo.diff_approach} - OD: ${mapinfo.diff_overall} - HP: ${mapinfo.diff_drain}`, `BPM: ${mapinfo.bpm} - Length: ${mapinfo.hit_length}/${mapinfo.total_length} s`)
+                .addField(`CS: ${mapinfo.diff_size} - AR: ${mapinfo.diff_approach} - OD: ${mapinfo.diff_overall} - HP: ${mapinfo.diff_drain}`, `BPM: ${mapinfo.bpm} - Length: ${time(mapinfo.hit_length)}/${time(mapinfo.total_length)}`)
                 .addField(`Last Update: ${mapinfo.last_update}`, `Star Rating: ${parseFloat(mapinfo.difficultyrating).toFixed(2)}`);
             logchannel.send({embed: embed})
         })
