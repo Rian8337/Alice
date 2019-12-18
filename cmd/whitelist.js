4e6237a2344ee4721e832699d242fafbd0108ece
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-18 08:06:58 +0700

    Update whitelist.js

--- cmd/whitelist.js
+++ cmd/whitelist.js
@@ -4,6 +4,10 @@ require("mongodb");
 var apikey = process.env.OSU_API_KEY;
 let config = require('../config.json');
 
+function time(second) {
+    return [Math.floor(second / 60), (second - Math.floor(second / 60) * 60).toString().padStart(2, "0")].join(":")
+}
+
 function mapstatusread(status) {
 	switch (status) {
 		case -2: return 16711711;
@@ -121,7 +125,7 @@ function whitelistInfo(link_in, hash_in, message, callback) {
                 "fields": [
                     {
                         "name": "CS: " + mapinfo.diff_size + " - AR: " + mapinfo.diff_approach + " - OD: " + mapinfo.diff_overall + " - HP: " + mapinfo.diff_drain ,
-                        "value": "BPM: " + mapinfo.bpm + " - Length: " + mapinfo.hit_length + "/" + mapinfo.total_length + " s"
+                        "value": "BPM: " + mapinfo.bpm + " - Length: " + time(mapinfo.hit_length) + "/" + time(mapinfo.total_length)
                     },
                     {
                         "name": "Last Update: " + mapinfo.last_update,
