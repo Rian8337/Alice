bc13875a18f626c059ec6197340fd1f1dd4ac97c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-23 05:55:29 +0700

    Update say.js

--- cmd/say.js
+++ cmd/say.js
@@ -1,4 +1,5 @@
 module.exports.run = (client, message, args) => {
+        if (message.author.id != '386742340968120321'') return;
 	let sayMessage = args.join(" ");
 	if (sayMessage.search("@everyone") != -1 || sayMessage.search("@here") != -1) {
 		message.channel.send("**S**: No no, you dummy");
