0dd32bb17775a944e68e215ddda324c6426789a8
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-22 19:27:02 +0700

    Update osu.js

--- cmd/osu.js
+++ cmd/osu.js
@@ -9,11 +9,10 @@ function progress (level) {
 }
 
 module.exports.run = (client, message, args, maindb, alicedb) => {
-    var rolecheck;
     try {
-        rolecheck = message.member.highestRole.hexColor
+        let rolecheck = message.member.roles
     } catch (e) {
-        rolecheck = "#000000"
+        return
     }
     var playerdb = alicedb.collection("osubind");
     var query = {discordid: message.author.id};
@@ -22,8 +21,9 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
             console.log(err);
             return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
         }
-        var username = args.slice(1).join(" ");
+        var username;
         if (args[0] === 'set') {
+            username = args.slice(1).join(" ");
             if (!username) return message.channel.send("❎ **| Hey, I don't know what account to bind!**");
             if (!res[0]) {
                 var insertVal = {
@@ -56,6 +56,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
             }
         }
         else {
+            username = res[0].username;
             var mode = args[0];
             if (mode === 'std') mode = 0;
             else if (mode === 'taiko') mode = 1;
@@ -63,30 +64,24 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
             else if (mode === 'mania') mode = 3;
             else mode = 0;
 
-            if (!args[1]) {
-                if (res[0]) username = res[0].username;
-                else return message.channel.send("❎ **| I'm sorry, you haven't set your osu! username yet. To do so, use `a!osu set <username>`.**");
-            }
-            console.log(username);
-
             var options = new URL("https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=" + username + "&m=" + mode);
-            var content = '';
+            var content;
 
-            var req = https.get(options, res => {
+            https.get(options, res => {
                 res.setEncoding("utf8");
                 res.on("data", chunk => {
-                    content += chunk
+                    content = chunk
                 });
                 res.on("error", err => {
                     console.log(err);
-                    return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from osu! API. Please try again! [0]**")
+                    return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from osu! API. Please try again!**")
                 });
                 res.on("end", () => {
                     var obj;
                     try {
                         obj = JSON.parse(content)
                     } catch (e) {
-                        return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from osu! API. Please try again! [1]**")
+                        return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from osu! API. Please try again!**")
                     }
                     if (!obj[0]) return message.channel.send("❎ **| I'm sorry, I cannot find the username!**");
                     var playerinfo = obj[0];
@@ -97,25 +92,28 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
                         .setColor(message.member.highestRole.hexColor)
                         .setFooter("Alice Synthesis Thirty", footer[index])
                         .addField("Rank", "#" + playerinfo.pp_rank, true)
+                        .addBlankField(true)
                         .addField("Country rank", playerinfo.country + " #" + playerinfo.pp_country_rank, true)
                         .addField("Accuracy", parseFloat(playerinfo.accuracy).toFixed(2) + "%", true)
+                        .addBlankField(true)
                         .addField("Play count", playerinfo.playcount, true)
                         .addField("Ranked score", parseInt(playerinfo.ranked_score).toLocaleString("en-US"), true)
+                        .addBlankField(true)
                         .addField("Total score", parseInt(playerinfo.total_score).toLocaleString("en-US"), true)
                         .addField("PP", playerinfo.pp_raw, true)
+                        .addBlankField(true)
                         .addField("Level", Math.floor(parseFloat(playerinfo.level)) + " (" + progress(playerinfo.level) + "%)", true)
                         .addField("Join date", playerinfo.join_date + " UTC", true)
                         .addField("User ID", playerinfo.user_id, true);
 
                     if (mode === 0) embed.setAuthor("osu!standard Profile for " + username, "https://osu.ppy.sh/images/flags/" + playerinfo.country + ".png", "https://osu.ppy.sh/users/" + playerinfo.user_id);
-                    if (mode === 1) embed.setAuthor("Taiko Profile for " + username, "https://a.ppy.sh/" + playerinfo.user_id, "https://osu.ppy.sh/users/" + playerinfo.user_id);
-                    if (mode === 2) embed.setAuthor("Catch the Beat Profile for " + username, "https://a.ppy.sh/" + playerinfo.user_id, "https://osu.ppy.sh/users/" + playerinfo.user_id);
-                    if (mode === 3) embed.setAuthor("osu!mania Profile for " + username, "https://a.ppy.sh/" + playerinfo.user_id, "https://osu.ppy.sh/users/" + playerinfo.user_id);
+                    if (mode === 1) embed.setAuthor("Taiko Profile for " + username, "https://a.ppy.sh/" + playerinfo.country, "https://osu.ppy.sh/users/" + playerinfo.user_id);
+                    if (mode === 2) embed.setAuthor("Catch the Beat Profile for " + username, "https://a.ppy.sh/" + playerinfo.country, "https://osu.ppy.sh/users/" + playerinfo.user_id);
+                    if (mode === 3) embed.setAuthor("osu!mania Profile for " + username, "https://a.ppy.sh/" + playerinfo.country, "https://osu.ppy.sh/users/" + playerinfo.user_id);
 
                     message.channel.send({embed: embed})
                 })
-            });
-            req.end()
+            })
         }
     })
 };
