ba85f0361e00f630051aaa8005fb764cb397b42e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-25 16:08:31 +0700

    Update bracket.js

--- cmd/bracket.js
+++ cmd/bracket.js
@@ -1,7 +1,14 @@
-module.exports.run = (client, message, args) => {
+module.exports.run = (client, message) => {
 	message.channel.send({file: 'bracket.png'});
-}
+};
+
+module.exports.config = {
+	description: "Views a bracket of tournament (outdated).",
+	usage: "bracket",
+	detail: "None",
+	permission: "None"
+};
 
 module.exports.help = {
 	name: "bracket"
-}
+};
