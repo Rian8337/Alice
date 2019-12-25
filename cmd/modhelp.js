4f1843ce0a06d1cbe95b8dc5be8e8d90ea40d630
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-25 18:55:18 +0700

    Update modhelp.js

--- cmd/modhelp.js
+++ cmd/modhelp.js
@@ -1,20 +1,20 @@
 var Discord = require('discord.js');
-var fs = require('fs');
+var modhelp = require('../modhelp.json');
 var config = require('../config.json');
 
 module.exports.run = (client, message, args) => {
+	var rolecheck;
+	try {
+		rolecheck = message.member.highestRole.hexColor
+	} catch (e) {
+		rolecheck = "#000000"
+	}
 	if (args[0]) {
 		let cmd = client.commands.get(args[0]);
 		if (cmd) {
-			var rolecheck;
-			try {
-				rolecheck = message.member.highestRole.hexColor
-			} catch (e) {
-				rolecheck = "#000000"
-			}
 			let footer = config.avatar_list;
 			const index = Math.floor(Math.random() * (footer.length - 1) + 1);
-			let help = `**${config.prefix}${args[0]}**\n${cmd.config.description || "No Description"}\n\n**Permission**: ${cmd.config.permission}\n**Usage:**\n\`${cmd.config.usage || "No Usage"}\``;
+			let help = `**${config.prefix}${args[0]}**\n${cmd.config.description}\n\n**Permission**: ${cmd.config.permission}\n**Usage:**\n\`${cmd.config.usage}\``;
 			let embed = new Discord.RichEmbed()
 				.setColor(rolecheck)
 				.setFooter("Alice Synthesis Thirty", footer[index])
@@ -23,13 +23,48 @@ module.exports.run = (client, message, args) => {
 			message.channel.send({embed: embed})
 		}
 	} else {
-		fs.readFile("modhelp.txt", function (err, data1) {
-			if (err) {
-				console.log(err);
-				return message.channel.send("Error retrieving data. Please try again!")
-			}
-			message.channel.send("Complete command list can be found at https://github.com/Rian8337/Alice\n```" + data1 + "```");
-		})
+		let helper = modhelp.helper;
+		let helperhelp = '';
+		for (var i = 0; i < helper.length; i++) {
+			helperhelp += "`" + helper[i] + "` "
+		}
+		helperhelp = helperhelp.slice(0, -1);
+
+		let moderator = modhelp.moderator;
+		let moderatorhelp = '';
+		for (i = 0; i < moderator.length; i++) {
+			moderatorhelp += "`" + moderator[i] + "` "
+		}
+		moderatorhelp = moderatorhelp.slice(0, -1);
+
+		let owner = modhelp.owner;
+		let ownerhelp = '';
+		for (i = 0; i < owner.length; i++) {
+			ownerhelp += "`" + owner[i] + "` "
+		}
+		ownerhelp = ownerhelp.slice(0, -1);
+
+		let botowner = modhelp.bot_owner;
+		let botownerhelp = '';
+		for (i = 0; i < botowner.length; i++) {
+			botownerhelp += "`" + botowner[i] + "` "
+		}
+		botownerhelp = botownerhelp.slice(0, -1);
+
+		let footer = config.avatar_list;
+		const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+		let embed = new Discord.RichEmbed()
+			.setTitle("Alice Synthesis Thirty Help\nModeration Commands")
+			.setDescription(`**Prefix: ${config.prefix}**\n\nFor detailed information about a command, use \`${config.prefix}modhelp [command name]\`.\nFor user commands, type \`${config.prefix}help\`.\n\nModerator can use Helper commands unless specified otherwise. Owner can use all commands.`)
+			.setThumbnail(client.user.avatarURL)
+			.setColor(rolecheck)
+			.setFooter("Alice Synthesis Thirty", footer[index])
+			.addField("Helper", helperhelp)
+			.addField("Moderator", moderatorhelp)
+			.addField("Owner", ownerhelp)
+			.addField("Bot Owner", botownerhelp);
+
+		message.channel.send({embed: embed}).catch(console.error)
 	}
 };
 
