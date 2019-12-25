7091d6574eaeb2833c33f5d41c2e3ed4ee7586b6
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-25 18:18:32 +0700

    Update manualcalc.js

--- cmd/manualcalc.js
+++ cmd/manualcalc.js
@@ -257,7 +257,7 @@ module.exports.run = (client, message, args) => {
 module.exports.config = {
 	description: "Calculates pp for an osu!standard map.",
 	usage: "manualcalc <map link / map ID> [(+<mod>) (<combo>x) (<acc>%) (<miss>m) (-d) (-p)]",
-	detail: "`map link/map ID`: The link or beatmap ID of the map [String/Integer]\n`mod`: Applied game modifications (HD, HR, etc) [String]\n`combo`: Max combo reached [Integer]\n`acc`: Accuracy gained [Float]\n`miss` Amount of misses [Integer]\n`-d`: Gives detailed response of droid pp\n`-p`: Gives detailed response of pp",
+	detail: "`map link/map ID`: The link or beatmap ID of the map [String/Integer]\n`mod`: Applied game modifications (HD, HR, etc) [String]\n`combo`: Max combo reached [Integer]\n`acc`: Accuracy gained [Float]\n`miss`: Amount of misses [Integer]\n`-d`: Gives detailed response of droid pp\n`-p`: Gives detailed response of pp",
 	permission: "None"
 };
 
