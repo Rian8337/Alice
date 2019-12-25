51a3c7a953e251f4fd475bdf65abfcd8a628f7e4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-25 14:32:11 +0700

    Update modhelp.js

--- cmd/modhelp.js
+++ cmd/modhelp.js
@@ -1,13 +1,36 @@
+var Discord = require('discord.js');
 var fs = require('fs');
+var config = require('../config.json');
 
-module.exports.run = (client, message) => {
-	fs.readFile("modhelp.txt", function(err, data1) {
-		if (err) {
-			console.log(err);
-			return message.channel.send("Error retrieving data. Please try again!")
+module.exports.run = (client, message, args) => {
+	if (args[0]) {
+		let cmd = client.commands.get(args[0]);
+		if (cmd) {
+			let footer = config.avatar_list;
+			const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+			let help = `**${config.prefix}${args[0]}**\n${cmd.config.description || "No Description"}\n\n**Permission**:${cmd.config.permission}\n**Usage:**\n\`${cmd.config.usage || "No Usage"}\``;
+			let embed = new Discord.RichEmbed()
+				.setFooter("Alice Synthesis Thirty", footer[index])
+				.setThumbnail(client.user.avatarURL)
+				.setDescription(help);
+			message.channel.send({embed: embed})
 		}
-		message.channel.send("Complete command list can be found at https://github.com/Rian8337/Alice\n```"+data1+"```");
-	});
+	} else {
+		fs.readFile("modhelp.txt", function (err, data1) {
+			if (err) {
+				console.log(err);
+				return message.channel.send("Error retrieving data. Please try again!")
+			}
+			message.channel.send("Complete command list can be found at https://github.com/Rian8337/Alice\n```" + data1 + "```");
+		})
+	}
+};
+
+module.exports.config = {
+	description: "Moderator help command.",
+	usage: "modhelp [cmd]",
+	detail: "`cmd`: Command name",
+	permission: "None"
 };
 
 module.exports.help = {
