9d8e1ee2e71b640c13d791f39d7446c49388600e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-25 16:03:24 +0700

    Update whitelistremove.js

--- cmd/whitelistremove.js
+++ cmd/whitelistremove.js
@@ -134,7 +134,7 @@ function whitelistInfo(link_in, hash_in, message, callback) {
 module.exports.config = {
     description: "Unwhitelists a beatmap.",
     usage: "whitelistremove <map link/map ID>",
-    detail: "`map link/map ID`: The beatmap link or ID to unwhitelist",
+    detail: "`map link/map ID`: The beatmap link or ID to unwhitelist [String]",
     permission: "Owner"
 };
 
