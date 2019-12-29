5cd1f9a8488e8f381c18cbccd4c1a747c9fb9fd4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-29 12:03:53 +0700

    Add files via upload

---
+++ cmd/timer.js
@@ -0,0 +1,32 @@
+var cd = new Set();
+
+module.exports.run = (client, message, args) => {
+    if (cd.has(message.author.id)) return message.channel.send("❎ **| Hey, you still have a timer active! Wait until that one is finished, please!**");
+    try {
+        let rolecheck = message.member.roles
+    } catch (e) {
+        return
+    }
+    if (!message.member.roles.find(r => r.name === 'Referee')) return message.channel.send("❎ **| I'm sorry, you don't have permission to use this.**");
+    var timelimit = parseInt(args[0]);
+    if (isNaN(timelimit)) return message.channel.send("❎ **| Hey, can you enter a valid time limit?**");
+    if (timelimit < 1 || timelimit > 1800 || timelimit == Infinity) return message.channel.send("❎ **| Hey, my timer has a limit of 30 minutes!**");
+
+    message.channel.send(`✅ **| Got it! I will remind you in ${timelimit} seconds!**`);
+    cd.add(message.author.id);
+    setTimeout(() => {
+        cd.delete(message.author.id);
+        message.channel.send(`✅ **| ${message.author}, the time is up!**`)
+    }, timelimit * 1000)
+};
+
+module.exports.config = {
+    description: "Set a timer (some sort of reminder).\nIntended for tournament use.",
+    usage: "timer <timelimit>",
+    detail: "`timelimit`: The time limit in seconds from 1 to 1800 [Integer]",
+    permission: "Referee"
+};
+
+module.exports.help = {
+    name: "timer"
+};
