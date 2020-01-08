51da60a152bba0281fdeb0e22ff6d25438460fe4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-01-08 13:33:37 +0700

    Update sayit.js

--- cmd/sayit.js
+++ cmd/sayit.js
@@ -1,7 +1,8 @@
 let config = require('../config.json');
 
 module.exports.run = (client, message, args) => {
-	if (message.author.id != '386742340968120321') return;
+	if (message.author.id != '386742340968120321' && !args[0]) return;
+        message.author.lastMessage.delete().then(() => {
 	let attachments = [];
 	if (message.attachments.size > 0) {
 		message.attachments.forEach(attachment => {
@@ -9,20 +10,12 @@ module.exports.run = (client, message, args) => {
 		})
 	}
 	let sayMessage = args.join(" ");
-        if (sayMessage.startsWith(config.prefix)) {
-                message.author.lastMessage.delete().then (() => {
-                        return
-                })
-        }
 	if (attachments.length > 0) {
-		message.author.lastMessage.delete(500).catch();
 		if (sayMessage) message.channel.send(sayMessage, {files: attachments});
 		else message.channel.send({files: attachments})
 	}
-	else {
-		message.author.lastMessage.delete().catch();
-		message.channel.send(sayMessage)
-	}
+	else message.channel.send(sayMessage)
+        })
 };
 
 module.exports.config = {
