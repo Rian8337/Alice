a684da2cb567c42f660cc90f502d27cba3a94c5f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-01-10 17:36:14 +0700

    Update trackfunc.js

--- cmd/trackfunc.js
+++ cmd/trackfunc.js
@@ -1,6 +1,11 @@
 var Discord = require('discord.js');
 var http = require('http');
+var https = require('https');
+var request = require('request');
+var apikey = process.env.OSU_API_KEY;
 var droidapikey = process.env.DROID_API_KEY;
+var osu = require('ojsama');
+var droid = require('./ojsamadroid');
 
 function modname(mod) {
     var res = '';
@@ -31,6 +36,96 @@ function rankread(imgsrc) {
         default: rank="unknown"
     }
     return rank
+}
+
+function modenum(mod) {
+    var res = 4;
+    if (mod.includes("r")) res += 16;
+    if (mod.includes("h")) res += 8;
+    if (mod.includes("d")) res += 64;
+    if (mod.includes("c")) res += 576;
+    if (mod.includes("n")) res += 1;
+    if (mod.includes("e")) res += 2;
+    if (mod.includes("t")) res += 256;
+    return res
+}
+
+function getMapPP(phash, pcombo, pacc, pmissc, pmod = "", cb) {
+    var options = new URL("https://osu.ppy.sh/api/get_beatmaps?k=" + apikey + "&h=" + phash);
+    var content = '';
+    var req = https.request(options, res => {
+        res.setEncoding('utf8');
+        res.on("data", chunk => {
+            content += chunk
+        });
+        res.on("end", () => {
+            var obj;
+            try {
+                obj = JSON.parse(content)
+            } catch (e) {
+                return cb(false)
+            }
+            if (!obj[0]) return cb(false);
+            var mapinfo = obj[0];
+            var mapid = mapinfo.beatmap_id;
+            if (mapinfo.mode != 0) return cb(false);
+            var mods = 4;
+            var acc_percent = 100;
+            var nmiss = 0;
+            var combo = mapinfo.max_combo;
+            if (pmod) mods = modenum(pmod);
+            if (pacc) acc_percent = parseFloat(pacc);
+            if (pcombo) combo = parseInt(pcombo);
+            if (pmissc) nmiss = parseInt(pmissc);
+
+            var nparser = new droid.parser();
+            var pcparser = new osu.parser();
+            var url = "https://osu.ppy.sh/osu/" + mapid;
+            request(url, (err, response, data) => {
+                if (!data) return cb(true, 0, 0);
+                nparser.feed(data);
+                pcparser.feed(data);
+                var pcmods = mods - 4;
+                var nmap = nparser.map;
+                var pcmap = pcparser.map;
+                var cur_od = nmap.od - 5;
+                var cur_ar = nmap.ar;
+                var cur_cs = nmap.cs - 4;
+                if (pmod.includes("r")) {
+                    mods -= 16;
+                    cur_ar = Math.min(cur_ar*1.4, 10);
+                    cur_od = Math.min(cur_od*1.4, 5);
+                    cur_cs += 1;
+                }
+                nmap.od = cur_od;
+                nmap.ar = cur_ar;
+                nmap.cs = cur_cs;
+
+                if (nmap.ncircles == 0 && nmap.nsliders == 0) return cb(true, 0, 0);
+
+                var nstars = new droid.diff().calc({map: nmap, mods: mods});
+                var pcstars = new osu.diff().calc({map: pcmap, mods: pcmods});
+
+                var npp = new droid.ppv2({
+                    stars: nstars,
+                    combo: combo,
+                    nmiss: nmiss,
+                    acc_percent: acc_percent
+                });
+
+                var pcpp = new osu.ppv2({
+                    stars: pcstars,
+                    combo: combo,
+                    nmiss: nmiss,
+                    acc_percent: acc_percent
+                });
+                var ppline = parseFloat(npp.toString().split(" ")[0]);
+                var pcppline = parseFloat(pcpp.toString().split(" ")[0]);
+                cb(true, ppline, pcppline)
+            })
+        })
+    });
+    req.end()
 }
 
 module.exports.run = (client, message = "", args = {}, maindb) => {
@@ -70,19 +165,23 @@ module.exports.run = (client, message = "", args = {}, maindb) => {
                         let ptime = new Date(play[i].date * 1000);
                         ptime.setUTCHours(ptime.getUTCHours() + 7);
                         let acc = (play[i].accuracy / 1000).toFixed(2);
-                        let mod = modname(play[i].mode);
+                        let mod = play[i].mode;
                         let miss = play[i].miss;
                         let rank = rankread(play[i].mark);
                         let combo = play[i].combo;
+                        let hash = play[i].hash;
+                        if (title) getMapPP(hash, combo, acc, miss, mod, (available, dpp, pp) => {
+                            let embed = new Discord.RichEmbed()
+                                .setAuthor(`Recent play for ${name}`, rank)
+                                .setTitle(title)
+                                .setColor(8311585);
 
-                        let embed = new Discord.RichEmbed()
-                            .setAuthor(`Recent play for ${name}`, rank)
-                            .setTitle(title)
-                            .setColor(8311585)
-                            .setDescription(`**Score**: \`${score}\` - Combo: \`${combo}x\` - Accuracy: \`${acc}%\` (\`${miss}\` x)\nMod: \`${mod}\`\nTime: \`${ptime.toUTCString()}\``);
+                            if (available) embed.setDescription(`**Score**: \`${score}\` - Combo: \`${combo}x\` - Accuracy: \`${acc}%\` (\`${miss}\` x)\nMod: \`${modname(mod)}\`\nTime: \`${ptime.toUTCString()}\`\n\`${dpp} dpp - ${pp} PC pp\``);
+                            else embed.setDescription(`**Score**: \`${score}\` - Combo: \`${combo}x\` - Accuracy: \`${acc}%\` (\`${miss}\` x)\nMod: \`${modname(mod)}\`\nTime: \`${ptime.toUTCString()}\``);
 
-                        client.channels.get("664880705372684318").send({embed: embed});
-                        client.channels.get("665106609382359041").send({embed: embed})
+                            client.channels.get("664880705372684318").send({embed: embed});
+                            client.channels.get("665106609382359041").send({embed: embed})
+                        })
                     }
                 })
             });
