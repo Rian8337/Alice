9375753175e1fe53db5269ee5673a1a349df2864
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-01-16 15:56:56 +0700

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -74,7 +74,8 @@ client.on("message", message => {
 			"https://cdn.discordapp.com/attachments/440319362407333939/666825359198519326/unknown.gif",
 			"https://cdn.discordapp.com/attachments/316545691545501706/667287014152077322/unknown.gif",
 			"https://cdn.discordapp.com/attachments/635532651779981313/666825419298701325/unknown.gif",
-			"https://cdn.discordapp.com/attachments/635532651779981313/662844781327810560/unknown.gif"
+			"https://cdn.discordapp.com/attachments/635532651779981313/662844781327810560/unknown.gif",
+			"https://cdn.discordapp.com/attachments/635532651779981313/637868500580433921/unknown.gif"
 		];
 		const index = Math.floor(Math.random() * (images.length - 1) + 1);
 		message.channel.send({files: [images[index]]});
