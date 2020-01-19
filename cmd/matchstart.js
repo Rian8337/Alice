38ba007be4fceb3aab6d382d02a570809b4be469
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-01-19 13:05:49 +0700

    Rewrite command

--- cmd/matchstart.js
+++ cmd/matchstart.js
@@ -1,38 +1,28 @@
-var Discord = require('discord.js');
-var config = require('../config.json');
-
-function time(second) {
-    return [Math.floor(second / 60), Math.ceil(second - Math.floor(second / 60) * 60).toString().padStart(2, "0")].join(":")
-}
+let Discord = require('discord.js');
+let config = require('../config.json');
 
 module.exports.run = (client, message, args, maindb, alicedb) => {
     if (message.channel instanceof Discord.DMChannel) return;
-    var rolecheck;
-    try {
-        rolecheck = message.member.highestRole.hexColor
-    } catch (e) {
-        rolecheck = "#000000"
-    }
-    if (!message.member.roles.find(r => r.name === 'Referee')) return message.channel.send("❎ **| I'm sorry, you don't have permission to use this.**");
-    let matchid = args[0];
-    if (!matchid) return message.channel.send("❎ **| Hey, I don't know what match to find!**");
-    let map = args[1];
+    if (message.member.roles == null || !message.member.roles.find(r => r.name === 'Referee')) return message.channel.send("❎ **| I'm sorry, you don't have permission to use this.**");
+
+    let map = args[0];
     if (!map) return message.channel.send("❎ **| Hey, I don't know what map is playing!**");
 
-    let matchdb = maindb.collection("matchinfo");
-    let mapdb = alicedb.collection("mapinfolength");
-    let query = {matchid: matchid};
+    let channeldb = alicedb.collection("matchchannel");
+    let mapdb = alicedb.collection("maplength");
+    let query = {channelid: message.channel.id};
 
-    matchdb.find(query).toArray((err, matchres) => {
+    channeldb.find(query).toArray((err, channelres) => {
         if (err) {
             console.log(err);
             return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
         }
-        if (!matchres[0]) return message.channel.send("❎ **| I'm sorry, I cannot find the match!**");
+        if (!channelres[0]) return message.channel.send("❎ **| I'm sorry, this channel hasn't been set for a match yet!**");
+        let matchid = channelres[0].matchid;
         let poolid = matchid.split(".")[0];
-        let poolquery = {poolid: poolid};
+        query = {poolid: poolid};
 
-        mapdb.find(poolquery).toArray((err, poolres) => {
+        mapdb.find(query).toArray((err, poolres) => {
             if (err) {
                 console.log(err);
                 return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
@@ -50,6 +40,12 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
             }
             if (!mapfound) return message.channel.send("❎ **| I'm sorry, I cannot find the map!**");
             if (map.toUpperCase().includes("DT") && poolid == "t8q") timelimit = Math.ceil(timelimit / 1.5);
+            var rolecheck;
+            try {
+                rolecheck = message.member.highestRole.hexColor
+            } catch (e) {
+                rolecheck = "#000000"
+            }
             let footer = config.avatar_list;
             const index = Math.floor(Math.random() * (footer.length - 1) + 1);
             let embed = new Discord.RichEmbed()
@@ -75,9 +71,9 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
 };
 
 module.exports.config = {
-    description: "Starts a round in a match.\nIntended for tournament use.",
-    usage: "matchstart <match id> <pick>",
-    detail: "`match id`: The ID of the match [String]\n`pick`: Current pick (NM1, NM2, etc) [String]",
+    description: "Starts a round in a match. Channel must be set with `matchset` beforehand.\nIntended for tournament use.",
+    usage: "matchstart <pick>",
+    detail: "`pick`: Current pick (NM1, NM2, etc) [String]",
     permission: "Referee"
 };
 
