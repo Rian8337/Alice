e79d35d9a860a4265217cd830780617b22fd9744
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-01-19 18:07:02 +0700

    Update fancyunban.js

--- cmd/fancyunban.js
+++ cmd/fancyunban.js
@@ -30,7 +30,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
         let footer = config.avatar_list;
         const index = Math.floor(Math.random() * (footer.length - 1) + 1);
         let embed = new Discord.RichEmbed()
-            .setTitle("Channel unban executed")
+            .setTitle("Lounge unban executed")
             .setColor(rolecheck)
             .setAuthor(message.author.tag, message.author.avatarURL)
             .setFooter("User ID: " + user.id, footer[index])
