04b71615746cd6ae5ff2d319ad4896a498976201
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-01-19 13:04:52 +0700

    Add files via upload

---
+++ cmd/matchset.js
@@ -0,0 +1,64 @@
+let Discord = require('discord.js');
+
+module.exports.run = (client, message, args, maindb, alicedb) => {
+    if (message.channel instanceof Discord.DMChannel) return;
+    if (message.member.roles == null || !message.member.roles.find(r => r.name === 'Referee')) return message.channel.send("❎ **| I'm sorry, you don't have permission to use this.**");
+    let matchid = args[0];
+    if (!matchid) return message.channel.send("❎ **| Hey, I don't know what match to set!**");
+    let matchdb = maindb.collection("matchinfo");
+    let channeldb = alicedb.collection("matchchannel");
+    let query = {matchid: matchid};
+
+    matchdb.find(query).toArray((err, matchres) => {
+        if (err) {
+            console.log(err);
+            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+        }
+        if (!matchres[0]) return message.channel.send("❎ **| I'm sorry, I cannot find the match!**");
+        query = {channelid: message.channel.id};
+        channeldb.find(query).toArray((err, channelres) => {
+            if (err) {
+                console.log(err);
+                return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+            }
+            if (channelres[0]) {
+                var updateVal = {
+                    $set: {
+                        matchid: matchid
+                    }
+                };
+                channeldb.updateOne(updateVal, query, err => {
+                    if (err) {
+                        console.log(err);
+                        return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                    }
+                    message.channel.send("✅ **| Successfully set this channel for match `" + matchid + "`.**")
+                })
+            }
+            else {
+                var insertVal = {
+                    channelid: message.channel.id,
+                    matchid: matchid
+                };
+                channeldb.insertOne(insertVal, err => {
+                    if (err) {
+                        console.log(err);
+                        return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                    }
+                    message.channel.send("✅ **| Successfully set this channel for match `" + matchid + "`.**")
+                })
+            }
+        })
+    })
+};
+
+module.exports.config = {
+    description: "Sets a match in a channel.\nIntended for tournament use.",
+    usage: "matchset <match id>",
+    detail: "`match id`: The ID of the match [String]",
+    permission: "Referee"
+};
+
+module.exports.help = {
+    name: "matchset"
+};
---
+++ cmd/matchunset.js
@@ -0,0 +1,33 @@
+let Discord = require('discord.js');
+
+module.exports.run = (client, message, args, maindb, alicedb) => {
+    if (message.channel instanceof Discord.DMChannel) return;
+    if (message.member.roles == null || !message.member.roles.find(r => r.name === 'Referee')) return message.channel.send("❎ **| I'm sorry, you don't have permission to use this.**");
+
+    let channeldb = alicedb.collection("matchchannel");
+    channeldb.find({channelid: message.channel.id}).toArray((err, res) => {
+        if (err) {
+            console.log(err);
+            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+        }
+        if (!res[0]) return message.channel.send("❎ **| I'm sorry, this channel has never been set for a match yet!**");
+        channeldb.deleteOne({channelid: message.channel.id}, err => {
+            if (err) {
+                console.log(err);
+                return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+            }
+            message.channel.send("✅ **| Successfully removed binded match in this channel.**")
+        })
+    })
+};
+
+module.exports.config = {
+    description: "Removes a match binded to the channel.\nIntended for tournament use.",
+    usage: "matchunset",
+    detail: "None",
+    permission: "Referee"
+};
+
+module.exports.help = {
+    name: "matchunset"
+};
