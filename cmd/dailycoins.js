b07320b0289e66fe26bbb964280295eb76363f7c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-01-26 19:13:20 +0700

    Add more features

--- cmd/dailycoins.js
+++ cmd/dailycoins.js
@@ -1,4 +1,6 @@
 let Discord = require('discord.js');
+let http = require('http');
+let droidapikey = process.env.DROID_API_KEY;
 
 function timeconvert (num) {
     let sec = parseInt(num);
@@ -10,76 +12,180 @@ function timeconvert (num) {
 
 module.exports.run = (client, message, args, maindb, alicedb) => {
     if (message.channel instanceof Discord.DMChannel) return;
-    if (message.author.id != '386742340968120321') return message.channel.send("❎ **| I'm sorry, this command is still in testing!**");
+    //if (message.guild.id != '316545691545501706') return message.channel.send("❎ **| I'm sorry, this command is only available in osu!droid (International) Discord server!**");
     let binddb = maindb.collection("userbind");
     let pointdb = alicedb.collection("playerpoints");
-    let query = {discordid: message.author.id};
-    binddb.find(query).toArray((err, userres) => {
-        if (err) {
-            console.log(err);
-            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
-        }
-        if (!userres[0]) return message.channel.send("❎ **| I'm sorry, your account is not binded. You need to use `a!userbind <uid>` first. To get uid, use `a!profilesearch <username>`.**");
-        let uid = userres[0].uid;
-        let username = userres[0].username;
-        pointdb.find(query).toArray((err, dailyres) => {
-            if (err) {
-                console.log(err);
-                return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
-            }
-            let daily = 100;
-            let coin = client.emojis.get("669532330980802561");
-            if (dailyres[0]) {
-                let timelimit = dailyres[0].dailycooldown - Math.floor(Date.now() / 1000);
-                if (timelimit > 0) {
-                    let time = timeconvert(timelimit);
-                    return message.channel.send(`❎ **| I'm sorry, you're still in cooldown! You can claim ${coin}Alice coins again in ${time[0] == 0?"":`${time[0]} hours`}${time[1] == 0?"":`, ${time[1]} minutes`}${time[2] == 0?"":`, ${time[2]} seconds`}.**`)
+    let coin = client.emojis.get("669532330980802561");
+    let curtime = Math.floor(Date.now() / 1000);
+    let query = {};
+    switch (args[0]) {
+        case "claim": {
+            query = {discordid: message.author.id};
+            binddb.find(query).toArray((err, userres) => {
+                if (err) {
+                    console.log(err);
+                    return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
                 }
-                let totalcoins = dailyres[0].alicecoins + daily;
-                message.channel.send(`✅ **| ${message.author}, you have claimed ${coin}\`${daily}\` Alice coins! You now have ${coin}\`${totalcoins}\` Alice coins.**`);
-                let updateVal = {
-                    $set: {
-                        dailycooldown: Math.floor(Date.now() / 1000) + 86400,
-                        alicecoins: totalcoins
-                    }
-                };
-                pointdb.updateOne(query, updateVal, err => {
+                if (!userres[0]) return message.channel.send("❎ **| I'm sorry, your account is not binded. You need to use `a!userbind <uid>` first. To get uid, use `a!profilesearch <username>`.**");
+                let uid = userres[0].uid;
+                let username = userres[0].username;
+                pointdb.find(query).toArray((err, dailyres) => {
                     if (err) {
                         console.log(err);
                         return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
                     }
-                    console.log("Alice coins updated")
-                })
-            } else {
-                message.channel.send(`✅ **| ${message.author}, you have claimed ${coin}\`${daily}\` Alice coins! You now have ${coin}\`${daily}\` Alice coins.**`);
-                let insertVal = {
-                    username: username,
-                    uid: uid,
-                    discordid: message.author.id,
-                    challenges: [],
-                    points: 0,
-                    dailycooldown: Math.floor(Date.now() / 1000) + 86400,
-                    alicecoins: daily
-                };
-                pointdb.insertOne(insertVal, err => {
-                    if (err) {
-                        console.log(err);
-                        return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                    let daily = 50;
+                    if (dailyres[0]) {
+                        let timelimit = dailyres[0].dailycooldown - curtime;
+                        if (timelimit > 0) {
+                            let time = timeconvert(timelimit);
+                            return message.channel.send(`❎ **| I'm sorry, you're still in cooldown! You can claim ${coin}Alice coins again in ${time[0] == 0 ? "" : `${time[0] == 1 ? `${time[0]} day` : `${time[0]} days`}`}${time[1] == 0 ? "" : `${time[0] == 0?"":", "}${time[1] == 1 ? `${time[1]} hour` : `${time[1]} hours`}`}${time[2] == 0 ? "" : `${time[1] == 0?"":", "}${time[2] == 1 ? `${time[2]} minute` : `${time[2]} minutes`}`}${time[3] == 0 ? "" : `${time[2] == 0?"":", "}${time[3] == 1 ? `${time[3]} second` : `${time[3]} seconds`}`}.**`)
+                        }
+                        let totalcoins = dailyres[0].alicecoins + daily;
+                        message.channel.send(`✅ **| ${message.author}, you have claimed ${coin}\`${daily}\` Alice coins! You now have ${coin}\`${totalcoins}\` Alice coins.**`);
+                        let updateVal = {
+                            $set: {
+                                dailycooldown: curtime + 86400,
+                                alicecoins: totalcoins
+                            }
+                        };
+                        pointdb.updateOne(query, updateVal, err => {
+                            if (err) {
+                                console.log(err);
+                                return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                            }
+                            console.log("Alice coins updated")
+                        })
+                    } else {
+                        message.channel.send(`✅ **| ${message.author}, you have claimed ${coin}\`${daily}\` Alice coins! You now have ${coin}\`${daily}\` Alice coins.**`);
+                        let insertVal = {
+                            username: username,
+                            uid: uid,
+                            discordid: message.author.id,
+                            challenges: [],
+                            points: 0,
+                            dailycooldown: curtime + 86400,
+                            alicecoins: daily
+                        };
+                        pointdb.insertOne(insertVal, err => {
+                            if (err) {
+                                console.log(err);
+                                return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                            }
+                            console.log("Alice coins updated")
+                        })
                     }
-                    console.log("Alice coins updated")
                 })
-            }
-        })
-    })
+            });
+            break
+        }
+        case "transfer": {
+            if (curtime - (message.member.joinedTimestamp / 1000) < 86400 * 7) return message.channel.send("❎ **| I'm sorry, you haven't been in the server for a week!**");
+            let totransfer = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+            if (!totransfer) return message.channel.send("❎ **| Hey, I don't know the user to give your coins to!**");
+            let amount = parseInt(args[2]);
+            if (isNaN(amount) || amount <= 0) return message.channel.send("❎ **| Hey, I need a valid amount to give!**");
+            query = {discordid: message.author.id};
+            binddb.find(query).toArray((err, userres) => {
+                if (err) {
+                    console.log(err);
+                    return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                }
+                if (!userres[0]) return message.channel.send("❎ **| I'm sorry, your account is not binded. You need to use `a!userbind <uid>` first. To get uid, use `a!profilesearch <username>`.**");
+                let uid = userres[0].uid;
+                let options = {
+                    host: "ops.dgsrz.com",
+                    port: 80,
+                    path: `/api/getuserinfo.php?apiKey=${droidapikey}&uid=${uid}`
+                };
+                let content = '';
+                let req = http.request(options, res => {
+                    res.setEncoding("utf8");
+                    res.on("data", chunk => {
+                        content += chunk
+                    });
+                    res.on("end", () => {
+                        let resarr = content.split("<br>");
+                        let headerres = resarr[0].split(" ");
+                        if (headerres[0] == 'FAILED') return message.channel.send("❎ **| I'm sorry, I cannot find your username!**");
+                        let obj = JSON.parse(resarr[1]);
+                        let rank = obj.rank;
+                        let limit;
+                        if (rank < 10) limit = 500;
+                        else if (rank < 50) limit = 350;
+                        else if (rank < 100) limit = 250;
+                        else if (rank < 500) limit = 100;
+                        else limit = 50;
+                        if (amount > limit) return message.channel.send(`❎ **| I'm sorry, your ${coin}Alice coins transfer limit is \`${limit}\`!**`);
+                        pointdb.find(query).toArray((err, pointres) => {
+                            if (err) {
+                                console.log(err);
+                                return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                            }
+                            if (!pointres[0]) return message.channel.send("❎ **| I'm sorry, you don't have enough coins!**");
+                            let alicecoins = pointres[0].alicecoins;
+                            if (alicecoins < amount) return message.channel.send("❎ **| I'm sorry, you don't have enough coins!**");
+                            message.channel.send(`❗**| Are you sure you want to transfer ${coin}\`${amount}\` Alice coins to ${totransfer}?**`).then(msg => {
+                                msg.react("✅").catch(console.error);
+                                let confirmation = false;
+                                let confirm = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id, {time: 15000});
+                                confirm.on("collect", () => {
+                                    confirmation = true;
+                                    msg.delete();
+                                    query = {discordid: totransfer.id};
+                                    pointdb.find(query).toArray((err, giveres) => {
+                                        if (err) {
+                                            console.log(err);
+                                            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                                        }
+                                        if (!giveres[0]) return message.channel.send("❎ **| I'm sorry, this user has not used any daily claims before!**");
+                                        let coins = giveres[0].alicecoins + amount;
+                                        message.channel.send(`✅ **| ${message.author}, successfully transferred ${coin}\`${amount}\` Alice coins to ${totransfer}. You now have ${coin}\`${alicecoins - amount}\` Alice coins.**`)
+                                        let updateVal = {
+                                            $set: {
+                                                alicecoins: coins
+                                            }
+                                        };
+                                        pointdb.updateOne({discordid: totransfer.id}, updateVal, err => {
+                                            if (err) return console.log(err);
+                                        })
+                                    })
+                                });
+                                confirm.on("end", () => {
+                                    if (!confirmation) {
+                                        msg.delete();
+                                        message.channel.send("❎ **| Timed out.**").then(m => m.delete(5000))
+                                    }
+                                })
+                            })
+                        })
+                    })
+                });
+                req.end()
+            });
+            break
+        }
+        default: {
+            query = {discordid: message.author.id};
+            pointdb.find(query).toArray((err, res) => {
+                if (err) {
+                    console.log(err);
+                    return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                }
+                if (res[0]) message.channel.send(`✅ **| ${message.author}, you have ${coin}\`${res[0].alicecoins}\` Alice coins.**`);
+                else message.channel.send(`✅ **| ${message.author}, you have ${coin}\`0\` Alice coins.**`)
+            })
+        }
+    }
 };
 
 module.exports.config = {
-    description: "Claims daily Alice coins.",
-    usage: "dailycoins",
+    description: "Main command for Alice coins.",
+    usage: "coins",
     detail: "None",
     permission: "None"
 };
 
 module.exports.help = {
-    name: "dailycoins"
+    name: "coins"
 };
