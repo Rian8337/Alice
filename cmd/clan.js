d62bcc4730bec11e992b15c37750892c2930ff1f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-12 13:19:36 +0700

    Fix reaction clear

--- cmd/clan.js
+++ cmd/clan.js
@@ -203,7 +203,8 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
                         });
                         
                         backward.on("end", () => {
-                            msg.reactions.deleteAll()
+                            msg.reactions.forEach(reaction => reaction.remove(message.author.id));
+                            msg.reactions.forEach(reaction => reaction.remove(client.user.id))
                         })
                     })
                 })
