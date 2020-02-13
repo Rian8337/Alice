f20e98448fbe99c089a4afea4e61a5d6dff8ac95
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-13 12:10:10 +0700

    Rewrite

--- cmd/profileme.js
+++ cmd/profileme.js
@@ -1,10 +1,9 @@
-var http = require('http');
-require("dotenv").config();
-var droidapikey = process.env.DROID_API_KEY;
+let Discord = require('discord.js');
 let config = require('../config.json');
+let osudroid = require('../modules/osu!droid');
 
 function modread(input) {
-	var res = '';
+	let res = '';
 	if (input.includes('n')) res += 'NF';
 	if (input.includes('h')) res += 'HD';
 	if (input.includes('r')) res += 'HR';
@@ -47,121 +46,37 @@ module.exports.run = (client, message, args, maindb) => {
 			console.log(err);
 			return message.channel.send("Error: Empty database response. Please try again!")
 		}
-		if (res[0]) {
-			let uid = res[0].uid;
-			var options = {
-				host: "ops.dgsrz.com",
-				port: 80,
-				path: "/profile.php?uid="+uid+".html"
-			};
-
-			var content = "";
-
-			var req = http.request(options, function(res) {
-				res.setEncoding("utf8");
-				res.on("data", function (chunk) {
-					content += chunk;
-				});
-				res.on("error", err1 => {
-					console.log(err1);
-					return message.channel.send("Error: Unable to retrieve user data. Please try again!")
-				});
-				res.on("end", function () {
-					const a = content;
-					let b = a.split('\n');
-					let avalink=""; let location="";
-					for (x = 0; x < b.length; x++) {
-						if (b[x].includes('h3 m-t-xs m-b-xs')) {
-							b[x-3]=b[x-3].replace('<img src="',"");
-							b[x-3]=b[x-3].replace('" class="img-circle">',"");
-							b[x-3]=b[x-3].trim();
-							avalink = b[x-3];
-							b[x+1]=b[x+1].replace('<small class="text-muted"><i class="fa fa-map-marker"><\/i>',"");
-							b[x+1]=b[x+1].replace("<\/small>","");
-							b[x+1]=b[x+1].trim();
-							location=b[x+1]
-						}
-					}
-					apiFetch(uid, avalink, location, message, client)
-				})
-			});
-			req.end();
-		}
-		else message.channel.send("❎ **| I'm sorry, the account is not binded. He/she/you need to use `a!userbind <uid>` first. To get uid, use `a!profilesearch <username>`.**")
-	})
-};
-
-function apiFetch(uid, avalink, location, message, client) {
-	var options = new URL("http://ops.dgsrz.com/api/getuserinfo.php?apiKey=" + droidapikey + "&uid=" + uid);
-	var content = "";
-
-	var req = http.get(options, function(res) {
-		res.setEncoding("utf8");
-		res.on("data", function (chunk) {
-			content += chunk;
-		});
-		res.on("error", err => {
-			return console.log(err)
-		});
-		res.on("end", function () {
-			var resarr = content.split('<br>');
-			var headerres = resarr[0].split(' ');
-			if (headerres[0] == 'FAILED') return message.channel.send("❎ **| I'm sorry, the user doesn't exist!**");
-			resarr.shift();
-			content = resarr.join("");
-			var obj;
-			try {
-				obj = JSON.parse(content);
-			} catch (e) {
-				return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from osu!droid API now. Please try again later!**")
-			}
-			var name = headerres[2];
-			var tscore = headerres[3];
-			var pcount = headerres[4];
-			var oacc = parseFloat(headerres[5])*100;
-			var rank = obj.rank;
-			var rplay = obj.recent[0];
-			if (!rplay) return message.channel.send("❎ **| I'm sorry, this player hasn't submitted any play!**");
-			var date = new Date(rplay.date * 1000);
-			date.setUTCHours(date.getUTCHours() + 8);
+		if (!res[0]) message.channel.send("The account is not binded, he/she/you need to use `a!userbind <uid>` first. To get uid, use `a!profilesearch <username>`");
+		let uid = res[0].uid;
+		new osudroid.PlayerInfo().get({uid: uid}, player => {
+			if (!player.name) return message.channel.send("❎ **| I'm sorry, I cannot find the player!**");
+			if (!player.recent_plays) return message.channel.send("❎ **| I'm sorry, this player hasn't submitted any play!**");
+			let name = player.name;
+			let avalink = player.avatarURL;
+			let location = player.location;
+			let tscore = player.score;
+			let pcount = player.play_count;
+			let oacc = player.accuracy;
+			let rplay = player.recent_plays[0];
+			let rank = player.rank;
+			let date = new Date(rplay.date * 1000);
+			date.setUTCHours(date.getUTCHours() + 7);
 			let footer = config.avatar_list;
 			const index = Math.floor(Math.random() * (footer.length - 1) + 1);
-			const embed = {
-				"description": "**Username: **"+name+"\n**Rank**: "+rank,
-				"color": 8102199,
-				"footer": {
-					"icon_url": footer[index],
-					"text": "Alice Synthesis Thirty"
-				},
-				"thumbnail": {
-					"url": avalink
-				},
-				"author": {
-					"name": "osu!droid profile (click here to view profile)",
-					"url": "http://ops.dgsrz.com/profile.php?uid="+uid,
-					"icon_url": "https://image.frl/p/beyefgeq5m7tobjg.jpg"
-				},
-				"fields": [
-					{
-						"name": "Total Score: " + parseInt(tscore).toLocaleString(),
-						"value": "Overall Accuracy: " + oacc.toFixed(2) +"%"
-					},
-					{
-						"name": "Play Count: " + pcount,
-						"value": "Location: " + location
-					},
-					{
-						"name": "Most Recent Play",
-						"value": client.emojis.get(rankEmote(rplay.mark)).toString() + " | " + rplay.filename + " " + modread(rplay.mode) + '\n' + rplay.score.toLocaleString() + ' / ' + rplay.combo + 'x / ' + parseFloat(rplay.accuracy)/1000 + '% / ' + rplay.miss + 'm \n ' + date.toUTCString()
-					}
-				]
-			};
+			let embed = new Discord.RichEmbed()
+				.setDescription(`**Username**: ${name}\n**Rank**: ${rank}`)
+				.setColor(8102199)
+				.setFooter("Alice Synthesis Thirty", footer[index])
+				.setThumbnail(avalink)
+				.setAuthor("osu!droid profile (click/tap here to view profile)", "https://image.frl/p/beyefgeq5m7tobjg.jpg", `https://ops.dgsrz.com/profile.php?uid=${uid}`)
+				.addField(`Total Score: ${tscore.toLocaleString()}`, `Overall Accuracy: ${oacc}%`)
+				.addField(`Play Count: ${pcount}`, `Location: ${location}`)
+				.addField("Most Recent Play", `${client.emojis.get(rankEmote(rplay.mark)).toString()} | ${rplay.filename} ${modread(rplay.mode)}\n${rplay.score.toLocaleString()} / ${rplay.combo}x / ${(parseFloat(rplay.accuracy) / 1000).toFixed(2)}% / ${rplay.miss}m\n${date.toUTCString()}`);
 
-			message.channel.send({ embed });
+			message.channel.send({embed: embed}).catch(console.error)
 		})
-	});
-	req.end()
-}
+	})
+};
 
 module.exports.config = {
 	description: "Retrieves an osu!droid profile (detailed).",
