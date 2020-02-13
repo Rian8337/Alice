bc293d1ababb025eb62edb2dae4bfbbf1a78b99f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-13 15:03:35 +0700

    Fix submitted count

--- cmd/pp.js
+++ cmd/pp.js
@@ -2,8 +2,8 @@ let Discord = require('discord.js');
 let config = require('../config.json');
 let osudroid = require('../modules/osu!droid');
 
-function calculatePP(message, whitelist, embed, i, pplist, playc, playentry, cb) {
-	if (!playentry[i]) return cb(false, true);
+function calculatePP(message, whitelist, embed, i, submitted, pplist, playc, playentry, cb) {
+	if (!playentry[i]) return cb(false, false, true);
 	let play = playentry[i];
 	whitelist.findOne({hashid: play.hash}, (err, wlres) => {
 		if (err) {
@@ -16,18 +16,18 @@ function calculatePP(message, whitelist, embed, i, pplist, playc, playentry, cb)
 		new osudroid.MapInfo().get(query, mapinfo => {
 			if (!mapinfo.title) {
 				message.channel.send("❎ **| I'm sorry, the map you've played can't be found on osu! beatmap listing, please make sure the map is submitted and up-to-date!**");
-				return cb()
+				return cb(false, false)
 			}
 			if ((mapinfo.approved == 3 || mapinfo.approved <= 0) && !wlres) {
 				message.channel.send("❎ **| I'm sorry, the PP system only accepts ranked, approved, whitelisted, or loved mapset right now!**");
-				return cb()
+				return cb(false, false)
 			}
 			let beatmapid = mapinfo.beatmap_id;
 			let mod = mapinfo.modConvert(play.mod);
 			new osudroid.MapStars().calculate({beatmap_id: beatmapid, mods: mod}, star => {
 				if (!star.droid_stars) {
 					message.channel.send("❎ **| I'm sorry, I'm having trouble on retrieving the map's droid star rating!**");
-					return cb()
+					return cb(false, false)
 				}
 				let npp = new osudroid.MapPP().calculate({
 					stars: star.droid_stars,
@@ -57,16 +57,16 @@ function calculatePP(message, whitelist, embed, i, pplist, playc, playentry, cb)
 					return b[2] - a[2]
 				});
 				while (pplist.length > 75) pplist.pop();
-				if (!dup) embed.addField(playinfo, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp`);
+				if (!dup) embed.addField(`${submitted}. ${playinfo}`, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp`);
 				else {
 					let x = 0;
 					for (x; x < pplist.length; x++) {
 						if (pplist[x][0] == play.hash) {
-							embed.addField(playinfo, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp | **Duplicate**`);
+							embed.addField(`${submitted}. ${playinfo}`, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp | **Duplicate**`);
 							break
 						}
 					}
-					if (x == pplist.length) embed.addField(playinfo, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp | **Worth no pp**`);
+					if (x == pplist.length) embed.addField(`${submitted}. ${playinfo}`, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp | **Worth no pp**`);
 				}
 				cb()
 			})
@@ -108,6 +108,7 @@ module.exports.run = (client, message, args, maindb) => {
 		let pplist = [];
 		let pptotal = 0;
 		let pre_pptotal = 0;
+		let submitted = 1;
 		let playc = 0;
 		if (userres[0].pp) pplist = userres[0].pp;
 		if (userres[0].pptotal) pre_pptotal = userres[0].pptotal;
@@ -144,7 +145,7 @@ module.exports.run = (client, message, args, maindb) => {
 				playentry.push(play)
 			}
 			let i = 0;
-			calculatePP(message, whitelist, embed, i, pplist, playc, playentry, function testResult(error = false, stopSign = false) {
+			calculatePP(message, whitelist, embed, i, submitted, pplist, playc, playentry, function testResult(error = false, success = true, stopSign = false) {
 				if (stopSign) {
 					let weight = 1;
 					for (let i in pplist) {
@@ -167,7 +168,8 @@ module.exports.run = (client, message, args, maindb) => {
 					return
 				}
 				if (!error) i++;
-				calculatePP(message, whitelist, embed, i, pplist, playc, playentry, testResult)
+				if (success) submitted++;
+				calculatePP(message, whitelist, embed, i, submitted, pplist, playc, playentry, testResult)
 			})
 		})
 	})
