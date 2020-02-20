73e7dfe25ffa1bfc1e971db2f45b4632b9aee8c9
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:26:05 +0700

    Version 2.0 release

--- cmd/recalc.js
+++ cmd/recalc.js
@@ -1,191 +1,96 @@
-var droid = require("./ojsamadroid");
-var osu = require("ojsama");
-var https = require("https");
-var mongodb = require("mongodb");
-var request = require("request");
-var apikey = process.env.OSU_API_KEY;
+const osudroid = require('../modules/osu!droid');
 
 function recalc(target, tlength, i, newtarget, binddb, uid, whitelist) {
-
 	if (i >= tlength) {
 		newtarget.sort(function(a, b) {return b[2] - a[2];});
-		var totalpp = 0;
-		var weight = 1;
-		for (x in newtarget) {
+		let totalpp = 0;
+		let weight = 1;
+		for (let x in newtarget) {
 			totalpp += newtarget[x][2] * weight;
 			weight *= 0.95;
 		}
 		console.log(totalpp.toFixed(2));
-		//TODO: update database
-		var updatedata = { $set : {
+		let updatedata = {
+			$set : {
 				pptotal: totalpp,
 				pp: newtarget
-			}};
-		binddb.updateOne({uid: uid}, updatedata, (err, res) => {
+			}
+		};
+		binddb.updateOne({uid: uid}, updatedata, (err) => {
 			if (err) return console.log(err);
 			console.log("User pp is updated. Total pp:" + totalpp);
 		});
-		return;
+		return
 	}
-
+	let modstring;
 	if (target[i][1].includes('+'))  {
-		var mapstring = target[i][1].split('+');
-		var modstring = mapstring[mapstring.length-1]
+		let mapstring = target[i][1].split('+');
+		modstring = mapstring[mapstring.length-1]
 	}
-	else var modstring = "";
 
-	var guessing_mode = false;
-
-	var isWhitelist = false;
-
-	var whitelistQuery = {hashid: target[i][0]};
+	let guessing_mode = true;
+	let whitelistQuery = {hashid: target[i][0]};
 
 	whitelist.findOne(whitelistQuery, (err, wlres) => {
+		let query = {hash: target[i][0]};
 		if (err) return recalc(target, tlength, i, newtarget, binddb, uid, whitelist);
-		if (wlres) isWhitelist = true;
-
-		if (isWhitelist) var options = new URL("https://osu.ppy.sh/api/get_beatmaps?k=" + apikey + "&b=" + wlres.mapid);
-		else var options = new URL("https://osu.ppy.sh/api/get_beatmaps?k=" + apikey + "&h=" + target[i][0]);
-
-		var content = "";
-
-		var req = https.get(options, function(res) {
-			res.setEncoding("utf8");
-			res.on("data", function (chunk) {
-				content += chunk;
+		if (wlres) query = {beatmap_id: wlres.mapid};
+		new osudroid.MapInfo().get(query, mapinfo => {
+			if (!mapinfo.title) {
+				console.log("Map not found");
+				return recalc(target, tlength, i+1, newtarget, binddb, uid, whitelist)
+			}
+			if (mapinfo.objects == 0) {
+				console.log("0 object found");
+				return recalc(target, tlength, i+1, newtarget, binddb, uid, whitelist)
+			}
+			let mods = osudroid.mods.droid_to_PC(modstring);
+			let acc_percent = 100;
+			if (target[i][4]) {
+				acc_percent = parseFloat(target[i][4]);
+				guessing_mode = false;
+			}
+			let combo = target[i][3] ? parseInt(target[i][3]) : mapinfo.max_combo;
+			let miss = target[i][5] ? parseInt(target[i][5]) : 0;
+			let star = new osudroid.MapStars().calculate({file: mapinfo.osu_file, mods: mods});
+			let npp = osudroid.ppv2({
+				stars: star.droid_stars,
+				combo: combo,
+				acc_percent: acc_percent,
+				miss: miss,
+				mode: "droid"
 			});
-			res.on("error", err1 => {
-				console.log(err1);
-				return recalc(target, tlength, i, newtarget, binddb, uid, whitelist)
-			});
-			res.on("end", function () {
-				var obj = JSON.parse(content);
-				if (!obj[0]) {console.log("Map not found"); return recalc(target, tlength, i+1, newtarget, binddb, uid, whitelist)}
-				var mapinfo = obj[0];
-				if (mapinfo.mode !=0) return;
-				//console.log(obj.beatmaps[0])
-				if (modstring) var mods = droid.modbits.from_string(modstring) + 4;
-				else var mods = 4;
-				if (target[i][4]) var acc_percent = parseFloat(target[i][4]);
-				else {var acc_percent = 100; guessing_mode = true;}
-				if (target[i][3]) var combo = parseInt(target[i][3]);
-				else var combo;
-				if (target[i][5]) var nmiss = parseInt(target[i][5]);
-				else var nmiss = 0;
-				var parser = new osu.parser();
-				var nparser = new droid.parser();
-				//console.log(acc_percent);
-				//var url = "https://osu.ppy.sh/osu/1031991";
-				var url = 'https://osu.ppy.sh/osu/' + mapinfo.beatmap_id;
-				request(url, function (err, response, data) {
-					parser.feed(data);
-					nparser.feed(data);
-					var map = parser.map;
-					var nmap = nparser.map;
-					var cur_od = map.od;
-					var cur_ar = map.ar;
-					var cur_cs = map.cs - 4;
-					// if (mods) {
-					// 	console.log("+" + osu.modbits.string(mods));
-					// }
-					if (modstring.includes("HR")) {
-						mods -= 16;
-						cur_ar = Math.min(cur_ar * 1.4, 10);
-						cur_od = Math.min(cur_od * 1.4, 10);
-						cur_cs++
-					}
-					if (modstring.includes("EZ")) {
-						mods -= 2;
-						cur_ar /= 2;
-						cur_od /= 2;
-						cur_cs--
-					}
-					let droidtoMS = 75 + 5 * (5 - cur_od);
-					if (modstring.includes("PR")) droidtoMS = 55 + 6 * (5 - cur_od);
-					cur_od = 5 - (droidtoMS - 50) / 6;
-					nmap.od = cur_od;
-					nmap.ar = cur_ar;
-					nmap.cs = cur_cs;
-
-					if (map.ncircles == 0 && map.nsliders == 0) {
-						console.log(target[i][0] + ' - Error: no object found');
-						console.log(target[i][2] + " -> " + target[i][2]);
-						newtarget.push([target[i][0], target[i][1], target[i][2], target[i][3], target[i][4], target[i][5]]);
-						recalc(target, tlength, i+1, newtarget, binddb, uid, whitelist);
-						return
-					}
-
-					var stars = new osu.diff().calc({map: map, mods: mods});
-					var nstars = new droid.diff().calc({map: nmap, mods: mods});
-					//console.log(stars.toString());
-
-
-					var pp = osu.ppv2({
-						stars: stars,
-						combo: combo,
-						nmiss: nmiss,
-						acc_percent: acc_percent,
-					});
-
-					var npp = droid.ppv2({
-						stars: nstars,
-						combo: combo,
-						nmiss: nmiss,
-						acc_percent: acc_percent,
-					});
-
-					parser.reset();
-					nparser.reset();
-					if (modstring.includes("HR")) { mods += 16; }
-
-					//console.log(stars.toString() + ' -> '+ nstars.toString());
-					//console.log(pp.toString() + ' -> '+ npp.toString());
-					var oldpp = parseFloat(pp.toString().split('(')[0]);
-					var newpp = parseFloat(npp.toString().split('(')[0]);
-					// console.log(acc_percent);
-					// console.log(guessing_mode);
-					var real_pp = 0;
-					if (guessing_mode) {real_pp = parseFloat(target[i][2]); real_pp = real_pp.toFixed(2)}
-					else real_pp = newpp;
-					console.log(target[i][2] + " -> " + real_pp);
-					if (guessing_mode) newtarget.push([target[i][0], target[i][1], real_pp]);
-					else newtarget.push([target[i][0], target[i][1], real_pp, target[i][3], target[i][4], target[i][5]]);
-					recalc(target, tlength, i+1, newtarget, binddb, uid, whitelist)
-				})
-			})
-		});
-		req.end()
+			let pp = parseFloat(npp.toString().split(" ")[0]);
+			let real_pp = guessing_mode ? parseFloat(target[i][2]).toFixed(2) : pp;
+			console.log(`${target[i][2]} -> ${real_pp}`);
+			newtarget.push(guessing_mode ? [target[i][0], target[i][1], real_pp] : [target[i][0], target[i][1], real_pp, target[i][3], target[i][4], target[i][5]]);
+			recalc(target, tlength, i+1, newtarget, binddb, whitelist)
+		})
 	})
 }
 
 module.exports.run = (client, message, args, maindb) => {
 	if (message.author.id != '132783516176875520' && message.author.id != '386742340968120321') return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
-	var uid = args[0];
-	//discordid = discordid.replace("<@", "");
-	//discordid = discordid.replace(">", "");
-	var newppentry = [];
-	var binddb = maindb.collection("userbind");
+	let uid = args[0];
+	let newppentry = [];
+	let binddb = maindb.collection("userbind");
 	let whitelist = maindb.collection("mapwhitelist");
 	binddb.findOne({uid: uid}, function(err, res) {
 		if (err) {
 			console.log(err);
 			return message.channel.send("Error: Empty database response. Please try again!")
 		}
-		if (!res) {console.log("user not found "); return;}
-		var ppentry = res.pp;
-		//var uid = res.uid;
+		if (!res) return message.channel.send("❎ **| I'm sorry, I cannot find the user's profile!**");
+		let ppentry = res.pp;
 		console.log(ppentry[0]);
-		recalc(ppentry, ppentry.length, 0, newppentry, binddb, uid, whitelist);
+		recalc(ppentry, ppentry.length, 0, newppentry, binddb, uid, whitelist)
 	})
 };
 
 module.exports.config = {
+	name: "recalc",
 	description: "Recalculates a user's droid pp profile.",
 	usage: "recalc <uid>",
 	detail: "`uid`: The uid to recalculate [Integer]",
 	permission: "Specific person (<@132783516176875520> and <@386742340968120321>)"
-};
-
-module.exports.help = {
-	name: "recalc"
 };
