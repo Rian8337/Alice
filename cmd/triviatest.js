ceff8756fcf8b7df91542c904b9b109129ebbdee
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:43:04 +0700

    Version 2.0 release

--- cmd/triviatest.js
+++ cmd/triviatest.js
@@ -1,10 +1,10 @@
-var fs = require('fs')
+const fs = require('fs');
 const config = require("../config.json");
 
 function shuffle(input) {
     var temp; var rpick;
     for (var i = 0; i < input.length*2; i++) {
-        rpick = parseInt(Math.floor(Math.random() * input.length))
+        rpick = parseInt(Math.floor(Math.random() * input.length));
         temp = input[rpick];
         input[rpick] = input[i % input.length];
         input[i % input.length] = temp;
@@ -37,9 +37,9 @@ module.exports.run = (client, message, args) => {
     //console.log(start);
     client.on("message", (reply) => {
         if (reply.channel == message.channel) submitted_answer.push([reply.author.id, reply.content, (reply.createdTimestamp - start) / 1000]);
-    })
-    var category = categoryRandomSelect()
-    console.log(category)
+    });
+    var category = categoryRandomSelect();
+    console.log(category);
     var category_name = "";
     var question_dir = "";
     if (!isNaN(parseInt(args[0]))) category = parseInt(args[0]);
@@ -73,11 +73,13 @@ module.exports.run = (client, message, args) => {
         case 26: question_dir = 'trivia/theater.txt'; category_name = 'Entertainment: Musicals & Theatres'; break;  
         case 27: question_dir = 'trivia/vehicle.txt'; category_name = 'Vehicles'; break;  
         case 28: question_dir = 'trivia/videogame.txt'; category_name = 'Entertainment: Video Games'; break; 
+        case 29: question_dir = 'trivia/english.txt'; category_name = 'The English Language'; break;
+        case 30: question_dir = 'trivia/logic.txt'; category_name = 'Logical Reasoning'; break;
         default: message.channel.send("Unknown type"); return;
     }
 	fs.readFile(question_dir , 'utf8', (err, data) => {
         var all_question = data.split('\n');
-        var avail_question = []
+        var avail_question = [];
         if (forced_type) {
             for (i in all_question) if (all_question[i].split(" | ")[1] == forced_type) {avail_question.push(all_question[i])}
             if (!avail_question[0]) {message.channel.send("No question of that type in this category, guess i need more question"); return;}
@@ -95,35 +97,43 @@ module.exports.run = (client, message, args) => {
         var type = parseInt(component[1]);
         var img_link = component[2];
         var question = component[3];
-        if (type === 1) {
+        if (type === 1 || type === 3) {
             var correct_answer = component[4];
             var correct_index
             var answer = [];
-            for (var i = 4; i < component.length; i++) answer.push(component[i]);
-            shuffle(answer);
+            if (type == 1) {
+                for (var i = 4; i < component.length; i++) answer.push(component[i]);
+                shuffle(answer);
+            }
+            if (type == 3) {
+                for (var i = 5; i < component.length; i++) answer.push(component[i]);
+            }
             for (var i = 0; i < answer.length; i++) if (answer[i] == correct_answer) { correct_index = i; break; }
             var answer_string = ""
             for (var i = 0; i < answer.length; i++) answer_string += String.fromCharCode(65+i) + ". " + answer[i] + "\n";
             var embed = {
-                "description": difficulty + "★ - " + category_name + " Question" ,
-				"color": 8102199,
+                "description": difficulty + "★ - " + category_name + " Question\n" + question ,
+                "color": 8102199,
+                "image" : {
+                    "url" : ""
+                },
 				"author": {
 					"name": "Trivia Question",
 					"icon_url": "https://image.frl/p/beyefgeq5m7tobjg.jpg"
 				},
 				"fields": [
 					{
-						"name": question,
+						"name": "Answers",
 						"value": answer_string
 					}
 				]
-            }
+            };
             if (img_link && img_link != "-") embed["image"]["url"] = img_link;
             message.channel.send({embed});
             embed = {
                 "description": "**The correct answer is " + String.fromCharCode(65+correct_index) + ". " + correct_answer + "**",
                 "color": 8102199
-            }
+            };
             setTimeout(() => {
                 answerCheck(String.fromCharCode(65+correct_index));
                 message.channel.send({embed})
@@ -149,13 +159,13 @@ module.exports.run = (client, message, args) => {
 						"value": "Please type out your answer (case insensitive)"
 					}
 				]
-            }
+            };
             if (img_link && img_link != "-") embed["image"]["url"] = img_link;
             message.channel.send({embed});
             embed = {
                 "description": "**The correct answer is " + correct_answer[0] + "**\nAccepted answers are: " + correct_answer.join(", "),
                 "color": 8102199
-            }
+            };
             setTimeout(() => {
                 answerCheck(correct_answer[0], correct_answer);
                 message.channel.send({embed})
@@ -167,7 +177,7 @@ module.exports.run = (client, message, args) => {
             submitted_answer.shift();
             submitted_answer.sort((a, b) => {
                 return (a[0] - b[0] == 0)? a[2] - b[2] : a[0] - b[0];
-            })
+            });
             for (var i = 1; i < submitted_answer.length; i++) {
                 if (submitted_answer[i][0] == submitted_answer[i-1][0]) {submitted_answer.splice(i-1, 1); i--;}
             }
@@ -183,14 +193,14 @@ module.exports.run = (client, message, args) => {
             
             correct_user.sort((a, b) => {
                 return a[2] - b[2];
-            })
+            });
             //console.table(submitted_answer);
             //console.table(correct_user);
             if (!correct_user[0]) {
                 var embed = {
                     "description": "Look like no one got that right.",
                     "color": 16773120
-                }
+                };
                 message.channel.send({embed})
             }
             else {
@@ -201,7 +211,7 @@ module.exports.run = (client, message, args) => {
                 var embed = {
                     "description": output,
                     "color": 8102199
-                }
+                };
                 message.channel.send({embed})
             }
         }
@@ -209,12 +219,9 @@ module.exports.run = (client, message, args) => {
 };
 
 module.exports.config = {
+    name: "triviatest",
     description: "A quiz command.",
     usage: "triviatest [type]",
     detail: "`type`: Quiz type [Integer]. See pinned message in bot channel for more details",
     permission: "None"
-};
-
-module.exports.help = {
-	name: "triviatest"
 };
