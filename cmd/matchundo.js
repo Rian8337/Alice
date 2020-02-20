b416d3e1294dd16ca7e8e8f2fbc607216afbffe6
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:08:32 +0700

    Version 2.0 release

--- cmd/matchundo.js
+++ cmd/matchundo.js
@@ -1,73 +1,45 @@
-var mongodb = require('mongodb');
-
-function statusread(status) {
-	let code = 0;
-	switch (status) {
-		case "scheduled": code = 16776960; break;
-		case "on-going": code = 65280; break;
-		case "completed": code = 16711680; break;
-	}
-	return code;
-}
-
 module.exports.run = (client, message, args, maindb) => {
-	try {
-        let rolecheck = message.member.roles
-    } catch (e) {
-        return
-    }
-	if (!message.member.roles.find("name", "Referee")) {
-		message.channel.send("You don't have enough permission to use this :3");
-		return;
-	}
+	if (message.channel instanceof Discord.DMChannel || message.member.roles == null || !message.member.roles.get("381965207427219456")) return message.channel.send("❎ **| I'm sorry, you don't have enough permission to do this.**");
 	let id = args[0];
-	if (id) {
-		let matchdb = maindb.collection("matchinfo");
-		let query = { matchid: id };
-		matchdb.find(query).toArray(function(err, res) {
-			if (err) throw err;
-			if (!res[0]) {
-				message.channel.send("Can't found the match");
+	if (!id) return message.channel.send("❎ **| Hey, can you give me a match ID?**");
+	let matchdb = maindb.collection("matchinfo");
+	let query = {matchid: id};
+	matchdb.find(query).toArray(function(err, res) {
+		if (err) {
+			console.log(err);
+			return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+		}
+		if (!res[0]) return message.channel.send("❎ **| I'm sorry, I can't find the match!**");
+		let t1score = 0;
+		let t2score = 0;
+		let result = res[0].result;
+		if (!result[0]) return message.channel.send("❎ **| I'm sorry, this match doesn't have any result yet!**");
+		for (let i in result) {
+			if (i % 2 == 0) t1score += result[i].pop();
+			else t2score += result[i].pop();
+		}
+		res[0].team[0][1] -= (t1score > t2score);
+		res[0].team[1][1] -= (t2score > t1score);
+		let update = {
+			$set: {
+				team: res[0].team,
+				result: result
 			}
-			else {
-				if (err) throw err;
-				let t1score = 0;
-				let t2score = 0;
-				let result = res[0].result
-				if (!result[0]) {
-					message.channel.send("No game have been played in this match");
-					return;
-				}
-				for (i in result) {
-					if (i % 2 == 0) t1score += result[i].pop();
-					else t2score += result[i].pop();
-				}
-				res[0].team[0][1] -= (t1score > t2score);
-				res[0].team[1][1] -= (t2score > t1score);
-				var update = {
-					$set: {
-						team: res[0].team,
-						result: result
-					}
-				}
-				matchdb.updateOne(query, update, function(err, res) {
-					if (err) throw err;
-					message.channel.send("Match result reverted");
-				});
+		};
+		matchdb.updateOne(query, update, function(err) {
+			if (err) {
+				console.log(err);
+				return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
 			}
-		});
-	}
-	else message.channel.send("Please specify match id");
-	
+			message.channel.send(`✅ **| Successfully reverted match \`${id}\`'s result.**`);
+		})
+	})
 };
 
 module.exports.config = {
+	name: "matchundo",
 	description: "Undo recent result in a match.",
 	usage: "matchundo <match ID>",
 	detail: "`match ID`: The match's ID [String]",
 	permission: "Referee"
-};
-
-module.exports.help = {
-	name: "matchundo"
 };
