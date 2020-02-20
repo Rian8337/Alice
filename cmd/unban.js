df47d3acdf119fafe94d39ba5ebfb1d6bce8606e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:46:05 +0700

    Version 2.0 release

--- cmd/unban.js
+++ cmd/unban.js
@@ -1,21 +1,21 @@
-let Discord = require('discord.js');
-let config = require('../config.json');
+const Discord = require('discord.js');
+const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
-    if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not available in DMs");
+    if (message.channel instanceof Discord.DMChannel) return message.channel.send("❎ **| I'm sorry, this command is not available in DMs.**");
     if (!message.member.hasPermission("ADMINISTRATOR", false, true, true)) return message.channel.send("❎  **| I'm sorry, you don't have the permission to use this.**");
     
     let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
-    if (!logchannel) return message.channel.send(`Please create ${config.management_channel} first!`);
+    if (!logchannel) return message.channel.send(`"❎ **| Hey, please create ${config.management_channel} first!**`);
     
-    let user = await client.fetchUser(args[0]);
-    if (!user) return message.channel.send("❎ **| Please specify the correct user ID to unban!**");
-    if (user.id == message.author.id) return message.channel.send("❎ **| You cannot unban yourself!**");
+    let user = await message.guild.fetchBan(args[0]);
+    if (!user) return message.channel.send("❎ **| Hey, please specify the correct user ID to unban!**");
+    if (user.user.id == message.author.id) return message.channel.send("❎ **| Hmm yes, you can totally unban yourself.**");
 
     let reason = args.slice(1).join(" ");
-    if (!reason) return message.channel.send("❎ **| Please enter your reason.**");
+    if (!reason) return message.channel.send("❎ **| Hey, please enter your unban reason!**");
 
-    message.guild.unban(user, reason).then (() => {
+    message.guild.unban(user.user, reason).then (() => {
         let footer = config.avatar_list;
         const index = Math.floor(Math.random() * (footer.length - 1) + 1);
 
@@ -24,9 +24,9 @@ module.exports.run = async (client, message, args) => {
             .setFooter("Alice Synthesis Thirty", footer[index])
             .setTimestamp(new Date())
             .setColor(message.member.highestRole.hexColor)
-            .setThumbnail(user.avatarURL)
+            .setThumbnail(user.user.avatarURL)
             .setTitle("Unban executed")
-            .addField("Unbanned user: " + user.username, "User ID: " + user.id)
+            .addField("Unbanned user: " + user.user.username, "User ID: " + user.user.id)
             .addField("=================", "Reason:\n" + reason);
 
         if (message.attachments.size > 0) {
@@ -43,12 +43,9 @@ module.exports.run = async (client, message, args) => {
 };
 
 module.exports.config = {
+    name: "unban",
     description: "Unbans a user.",
     usage: "unban <user> <reason>",
     detail: "`user`: The user to unban [User ID]\n`reason`: Reason for unbanning [String]",
     permission: "Owner"
-};
-
-module.exports.help = {
-    name: "unban"
 };
