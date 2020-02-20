bc49f40a513500424aacc689d8126db260e0837a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 14:19:36 +0700

    Version 2.0 release

--- cmd/bulkdelete.js
+++ cmd/bulkdelete.js
@@ -1,21 +1,15 @@
-let Discord = require('discord.js');
-let config = require('../config.json');
+const Discord = require('discord.js');
+const config = require('../config.json');
 
 module.exports.run = (client, message, args) => {
-    try {
-        let rolecheck = message.member.roles
-    } catch (e) {
-        return
-    }
-    if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not available in DMs");
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do this");
+    if (message.channel instanceof Discord.DMChannel) return message.channel.send("❎ **| I'm sorry, this command is not available in DMs.**");
+    if (!message.member.hasPermission("MANAGE_MESSAGES", false, true, true)) return message.channel.send("❎ **| I'm sorry, you don't have permission to do this.**");
 
     let todelete = parseInt(args[0]);
-    if (!todelete) return message.channel.send("Please specify number of messages to delete");
-    if (isNaN(todelete)) return message.channel.send("Invalid number of messages to delete");
-    if (todelete < 2 || todelete > 100) return message.channel.send("Invalid number of messages to delete, must be in range of 2-100");
+    if (!todelete) return message.channel.send("❎ **| Hey, I don't know the amount of messages to delete!**");
+    if (isNaN(todelete) || todelete < 2 || todelete > 100) return message.channel.send("❎ **| I'm sorry, looks like the number of messages to delete is invalid. Must be in range of 2-100!**");
 
-    message.author.lastMessage.delete().then (() => {
+    message.author.lastMessage.delete().then(() => {
         message.channel.bulkDelete(todelete).then(() => {
             let footer = config.avatar_list;
             const index = Math.floor(Math.random() * (footer.length - 1) + 1);
@@ -28,20 +22,17 @@ module.exports.run = (client, message, args) => {
                 .setFooter("Alice Synthesis Thirty", footer[index])
                 .addField("Amount of messages", todelete);
 
-            message.channel.send({embed}).then(msg => {
+            message.channel.send({embed: embed}).then(msg => {
                 msg.delete(10000)
-            }).catch(e => console.log(e))
+            }).catch(console.error)
         })
     })
 };
 
 module.exports.config = {
+    name: "bulkdelete",
     description: "Deletes a specified amount of messages.",
-    usage: "bulkdelete <amount of messages to delete>",
-    detail: "Amount of messages must be an integer between 2 and 100.",
-    permission: "Manage Messages"
-};
-
-module.exports.help = {
-    name: "bulkdelete"
+    usage: "bulkdelete <amount>",
+    detail: "`amount`: Amount of messages to delete. Must be in range of 2-100. [Integer]",
+    permission: "Manage Messages Permission"
 };
