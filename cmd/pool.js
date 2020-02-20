80c25b697edbbb26f6ea7a393919bda244c78bdc
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:20:02 +0700

    Version 2.0 release

--- cmd/pool.js
+++ cmd/pool.js
@@ -1,19 +1,16 @@
-var fs = require('fs');
+const fs = require('fs');
 
 module.exports.run = (client, message, args) => {
-    let filepath = "./pool/" + args[0] + ".png";
+    const filepath = "./pool/" + args[0] + ".png";
     fs.readFile(filepath, err => {
         message.channel.send(err?"❎ **| I'm sorry, that pool does not exist!**": {file: filepath})
     })
 };
 
 module.exports.config = {
+    name: "pool",
     description: "Views the pool of a tournament.",
     usage: "pool <pool id>",
     detail: "`pool id`: The ID of the pool",
     permission: "None"
-};
-
-module.exports.help = {
-    name: "pool"
 };
