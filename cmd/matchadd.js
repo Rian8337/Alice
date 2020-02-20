81f36043d5f3c53aab01f3a97205068e5919c41b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 14:50:17 +0700

    Version 2.0 release

--- cmd/matchadd.js
+++ cmd/matchadd.js
@@ -1,74 +1,61 @@
-var mongodb = require('mongodb');
+const Discord = require('discord.js');
 
 module.exports.run = (client, message, args, maindb) => {
-	try {
-        let rolecheck = message.member.roles
-    } catch (e) {
-        return
-    }
-	if (message.member.roles.find("name", "Referee")) {
-		if (args.length > 4) {
-			let id = args[0]; var i = 1; let name  = ""; let inName = false;
-			for (i; i < args.length; i++) {
-				if (args[i].includes("(")) inName = true;
-				if (inName) name = name + args[i] + " ";
-				if (args[i].includes(")")) break;
-			}
-			i++;
-			var team = [];
-			team.push([args[i], 0]);
-			team.push([args[i+1], 0]);
-			i+=2;
-			if ((args.length-i)%4==0 && (args.length-i) > 0) {
-				var player = [];
-				let nP = 0;
-				while (i < args.length) {
-					player.push([args[i], args[i+1]]);
-					nP++;
-					i += 2;
-				}
-				var maps = [];
-				var result = [];
-				var status = "scheduled";
-				for (var k = 0; k < nP; k++) result.push([]);
-				let matchdb = maindb.collection("matchinfo");
-				let query = { matchid: id };
-				var matchinfo = {
-					matchid: id,
-					name: name,
-					team: team,
-					player: player,
-					status: status,
-					result: result
-				};
-				matchdb.find(query).toArray(function(err, res) {
-					if (err) throw err;
-					if (!res[0]) {
-						matchdb.insertOne(matchinfo, function(err, res) {
-							if (err) throw err;
-							console.log("match added");
-							message.channel.send("Match added");
-						})
-					}
-					else {
-						message.channel.send("A match with the same id has already existed");
-					}
-				})
-			}
-			else message.channel.send("Not enough input");
-		}
-		else message.channel.send("Not enough input");
+	if (message.channel instanceof Discord.DMChannel || message.member.roles == null || !message.member.roles.get("381965207427219456")) return message.channel.send("❎ **| I'm sorry, you don't have enough permission to do this.**");
+	if (args.length <= 4) return message.channel.send("❎ **| I'm sorry, I need more input!**");
+	let id = args[0]; let i = 1; let name  = ""; let inName = false;
+	for (i; i < args.length; i++) {
+		if (args[i].includes("(")) inName = true;
+		if (inName) name = name + args[i] + " ";
+		if (args[i].includes(")")) break;
 	}
-	else message.channel.send("You don't have enough permission to use this :3");
+	i++;
+	let team = [];
+	team.push([args[i], 0]);
+	team.push([args[i+1], 0]);
+	i+=2;
+	if ((args.length-i)%4!=0 && (args.length-i) <= 0) return message.channel.send("❎ **| I'm sorry, I need more input!**");
+	let player = [];
+	let nP = 0;
+	while (i < args.length) {
+		player.push([args[i], args[i+1]]);
+		nP++;
+		i += 2;
+	}
+	let result = [];
+	let status = "scheduled";
+	for (let k = 0; k < nP; k++) result.push([]);
+	let matchdb = maindb.collection("matchinfo");
+	let query = { matchid: id };
+	let matchinfo = {
+		matchid: id,
+		name: name,
+		team: team,
+		player: player,
+		status: status,
+		result: result
+	};
+	matchdb.find(query).toArray(function(err, res) {
+		if (err) {
+			console.log(err);
+			return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+		}
+		if (res[0]) return message.channel.send("❎ **| I'm sorry, a match with the same match ID already exist!**");
+		matchdb.insertOne(matchinfo, function(err) {
+			if (err) {
+				console.log(err);
+				return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+			}
+			console.log("match added");
+			message.channel.send(`✅ **| Successfully added match \`${id}\`.**`)
+		})
+	})
 };
 
 module.exports.config = {
+	name: "matchadd",
 	description: "Adds a match.",
 	usage: "matchadd <match ID> <player/team> <uid> <...>",
 	detail: "`match ID`: The match's ID [String]\n`player/team`: Player or team name [String]\n`uid`: The uid of player [Integer]",
 	permission: "Referee"
-};
-
-module.exports.help = {
-	name: "matchadd"
 };
