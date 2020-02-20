0c4829f0c41c49082b64cddb68ca36b1ff9fb1a2
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 14:42:15 +0700

    Version 2.0 release

--- cmd/help.js
+++ cmd/help.js
@@ -1,9 +1,9 @@
-var Discord = require('discord.js');
-var config = require('../config.json');
-var help = require('../help.json');
+const Discord = require('discord.js');
+const config = require('../config.json');
+const help = require('../help.json');
 
 module.exports.run = (client, message, args) => {
-	var rolecheck;
+	let rolecheck;
 	try {
 		rolecheck = message.member.highestRole.hexColor
 	} catch (e) {
@@ -11,42 +11,41 @@ module.exports.run = (client, message, args) => {
 	}
 	if (args[0]) {
 		let cmd = client.commands.get(args[0]);
-		if (cmd) {
-			let footer = config.avatar_list;
-			const index = Math.floor(Math.random() * (footer.length - 1) + 1);
-			let help = `${cmd.config.description}\n\n\`<...>\`: required arguments\n\`[...]\`: optional arguments\n\n**Permission: **${cmd.config.permission}\n**Usage:**\n\`${cmd.config.usage}\`\n**Details:**\n${cmd.config.detail}`;
-			let embed = new Discord.RichEmbed()
-				.setTitle(config.prefix + args[0])
-				.setColor(rolecheck)
-				.setFooter("Alice Synthesis Thirty", footer[index])
-				.setDescription(help);
-			message.channel.send({embed: embed})
-		} else message.channel.send("❎ **| I'm sorry, I cannot find the command you are looking for!**")
+		if (!cmd) return message.channel.send("❎ **| I'm sorry, I cannot find the command you are looking for!**");
+		let footer = config.avatar_list;
+		const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+		let help = `${cmd.config.description}\n\n\`<...>\`: required arguments\n\`[...]\`: optional arguments\n\n**Permission: **${cmd.config.permission}\n**Usage:**\n\`${cmd.config.usage}\`\n**Details:**\n${cmd.config.detail}`;
+		let embed = new Discord.RichEmbed()
+			.setTitle(config.prefix + args[0])
+			.setColor(rolecheck)
+			.setFooter("Alice Synthesis Thirty", footer[index])
+			.setDescription(help);
+		message.channel.send({embed: embed}).catch(console.error)
 	} else {
 		let general = help.general;
 		let genhelp = '';
-		for (var i = 0; i < general.length; i++) {
+		for (let i = 0; i < general.length; i++) {
 			genhelp += "`" + general[i] + "` ";
 		}
 		genhelp = genhelp.slice(0, -1);
 
 		let droid = help.droid;
 		let droidhelp = '';
-		for (i = 0; i < droid.length; i++) {
+		for (let i = 0; i < droid.length; i++) {
 			droidhelp += "`" + droid[i] + "` ";
 		}
 		droidhelp = droidhelp.slice(0, -1);
 
 		let pp = help.dpp;
 		let pphelp = 'NOTE: This system does not automatically submit your plays. You have to do it manually using the command provided below. Moreover, you can only submit up to 50 of your recent plays. If you want to submit all your previous plays, contact a helper or moderator.\n';
-		for (i = 0; i < pp.length; i++) {
+		for (let i = 0; i < pp.length; i++) {
 			pphelp += "`" + pp[i] + "` ";
 		}
 		pphelp = pphelp.slice(0, -1);
 
 		let malody = help.malody;
 		let malodyhelp = '';
-		for (i = 0; i < malody.length; i++) {
+		for (let i = 0; i < malody.length; i++) {
 			malodyhelp += "`" + malody[i] + "` ";
 		}
 		malodyhelp = malodyhelp.slice(0, -1);
@@ -69,12 +68,9 @@ module.exports.run = (client, message, args) => {
 };
 
 module.exports.config = {
+	name: "help",
 	description: "General help command.",
 	usage: "help [cmd]",
 	detail: "`cmd`: Command name",
 	permission: "None"
-};
-
-module.exports.help = {
-	name: "help"
 };
