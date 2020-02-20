0cd2452ef3e09dc982949bcf766ba8f427766728
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 13:33:31 +0700

    Version 2.0 release

--- cmd/ask.js
+++ cmd/ask.js
@@ -8,7 +8,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
             return message.channel.send("❎ **| I'm sorry I'm having trouble receiving response from database. Please try again!**")
         }
         if (res[0]) {
-            var count = res[0].count;
+            let count = res[0].count;
             if (count === 1) message.channel.send(`✅ **| ${message.author.username}, you have asked me \`${count}\` time.**`);
             else message.channel.send(`✅ **| ${message.author.username}, you have asked me \`${count}\` times.**`)
         }
@@ -17,12 +17,9 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
 };
 
 module.exports.config = {
+    name: "ask",
     description: "See how many times you have asked me.",
     usage: "ask",
     detail: "None",
     permission: "None"
-};
-
-module.exports.help = {
-    name: "ask"
 };
