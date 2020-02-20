855f1f70996e1e33ea00aff3640ca6c32e5404fc
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:10:57 +0700

    Version 2.0 release

--- cmd/mudaeban.js
+++ cmd/mudaeban.js
@@ -1,10 +1,9 @@
-var Discord = require("discord.js");
-var config = require("../config.json");
+const Discord = require("discord.js");
+const config = require("../config.json");
 
 function isEligible(member) {
-    var res = 0;
-    var eligibleRoleList = config.mudae_ban; //mute_permission
-    //console.log(eligibleRoleList)
+    let res = 0;
+    let eligibleRoleList = config.mudae_ban; //mute_permission
     eligibleRoleList.forEach((id) => {
         if(member.roles.has(id[0])) res = id[1]
     });
@@ -12,8 +11,8 @@ function isEligible(member) {
 }
 
 function isImmuned(member) {
-    var res = 0;
-    var immunedRoleList = config.mudae_immune;
+    let res = 0;
+    let immunedRoleList = config.mudae_immune;
     immunedRoleList.forEach((id) => {
         if(member.roles.has(id)) {console.log("Immune role found"); res = 1}
     });
@@ -31,56 +30,39 @@ function timeconvert (num) {
 module.exports.run = async (client, message, args) => {
     if (message.channel instanceof Discord.DMChannel || message.member.roles == null) return;
     if (message.guild.id != "635532651029332000") return;
-    var timeLimit = isEligible(message.member);
-    if (timeLimit == 0) {
-        message.channel.send("You don't have permission to use this");
-        return;
-    }
+
+    let timeLimit = isEligible(message.member);
+    if (timeLimit == 0) return message.channel.send("You don't have permission to use this");
+
     let toban = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if (!toban) return;
-    if (isImmuned(toban)) {
-        message.channel.send("You can't ban this user");
-        return;
-    }
+    if (isImmuned(toban)) return message.channel.send("You can't ban this user");
     let reason = args.slice(2).join(" ");
-    let bantime = args[1];
-    if(!bantime) {
-        message.channel.send("Ban time is not defined");
-        return;
-    }
-    if (isNaN(bantime)) {
-        message.channel.send("Invalid time limit, only send number of seconds");
-        return;
-    }
-    if (bantime < 1) {
-        message.channel.send("Invalid time limit, minimum ban time is 1 second");
-        return;
-    }
-    if (timeLimit != -1 && timeLimit < bantime) {
-        message.channel.send("You don't have enough permission to ban a user for longer than " + timeLimit + "s");
-        return;
-    }
-    if (!reason) {
-        message.channel.send("Please add a reason.");
-        return;
-    }
 
-    let banrole = message.guild.roles.find(`name`, "mudae-ban");
+    let bantime = args[1];
+    if (!bantime) return message.channel.send("Ban time is not defined");
+    if (isNaN(bantime)) return message.channel.send("Invalid time limit, only send number of seconds");
+    if (bantime < 1) return message.channel.send("Invalid time limit, minimum ban time is 1 second");
+    if (timeLimit != -1 && timeLimit < bantime) return message.channel.send("You don't have enough permission to ban a user for longer than " + timeLimit + "s");
+
+    if (!reason) return message.channel.send("Please add a reason.");
+
+    let banrole = message.guild.roles.find(r => r.name === 'mudae-ban');
     //start of create role
-    if(!banrole){
-        try{
+    if (!banrole) {
+        try {
             banrole = await message.guild.createRole({
                 name: "mudae-ban",
                 color: "#000000",
                 permissions:[]
-            })
-            message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(banrole, {
+            });
+            message.guild.channels.forEach(channel => {
+                channel.overwritePermissions(banrole, {
                     SEND_MESSAGES: false,
                     ADD_REACTIONS: false
-                });
-            });
-        }catch(e){
+                }).catch(console.error)
+            })
+        } catch(e) {
             console.log(e.stack);
         }
     }
@@ -103,33 +85,27 @@ module.exports.run = async (client, message, args) => {
         .addField("Length: " + timeconvert(bantime), "=========================")
         .addField("Reason: ", reason);
 
-    let channel = message.guild.channels.find(c => c.name === config.management_channel);
+    let channel = message.guild.channels.get("648445681551409152");
     if (!channel) return message.reply("Please create a mute log channel first!");
     channel.send(banembed);
 
     toban.addRole(banrole.id)
         .catch(console.error);
 
-    let mudaerole = message.guild.roles.find("name", "Mudae Player");
+    let mudaerole = message.guild.roles.find(r => r.name === "Mudae Player");
     toban.removeRole(mudaerole.id)
         .catch(console.error);
 
     setTimeout(function(){
         toban.removeRole(banrole.id);
         toban.addRole(mudaerole.id)
-    }, bantime * 1000);
-
-
-//end of module
+    }, bantime * 1000)
 };
 
 module.exports.config = {
+    name: "mudae-ban",
     description: "Temporarily bans a user from Mudae (only applies in droid cafe).",
     usage: "mudaeban <user> <duration> <reason>",
     detail: "`user`: The user to ban [UserResolvable (mention or user ID)]\n`duration`: Ban duration in seconds [Float]\n`reason`: Reason for banning [String]",
     permission: "Manager"
-};
-
-module.exports.help = {
-    name: "mudaeban"
 };
