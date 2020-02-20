ab8f52cc672b6c9e6f8530034450993460ef2d91
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:35:14 +0700

    Version 2.0 release

--- cmd/say.js
+++ cmd/say.js
@@ -1,20 +1,16 @@
 module.exports.run = (client, message, args) => {
-        if (message.author.id != '386742340968120321') return;
 	let sayMessage = args.join(" ");
 	if (sayMessage.search("@everyone") != -1 || sayMessage.search("@here") != -1) {
-		message.channel.send("**S**: No no, you dummy");
+		message.channel.send("No no, you dummy");
 		return;
 	}
-	message.channel.send("**S**: " + sayMessage);
+	message.channel.send(sayMessage);
 };
 
 module.exports.config = {
+	name: "say",
 	description: "Says a message with the bot.",
 	usage: "say <...>",
 	detail: "`...`: Message to say [String]",
 	permission: "Bot Owner"
-};
-
-module.exports.help = {
-	name: "say"
 };
