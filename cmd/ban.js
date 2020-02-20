03e9faec8f473e24ceb0518c5ea505bdcc07dd2a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 14:14:24 +0700

    Version 2.0 release

--- cmd/ban.js
+++ cmd/ban.js
@@ -1,26 +1,25 @@
-let Discord = require('discord.js');
-let config = require('../config.json');
+const Discord = require('discord.js');
+const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
     if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not available in DMs");
     if (!message.member.hasPermission("ADMINISTRATOR", false, true, true)) return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
 
     let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
-    if (!logchannel) return message.channel.send(`Please create ${config.management_channel} first!`);
+    if (!logchannel) return message.channel.send(`"❎ **| Hey, please create ${config.management_channel} first!**`);
 
     let userid = args[0];
-    if (!userid) return message.channel.send("❎ **| Please specify the correct user to ban!**");
-    userid = userid.replace('<@!','');
-    userid = userid.replace('<@','');
-    userid = userid.replace('>','');
+    if (!userid) return message.channel.send("❎ **| Hey, please specify the correct user to ban!**");
+    userid = userid.replace('<@!','').replace('<@','').replace('>','');
 
-    if (isNaN(userid)) return message.channel.send("❎ **| Please specify the correct user to ban!**");
-    if (userid == message.author.id) return message.channel.send("❎ **| You cannot ban yourself!**");
+    if (isNaN(parseInt(userid))) return message.channel.send("❎ **| Hey, please specify the correct user to ban!**");
+    if (userid == message.author.id) return message.channel.send("❎ **| Why would you ban yourself?**");
 
     let toban = await client.fetchUser(userid);
     if (!toban) return message.channel.send("❎ **| I'm sorry, I cannot find the user!**");
+    if (toban.bot) return message.channel.send("❎ **| I'm sorry, you cannot ban bots!**");
     let reason = args.slice(1).join(" ");
-    if (!reason) return message.channel.send("❎ **| Please enter your reason.**");
+    if (!reason) return message.channel.send("❎ **| Hey, please enter your ban reason!**");
 
     message.guild.ban(toban, {reason: reason + ` (banned by ${message.author.username})`}).then (() => {
         let footer = config.avatar_list;
@@ -39,9 +38,9 @@ module.exports.run = async (client, message, args) => {
         if (message.attachments.size > 0) {
             let attachments = [];
             message.attachments.forEach(attachment => {
-                attachments.push(attachment.proxyURL)
+                attachments.push(attachment.proxyURL);
+                if (attachments.length === message.attachments.size) logchannel.send({embed: embed, files: attachments})
             });
-            logchannel.send({embed: embed, files: attachments})
         }
         else logchannel.send({embed: embed});
 
@@ -50,12 +49,9 @@ module.exports.run = async (client, message, args) => {
 };
 
 module.exports.config = {
+    name: "ban",
     description: "Bans a user from the server.",
     usage: "ban <user> <reason>",
     detail: "`user`: The user to ban [UserResolvable (mention or user ID)]\n`reason`: Reason for banning [String]",
     permission: "Owner"
-};
-
-module.exports.help = {
-    name: "ban"
 };
