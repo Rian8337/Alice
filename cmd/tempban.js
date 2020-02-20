c3472bd2ae3a94333d30f3188330de857c5e4a08
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:39:28 +0700

    Version 2.0 release

--- cmd/tempban.js
+++ cmd/tempban.js
@@ -1,43 +1,41 @@
-let Discord = require('discord.js');
-let config = require('../config.json');
+const Discord = require('discord.js');
+const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
-    if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not allowed in DMs");
+    if (message.channel instanceof Discord.DMChannel) return message.channel.send("❎ **| I'm sorry, this command is not allowed in DMs.**");
     if (!message.member.hasPermission("ADMINISTRATOR", false, true, true)) return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
 
     let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
     if (!logchannel) return message.channel.send(`Please create ${config.management_channel} first!`);
 
     let userid = args[0];
-    if (!userid) return message.channel.send("Please specify the correct user to ban!");
-    userid = userid.replace('<@!','');
-    userid = userid.replace('<@','');
-    userid = userid.replace('>','');
+    if (!userid) return message.channel.send("❎ **| Hey, can you please specify someone to ban?**");
+    userid = userid.replace('<@!','').replace('<@', '').replace('>', '');
 
-    if (isNaN(Number(userid))) return message.channel.send("Please specify the correct user to ban!");
+    if (isNaN(parseInt(userid))) return message.channel.send("❎ **| Hey, that user is invalid!");
 
-    if (userid === message.author.id) return message.channel.send("You cannot ban yourself!");
+    if (userid === message.author.id) return message.channel.send("❎ **| Why would you ban yourself, you fool?**");
 
     let banlist = await message.guild.fetchBans();
     let banned = banlist.get(userid);
-    if (banned) return message.channel.send("User is already banned!");
+    if (banned) return message.channel.send("❎ **| I'm sorry, this user is already banned!**");
 
     let toban = await client.fetchUser(userid);
-    if (!toban) return message.channel.send("User not found!");
+    if (!toban) return message.channel.send("❎ **| I'm sorry, I can't find the user to ban!**");
+    if (toban.bot) return message.channel.send("❎ **| I'm sorry, I cannot find the user!**");
 
     let bantime = args[1];
-    if (!bantime) return message.channel.send("Please specify ban time!");
+    if (!bantime) return message.channel.send("❎ **| Hey, please specify correct ban time!");
     if (bantime.endsWith("h")) bantime = parseFloat(bantime) / 24;
     else if (bantime.endsWith("d")) bantime = parseFloat(bantime);
-    else return message.channel.send("Please specify if time is in hours or days!");
-    if (isNaN(bantime) || bantime <= 0) return message.channel.send("Invalid ban time");
+    else return message.channel.send("❎ **| Hey, please specify if time is in hours or days!*8");
+    if (isNaN(bantime) || bantime <= 0) return message.channel.send("❎ **| I'm sorry, that ban time is invalid!**");
 
     let reason = args.slice(2).join(" ");
-    if (!reason) return message.channel.send("Please enter your reason.");
-    reason += " (banned by " + message.author.username + ")";
+    if (!reason) return message.channel.send("❎ **| Hey, please enter your ban reason!**");
 
     message.guild.ban(toban, {reason: reason}).then (() => {
-        message.author.lastMessage.delete();
+        message.author.lastMessage.delete().catch(console.error);
         let footer = config.avatar_list;
         const index = Math.floor(Math.random() * (footer.length - 1) + 1);
         let embed = new Discord.RichEmbed()
@@ -69,12 +67,9 @@ module.exports.run = async (client, message, args) => {
 };
 
 module.exports.config = {
+    name: "tempban",
     description: "Temporarily bans a user.",
     usage: "tempban <user> <[hours]h / [days]d> <reason>",
     detail: "`user`: The user to ban [UserResolvable (mention or user ID)]\n`hours`: Time to ban in hours [Float]\n`days`: Time to ban in days [Float]\n`reason`: Reason for banning [String]",
     permission: "Owner"
-};
-
-module.exports.help = {
-    name: "tempban"
 };
