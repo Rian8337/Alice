34acff36d84c21f40107d633b25d19ef0ce06ed4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 14:52:47 +0700

    Version 2.0 release

--- cmd/matchend.js
+++ cmd/matchend.js
@@ -1,81 +1,57 @@
-var mongodb = require('mongodb');
-
-function statusread(status) {
-	let code = 0;
-	switch (status) {
-		case "scheduled": code = 16776960; break;
-		case "on-going": code = 65280; break;
-		case "completed": code = 16711680; break;
-	}
-	return code;
-}
-
 module.exports.run = (client, message, args, maindb) => {
-	try {
-        let rolecheck = message.member.roles
-    } catch (e) {
-        return
-    }
-	if (message.member.roles.find("name", "Referee")) {
-		let id = args[0];
-		if (id) {
-			let matchdb = maindb.collection("matchinfo");
-			let query = { matchid: id };
-			matchdb.find(query).toArray(function(err, res) {
-				if (err) throw err;
-				if (!res[0]) {
-					message.channel.send("Can't found the match");
-				}
-				else {
-					if (res[0].status == "completed") message.channel.send("Match has already ended");
-					else {
-						var update = {
-							$set: {
-								status: "completed"
-							}
-						}
-						matchdb.updateOne(query, update, function(err, res2) {
-							if (err) throw err;
-							console.log("match ended");
-							let name = res[0].name;
-							let t1name = res[0].team[0][0];
-							let t2name = res[0].team[1][0];
-							let t1score = res[0].team[0][1];
-							let t2score = res[0].team[1][1];
-							const embed = {
-								"title": name,
-								"color": 16711680,
-								"fields": [
-									{
-										"name": t1name,
-										"value": "**" + t1score + "**",
-										"inline": true
-									},
-									{
-										"name": t2name,
-										"value": "**" + t2score + "**",
-										"inline": true
-									}
-								]
-							};
-							message.channel.send("Match ended", { embed });
-						});
+	if (message.channel instanceof Discord.DMChannel || message.member.roles == null || !message.member.roles.get("381965207427219456")) return message.channel.send("❎ **| I'm sorry, you don't have enough permission to do this.**");
+	let id = args[0];
+	if (!id) return message.channel.send("❎ **| Hey, can you give me a match ID?**");
+	let matchdb = maindb.collection("matchinfo");
+	let query = {matchid: id};
+	matchdb.find(query).toArray(function (err, res) {
+		if (err) {
+			console.log(err);
+			return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+		}
+		if (!res[0]) return message.channel.send("❎ **| I'm sorry, I can't find the match!**");
+		if (res[0].status == "completed") return message.channel.send("❎ **| I'm sorry, the match has ended!**");
+		let update = {
+			$set: {
+				status: "completed"
+			}
+		};
+		matchdb.updateOne(query, update, function (err) {
+			if (err) {
+				console.log(err);
+				return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+			}
+			console.log("match ended");
+			let name = res[0].name;
+			let t1name = res[0].team[0][0];
+			let t2name = res[0].team[1][0];
+			let t1score = res[0].team[0][1];
+			let t2score = res[0].team[1][1];
+			const embed = {
+				"title": name,
+				"color": 16711680,
+				"fields": [
+					{
+						"name": t1name,
+						"value": "**" + t1score + "**",
+						"inline": true
+					},
+					{
+						"name": t2name,
+						"value": "**" + t2score + "**",
+						"inline": true
 					}
-				}
-			});
-		}
-		else message.channel.send("Please specify match id");
-		}
-	else message.channel.send("You don't have enough permission to use this :3");
+				]
+			};
+			message.channel.send(`✅ **| Successfully ended match \`${id}\`**`, {embed: embed});
+		})
+	})
 };
 
 module.exports.config = {
+	name: "matchend",
 	description: "Ends a match.",
 	usage: "matchend <match ID>",
 	detail: "`match ID`: The match's ID [String]",
 	permission: "Referee"
-};
-
-module.exports.help = {
-	name: "matchend"
 };
