aa7807b14261c846187be6e01776ad56a8ed128e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-20 15:22:49 +0700

    Version 2.0 release

--- cmd/ppwipe.js
+++ cmd/ppwipe.js
@@ -1,21 +1,17 @@
-var Discord = require('discord.js');
-let config = require('../config.json');
+const Discord = require('discord.js');
+const config = require('../config.json');
 
 module.exports.run = (client, message, args, maindb) => {
-	if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not available in DMs");
-    	if (message.author.id != '132783516176875520' && message.author.id != '386742340968120321') return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
+	if (message.author.id != '132783516176875520' && message.author.id != '386742340968120321') return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
 
 	let guild = client.guilds.get('528941000555757598');
 	let logchannel = guild.channels.get('638671295470370827');
-	if (!logchannel) return message.channel.send("Please create #pp-log first!");
+	if (!logchannel) return message.channel.send("❎ **| Please create #pp-log first!**");
 
 	let ufind = args[0];
-	if (!args[0]) return message.channel.send("Please mention a user");
-	ufind = ufind.replace('<@!','');
-	ufind = ufind.replace('<@','');
-	ufind = ufind.replace('>','');
+	if (!args[0]) return message.channel.send("❎ **| Hey, can you mention a user? Unless you want me to delete your own plays, if that's your thing.**");
+	ufind.replace("<@!", "").replace("<@", "").replace(">", "");
 
-	console.log(ufind);
 	let binddb = maindb.collection("userbind");
 	let query = {discordid: ufind};
 	binddb.find(query).toArray(function (err, userres) {
@@ -23,57 +19,48 @@ module.exports.run = (client, message, args, maindb) => {
 			console.log(err);
 			return message.channel.send("Error: Empty database response. Please try again!")
 		}
-		if (userres[0]) {
-			let uid = userres[0].uid;
-			let discordid = userres[0].discordid;
-			let username = userres[0].username;
-			if (userres[0].pp) var pplist = userres[0].pp;
-			else var pplist = [];
-			if (userres[0].pptotal) var pre_pptotal = userres[0].pptotal;
-			else var pre_pptotal = 0;
-			if (userres[0].playc) var playc = userres[0].playc;
-			else var playc = 0;
+		if (!userres[0]) return message.channel.send("❎ **| I'm sorry, the account is not binded. He/she/you need to use `a!userbind <uid>` first. To get uid, use `a!profilesearch <username>`.**");
+		let uid = userres[0].uid;
+		let discordid = userres[0].discordid;
+		let username = userres[0].username;
+		let pre_pptotal = userres[0].pptotal;
+		let playc = userres[0].playc;
 
-			pplist = [];
-			message.channel.send("User pp data has been wiped");
+		message.channel.send("✅ **| Successfully wiped user's pp data!**");
 
-			let footer = config.avatar_list;
-			const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+		let footer = config.avatar_list;
+		const index = Math.floor(Math.random() * (footer.length - 1) + 1);
 
-			const embed = new Discord.RichEmbed()
-				.setTitle("__PP data wipe performed__")
-				.setColor("#188c1f")
-				.setFooter("Alice Synthesis Thirty", footer[index])
-				.setTimestamp(new Date())
-				.addField("**User stats**", `Discord User: <@${discordid}>\nUsername: ${username}\nUid: ${uid}`)
-				.addField("**PP stats**", `PP count: ${parseFloat(pre_pptotal).toFixed(2)} pp\nPlay count: ${playc}`);
-			logchannel.send({embed});
+		const embed = new Discord.RichEmbed()
+			.setTitle("__PP data wipe performed__")
+			.setColor("#188c1f")
+			.setFooter("Alice Synthesis Thirty", footer[index])
+			.setTimestamp(new Date())
+			.addField("**User stats**", `Discord User: <@${discordid}>\nUsername: ${username}\nUid: ${uid}`)
+			.addField("**PP stats**", `PP count: ${parseFloat(pre_pptotal).toFixed(2)} pp\nPlay count: ${playc}`);
+		logchannel.send({embed});
 
-			var pptotal = 0;
-			playc = 0;
-			var updateVal = {
-				$set: {
-					pptotal: pptotal,
-					pp: pplist,
-					playc: playc
-				}
-			};
-			binddb.updateOne(query, updateVal, function (err) {
-				if (err) throw err;
-				console.log('pp updated');
-				addcount = 0;
-			})
-		} else message.channel.send("The account is not binded, he/she/you need to use `&userbind <uid>` first. To get uid, use `&profilesearch <username>`")
+		let updateVal = {
+			$set: {
+				pptotal: 0,
+				pp: [],
+				playc: 0
+			}
+		};
+		binddb.updateOne(query, updateVal, function (err) {
+			if (err) {
+				console.log(err);
+				return message.channel.send("Error: Empty database response. Please try again!")
+			}
+			console.log('pp updated');
+		})
 	})
 };
 
 module.exports.config = {
+	name: "ppwipe",
 	description: "Wipes a user's droid pp data.",
 	usage: "ppwipe <user>",
 	detail: "`user`: The user to wipe [UserResolvable (mention or user ID)]",
 	permission: "Specific person (<@132783516176875520> and <@386742340968120321>)"
-};
-
-module.exports.help = {
-	name: "ppwipe"
 };
