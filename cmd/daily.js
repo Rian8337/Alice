db542ec8f4ced300b92d91553bfe67ba64a72fcc
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-21 23:53:00 +0700

    Fix manual insane bonus type

--- cmd/daily.js
+++ cmd/daily.js
@@ -879,7 +879,8 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
                             if (challengeid.includes("w")) bonus = dailyres[0].bonus;
                             else bonus = dailyres[0].bonus[3];
                             if (challengeid.includes("d") && !bonus) return message.channel.send("❎ **| I'm sorry, `insane` bonus type is only available for weekly challenges!**");
-                            index = 1;
+                            if (challengeid.includes("d")) index = 4;
+                            else index = 1;
                             break
                         }
                     }
