ca59175fb58df31e192333ef15d1e3bec1fa269e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-02-27 18:31:48 +0700

    Fix string

--- cmd/completepp.js
+++ cmd/completepp.js
@@ -49,7 +49,7 @@ function calculatePP(ppentries, entry, cb) {
             miss: miss,
             mode: "droid"
         });
-        let playinfo = mapinfo.showStatistics(mods, 0);
+        let playinfo = mapinfo.full_title;
         let pp = parseFloat(npp.toString().split(" ")[0]);
         let ppentry = [entry[8], playinfo, pp, combo.toString() + "x", acc_percent.toString() + "%", miss.toString()];
         if (!isNaN(pp)) ppentries.push(ppentry);
