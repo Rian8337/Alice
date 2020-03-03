3199394ab1b6d6ac94006dc22217b6d959f3155f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 20:20:50 +0700

    Discord.js v12 update

--- cmd/tempban.js
+++ cmd/tempban.js
@@ -3,9 +3,9 @@ const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
     if (message.channel instanceof Discord.DMChannel) return message.channel.send("❎ **| I'm sorry, this command is not allowed in DMs.**");
-    if (!message.member.hasPermission("ADMINISTRATOR", false, true, true)) return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
+    if (!message.member.hasPermission("ADMINISTRATOR", {checkAdmin: true, checkOwner: true})) return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
 
-    let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
+    let logchannel = message.guild.channels.cache.find((c) => c.name === config.management_channel);
     if (!logchannel) return message.channel.send(`Please create ${config.management_channel} first!`);
 
     let userid = args[0];
@@ -16,54 +16,59 @@ module.exports.run = async (client, message, args) => {
 
     if (userid === message.author.id) return message.channel.send("❎ **| Why would you ban yourself, you fool?**");
 
-    let banlist = await message.guild.fetchBans();
-    let banned = banlist.get(userid);
+    let banned = await message.guild.fetchBan(userid);
     if (banned) return message.channel.send("❎ **| I'm sorry, this user is already banned!**");
 
-    let toban = await client.fetchUser(userid);
+    let toban = await client.users.fetch(userid);
     if (!toban) return message.channel.send("❎ **| I'm sorry, I can't find the user to ban!**");
-    if (toban.bot) return message.channel.send("❎ **| I'm sorry, I cannot find the user!**");
+    if (toban.bot) return message.channel.send("❎ **| I'm sorry, this user is a bot!**");
 
     let bantime = args[1];
     if (!bantime) return message.channel.send("❎ **| Hey, please specify correct ban time!");
     if (bantime.endsWith("h")) bantime = parseFloat(bantime) / 24;
     else if (bantime.endsWith("d")) bantime = parseFloat(bantime);
-    else return message.channel.send("❎ **| Hey, please specify if time is in hours or days!*8");
+    else return message.channel.send("❎ **| Hey, please specify if time is in hours or days!**");
     if (isNaN(bantime) || bantime <= 0) return message.channel.send("❎ **| I'm sorry, that ban time is invalid!**");
 
     let reason = args.slice(2).join(" ");
     if (!reason) return message.channel.send("❎ **| Hey, please enter your ban reason!**");
 
-    message.guild.ban(toban, {reason: reason}).then (() => {
+    message.guild.members.ban(toban, {reason: reason}).then (() => {
         message.author.lastMessage.delete().catch(console.error);
         let footer = config.avatar_list;
-        const index = Math.floor(Math.random() * (footer.length - 1) + 1);
-        let embed = new Discord.RichEmbed()
-            .setAuthor(message.author.tag, message.author.avatarURL)
+        const index = Math.floor(Math.random() * footer.length);
+        let rolecheck;
+        try {
+            rolecheck = message.member.roles.highest.hexColor
+        } catch (e) {
+            rolecheck = "#000000"
+        }
+        let embed = new Discord.MessageEmbed()
+            .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
             .setFooter("Alice Synthesis Thirty", footer[index])
             .setTimestamp(new Date())
-            .setColor(message.member.highestRole.hexColor)
+            .setColor(rolecheck)
             .setTitle("Temporary ban executed")
             .addField("Banned user: " + toban.username + "\nUser ID: " + userid, "Length: " + (bantime * 24) + " hour(s)")
             .addField("=========================", "Reason:\n" + reason);
 
-        logchannel.send({embed});
+        logchannel.send({embed: embed});
 
         setTimeout(() => {
-            message.guild.unban(toban, "Ban time is over").then (() => {
-                embed = new Discord.RichEmbed()
-                    .setAuthor(message.author.tag, message.author.avatarURL)
+            message.guild.members.unban(toban, "Ban time is over").then(() => {
+                embed = new Discord.MessageEmbed()
+                    .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
                     .setFooter("Alice Synthesis Thirty", footer[index])
                     .setTimestamp(new Date())
-                    .setColor(message.member.highestRole.hexColor)
+                    .setColor(rolecheck)
                     .setTitle("User unbanned")
                     .addField("Unanned user: " + toban.username + "\nUser ID: " + userid, "Ban length: " + (bantime * 24) + " hour(s)")
                     .addField("=========================", "Ban reason:\n" + reason);
 
-                logchannel.send({embed});
-            }).catch(e => console.log(e))
+                logchannel.send({embed: embed});
+            }).catch(console.error)
         }, bantime * 24 * 3600 * 1000)
-    }).catch(e => console.log(e))
+    }).catch(console.error)
 };
 
 module.exports.config = {
