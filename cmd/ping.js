983879796df043242a7fad3ac496fe3e5f4bffa4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 19:54:51 +0700

    Discord.js v12 update

--- cmd/ping.js
+++ cmd/ping.js
@@ -4,11 +4,18 @@ const config = require('../config.json');
 module.exports.run = (client, message) => {
 	console.log("Pong!");
 	let time = Date.now() - message.createdTimestamp;
+
+	let rolecheck;
+	try {
+		rolecheck = message.member.roles.highest.hexColor
+	} catch (e) {
+		rolecheck = "#000000"
+	}
 	let footer = config.avatar_list;
-	const index = Math.floor(Math.random() * (footer.length - 1) + 1);
-	let embed = new Discord.RichEmbed()
+	const index = Math.floor(Math.random() * footer.length);
+	let embed = new Discord.MessageEmbed()
 		.setAuthor("Pong!")
-		.setColor(message.member.highestRole.hexColor)
+		.setColor(rolecheck)
 		.setFooter("Alice Synthesis Thirty", footer[index])
 		.addField("Response time: ", `${time} ms`);
 	message.channel.send({embed: embed}).catch(console.error);
