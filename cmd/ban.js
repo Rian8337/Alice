64b7542b626c9ef1980f04f732e0c317276aaaf8
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 18:50:26 +0700

    Discord.js v12 update

--- cmd/ban.js
+++ cmd/ban.js
@@ -3,9 +3,9 @@ const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
     if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not available in DMs");
-    if (!message.member.hasPermission("ADMINISTRATOR", false, true, true)) return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
+    if (!message.member.hasPermission("ADMINISTRATOR", {checkAdmin: true, checkOwner: true})) return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
 
-    let logchannel = message.guild.channels.find(c => c.name === config.management_channel);
+    let logchannel = message.guild.channels.cache.find((c) => c.name === config.management_channel);
     if (!logchannel) return message.channel.send(`"❎ **| Hey, please create ${config.management_channel} first!**`);
 
     let userid = args[0];
@@ -15,29 +15,29 @@ module.exports.run = async (client, message, args) => {
     if (isNaN(parseInt(userid))) return message.channel.send("❎ **| Hey, please specify the correct user to ban!**");
     if (userid == message.author.id) return message.channel.send("❎ **| Why would you ban yourself?**");
 
-    let toban = await client.fetchUser(userid);
+    let toban = await client.users.fetch(userid);
     if (!toban) return message.channel.send("❎ **| I'm sorry, I cannot find the user!**");
     if (toban.bot) return message.channel.send("❎ **| I'm sorry, you cannot ban bots!**");
     let reason = args.slice(1).join(" ");
     if (!reason) return message.channel.send("❎ **| Hey, please enter your ban reason!**");
 
-    message.guild.ban(toban, {reason: reason + ` (banned by ${message.author.username})`}).then (() => {
+    message.guild.members.ban(toban, {reason: reason + ` (banned by ${message.author.username})`}).then(() => {
         let footer = config.avatar_list;
-        const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+        const index = Math.floor(Math.random() * footer.length);
 
-        let embed = new Discord.RichEmbed()
-            .setAuthor(message.author.tag, message.author.avatarURL)
+        let embed = new Discord.MessageEmbed()
+            .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
             .setFooter("Alice Synthesis Thirty", footer[index])
             .setTimestamp(new Date())
-            .setColor(message.member.highestRole.hexColor)
-            .setThumbnail(toban.avatarURL)
+            .setColor(message.member.roles.highest.hexColor)
+            .setThumbnail(toban.avatarURL())
             .setTitle("Ban executed")
             .addField("Banned user: " + toban.username, "User ID: " + userid)
             .addField("=========================", "Reason:\n" + reason);
 
         if (message.attachments.size > 0) {
             let attachments = [];
-            message.attachments.forEach(attachment => {
+            message.attachments.forEach((attachment) => {
                 attachments.push(attachment.proxyURL);
                 if (attachments.length === message.attachments.size) logchannel.send({embed: embed, files: attachments})
             });
