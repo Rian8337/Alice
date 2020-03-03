c7950e8f6e1b0e280d4cbf9a161a031c12b0fb04
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 19:30:07 +0700

    Discord.js v12 update

--- cmd/manualcalc.js
+++ cmd/manualcalc.js
@@ -1,86 +1,146 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
-const osudroid = require('../modules/osu!droid');
 
-module.exports.run = (client, message, args, maindb, alicedb, current_map) => {
-	let beatmapid;
-	let combo;
-	let acc = 100;
-	let missc = 0;
-	let mod = '';
-	let ndetail = false;
-	let pcdetail = false;
-	if (!args[0]) return message.channel.send("❎ **| Hey, how am I supposed to calculate when I don't know what to calculate?**");
-	let a = args[0].split("/");
-	beatmapid = a[a.length-1];
-	for (let i = 1; i < args.length; i++) {
-		if (args[i].endsWith("%")) acc = parseFloat(args[i]);
-		if (args[i].endsWith("m")) missc = parseInt(args[i]);
-		if (args[i].endsWith("x")) combo = parseInt(args[i]);
-		if (args[i].startsWith("+")) mod = args[i].replace("+", "").toUpperCase();
-		if (args[i].startsWith("-d")) ndetail = true;
-		if (args[i].startsWith("-p")) pcdetail = true
-	}
-	new osudroid.MapInfo().get({beatmap_id: beatmapid}, mapinfo => {
-		if (!mapinfo.title) return message.channel.send("❎ **| I'm sorry, I cannot find the map that you are looking for!**");
-		if (!mapinfo.objects) return message.channel.send("❎ **| I'm sorry, it seems like the map has 0 objects!**");
-		if (!combo) combo = mapinfo.max_combo;
-		let max_score = mapinfo.max_score(mod);
-		let star = new osudroid.MapStars().calculate({file: mapinfo.osu_file, mods: mod});
-		let starsline = parseFloat(star.droid_stars.toString().split(" ")[0]);
-		let pcstarsline = parseFloat(star.pc_stars.toString().split(" ")[0]);
-		let npp = osudroid.ppv2({
-			stars: star.droid_stars,
-			combo: combo,
-			miss: missc,
-			acc_percent: acc,
-			mode: "droid"
-		});
-		let pcpp = osudroid.ppv2({
-			stars: star.pc_stars,
-			combo: combo,
-			miss: missc,
-			acc_percent: acc,
-			mode: "osu"
-		});
-		let ppline = parseFloat(npp.toString().split(" ")[0]);
-		let pcppline = parseFloat(pcpp.toString().split(" ")[0]);
+function scoreCalc(mode, score, maxscore, accuracy, misscount) {
+	let hddt = false;
+	if (mode == 'dt' && score.includes("h")) hddt = true;
+	let newscore = parseInt(score)/maxscore*600000 + (Math.pow((accuracy/100), 4)*400000);
+	newscore -= misscount * 0.003 * newscore;
+	if (!hddt) return newscore;
+	else return Math.round(newscore/1.0625)
+}
 
-		let footer = config.avatar_list;
-		const index = Math.floor(Math.random() * footer.length);
-		let embed = new Discord.MessageEmbed()
-			.setFooter("Alice Synthesis Thirty", footer[index])
-			.setThumbnail(`https://b.ppy.sh/thumb/${mapinfo.beatmapset_id}.jpg`)
-			.setColor(mapinfo.statusColor(mapinfo.approved))
-			.setAuthor("Map Found", "https://image.frl/p/aoeh1ejvz3zmv5p1.jpg")
-			.setTitle(mapinfo.showStatistics(mod, 0))
-			.setDescription(mapinfo.showStatistics(mod, 1))
-			.setURL(`https://osu.ppy.sh/b/${mapinfo.beatmap_id}`)
-			.addField(mapinfo.showStatistics(mod, 2), `${mapinfo.showStatistics(mod, 3)}\n**Max score**: ${max_score.toLocaleString()}`)
-			.addField(mapinfo.showStatistics(mod, 4), `${mapinfo.showStatistics(mod, 5)}\n**Result**: ${combo}/${mapinfo.max_combo}x / ${acc}% / ${missc} miss(es)`)
-			.addField(`**Droid pp (Experimental)**: __${ppline} pp__ - ${starsline} stars`, `**PC pp**: ${pcppline} pp - ${pcstarsline} stars`);
-
-		if (ndetail) message.channel.send(`Raw droid pp: ${npp.toString()}`);
-		if (pcdetail) message.channel.send(`Raw PC pp: ${pcpp.toString()}`);
-		message.channel.send({embed: embed}).catch(console.error);
-
-		let time = Date.now();
-		let entry = [time, message.channel.id, mapinfo.hash];
-		let found = false;
-		for (let i = 0; i < current_map.length; i++) {
-			if (current_map[i][1] != message.channel.id) continue;
-			current_map[i] = entry;
-			found = true;
-			break
+module.exports.run = (client, message, args, maindb) => {
+	if (message.channel instanceof Discord.DMChannel || message.member.roles == null || !message.member.roles.cache.find((r) => r.name === 'Referee')) return message.channel.send("❎ **| I'm sorry, you don't have enough permission to do this.**");
+	let id = args[0];
+	if (!id) return message.channel.send("❎ **| Hey, please specify a match ID!**");
+	let mapid = args[1];
+	if (!mapid) return message.channel.send("❎ **| Hey, please specify a map ID!**");
+	let matchdb = maindb.collection("matchinfo");
+	let mapdb = maindb.collection("mapinfo");
+	let query = { matchid: id };
+	let pscore = [];
+	let t1score = 0; let t2score = 0;
+	let displayRes1 = ""; let displayRes2 = ""; let displayRes3 = ""; let color = 0;
+	let poolid = id.split(".")[0];
+	let poolquery = { poolid: poolid };
+	matchdb.find(query).toArray(function(err, res) {
+		if (err) {
+			console.log(err);
+			return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
 		}
-		if (!found) current_map.push(entry)
+		if (!res[0]) return message.channel.send("❎ **| I'm sorry, I can't find the match!**");
+		mapdb.find(poolquery).toArray(function(err, poolres) {
+			if (err) {
+				console.log(err);
+				return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+			}
+			if (!poolres[0]) return message.channel.send("❎ **| I'm sorry, I can't find the pool!**");
+			if (mapid >= poolres[0].map.length || mapid < 0) return message.channel.send("❎ **| I'm sorry, I can't find the map!**");
+			if (args.length - 2 < (res[0].result.length) * 3) return message.channel.send("❎ **| Hey, I need more data!**");
+
+			for (let i = 0; i < res[0].player.length; i++) pscore.push(scoreCalc(poolres[0].map[mapid-1][0], args[2+i*3], parseInt(poolres[0].map[mapid-1][2]), parseFloat(args[2+i*3+1]), parseInt(args[2+i*3+2])));
+
+			for (let k in pscore) {
+				if (k % 2 == 0) {
+					t1score += pscore[k];
+					if (pscore[k] == 0) displayRes1 += `${pscore.length > 2 ? res[0].player[k][0] : res[0].team[k][0]} (N/A): **0** - Failed\n`;
+					else displayRes1 += `${pscore.length > 2 ? res[0].player[k][0] : res[0].team[k][0]} (N/A): **${Math.round(pscore[k])}** - ${args[2+k*3+1]} - ${args[2+k*3+2]} miss\n`
+				}
+				else {
+					t2score += pscore[k];
+					if (pscore[k] == 0) displayRes2 += `${pscore.length > 2 ? res[0].player[k][0] : res[0].team[k][0]} (N/A): **0** - Failed\n`;
+					else displayRes2 += `${pscore.length > 2 ? res[0].player[k][0] : res[0].team[k][0]} (N/A): **${Math.round(pscore[k])}** - ${args[2+k*3+1]} - ${args[2+k*3+2]} miss\n`
+				}
+			}
+			t1score = Math.round(t1score);
+			t2score = Math.round(t2score);
+			if (t1score > t2score) {displayRes3 = `${res[0].team[0][0]} won by ` + Math.abs(t1score - t2score); color = 16711680;}
+			else if (t1score < t2score) {displayRes3 = `${res[0].team[1][0]} won by ` + Math.abs(t1score - t2score); color = 262399;}
+			else displayRes3 = "It's a Draw";
+			let footer = config.avatar_list;
+			const index = Math.floor(Math.random() * footer.length);
+			let embed = {
+				"title": poolres[0].map[mapid-1][1],
+				"color": color,
+				"thumbnail": {
+					"url": "https://cdn.discordapp.com/embed/avatars/0.png"
+				},
+				"footer": {
+					"icon_url": footer[index],
+					"text": "Alice Synthesis Thirty"
+				},
+				"author": {
+					"name": res[0].name
+				},
+				"fields": [
+					{
+						"name": res[0].team[0][0] + ": " + t1score,
+						"value": displayRes1
+					},
+					{
+						"name": res[0].team[1][0] + ": " + t2score,
+						"value": displayRes2
+					},
+					{
+						"name": "=================================",
+						"value": "**" + displayRes3 + "**"
+					}
+				]
+			};
+			message.channel.send({embed: embed}).catch(console.error);
+			let name = res[0].name;
+			let t1name = res[0].team[0][0];
+			let t2name = res[0].team[1][0];
+			let t1win = res[0].team[0][1] + (t1score > t2score);
+			let t2win = res[0].team[1][1] + (t1score < t2score);
+			res[0].team[0][1] = t1win;
+			res[0].team[1][1] = t2win;
+			let result = res[0].result;
+			embed = {
+				"title": name,
+				"color": 65280,
+				"footer": {
+					"icon_url": footer[index],
+					"text": "Alice Synthesis Thirty"
+				},
+				"fields": [
+					{
+						"name": t1name,
+						"value": "**" + t1win + "**",
+						"inline": true
+					},
+					{
+						"name": t2name,
+						"value": "**" + t2win + "**",
+						"inline": true
+					}
+				]
+			};
+			message.channel.send({embed: embed}).catch(console.error);
+			for (let p in pscore) result[p].push(pscore[p]);
+			let update = {
+				$set: {
+					status: "on-going",
+					team: res[0].team,
+					result: result
+				}
+			};
+			matchdb.updateOne(query, update, function(err) {
+				if (err) {
+					console.log(err);
+					return message.channel.send("")
+				}
+				console.log("match info updated");
+			})
+		})
 	})
 };
 
 module.exports.config = {
-	name: "manualcalc",
-	description: "Calculates pp for an osu!standard map.",
-	usage: "manualcalc <map link / map ID> [(+<mod>) (<combo>x) (<acc>%) (<miss>m) (-d) (-p)]",
-	detail: "`map link/map ID`: The link or beatmap ID of the map [String/Integer]\n`mod`: Applied game modifications (HD, HR, etc) [String]\n`combo`: Max combo reached [Integer]\n`acc`: Accuracy gained [Float]\n`miss`: Amount of misses [Integer]\n`-d`: Gives detailed response of droid pp\n`-p`: Gives detailed response of pp",
-	permission: "None"
+	name: "manualsubmit",
+	description: "Manually submits a match.",
+	usage: "manualsubmit <match id> <map sort> <score 1>[h] <acc 1> <miss 1> <score 2>[h] <acc 2> <miss 2> <...>",
+	detail: "`match id`: The ID of the match [String]\n`map sort` The order of the map in pool [Integer]\n`score n`: Score achieved by player n [Integer]\n`h`: Means the player played HDDT (applies score penalty, only works if pick is a DT pick) [String]\n`acc n`: Accuracy achieved by player n [Float]\n`miss n`: Amount of misses from player n [Integer]",
+	permission: "Referee"
 };
