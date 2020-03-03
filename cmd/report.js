c3e7fe70cea973bbc96696a09d0fe3d8e3599ce0
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 20:14:37 +0700

    Discord.js v12 update

--- cmd/report.js
+++ cmd/report.js
@@ -5,19 +5,19 @@ const cd = new Set();
 module.exports.run = async (client, message, args) => {
     if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not allowed in DMs");
     if (!args[0]) return;
-    if (message.member.roles.find(r => r.name === 'report-ban')) {
+    if (message.member.roles.cache.find((r) => r.name === 'report-ban')) {
         message.author.lastMessage.delete().catch(console.error);
         return message.reply("you were banned from submitting reports!").then (message => {
-            message.delete(5000)
+            message.delete({timeout: 5000})
         });
     }
-    let channel = message.guild.channels.find(c => c.name === config.report_channel);
+    let channel = message.guild.channels.cache.find((c) => c.name === config.report_channel);
     if (!channel) return message.reply(`please create #${config.report_channel} first!`);
     let user = message.author.id;
-    if (message.member.roles.find(r => r.name === 'Helper') || message.member.roles.find(r => r.name === 'Moderator')) cd.delete(user);
-    if (cd.has(user)) return message.reply("you are still on cooldown!").then(message => message.delete(5000));
+    if (message.member.roles.cache.find((r) => r.name === 'Helper') || message.member.roles.cache.find((r) => r.name === 'Moderator')) cd.delete(user);
+    if (cd.has(user)) return message.reply("you are still on cooldown!").then(message => message.delete({timeout: 5000}));
 
-    let toreport = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let toreport = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
     if (!toreport) return message.reply("please make sure you have entered the correct user!");
     if (toreport.hasPermission("ADMINISTRATOR")) return message.reply("you cannot report this user!");
     if (toreport.id == message.author.id) return message.reply("you cannot report yourself!");
@@ -28,23 +28,23 @@ module.exports.run = async (client, message, args) => {
 
     let rolecheck;
     try {
-        rolecheck = message.member.highestRole.hexColor
+        rolecheck = message.member.roles.highest.hexColor
     } catch (e) {
         rolecheck = "#000000"
     }
-    let reportembed = new Discord.RichEmbed()
-        .setAuthor(message.author.tag, message.author.avatarURL)
+    let reportembed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
         .setColor(rolecheck)
         .setTimestamp(new Date())
         .setFooter("React to this message upon completing report based on decision given")
         .addField("Reported user: " + toreport.user.username, "Reported in: " + message.channel)
         .addField("Reason: ", reason);
 
-    channel.send(reportembed);
+    channel.send("<@369108742077284353> <@259345965046038528>", {embed: reportembed});
     let footer = config.avatar_list;
-    const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+    const index = Math.floor(Math.random() * footer.length);
 
-    let replyembed = new Discord.RichEmbed()
+    let replyembed = new Discord.MessageEmbed()
         .setTitle("Report statistics")
         .setColor("#527ea3")
         .setTimestamp(new Date())
@@ -53,15 +53,15 @@ module.exports.run = async (client, message, args) => {
         .addField("Reason: " + reason, "Make sure you have evidence ready!\nAbuse of this command will make you unable to submit reports.");
 
     try {
-        await message.author.send(replyembed);
+        await message.author.send({embed: replyembed});
     } catch (e) {
-        message.reply("your DM is locked, so you didn't receive a copy of your report. Sorry!").then (message => {
-            message.delete(5000)
+        message.reply("your DM is locked, so you didn't receive a copy of your report. Sorry!").then(message => {
+            message.delete({timeout: 5000})
         })
     }
 
     let cooldown = config.member_cooldown;
-    if (!message.member.roles.find(r => r.name === 'Helper') && !message.member.roles.find(r => r.name === 'Moderator')) {
+    if (!message.member.roles.cache.find((r) => r.name === 'Helper') && !message.member.roles.cache.find((r) => r.name === 'Moderator')) {
         cd.add(user);
         setTimeout(() => {
             cd.delete(user)
