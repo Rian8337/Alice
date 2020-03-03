216e0b56af2582b0788409288aae6c545117e0fb
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 20:34:25 +0700

    Discord.js v12 update

--- cmd/verify.js
+++ cmd/verify.js
@@ -5,22 +5,22 @@ function isEligible(member) {
     let res = 0;
     let eligibleRoleList = config.verify_perm; //verify_permission
     eligibleRoleList.forEach((id) => {
-        if(member.roles.has(id)) res = 1;
+        if(member.roles.cache.has(id)) res = 1;
     });
-    return res;
+    return res
 }
 
 module.exports.run = (client, message, args) => {
     if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not allowed in DMs");
     if (!isEligible(message.member)) return message.channel.send("❎ **| You don't have permission to use this.**");
-    let verifying = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let verifying = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
     if (!verifying) return message.channel.send("❎ **| Hey, please mention a valid user to verify!**");
-    let memberrole = message.guild.roles.find(r => r.name === 'Member');
+    let memberrole = message.guild.roles.cache.find((r) => r.name === 'Member');
     if (!memberrole) return message.channel.send("Role not found");
 
-    if (!verifying.roles.has(memberrole.id)) {
+    if (!verifying.roles.cache.has(memberrole.id)) {
         message.channel.send("✅ **| User has been verified.**");
-        verifying.addRole(memberrole).catch(console.error)
+        verifying.roles.add(memberrole).catch(console.error)
     }
     else message.channel.send("❎ **| User is already verified!**")
 };
