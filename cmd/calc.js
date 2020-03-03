590157070d21e5cee10ae37145c00fd17ee94b71
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 18:58:26 +0700

    Add command

---
+++ cmd/calc.js
@@ -0,0 +1,80 @@
+// done rewriting
+const Discord = require('discord.js');
+const osudroid = require('../modules/osu!droid');
+const config = require('../config.json');
+
+module.exports.run = (client, message, args, maindb, alicedb, current_map) => {
+    let hash;
+    for (let i = 0; i < current_map.length; i++) {
+        if (Date.now() - current_map[i][0] > 300000 || current_map[i][1] != message.channel.id) continue;
+        hash = current_map[i][2];
+        break
+    }
+    if (!hash) return message.channel.send("❎ **| I'm sorry, there is no map being talked in the channel!**");
+
+    let combo;
+    let acc = 100;
+    let missc = 0;
+    let mod = '';
+    let ndetail = false;
+    let pcdetail = false;
+    for (let i = 1; i < args.length; i++) {
+        if (args[i].endsWith("%")) acc = parseFloat(args[i]);
+        if (args[i].endsWith("m")) missc = parseInt(args[i]);
+        if (args[i].endsWith("x")) combo = parseInt(args[i]);
+        if (args[i].startsWith("+")) mod = args[i].replace("+", "").toUpperCase();
+        if (args[i].startsWith("-d")) ndetail = true;
+        if (args[i].startsWith("-p")) pcdetail = true
+    }
+
+    new osudroid.MapInfo().get({hash: hash}, mapinfo => {
+        if (!mapinfo.title || !mapinfo.objects || mapinfo.mode != 0) return;
+        if (!combo) combo = mapinfo.max_combo;
+        let max_score = mapinfo.max_score(mod);
+        let star = new osudroid.MapStars().calculate({file: mapinfo.osu_file, mods: mod});
+        let starsline = parseFloat(star.droid_stars.toString().split(" ")[0]);
+        let pcstarsline = parseFloat(star.pc_stars.toString().split(" ")[0]);
+        let npp = osudroid.ppv2({
+            stars: star.droid_stars,
+            combo: combo,
+            miss: missc,
+            acc_percent: acc,
+            mode: "droid"
+        });
+        let pcpp = osudroid.ppv2({
+            stars: star.pc_stars,
+            combo: combo,
+            miss: missc,
+            acc_percent: acc,
+            mode: "osu"
+        });
+        let ppline = parseFloat(npp.toString().split(" ")[0]);
+        let pcppline = parseFloat(pcpp.toString().split(" ")[0]);
+
+        let footer = config.avatar_list;
+        const index = Math.floor(Math.random() * (footer.length - 1) + 1);
+        let embed = new Discord.MessageEmbed()
+            .setFooter("Alice Synthesis Thirty", footer[index])
+            .setThumbnail(`https://b.ppy.sh/thumb/${mapinfo.beatmapset_id}.jpg`)
+            .setColor(mapinfo.statusColor())
+            .setAuthor("Map Found", "https://image.frl/p/aoeh1ejvz3zmv5p1.jpg")
+            .setTitle(mapinfo.showStatistics(mod, 0))
+            .setDescription(mapinfo.showStatistics(mod, 1))
+            .setURL(`https://osu.ppy.sh/b/${mapinfo.beatmap_id}`)
+            .addField(mapinfo.showStatistics(mod, 2), `${mapinfo.showStatistics(mod, 3)}\n**Max score**: ${max_score.toLocaleString()}`)
+            .addField(mapinfo.showStatistics(mod, 4), `${mapinfo.showStatistics(mod, 5)}\n**Result**: ${combo}/${mapinfo.max_combo}x / ${acc}% / ${missc} miss(es)`)
+            .addField(`**Droid pp (Experimental)**: __${ppline} pp__ - ${starsline} stars`, `**PC pp**: ${pcppline} pp - ${pcstarsline} stars`);
+
+        if (ndetail) message.channel.send(`Raw droid pp: ${npp.toString()}`);
+        if (pcdetail) message.channel.send(`Raw PC pp: ${pcpp.toString()}`);
+        message.channel.send({embed: embed}).catch(console.error);
+    })
+};
+
+module.exports.config = {
+    name: "calc",
+    description: "Automatically calculates pp for an osu!standard map.",
+    usage: "None",
+    detail: "None",
+    permission: "None"
+};
