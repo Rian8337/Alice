2974384eb61ed3a0ee40600560cf979daa4c26da
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 19:25:20 +0700

    Discord.js v12 update

--- cmd/help.js
+++ cmd/help.js
@@ -1,3 +1,4 @@
+// done rewriting
 const Discord = require('discord.js');
 const config = require('../config.json');
 const help = require('../help.json');
@@ -5,17 +6,17 @@ const help = require('../help.json');
 module.exports.run = (client, message, args) => {
 	let rolecheck;
 	try {
-		rolecheck = message.member.highestRole.hexColor
+		rolecheck = message.member.roles.highest.hexColor
 	} catch (e) {
 		rolecheck = "#000000"
 	}
+	let footer = config.avatar_list;
+	const index = Math.floor(Math.random() * footer.length);
 	if (args[0]) {
 		let cmd = client.commands.get(args[0]);
 		if (!cmd) return message.channel.send("❎ **| I'm sorry, I cannot find the command you are looking for!**");
-		let footer = config.avatar_list;
-		const index = Math.floor(Math.random() * (footer.length - 1) + 1);
 		let help = `${cmd.config.description}\n\n\`<...>\`: required arguments\n\`[...]\`: optional arguments\n\n**Permission: **${cmd.config.permission}\n**Usage:**\n\`${cmd.config.usage}\`\n**Details:**\n${cmd.config.detail}`;
-		let embed = new Discord.RichEmbed()
+		let embed = new Discord.MessageEmbed()
 			.setTitle(config.prefix + args[0])
 			.setColor(rolecheck)
 			.setFooter("Alice Synthesis Thirty", footer[index])
@@ -50,12 +51,10 @@ module.exports.run = (client, message, args) => {
 		}
 		malodyhelp = malodyhelp.slice(0, -1);
 
-		let footer = config.avatar_list;
-		const index = Math.floor(Math.random() * (footer.length - 1) + 1);
-		let embed = new Discord.RichEmbed()
+		let embed = new Discord.MessageEmbed()
 			.setTitle("Alice Synthesis Thirty Help\nUser Commands")
 			.setDescription(`Made by <@132783516176875520> and <@386742340968120321>.\nComplete command list can be found [here](https://github.com/Rian8337/Alice).\n**Prefix: ${config.prefix}**\n\nFor detailed information about a command, use \`${config.prefix}help [command name]\`.\nFor moderation commands, type \`${config.prefix}modhelp\`.`)
-			.setThumbnail(client.user.avatarURL)
+			.setThumbnail(client.user.avatarURL({dynamic: true}))
 			.setColor(rolecheck)
 			.setFooter("Alice Synthesis Thirty", footer[index])
 			.addField("General", genhelp)
