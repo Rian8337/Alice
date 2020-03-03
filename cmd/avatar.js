db2fc40710537421e94ae1dd36dc8ebc2e6b5172
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-03 18:48:04 +0700

    Discord.js v12 update

--- cmd/avatar.js
+++ cmd/avatar.js
@@ -5,22 +5,22 @@ module.exports.run = async (client, message, args) => {
     let user = message.author;
     let rolecheck;
     try {
-        rolecheck = message.member.highestRole.hexColor
+        rolecheck = message.member.roles.highest.hexColor
     } catch (e) {
         rolecheck = "#000000"
     }
     let footer = config.avatar_list;
-    const index = Math.floor(Math.random() * (footer.length - 1) + 1);
-    let embed = new Discord.RichEmbed()
+    const index = Math.floor(Math.random() * footer.length);
+    let embed = new Discord.MessageEmbed()
         .setColor(rolecheck)
         .setTimestamp(new Date())
         .setFooter("Alice Synthesis Thirty", footer[index]);
 
     if (args[0]) {
-        user = await client.fetchUser(args[0] || message.mentions.users.first().id);
+        user = await client.users.fetch(args[0] || message.mentions.users.first().id);
         if (!user) return message.channel.send("❎ **| I'm sorry, I cannot find the user you are looking for!**");
     }
-    embed.setDescription(`**${user.tag}**`).setImage(user.avatarURL);
+    embed.setDescription(`**${user.tag}**`).setImage(user.avatarURL({dynamic: true}));
 
     await message.channel.send({embed: embed})
 };
