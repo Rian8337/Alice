e1dd84e7575299767a24ddb2b581ea1e87e31193
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-04 02:50:35 +0700

    Fix welcome message

--- mybot.js
+++ mybot.js
@@ -236,7 +236,7 @@ client.on("guildMemberAdd", member => {
 client.on("guildMemberUpdate", oldMember => {
 	if (oldMember.user.bot) return;
 	let general = oldMember.guild.channels.cache.get("316545691545501706");
-	if (!general || oldMember.roles.cache.find((r) => r.name === "Member")) return;
+	if (!general || oldMember.roles.cache.size > 0) return;
 	fs.readFile("welcome.txt", 'utf8', (err, data) => {
 		if (err) return console.log(err);
 		let welcomeMessage = `Welcome to ${oldMember.guild.name}, <@${oldMember.id}>!`;
