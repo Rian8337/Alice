b21d00991581483a7acee50f9a07c46e9975428d
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-04 12:20:11 +0700

    Change attachment processing method

--- mybot.js
+++ mybot.js
@@ -349,31 +349,30 @@ client.on("messageDelete", message => {
 		if (!logchannel) return;
 		let attachments = [];
 		message.attachments.forEach((attachment) => {
-			attachments.push(attachment.proxyURL)
+			attachments.push(attachment.proxyURL);
+			if (attachments.length == message.attachments.size) messageLog.send("Image attached", {files: attachments})
 		});
-		logchannel.send("Image attached", {files: attachments})
+		return
 	}
-	else {
-		logchannel = message.guild.channels.cache.find((c) => c.name === config.log_channel);
-		if (!logchannel) return;
-		const embed = new Discord.MessageEmbed()
-			.setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-			.setFooter(`Author ID: ${message.author.id} | Message ID: ${message.id}`)
-			.setTimestamp(new Date())
-			.setColor("#cb8900")
-			.setTitle("Message deleted")
-			.addField("Channel", message.channel);
+	logchannel = message.guild.channels.cache.find((c) => c.name === config.log_channel);
+	if (!logchannel) return;
+	const embed = new Discord.MessageEmbed()
+		.setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+		.setFooter(`Author ID: ${message.author.id} | Message ID: ${message.id}`)
+		.setTimestamp(new Date())
+		.setColor("#cb8900")
+		.setTitle("Message deleted")
+		.addField("Channel", message.channel);
 
-		if (message.content) embed.addField("Content", message.content.substring(0, 1024));
-		logchannel.send({embed: embed});
+	if (message.content) embed.addField("Content", message.content.substring(0, 1024));
+	logchannel.send({embed: embed});
 
-		if (message.attachments.size > 0) {
-			let attachments = [];
-			message.attachments.forEach((attachment) => {
-				attachments.push(attachment.proxyURL)
-			});
-			logchannel.send({files: attachments})
-		}
+	if (message.attachments.size > 0) {
+		let attachments = [];
+		message.attachments.forEach((attachment) => {
+			attachments.push(attachment.proxyURL);
+			if (attachments.length == message.attachments.size) logchannel.send("Image attached", {files: attachments})
+		})
 	}
 });
 
