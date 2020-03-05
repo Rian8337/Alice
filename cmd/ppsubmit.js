bda42be6bfa99c6cf6e8699de6e4e3f309322e62
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-05 16:23:16 +0700

    Detect if osu! file is not available

--- cmd/ppsubmit.js
+++ cmd/ppsubmit.js
@@ -14,6 +14,10 @@ function calculatePP(message, whitelist, embed, i, submitted, pplist, playc, pla
 		let query = {hash: play.hash};
 		if (wlres) query = {beatmap_id: wlres.mapid};
 		new osudroid.MapInfo().get(query, mapinfo => {
+			if (!mapinfo.osu_file) {
+				message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from osu! servers. Please try again!**");
+				return cb(false, false)
+			}
 			if (!mapinfo.title) {
 				message.channel.send("❎ **| I'm sorry, the map you've played can't be found on osu! beatmap listing, please make sure the map is submitted and up-to-date!**");
 				return cb(false, false)
