1682d77110d5539cf1e95e3f6470939e34587a80
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-07 20:43:25 +0700

    Fix mentions

--- cmd/avatar.js
+++ cmd/avatar.js
@@ -16,7 +16,7 @@ module.exports.run = async (client, message, args) => {
         .setFooter("Alice Synthesis Thirty", footer[index]);
 
     if (args[0]) {
-        user = await client.users.fetch(args[0] || message.mentions.users.first().id);
+        user = await client.users.fetch(message.mentions.users.first().id|| args[0]);
         if (!user) return message.channel.send("❎ **| I'm sorry, I cannot find the user you are looking for!**")
     }
     embed.setDescription(`**${user.tag}**`).setImage(user.avatarURL({dynamic: true}));
