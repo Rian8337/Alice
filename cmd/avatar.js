d803d672e6fb6cf2c8b46f445a1cebc9551444b6
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-09 22:32:11 +0700

    Fix mention

--- cmd/avatar.js
+++ cmd/avatar.js
@@ -17,7 +17,7 @@ module.exports.run = async (client, message, args) => {
 
     if (args[0]) {
         user = args[0].replace("<@!", "").replace("<@", "").replace(">", "");
-        user = await client.users.fetch(args[0]);
+        user = await client.users.fetch(user);
         if (!user) return message.channel.send("❎ **| I'm sorry, I cannot find the user you are looking for!**")
     }
     embed.setDescription(`**${user.tag}**`).setImage(user.avatarURL({dynamic: true}));
