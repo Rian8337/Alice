73569019486d530ffa20859402f4c5d90aedbe51
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-16 16:51:28 +0700

    Move file

--- cmd/Staff/fancylock.js
+++ cmd/Staff/fancylock.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../config.json');
+const config = require('../../config.json');
 
 module.exports.run = (client, message, args, maindb, alicedb) => {
     if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not available in DMs");
