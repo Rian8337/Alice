105f7e388db2ce9b740b5b4a557add55d46f6cb3
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-16 17:07:00 +0700

    Move file

--- cmd/General/ping.js
+++ cmd/General/ping.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../config.json');
+const config = require('../../config.json');
 
 module.exports.run = (client, message) => {
 	console.log("Pong!");
