933cc0c6e912d164331608390b024cea38d9c4ab
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-16 17:05:48 +0700

    Move file

--- cmd/osu! and osu!droid/osu.js
+++ cmd/osu! and osu!droid/osu.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const https = require('https');
 const apikey = process.env.OSU_API_KEY;
-const config = require('../config.json');
+const config = require('../../config.json');
 
 function progress(level) {
     let final = (parseFloat(level) - Math.floor(parseFloat(level))) * 100;
