ea6bcc91eea32f1c90cccf9e4974d90f5b836de2
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-16 16:39:24 +0700

    Move file

--- cmd/General/avatar.js
+++ cmd/General/avatar.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../config.json');
+const config = require('../../config.json');
 
 module.exports.run = async (client, message, args) => {
     let user = message.author;
