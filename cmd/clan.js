84c45e8668528dccbfa48ba7f9152ca3fb003b2a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-16 16:44:37 +0700

    Move file

--- cmd/clan.js
+++ cmd/clan.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../config.json');
+const config = require('../../config.json');
 const cd = new Set();
 
 function capitalizeString(string = "") {
