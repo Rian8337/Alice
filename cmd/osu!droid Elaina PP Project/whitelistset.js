9d9008b9ef07d696c3c313c2dbfdbe5f1c467243
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-16 17:25:50 +0700

    Move file

--- cmd/osu!droid Elaina PP Project/whitelistset.js
+++ cmd/osu!droid Elaina PP Project/whitelistset.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const https = require("https");
 const apikey = process.env.OSU_API_KEY;
-const config = require('../config.json');
+const config = require('../../config.json');
+
 function mapstatusread(status) {
 	switch (status) {
 		case -2: return 16711711;
