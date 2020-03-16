cd87a3fb69f3f092ebf9429edc729fe57e4f7d96
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-16 17:23:00 +0700

    Move file

--- cmd/Bot Creators/unwhitelistset.js
+++ cmd/Bot Creators/unwhitelistset.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const https = require("https");
 const apikey = process.env.OSU_API_KEY;
-const config = require('../config.json');
+const config = require('../../config.json');
 
 function mapstatusread(status) {
 	switch (status) {
