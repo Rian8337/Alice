ed5e3233f5ba8ac5afe12aeb1dbbebf0c8506b61
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-18 02:05:06 +0700

    Fix pictureConfig

--- cmd/osu! and osu!droid/profileid.js
+++ cmd/osu! and osu!droid/profileid.js
@@ -32,9 +32,11 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
 						return message.channel.send("Error: Empty database response. Please try again!")
 					}
 					let coins = 0;
-					if (pointres) coins = pointres.alicecoins;
-					let pictureConfig = pointres.picture_config;
-					if (!pictureConfig) pictureConfig = {};
+					let pictureConfig = {};
+					if (pointres) {
+					coins = pointres.alicecoins;
+					pictureConfig = pointres.picture_config
+					}
 
 					// background
 					let backgroundImage = pictureConfig.activeBackground;
