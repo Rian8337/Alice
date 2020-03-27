4e538837a10a0f91f50bcab6d6303f3344f93c04
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-27 13:57:45 +0700

    Add if FC pp

--- cmd/osu! and osu!droid/recent.js
+++ cmd/osu! and osu!droid/recent.js
@@ -74,15 +74,41 @@ module.exports.run = (client, message, args, maindb, alicedb, current_map) => {
 
 			embed = new Discord.MessageEmbed()
 				.setFooter("Alice Synthesis Thirty", footer[index])
-				.setThumbnail(`https://b.ppy.sh/thumb/${mapinfo.beatmapset_id}.jpg`)
+				.setThumbnail(`https://b.ppy.sh/thumb/${mapinfo.beatmapset_id}l.jpg`)
 				.setColor(mapinfo.statusColor(mapinfo.approved))
 				.setAuthor("Map Found", "https://image.frl/p/aoeh1ejvz3zmv5p1.jpg")
 				.setTitle(mapinfo.showStatistics(mod, 0))
 				.setURL(`https://osu.ppy.sh/b/${mapinfo.beatmap_id}`)
 				.setDescription(mapinfo.showStatistics(mod, 1))
 				.addField(mapinfo.showStatistics(mod, 2), mapinfo.showStatistics(mod, 3))
-				.addField(mapinfo.showStatistics(mod, 4), mapinfo.showStatistics(mod, 5))
-				.addField(`**Droid pp (Experimental)**: __${dpp} pp__ - ${droid_stars} stars`, `**PC pp**: ${pp} pp - ${pc_stars} stars`);
+				.addField(mapinfo.showStatistics(mod, 4), mapinfo.showStatistics(mod, 5));
+
+			if (miss > 0 || combo < mapinfo.max_combo) {
+				let if_fc_acc = new osudroid.Accuracy({
+					n300: npp.computed_accuracy.n300,
+					n100: npp.computed_accuracy.n100,
+					n50: npp.computed_accuracy.n50,
+					nmiss: 0,
+					nobjects: mapinfo.objects
+				}).value() * 100;
+				let if_fc_dpp = osudroid.ppv2({
+					stars: star.droid_stars,
+					combo: mapinfo.max_combo,
+					acc_percent: if_fc_acc,
+					miss: 0,
+					mode: "droid"
+				});
+				let if_fc_pp = osudroid.ppv2({
+					stars: star.pc_stars,
+					combo: mapinfo.max_combo,
+					acc_percent: if_fc_acc,
+					miss: 0,
+					mode: "osu"
+				});
+				let dline = parseFloat(if_fc_dpp.toString().split(" ")[0]);
+				let pline = parseFloat(if_fc_pp.toString().split(" ")[0]);
+				embed.addField(`**Droid pp (Experimental)**: __${dpp} pp__ - ${droid_stars} stars\n**Droid pp (if FC)**: __${dline} pp__ **(${if_fc_acc.toFixed(2)}%)**`, `**PC pp**: ${pp} pp - ${pc_stars} stars\n**PC pp (if FC)**: __${pline} pp__ **(${if_fc_acc.toFixed(2)}%)**`)
+			} else embed.addField(`**Droid pp (Experimental)**: __${dpp} pp__ - ${droid_stars} stars`, `**PC pp**: ${pp} pp - ${pc_stars} stars`);
 
 			message.channel.send({embed: embed}).catch(console.error)
 		})
@@ -91,7 +117,7 @@ module.exports.run = (client, message, args, maindb, alicedb, current_map) => {
 
 module.exports.config = {
 	name: "recent",
-	description: "Retrieves an osu!droid account's recent play based on uid.",
+	description: "Retrieves an droid account's recent play based on uid.",
 	usage: "recent <uid>",
 	detail: "`uid`: The uid to retrieve [Integer]",
 	permission: "None"
