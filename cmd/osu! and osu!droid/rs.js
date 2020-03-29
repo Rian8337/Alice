c79164885dedeab0071067ba981eac535d3bbba0
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-29 15:20:22 +0700

    Remove map expiration

--- cmd/osu! and osu!droid/rs.js
+++ cmd/osu! and osu!droid/rs.js
@@ -37,16 +37,10 @@ module.exports.run = (client, message, args, maindb, alicedb, current_map) => {
                 .setFooter("Alice Synthesis Thirty", footer[index])
                 .setColor(8311585);
 
-            let time = Date.now();
-            let entry = [time, message.channel.id, hash];
-            let found = false;
-            for (let i = 0; i < current_map.length; i++) {
-                if (current_map[i][1] != message.channel.id) continue;
-                current_map[i] = entry;
-                found = true;
-                break
-            }
-            if (!found) current_map.push(entry);
+            let entry = [message.channel.id, hash];
+            let map_index = current_map.findIndex(map => map[0] === message.channel.id);
+            if (map_index === -1) current_map.push(entry);
+            else current_map[map_index] = entry;
 
             new osudroid.MapInfo().get({hash: hash}, mapinfo => {
                 if (!mapinfo.title || !mapinfo.objects || !mapinfo.osu_file) {
