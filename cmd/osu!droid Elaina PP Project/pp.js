25779938386985f86e478bff1f66e0b0cf2f1d56
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-03-31 15:48:14 +0700

    Add support to submit play per beatmap ID

--- cmd/osu!droid Elaina PP Project/pp.js
+++ cmd/osu!droid Elaina PP Project/pp.js
@@ -78,108 +78,209 @@ function calculatePP(message, whitelist, embed, i, submitted, pplist, playc, pla
 
 module.exports.run = (client, message, args, maindb) => {
 	if (message.channel instanceof Discord.DMChannel) return message.channel.send("❎ **| I'm sorry, this command is not available in DMs.**");
-	let channels = config.pp_channel;
-	let found = false;
-	for (let i = 0; i < channels.length; i++) {
-		if (message.channel.id != channels[i]) continue;
-		found = true;
-		break
-	}
-	if (!found) return message.channel.send("❎ **| I'm sorry, this command is not allowed in here!**");
-	let ufind = message.author.id;
-	let offset = 1;
-	let start = 1;
-	if (args[0]) offset = parseInt(args[0]);
-	if (args[1]) start = parseInt(args[1]);
-	if (isNaN(offset)) offset = 1;
-	if (isNaN(start)) start = 1;
-	if (offset > 5 || offset < 1) return message.channel.send("❎ **| I cannot submit that many plays at once! I can only do up to 5!**");
-	if (start + offset - 1 > 50) return message.channel.send('❎ **| I think you went over the limit. You can only submit up to 50 of your recent plays!**');
+
 	let binddb = maindb.collection("userbind");
 	let whitelist = maindb.collection("mapwhitelist");
+
+	let ufind = message.author.id;
 	let query = {discordid: ufind};
-	binddb.find(query).toArray(function (err, userres) {
+	binddb.findOne(query, (err, res) => {
 		if (err) {
 			console.log(err);
 			return message.channel.send("Error: Empty database response. Please try again!")
 		}
-		if (!userres[0]) return message.channel.send("❎ **| I'm sorry, your account is not binded. You need to use `a!userbind <uid>` first. To get uid, use `a!profilesearch <username>`.**");
-		let uid = userres[0].uid;
+		if (!res) return message.channel.send("❎ **| I'm sorry, your account is not binded. You need to use `a!userbind <uid>` first. To get uid, use `a!profilesearch <username>`.**");
+		let uid = res.uid;
 		let pplist = [];
 		let pptotal = 0;
 		let pre_pptotal = 0;
 		let submitted = 1;
 		let playc = 0;
-		if (userres[0].pp) pplist = userres[0].pp;
-		if (userres[0].pptotal) pre_pptotal = userres[0].pptotal;
-		if (userres[0].playc) playc = userres[0].playc;
-		new osudroid.PlayerInfo().get({uid: uid}, player => {
-			if (!player.name) return message.channel.send("❎ **| I'm sorry, I cannot find your profile!**");
-			if (!player.recent_plays) return message.channel.send("❎ **| I'm sorry, you haven't submitted any play!**");
-			let rplay = player.recent_plays;
-			let playentry = [];
-			let footer = config.avatar_list;
-			const index = Math.floor(Math.random() * footer.length);
-			let rolecheck;
-			try {
-				rolecheck = message.member.roles.highest.hexColor
-			} catch (e) {
-				rolecheck = "#000000"
-			}
-			let embed = new Discord.MessageEmbed()
-				.setTitle("PP submission info")
-				.setFooter("Alice Synthesis Thirty", footer[index])
-				.setColor(rolecheck);
+		if (res) {
+			pplist = res.pp;
+			pre_pptotal = res.pptotal;
+			playc = res.playc
+		}
+		let footer = config.avatar_list;
+		const index = Math.floor(Math.random() * footer.length);
+		let rolecheck;
+		try {
+			rolecheck = message.member.roles.highest.hexColor
+		} catch (e) {
+			rolecheck = "#000000"
+		}
+		let embed = new Discord.MessageEmbed()
+			.setTitle("PP submission info")
+			.setFooter("Alice Synthesis Thirty", footer[index])
+			.setColor(rolecheck);
 
-			for (let i = start - 1; i < start + offset - 1; i++) {
-				if (!rplay[i]) break;
-				let play = {
-					title: "", accuracy: "", miss: "", combo: "", mod: "", hash: ""
-				};
-				play.title = rplay[i].filename;
-				play.accuracy = parseFloat((parseInt(rplay[i].accuracy) / 1000).toFixed(2));
-				play.miss = rplay[i].miss;
-				play.combo = rplay[i].combo;
-				play.mod = rplay[i].mode;
-				play.hash = rplay[i].hash;
-				playentry.push(play)
-			}
-			let i = 0;
-			calculatePP(message, whitelist, embed, i, submitted, pplist, playc, playentry, function testResult(error = false, success = true, stopSign = false) {
-				if (stopSign) {
-					if (submitted == 1) return;
-					let weight = 1;
-					for (let i in pplist) {
-						pptotal += weight * pplist[i][2];
-						weight *= 0.95;
-					}
-					let diff = pptotal - pre_pptotal;
-					embed.setDescription(`Total PP: **${pptotal.toFixed(2)} pp**\nPP gained: **${diff.toFixed(2)} pp**`);
-					message.channel.send(`✅ **| ${message.author}, successfully submitted your play(s). More info in embed.**`, {embed: embed});
-					let updateVal = {
-						$set: {
-							pptotal: pptotal,
-							pp: pplist,
-							playc: playc
-						}
-					};
-					binddb.updateOne(query, updateVal, function (err) {
-						if (err) throw err
-					});
-					return
+		switch (args[0]) {
+			case "past": {
+				let channels = config.pp_channel;
+				let channel_index = channels.findIndex(id => message.channel.id === id);
+				if (channel_index === -1) return message.channel.send("❎ **| I'm sorry, this command is not allowed in here!**");
+
+				let beatmap = args[1];
+				if (!beatmap) return message.channel.send("❎ **| Hey, please give me a beatmap to submit!**");
+				if (typeof beatmap !== "number") {
+					let a = beatmap.split("/");
+					beatmap = parseInt(a[a.length - 1]);
+					if (isNaN(beatmap)) return message.channel.send("❎ **| Hey, that beatmap ID is not valid!**")
 				}
-				if (!error) i++;
-				if (success) submitted++;
-				calculatePP(message, whitelist, embed, i, submitted, pplist, playc, playentry, testResult)
-			})
-		})
+				new osudroid.MapInfo().get({beatmap_id: beatmap}, mapinfo => {
+					if (!mapinfo.title) return message.channel.send("❎ **| I'm sorry, that map does not exist in osu! database!**");
+					if (!mapinfo.objects) return message.channel.send("❎ **| I'm sorry, it seems like the map has 0 objects!**");
+					if (!mapinfo.osu_file) return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from osu! servers. Please try again!**");
+					let hash = mapinfo.hash;
+
+					new osudroid.PlayInfo().getFromHash({uid: uid, hash: hash}, play => {
+						if (!play.title) return message.channel.send("❎ **| I'm sorry, you don't have any plays submitted in this map!**");
+						let combo = play.combo;
+						let acc = play.accuracy;
+						let mod = play.mods;
+						let miss = play.miss;
+
+						whitelist.findOne({hashid: play.hash}, (err, wlres) => {
+							if (err) {
+								console.log(err);
+								return message.channel.send("❎ **| I'm sorry, I'm having trouble on retrieving the map's whitelist info!**");
+							}
+							if ((mapinfo.approved == 3 || mapinfo.approved <= 0) && !wlres) return message.channel.send("❎ **| I'm sorry, the PP system only accepts ranked, approved, whitelisted, or loved mapset right now!**");
+							let star = new osudroid.MapStars().calculate({file: mapinfo.osu_file, mods: mod});
+							let npp = osudroid.ppv2({
+								stars: star.droid_stars,
+								combo: combo,
+								acc_percent: acc,
+								miss: miss,
+								mode: "droid"
+							});
+							let pp = parseFloat(npp.toString().split(" ")[0]);
+							let playinfo = `${mapinfo.artist} - ${mapinfo.title} (${mapinfo.creator}) [${mapinfo.version}]${mod ? ` +${mod}` : ""}`;
+							let ppentry = [play.hash, playinfo, pp, play.combo, play.accuracy, play.miss];
+							if (isNaN(pp)) message.channel.send("❎ **| I'm sorry, I'm having trouble on retrieving the map's pp data!**");
+
+							playc++;
+							let dup = false;
+							for (let i in pplist) {
+								if (ppentry[0] == pplist[i][0]) {
+									pplist[i] = ppentry;
+									dup = true;
+									break
+								}
+							}
+							if (!dup) pplist.push(ppentry);
+
+							pplist.sort(function (a, b) {
+								return b[2] - a[2]
+							});
+							if (pplist.length > 75) pplist.splice(75);
+							if (dup) embed.addField(playinfo, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp | **Duplicate**`);
+							else {
+								let x = 0;
+								for (x; x < pplist.length; x++) {
+									if (pplist[x][1].includes(playinfo)) {
+										embed.addField(playinfo, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp`);
+										break
+									}
+								}
+								if (x == pplist.length) embed.addField(playinfo, `${play.combo}x | ${play.accuracy}% | ${play.miss} ❌ | ${pp}pp | **Worth no pp**`);
+							}
+
+							let pptotal = 0;
+							let weight = 1;
+							for (let i in pplist) {
+								pptotal += weight * pplist[i][2];
+								weight *= 0.95;
+							}
+							let diff = pptotal - pre_pptotal;
+							embed.setDescription(`Total PP: **${pptotal.toFixed(2)} pp**\nPP gained: **${diff.toFixed(2)} pp**`);
+							message.channel.send(`✅ **| ${message.author}, successfully submitted your play(s). More info in embed.**`, {embed: embed});
+							let updateVal = {
+								$set: {
+									pptotal: pptotal,
+									pp: pplist,
+									playc: playc
+								}
+							};
+							binddb.updateOne({discordid: message.author.id}, updateVal, function (err) {
+								if (err) throw err
+							});
+						})
+					})
+				});
+				break
+			}
+			default: {
+				let channels = config.pp_channel;
+				let channel_index = channels.findIndex(id => message.channel.id === id);
+				if (channel_index === -1) return message.channel.send("❎ **| I'm sorry, this command is not allowed in here!**");
+
+				let offset = 1;
+				let start = 1;
+				if (args[0]) offset = parseInt(args[0]);
+				if (args[1]) start = parseInt(args[1]);
+				if (isNaN(offset)) offset = 1;
+				if (isNaN(start)) start = 1;
+				if (offset > 5 || offset < 1) return message.channel.send("❎ **| I cannot submit that many plays at once! I can only do up to 5!**");
+				if (start + offset - 1 > 50) return message.channel.send('❎ **| I think you went over the limit. You can only submit up to 50 of your recent plays!**');
+
+				new osudroid.PlayerInfo().get({uid: uid}, player => {
+					if (!player.name) return message.channel.send("❎ **| I'm sorry, I cannot find your profile!**");
+					if (!player.recent_plays) return message.channel.send("❎ **| I'm sorry, you haven't submitted any play!**");
+					let rplay = player.recent_plays;
+					let playentry = [];
+
+					for (let i = start - 1; i < start + offset - 1; i++) {
+						if (!rplay[i]) break;
+						let play = {
+							title: "", accuracy: "", miss: "", combo: "", mod: "", hash: ""
+						};
+						play.title = rplay[i].filename;
+						play.accuracy = parseFloat((parseInt(rplay[i].accuracy) / 1000).toFixed(2));
+						play.miss = rplay[i].miss;
+						play.combo = rplay[i].combo;
+						play.mod = rplay[i].mode;
+						play.hash = rplay[i].hash;
+						playentry.push(play)
+					}
+					let i = 0;
+					calculatePP(message, whitelist, embed, i, submitted, pplist, playc, playentry, function testResult(error = false, success = true, stopSign = false) {
+						if (stopSign) {
+							if (submitted == 1) return;
+							let weight = 1;
+							for (let i in pplist) {
+								pptotal += weight * pplist[i][2];
+								weight *= 0.95;
+							}
+							let diff = pptotal - pre_pptotal;
+							embed.setDescription(`Total PP: **${pptotal.toFixed(2)} pp**\nPP gained: **${diff.toFixed(2)} pp**`);
+							message.channel.send(`✅ **| ${message.author}, successfully submitted your play(s). More info in embed.**`, {embed: embed});
+							let updateVal = {
+								$set: {
+									pptotal: pptotal,
+									pp: pplist,
+									playc: playc
+								}
+							};
+							binddb.updateOne(query, updateVal, function (err) {
+								if (err) throw err
+							});
+							return
+						}
+						if (!error) i++;
+						if (success) submitted++;
+						calculatePP(message, whitelist, embed, i, submitted, pplist, playc, playentry, testResult)
+					})
+				})
+			}
+		}
 	})
 };
 
 module.exports.config = {
 	name: "pp",
 	description: "Submits plays from user's profile into the user's droid pp profile. Only allowed in bot channel and pp project channel in osu!droid International Discord server.",
-	usage: "pp [offset] [start]",
-	detail: "`offset`: The amount of play to submit from 1 to 5, defaults to 1 [Integer]\n`start`: The position in your recent play list that you want to start submitting, up to 50, defaults to 1 [Integer]",
+	usage: "pp [offset] [start]\npp past <beatmap link/ID>",
+	detail: "`beatmap link/ID`: The link or ID of the beatmap that you want to submit [Integer/String]\n`offset`: The amount of play to submit from 1 to 5, defaults to 1 [Integer]\n`start`: The position in your recent play list that you want to start submitting, up to 50, defaults to 1 [Integer]",
 	permission: "None"
 };
