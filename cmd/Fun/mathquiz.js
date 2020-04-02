d38267778c40c954f942fdb561992a5025dffdc5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-02 14:57:39 +0700

    Add factorial

--- cmd/Fun/mathquiz.js
+++ cmd/Fun/mathquiz.js
@@ -1,12 +1,133 @@
 let cd = new Set();
 
 function createRandomNumber(min, max) {
-    min = Math.floor(min);
-    max = Math.ceil(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.max(1, Math.floor(Math.random() * (max - min + 1)) + min)
 }
 
-module.exports.run = (client, message, args) => {
+function isPrime(num) {
+    if (num < 2)
+        return false;
+
+    let sqrt_num = Math.floor(Math.sqrt(num));
+    for (let i = 2; i < sqrt_num; i++)
+        if (num % i === 0) return false;
+
+    return true
+}
+
+function calculateFactorial(num) {
+    let result = num;
+    while (num > 1) {
+        --num;
+        result *= num
+    }
+    return result
+}
+
+function generateNumber(level, operator) {
+    switch (operator) {
+        case "+":
+        case "-":
+            return createRandomNumber(Math.random() * 5 * level / 2, Math.max(5 * level / 2, Math.random() * 15 * level / 2));
+        case "/":
+        case "*":
+            return createRandomNumber(Math.random() * 5 * Math.max(1, Math.random() * level / 2), Math.random() * 10 * Math.max(1, Math.random() * level / 2));
+        case "!":
+            return createRandomNumber(Math.random() * Math.max(1, Math.random() * level / 15), Math.random() * 5 * Math.max(1, Math.random() * level / 15));
+    }
+}
+
+async function generateEquation(level, operator_amount) {
+    let operators = ['/', '*', '+', '-'];
+    let prev_operator_amount = operator_amount;
+    let equation = '';
+    let real_equation = '';
+    let answer = Number.NaN;
+    let prime_count = 0;
+    let last_operator = '';
+    let infinity_count = 0;
+
+    while (!Number.isInteger(answer)) {
+        if (infinity_count === 50) break;
+
+        while (operator_amount > 0) {
+            const index = Math.floor(Math.random() * operators.length);
+            let operator = operators[index];
+            let number = generateNumber(level, operator);
+            let mul_or_div = operator === '/' || operator === '*' || last_operator === '/' || last_operator === '*';
+            if (mul_or_div) {
+                while ((!isPrime(number) && prime_count < Math.floor(level / 10)))
+                    number = generateNumber(level, operator);
+                ++prime_count
+            }
+
+            // use RNG to determine putting factorial
+            let factorial = level >= 11 && level + Math.random() * level >= 20;
+            if (factorial) {
+                // nerf number for factorials
+                number = generateNumber(level, "!");
+                while (number <= 2)
+                    number = generateNumber(level, "!")
+            }
+
+            last_operator = operator;
+
+            if (factorial) {
+                equation += `${calculateFactorial(number)} ${operator} `;
+                real_equation += `${number}! ${operator} `
+            } else {
+                equation += `${number} ${operator} `;
+                real_equation += `${number} ${operator} `
+            }
+
+            --operator_amount
+        }
+
+        let number = generateNumber(level, last_operator);
+        let mul_or_div = last_operator === '/' || last_operator === '*';
+        if (mul_or_div)
+            while ((!isPrime(number) && prime_count < Math.floor(level / 5)))
+                number = generateNumber(level, last_operator);
+
+        // use RNG to determine putting factorial
+        let factorial = level >= 10 && Math.random() >= 1 - level / 25;
+        if (factorial) {
+            // nerf number for factorials
+            number = generateNumber(level, "!");
+            while (number <= 2)
+                number = generateNumber(level, "!")
+        }
+
+        if (factorial) {
+            equation += calculateFactorial(number);
+            real_equation += `${number}!`
+        } else {
+            equation += number;
+            real_equation += number
+        }
+        answer = eval(equation);
+
+        const min_mul_div_threshold = Math.min(prev_operator_amount, Math.floor(level / 10));
+        const max_mul_div_threshold = level / 5;
+        const mul_div_amount = (equation.match(/\//g)||[]).length + (equation.match(/\*/g)||[]).length;
+        if (answer === Infinity || answer === -Infinity) ++infinity_count;
+        if (
+            !Number.isInteger(answer) ||
+            (level >= 5 && mul_div_amount < min_mul_div_threshold && mul_div_amount > max_mul_div_threshold)
+        ) {
+            answer = Number.NaN;
+            equation = '';
+            real_equation = '';
+            operator_amount = prev_operator_amount
+        }
+    }
+
+    return [real_equation, answer]
+}
+
+module.exports.run = async (client, message, args) => {
     if (cd.has(message.author.id)) return message.channel.send("❎ **| Hey, you still have an equation to solve! Please solve that one first before creating another equation!**");
     let level = args[0];
     if (!level) level = 1;
@@ -20,50 +141,12 @@ module.exports.run = (client, message, args) => {
     if (operator_amount < 1 || operator_amount > 10) return message.channel.send("❎ **| I'm sorry, operator amount range is from 1-10!**");
     operator_amount = parseInt(operator_amount);
 
-    let prev_operator_amount = operator_amount;
-    let operators = ['/', '*', '+', '-'];
-    let equation = '';
-    let answer = Number.NaN;
+    let val = await generateEquation(level, operator_amount);
+    let equation = val[0];
+    if (!equation) return message.channel.send("❎ **| I'm sorry, the equation generator had problems generating your equation, please try again!**");
+    let answer = val[1];
 
-    while (!Number.isInteger(answer)) {
-        while (operator_amount > 0) {
-            let number = createRandomNumber(Math.random() * 5 * Math.max(1, level / 2), Math.max(5 * Math.max(1, level / 2), Math.random() * 15 * level / 2));
-            const index = Math.floor(Math.random() * operators.length);
-            if (operators[index] === '/' || operators[index] === '*') {
-                number = createRandomNumber(Math.random() * 5 * Math.random() * Math.max(1, (level / 2)), Math.random() * 10 * Math.max(1, Math.random() * level / 2));
-                if (level >= 3 || operators[index] === '/') {
-                    while (number === 0) number = createRandomNumber(Math.random() * 5 * Math.max(1, Math.random() * level / 2), Math.random() * 10 * Math.max(1, Math.random() * level / 2));
-                }
-            }
-            equation += `${number} ${operators[index]} `;
-            --operator_amount
-        }
-        let last_operator = equation.substring(equation.length - 2).trim();
-        if (last_operator === '/' || last_operator === '*') {
-            let number = createRandomNumber(Math.random() * 5 * Math.random() * Math.max(1, Math.random() * level / 2), Math.random() * 10 * Math.random() * Math.max(1, Math.random() * level / 2));
-            if (last_operator === '/') {
-                while (number === 0) {
-                    number = createRandomNumber(Math.random() * 5 * Math.random() * Math.max(1, Math.random() * level / 2), Math.random() * 10 * Math.random() * Math.max(1, Math.random() * level / 2));
-                }
-            }
-            equation += number
-        }
-        else equation += createRandomNumber(Math.random() * 5 * level, Math.max(5 * Math.max(1, level / 2), Math.random() * 15 * Math.max(1, level / 2)));
-        answer = eval(equation);
-        if (
-            !Number.isInteger(answer) ||
-            (level >= 5 && (
-                        (equation.match(/\//g)||[]).length + (equation.match(/\*/g)||[]).length !== Math.min(prev_operator_amount, Math.floor(level / 5))
-                )
-            )
-        ) {
-            answer = Number.NaN;
-            equation = '';
-            operator_amount = prev_operator_amount
-        }
-    }
-
-    message.channel.send(`❗**| ${message.author}, here is your equation:\n\`Operator count ${prev_operator_amount}, level ${level}\`\n\`${equation} = ...\`\n\nYou have 30 seconds to solve it.**`).then(msg => {
+    message.channel.send(`❗**| ${message.author}, here is your equation:\n\`Operator count ${operator_amount}, level ${level}\`\n\`\`\`fix\n${equation} = ...\`\`\`You have 30 seconds to solve it.**`).then(msg => {
         cd.add(message.author.id);
         let collector = message.channel.createMessageCollector(m => parseInt(m.content) === answer && m.author.id === message.author.id, {time: 30000, max: 1});
         let correct = false;
@@ -71,13 +154,13 @@ module.exports.run = (client, message, args) => {
             msg.delete().catch(console.error);
             correct = true;
             let timeDiff = Date.now() - msg.createdTimestamp;
-            message.channel.send(`✅ **| ${message.author}, your answer is correct! It took you ${timeDiff / 1000}s!\n\`${equation} = ${answer}\`**`);
+            message.channel.send(`✅ **| ${message.author}, your answer is correct! It took you ${timeDiff / 1000}s!\n\`\`\`fix\n${equation} = ${answer}\`\`\`**`);
             cd.delete(message.author.id)
         });
         collector.on('end', () => {
             if (!correct) {
                 msg.delete().catch(console.error);
-                message.channel.send(`❎ **| ${message.author}, timed out. The correct answer is\n\`${equation} = ${answer}\`.**`);
+                message.channel.send(`❎ **| ${message.author}, timed out. The correct answer is:\n\`\`\`fix\n${equation} = ${answer}\`\`\`**`);
                 cd.delete(message.author.id)
             }
         })
@@ -86,7 +169,7 @@ module.exports.run = (client, message, args) => {
 
 module.exports.config = {
     name: "mathquiz",
-    description: "Creates a simple math equation that you have to solve within 30 seconds.",
+    description: "Creates a simple math equation that you have to solve within 30 seconds.\n\nFor reference:\n`+`: addition\n`-`: subtraction\n`*`: multiplication\n`/`: division\n`!`: factorial\nOperator precedence: `*`/`/` -> `+`/`-`",
     usage: "mathquiz [level] [n operators]",
     detail: "`level`: Difficulty level of equation, defaults to 1, range is from 1 to 20 [Integer]\n`n operators`: The amount of operators to be used in the equation. Defaults to 4, range is from 1 to 10 [Integer]",
     permission: "None"
