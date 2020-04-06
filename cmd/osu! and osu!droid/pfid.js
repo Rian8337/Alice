90b74b884f4849b20e2c598564e16c3aa3674890
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-07 00:10:57 +0700

    Exclude "LL"

--- cmd/osu! and osu!droid/pfid.js
+++ cmd/osu! and osu!droid/pfid.js
@@ -113,7 +113,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
 					c.fillText(`Play Count: ${player.play_count.toLocaleString()}`, 169, 104);
 					if (res && res.pptotal) c.fillText(`Droid pp: ${res.pptotal.toFixed(2)}pp`, 169, 122);
 					if (res && res.clan) c.fillText(`Clan: ${res.clan}`, 169, 140);
-					c.fillText(player.location, 451, flag.height + 20);
+					if (player.location !== "LL") c.fillText(player.location, 451, flag.height + 20);
 
 					// ranked level
 					let textColor = pictureConfig.textColor;
