9839c2c1105367a6503084d0a2c4c1c4a482c887
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-06 21:34:43 +0700

    Tidy welcome message

--- mybot.js
+++ mybot.js
@@ -358,7 +358,7 @@ client.on("message", message => {
 client.on("guildMemberAdd", member => {
 	let channel = member.guild.channels.cache.get("360716684174032896");
 	if (!channel) return;
-	let joinMessage = `Welcome to ${member.guild.name}'s ${channel}, <@${member.id}>.\nTo verify yourself as someone who plays osu!droid or interested in the game and open the rest of the server, you can follow *any* of the following methods:\n- post your osu!droid screenshot (main menu if you are an online player or recent result (score) if you are an offline player). If you've just created an osu!droid account, please submit a score to the account before verifying\n- post your osu! profile (screenshot or link to profile) and reason why you join this server\n\nafter that, you can ping Moderator or Helper role and wait for one to come to verify you.\n\n**Do note that you have 1 week to verify, otherwise you will be automatically kicked.**`;
+	let joinMessage = `Welcome to ${member.guild.name}'s ${channel}, <@${member.id}>.\nTo verify yourself as someone who plays osu!droid or interested in the game and open the rest of the server, you can follow *any* of the following methods:\n\n- post your osu!droid screenshot (main menu if you are an online player or recent result (score) if you are an offline player). If you've just created an osu!droid account, please submit a score to the account before verifying\n\n- post your osu! profile (screenshot or link to profile) and reason why you join this server\n\nafter that, you can ping Moderator or Helper role and wait for one to come to verify you.\n\n**Do note that you have 1 week to verify, otherwise you will be automatically kicked.**`;
 	channel.send(joinMessage)
 });
 
