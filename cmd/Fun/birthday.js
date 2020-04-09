60016631ff62d004e5aa2338f7ae492ac4d4a0c5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-09 16:45:54 +0700

    Fix birthday date setting

--- cmd/Fun/birthday.js
+++ cmd/Fun/birthday.js
@@ -4,64 +4,63 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
     if (message.channel instanceof Discord.DMChannel) return;
     let birthday = alicedb.collection("birthday");
 
-    let month = parseInt(args[1]);
-    if (isNaN(month) || month < 1 || month > 12) return message.channel.send("❎ **| Hey, that's an invalid date!**");
-    let date = parseInt(args[2]);
-    if (isNaN(date) || date < 1 || date > 31) return message.channel.send("❎ **| I'm sorry, that's an invalid month!**");
+    let month = parseInt(args[0]);
+    if (isNaN(month) || month < 1 || month > 12) return message.channel.send("❎ **| Hey, that's an invalid month!**");
+    let date = parseInt(args[1]);
+    if (isNaN(date) || date < 1 || date > 31) return message.channel.send("❎ **| I'm sorry, that's an invalid date!**");
     --month;
 
     let max_date;
     if (month % 2 === 0) max_date = 31;
-    else {
-        if (month === 1) max_date = 29;
-        else max_date = 30
-    }
+    else if (month === 1) max_date = 29;
+    else max_date = 30;
 
     if (date > max_date) return message.channel.send("❎ **| Hey, that's an invalid date!**");
 
-    let string = date.toString();
+    let string = '';
     switch (month) {
-        case 0: string += 'Jan'; break;
-        case 1: string += 'Feb'; break;
-        case 2: string += 'Mar'; break;
-        case 3: string += 'Apr'; break;
+        case 0: string += 'January'; break;
+        case 1: string += 'February'; break;
+        case 2: string += 'March'; break;
+        case 3: string += 'April'; break;
         case 4: string += 'May'; break;
-        case 5: string += 'Jun'; break;
-        case 6: string += 'Jul'; break;
-        case 7: string += 'Aug'; break;
-        case 8: string += 'Sep'; break;
-        case 9: string += 'Oct'; break;
-        case 10: string += 'Nov'; break;
-        case 11: string += 'Des'
+        case 5: string += 'June'; break;
+        case 6: string += 'July'; break;
+        case 7: string += 'August'; break;
+        case 8: string += 'September'; break;
+        case 9: string += 'October'; break;
+        case 10: string += 'November'; break;
+        case 11: string += 'December'
     }
+    string += ' ' + date.toString()
 
-    message.channel.send(`❗**| ${message.author}, are you sure you want to set your birthday to \`${string}\`? You won't be able to change it afterwards!**`).then(msg => {
-        msg.react("✅").catch(console.error);
-        let confirmation = false;
-        let confirm = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id, {time: 20000});
-        confirm.on("collect", () => {
-            confirmation = true;
-            msg.delete();
+    birthday.findOne({discordid: message.author.id}, (err, res) => {
+        if (err) {
+            console.log(err);
+            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+        }
+        if (res) return message.channel.send("❎ **| I'm sorry, you've set your birthday date before!**");
+        message.channel.send(`❗**| ${message.author}, are you sure you want to set your birthday to \`${string}\`? You won't be able to change it afterwards!**`).then(msg => {
+            msg.react("✅").catch(console.error);
+            let confirmation = false;
+            let confirm = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id, {time: 20000});
+            confirm.on("collect", () => {
+                confirmation = true;
+                msg.delete();
 
-            // detect if date entry is 29 Feb
-            const isLeapYear = month === 1 && date === 29;
-            if (isLeapYear) {
-                month = 2;
-                date = 1
-            }
-
-            let insertVal = {
-                discordid: message.author.id,
-                date: date,
-                month: month,
-                isLeapYear: isLeapYear
-            };
-            birthday.findOne({discordid: message.author.id}, (err, res) => {
-                if (err) {
-                    console.log(err);
-                    return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
+                // detect if date entry is 29 Feb
+                const isLeapYear = month === 1 && date === 29;
+                if (isLeapYear) {
+                    month = 2;
+                    date = 1
                 }
-                if (res) return message.channel.send("❎ **| I'm sorry, you've set your birthday date before!**")
+
+                let insertVal = {
+                    discordid: message.author.id,
+                    date: date,
+                    month: month,
+                    isLeapYear: isLeapYear
+                };
                 birthday.insertOne(insertVal, err => {
                     if (err) {
                         console.log(err);
@@ -69,21 +68,21 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
                     }
                     message.channel.send(`✅ **| ${message.author}, successfully set your birthday to \`${string}\`.**`)
                 })
+            });
+            confirm.on("end", () => {
+                if (!confirmation) {
+                    msg.delete();
+                    message.channel.send("❎ **| Timed out.**").then(m => m.delete({timeout: 5000}))
+                }
             })
-        });
-        confirm.on("end", () => {
-            if (!confirmation) {
-                msg.delete();
-                message.channel.send("❎ **| Timed out.**").then(m => m.delete({timeout: 5000}))
-            }
         })
     })
 };
 
 module.exports.config = {
     name: "birthday",
-    string: "Sets your birthday date. Get 1000 Alice coins and a happy birthday role in your birthday!",
+    string: "Sets your birthday date. Get 1000 Alice coins and a happy birthday role in your birthday!\n\nIf someone's birthday is February 29, it will be celebrated in March 1 in nonleap years.",
     usage: "birthday <month> <date>",
-    detail: "`date`: Date of birthday (",
+    detail: "`date`: Date of birthday [Integer]\n`month`: Month of birthday [Integer]",
     permission: "None"
 };
