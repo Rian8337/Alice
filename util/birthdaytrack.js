13955fad3dae390e4ceda13bc0e487f7bd88b44f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-12 12:41:50 +0700

    Make timezone optional

--- util/birthdaytrack.js
+++ util/birthdaytrack.js
@@ -1,98 +1,91 @@
-module.exports.run = (client, maindb, alicedb) => {
-    let d = new Date();
-    let date = d.getUTCDate();
-    let month = d.getUTCMonth();
-    let year = d.getUTCFullYear();
-    const hour = d.getUTCHours();
+const Discord = require('discord.js');
 
-    // detect if it's leap year and if it is detect if current date is 29 Feb
-    const isLeapYear = year % 4 === 0;
-    if (isLeapYear && month === 1 && date === 29) {
-        date = 1;
-        month = 2
-    }
-
-    let guild = client.guilds.cache.get('316545691545501706');
-    let role = guild.roles.cache.get("695201338182860860");
-    if (!role) return;
-
-    let binddb = maindb.collection("userbind");
+module.exports.run = (client, message, args, maindb, alicedb) => {
+    if (message.channel instanceof Discord.DMChannel) return;
     let birthday = alicedb.collection("birthday");
-    let points = alicedb.collection("playerpoints");
 
-    birthday.find({}).toArray((err, res) => {
-        if (err) return console.log(err);
-        let current_birthday = [];
-        let timezone = -12;
+    let month = parseInt(args[0]);
+    if (isNaN(month) || month < 1 || month > 12) return message.channel.send("❎ **| Hey, that's an invalid month!**");
+    let date = parseInt(args[1]);
+    if (isNaN(date) || date < 1 || date > 31) return message.channel.send("❎ **| Hey, that's an invalid date!**");
+    --month;
+    let timezone = parseInt(args[2]);
+    if (isNaN(timezone) || timezone < -12 || timezone > 12) timezone = 0;
 
-        for (timezone; timezone < 13; ++timezone) {
-            d.setUTCHours(hour + timezone);
-            date = d.getUTCDate();
-            month = d.getUTCMonth();
-            if (isLeapYear) {
-                if (month === 1 && date === 29) {
-                    date = 1;
-                    month = 2
-                }
-                current_birthday.push(res.filter((entry) => entry.date === date && entry.month === month && entry.timezone === timezone && entry.isLeapYear === isLeapYear))
-            } else current_birthday.push(res.filter((entry) => entry.date === date && entry.month === month && entry.timezone === timezone ));
+    let max_date;
+    if (month % 2 === 0) max_date = 31;
+    else if (month === 1) max_date = 29;
+    else max_date = 30;
 
-            current_birthday.flat();
+    if (date > max_date) return message.channel.send("❎ **| Hey, that's an invalid date!**");
+
+    let string = '';
+    switch (month) {
+        case 0: string += 'January'; break;
+        case 1: string += 'February'; break;
+        case 2: string += 'March'; break;
+        case 3: string += 'April'; break;
+        case 4: string += 'May'; break;
+        case 5: string += 'June'; break;
+        case 6: string += 'July'; break;
+        case 7: string += 'August'; break;
+        case 8: string += 'September'; break;
+        case 9: string += 'October'; break;
+        case 10: string += 'November'; break;
+        case 11: string += 'December'
+    }
+    string += ' ' + date.toString()
+
+    birthday.findOne({discordid: message.author.id}, (err, res) => {
+        if (err) {
+            console.log(err);
+            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
         }
+        if (res) return message.channel.send("❎ **| I'm sorry, you've set your birthday date before!**");
+        message.channel.send(`❗**| ${message.author}, are you sure you want to set your birthday to \`${string}\` at UTC${timezone}? You won't be able to change it afterwards!**`).then(msg => {
+            msg.react("✅").catch(console.error);
+            let confirmation = false;
+            let confirm = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id, {time: 20000});
+            confirm.on("collect", () => {
+                confirmation = true;
+                msg.delete();
 
-        let i = 0;
-        role.members.forEach((member) => {
-            let index = current_birthday.findIndex((entry) => entry.discordid = member.id);
-            if (index === -1) member.roles.remove(role, "Not birthday anymore").catch(console.error);
-            ++i;
-            if (i !== role.members.size) return;
+                // detect if date entry is 29 Feb
+                const isLeapYear = month === 1 && date === 29;
+                if (isLeapYear) {
+                    month = 2;
+                    date = 1
+                }
 
-            current_birthday.forEach((entry) => {
-                let roles = member.roles;
-                let birthday_role = roles.cache.get(role.id);
-                if (birthday_role) return;
-
-                roles.add(role, "Happy birthday!").catch(console.error);
-                if (member.user.bot) return;
-                points.findOne({discordid: entry.discordid}, (err, userres) => {
-                    if (err) return console.log(err);
-                    if (userres) {
-                        let coins = userres.alicecoins;
-                        coins += 1000;
-                        let updateVal = {
-                            $set: {
-                                alicecoins: coins
-                            }
-                        };
-                        points.updateOne({discordid: entry.discordid}, updateVal, err => {
-                            if (err) return console.log(err)
-                        })
-                    } else {
-                        binddb.findOne({discordid: entry.discordid}, (err, ures) => {
-                            if (err) return console.log(err);
-                            let username = ures.username;
-                            let uid = ures.uid;
-                            let insertVal = {
-                                username: username,
-                                uid: uid,
-                                discordid: entry.discordid,
-                                challenges: [],
-                                points: 0,
-                                dailycooldown: 0,
-                                alicecoins: 1000
-                            };
-
-                            points.insertOne(insertVal, err => {
-                                if (err) return console.log(err)
-                            })
-                        })
+                let insertVal = {
+                    discordid: message.author.id,
+                    date: date,
+                    month: month,
+                    timezone: timezone,
+                    isLeapYear: isLeapYear
+                };
+                birthday.insertOne(insertVal, err => {
+                    if (err) {
+                        console.log(err);
+                        return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
                     }
+                    message.channel.send(`✅ **| ${message.author}, successfully set your birthday to \`${string}\` at UTC${timezone}.**`)
                 })
+            });
+            confirm.on("end", () => {
+                if (!confirmation) {
+                    msg.delete();
+                    message.channel.send("❎ **| Timed out.**").then(m => m.delete({timeout: 5000}))
+                }
             })
         })
     })
 };
 
 module.exports.config = {
-    name: 'birthdaytrack'
+    name: "birthday",
+    description: "Sets your birthday date. Get 1000 Alice coins and a happy birthday role in your birthday! Once set, a birthday date cannot be changed.\n\nIf someone's birthday is February 29, it will be celebrated in March 1 in nonleap years.",
+    usage: "birthday <month> <date> [timezone]",
+    detail: "`date`: Date of birthday [Integer]\n`month`: Month of birthday [Integer]\n`timezone`: Timezone of where you live. Defaults to 0 [Integer]",
+    permission: "None"
 };
