9d84bbe1532793f6a2c0fc5767eebcba260fe9f0
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-13 12:28:17 +0700

    Kick after 1 day

--- util/unverified.js
+++ util/unverified.js
@@ -1,41 +1,13 @@
-const Discord = require('discord.js');
-
-module.exports.run = (client, alicedb) => {
+module.exports.run = client => {
     let guild = client.guilds.cache.get("316545691545501706");
-    let channel = guild.channels.cache.get("360716684174032896");
     let unverified = guild.members.cache.filter((member) => !member.roles.cache.find((r) => r.name === 'Member') && !member.user.bot);
     if (unverified.size === 0) return;
     let count = 0;
-    let unverified_db = alicedb.collection("unverified");
     for (const [, member] of unverified.entries()) {
-        if (Date.now() - member.joinedTimestamp > 86400 * 1000 * 7) {
-            count++;
-            let join_date = member.joinedAt;
-            member.kick(`Unverified prune${join_date != null ? `(user joined at ${join_date.toUTCString()})` : ""}`).catch(console.error);
-            unverified_db.findOne({discordid: member.id}, (err, res) => {
-                if (err) console.log(err);
-                if (res) unverified_db.deleteOne({discordid: member.id}, err => {
-                    if (err) console.log(err)
-                })
-            });
-            continue
-        }
-        if (Date.now() - member.joinedTimestamp > 86400 * 1000 * 5) {
-            unverified_db.findOne({discordid: member.id}, (err, res) => {
-                if (err) console.log(err);
-                if (!res) {
-                    let verify_date = Math.floor((member.joinedTimestamp + 86400 * 1000 * 7 - Date.now()) / 1000);
-                    let hour = Math.floor(verify_date / 3600);
-                    let minute = Math.floor((verify_date - hour * 3600) / 60);
-                    let second = verify_date - hour * 3600 - minute * 60;
-                    let time_string = `${hour === 0 ? "" : `${hour} ${hour === 1 ? "hour" : "hours"}`}${minute === 0 ? "" : `, ${minute} ${minute === 1 ? "minute" : "minutes"}`}${second === 0 ? "" : `, ${second} ${second === 1 ? "second" : "seconds"}`}`;
-                    channel.send(`❗**| ${member}, you have ${time_string} to verify before you get kicked for being unverified for too long!**`);
-                    unverified_db.insertOne({discordid: member.id}, err => {
-                        if (err) console.log(err)
-                    })
-                }
-            })
-        }
+        if (Date.now() - member.joinedTimestamp < 86400) continue;
+        count++;
+        let join_date = member.joinedAt;
+        member.kick(`Unverified prune (user joined at ${join_date.toUTCString()})`).catch(console.error);
     }
     if (count > 0) console.log(`Pruned ${count} user(s)`)
 };
