ad146d2c42328dcb4456453a16bcb9af4dbb5df4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-13 12:29:02 +0700

    Change verify time to 1 day

--- mybot.js
+++ mybot.js
@@ -95,7 +95,7 @@ client.on("ready", () => {
 	
     // API check and unverified prune
 	setInterval(() => {
-                client.utils.get("unverified").run(client, alicedb);
+		client.utils.get("unverified").run(client);
 		http.request(`http://ops.dgsrz.com/api/getuserinfo.php?apiKey=${droidapikey}&uid=51076`, res => {
 			res.setEncoding("utf8");
 			res.setTimeout(5000);
@@ -356,7 +356,7 @@ client.on("message", message => {
 client.on("guildMemberAdd", member => {
 	let channel = member.guild.channels.cache.get("360716684174032896");
 	if (!channel) return;
-	let joinMessage = `Welcome to ${member.guild.name}'s ${channel}, <@${member.id}>.\nTo verify yourself as someone who plays osu!droid or interested in the game and open the rest of the server, you can follow *any* of the following methods:\n\n- post your osu!droid screenshot (main menu if you are an online player or recent result (score) if you are an offline player). If you've just created an osu!droid account, please submit a score to the account before verifying\n\n- post your osu! profile (screenshot or link to profile) and reason why you join this server\n\nafter that, you can ping Moderator or Helper role and wait for one to come to verify you.\n\n**Do note that you have 1 week to verify, otherwise you will be automatically kicked.**`;
+	let joinMessage = `Welcome to ${member.guild.name}'s ${channel}, <@${member.id}>.\nTo verify yourself as someone who plays osu!droid or interested in the game and open the rest of the server, you can follow *any* of the following methods:\n\n- post your osu!droid screenshot (main menu if you are an online player or recent result (score) if you are an offline player). If you've just created an osu!droid account, please submit a score to the account before verifying\n\n- post your osu! profile (screenshot or link to profile) and reason why you join this server\n\nafter that, you can ping Moderator or Helper role and wait for one to come to verify you.\n\n**Do note that you have 1 day to verify, otherwise you will be automatically kicked.**`;
 	channel.send(joinMessage)
 });
 
