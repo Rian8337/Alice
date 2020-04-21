1519d977acec395ea5366ab73074251dd5438ccf
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-21 13:05:21 +0700

    Fix name logging

--- cmd/osu! and osu!droid/namechange.js
+++ cmd/osu! and osu!droid/namechange.js
@@ -12,7 +12,7 @@ function processEmbed(res, page, footer, index) {
     
     for (let i = 10 * (page - 1); i < 10 + 10 * (page - 1); i++) {
         if (!res[i]) break;
-        embed.addField(`**${i+1}**. **${res[i].current_username} (${res.uid})**`, `**Discord Account**: <@${res[i].discordid}> (${res[i].discordid})\n**Username requested:** ${res[i].new_username}\n**Creation date**: ${new Date((res[i].cooldown - 86400 * 30) * 1000).toUTCString()}\n[Screenshot Attachment](${res[i].attachment}) (only viewable to <@386742340968120321>)`)
+        embed.addField(`**${i+1}**. **${res[i].current_username} (${res[i].uid})**`, `**Discord Account**: <@${res[i].discordid}> (${res[i].discordid})\n**Username requested:** ${res[i].new_username}\n**Creation date**: ${new Date((res[i].cooldown - 86400 * 30) * 1000).toUTCString()}\n[Screenshot Attachment](${res[i].attachment}) (only viewable to <@386742340968120321>)`)
     }
     
     return embed
@@ -106,7 +106,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
                             return
                         }
 
-                        prev_names.push(new_name);
+                        prev_names.push(old_name);
 
                         updateVal = {
                             $set: {
