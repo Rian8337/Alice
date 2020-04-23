e67fab94d6bc5c70ae5099354b351b3ff2b0828f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-23 22:08:14 +0700

    Make timezone optional

--- cmd/Fun/birthday.js
+++ cmd/Fun/birthday.js
@@ -10,7 +10,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
     if (isNaN(date) || date < 1 || date > 31) return message.channel.send("❎ **| Hey, that's an invalid date!**");
     --month;
     let timezone = parseInt(args[2]);
-    if (isNaN(timezone) || timezone < -12 || timezone > 12) return message.channel.send("❎ **| Hey, that's an invalid timezone!**");
+    if (isNaN(timezone) || timezone < -12 || timezone > 12) timezone = 0;
 
     let max_date;
     if (month % 2 === 0) max_date = 31;
@@ -42,7 +42,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
             return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
         }
         if (res) return message.channel.send("❎ **| I'm sorry, you've set your birthday date before!**");
-        message.channel.send(`❗**| ${message.author}, are you sure you want to set your birthday to \`${string}\` at UTC${timezone}? You won't be able to change it afterwards!**`).then(msg => {
+        message.channel.send(`❗**| ${message.author}, are you sure you want to set your birthday to \`${string}\` at UTC${timezone >= 0 ? `+${timezone}` : timezone}? You won't be able to change it afterwards!**`).then(msg => {
             msg.react("✅").catch(console.error);
             let confirmation = false;
             let confirm = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id, {time: 20000});
@@ -85,7 +85,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
 module.exports.config = {
     name: "birthday",
     description: "Sets your birthday date. Get 1000 Alice coins and a happy birthday role in your birthday! Once set, a birthday date cannot be changed.\n\nIf someone's birthday is February 29, it will be celebrated in March 1 in nonleap years.",
-    usage: "birthday <month> <date> <timezone>",
-    detail: "`date`: Date of birthday [Integer]\n`month`: Month of birthday [Integer]\n`timezone`: Timezone of where you live [Integer]",
+    usage: "birthday <month> <date> [timezone]",
+    detail: "`date`: Date of birthday [Integer]\n`month`: Month of birthday [Integer]\n`timezone`: Timezone of where you live. Defaults at 0 [Integer]",
     permission: "None"
 };
