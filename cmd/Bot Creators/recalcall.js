0c4f4260b0a419c850e189320d0ccd10895fdfdc
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-24 16:19:58 +0700

    Change callback to await

--- cmd/Bot Creators/recalcall.js
+++ cmd/Bot Creators/recalcall.js
@@ -10,9 +10,9 @@ function retrieveList(res, i, cb) {
     cb(list)
 }
 
-function recalcPlay(target, i, newtarget, whitelist, cb) {
+async function recalcPlay(target, i, newtarget, whitelist, cb) {
     if (!target[i]) return cb(false, true);
-    let mods = "";
+    let mods = '';
     if (target[i][1].includes('+'))  {
         let mapstring = target[i][1].split('+');
         mods = mapstring[mapstring.length-1];
@@ -22,43 +22,42 @@ function recalcPlay(target, i, newtarget, whitelist, cb) {
     let guessing_mode = true;
     let whitelistQuery = {hashid: target[i][0]};
 
-    whitelist.findOne(whitelistQuery, (err, wlres) => {
+    whitelist.findOne(whitelistQuery, async (err, wlres) => {
         let query = {hash: target[i][0]};
         if (err) {
             console.log("Whitelist find error");
             return cb(true)
         }
         if (wlres) query = {beatmap_id: wlres.mapid};
-        new osudroid.MapInfo().get(query, mapinfo => {
-            if (!mapinfo.title) {
-                console.log("Map not found");
-                return cb()
-            }
-            if (!mapinfo.objects) {
-                console.log("0 objects found");
-                return cb()
-            }
-            let acc_percent = 100;
-            if (target[i][4]) {
-                acc_percent = parseFloat(target[i][4]);
-                guessing_mode = false;
-            }
-            let combo = target[i][3] ? parseInt(target[i][3]) : mapinfo.max_combo;
-            let miss = target[i][5] ? parseInt(target[i][5]) : 0;
-            let star = new osudroid.MapStars().calculate({file: mapinfo.osu_file, mods: mods});
-            let npp = osudroid.ppv2({
-                stars: star.droid_stars,
-                combo: combo,
-                acc_percent: acc_percent,
-                miss: miss,
-                mode: "droid"
-            });
-            let pp = parseFloat(npp.toString().split(" ")[0]);
-            let real_pp = guessing_mode ? parseFloat(target[i][2]).toFixed(2) : pp;
-            console.log(`${target[i][2]} -> ${real_pp}`);
-            newtarget.push(guessing_mode ? [target[i][0], target[i][1], real_pp] : [target[i][0], target[i][1], real_pp, target[i][3], target[i][4], target[i][5]]);
-            cb()
-        })
+        const mapinfo = await new osudroid.MapInfo().get(query).catch(console.error);
+        if (!mapinfo.title) {
+            console.log("Map not found");
+            return cb()
+        }
+        if (!mapinfo.objects) {
+            console.log("0 objects found");
+            return cb()
+        }
+        let acc_percent = 100;
+        if (target[i][4]) {
+            acc_percent = parseFloat(target[i][4]);
+            guessing_mode = false;
+        }
+        let combo = target[i][3] ? parseInt(target[i][3]) : mapinfo.max_combo;
+        let miss = target[i][5] ? parseInt(target[i][5]) : 0;
+        let star = new osudroid.MapStars().calculate({file: mapinfo.osu_file, mods: mods});
+        let npp = osudroid.ppv2({
+            stars: star.droid_stars,
+            combo: combo,
+            acc_percent: acc_percent,
+            miss: miss,
+            mode: "droid"
+        });
+        let pp = parseFloat(npp.toString().split(" ")[0]);
+        let real_pp = guessing_mode ? parseFloat(target[i][2]).toFixed(2) : pp;
+        console.log(`${target[i][2]} -> ${real_pp}`);
+        newtarget.push(guessing_mode ? [target[i][0], target[i][1], real_pp] : [target[i][0], target[i][1], real_pp, target[i][3], target[i][4], target[i][5]]);
+        cb()
     })
 }
 
@@ -79,7 +78,7 @@ module.exports.run = (client, message, args, maindb) => {
                 if (err) throw err;
                 let i = 0;
                 message.channel.send(`❗**| Current progress: ${i}/${res.length} players recalculated (${(i * 100 / res.length).toFixed(2)}%)**`).then(m => {
-                    retrieveList(res, i, function testList(list, stopSign = false) {
+                    retrieveList(res, i, async function testList(list, stopSign = false) {
                         if (stopSign) {
                             m.edit(`❗**| Current progress: ${i}/${res.length} players recalculated (${(i * 100 / res.length).toFixed(2)}%)**`).catch(console.error);
                             return message.channel.send(`✅ **| ${message.author}, recalculation process complete!**`);
@@ -92,11 +91,11 @@ module.exports.run = (client, message, args, maindb) => {
                         console.log("Uid:", uid);
                         if (!ppentry) {
                             i++;
-                            return retrieveList(res, i, testList)
+                            return retrieveList(res, i, await testList)
                         }
-                        recalcPlay(ppentry, count, newppentry, whitelist, function testPlay(error = false, stopFlag = false) {
+                        await recalcPlay(ppentry, count, newppentry, whitelist, async function testPlay(error = false, stopFlag = false) {
                             if (!error) count++;
-                            if (count < ppentry.length && !stopFlag) return recalcPlay(ppentry, count, newppentry, whitelist, testPlay);
+                            if (count < ppentry.length && !stopFlag) return await recalcPlay(ppentry, count, newppentry, whitelist, await testPlay);
                             newppentry.sort((a, b) => {return b[2] - a[2]});
                             let totalpp = 0;
                             let weight = 1;
@@ -110,19 +109,20 @@ module.exports.run = (client, message, args, maindb) => {
                                     pp: newppentry
                                 }
                             };
-                            binddb.updateOne({discordid: discordid}, updatedata, err => {
+                            binddb.updateOne({discordid: discordid}, updatedata, async err => {
                                 if (err) {
                                     console.log("Error inserting data to database");
                                     console.log(err);
                                     if (!error) count--;
-                                    return testPlay
+                                    await testPlay;
+                                    return
                                 }
                                 console.log(totalpp);
                                 console.log("Done");
                                 i++;
                                 console.log(`${i}/${res.length} players recalculated (${(i * 100 / res.length).toFixed(2)}%)`);
                                 m.edit(`❗**| Current progress: ${i}/${res.length} players recalculated (${(i * 100 / res.length).toFixed(2)}%)**`).catch(console.error);
-                                retrieveList(res, i, testList)
+                                retrieveList(res, i, await testList)
                             })
                         })
                     })
