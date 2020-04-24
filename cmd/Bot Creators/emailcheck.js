58abca59fb6dcaa08534d0b306ed56ae5e00f9c3
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-24 11:12:20 +0700

    Change callback to await

--- cmd/Bot Creators/emailcheck.js
+++ cmd/Bot Creators/emailcheck.js
@@ -2,43 +2,42 @@ const Discord = require('discord.js');
 const config = require('../../config.json');
 const osudroid = require('../../modules/osu!droid');
 
-module.exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
 	if (message.author.id != '132783516176875520' && message.author.id != '386742340968120321') return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
 	let uid = args[0];
 	if (isNaN(uid)) return message.channel.send("❎ **| I'm sorry, that uid is not valid.**");
-	new osudroid.PlayerInfo().get({uid: uid}, player => {
-		if (!player.name) return message.channel.send("❎ **| I'm sorry, I cannot find the user you are looking for!**");
-		let name = player.name;
-		let email = player.email;
+	const player = await new osudroid.PlayerInfo().get({uid: uid}).catch(console.error);
+	if (!player.name) return message.channel.send("❎ **| I'm sorry, I cannot find the user you are looking for!**");
+	let name = player.name;
+	let email = player.email;
 
-		let rolecheck;
-		try {
-			rolecheck = message.member.roles.highest.hexColor
-		} catch (e) {
-			rolecheck = "#000000"
-		}
-		let footer = config.avatar_list;
-		const index = Math.floor(Math.random() * footer.length);
-		let embed = new Discord.MessageEmbed()
-			.setTitle("User profile")
-			.setColor(rolecheck)
-			.setFooter("Alice Synthesis Thirty", footer[index])
-			.addField("Username", name)
-			.addField("Uid", uid)
-			.addField("Email", email);
+	let rolecheck;
+	try {
+		rolecheck = message.member.roles.highest.hexColor
+	} catch (e) {
+		rolecheck = "#000000"
+	}
+	let footer = config.avatar_list;
+	const index = Math.floor(Math.random() * footer.length);
+	let embed = new Discord.MessageEmbed()
+		.setTitle("User profile")
+		.setColor(rolecheck)
+		.setFooter("Alice Synthesis Thirty", footer[index])
+		.addField("Username", name)
+		.addField("Uid", uid)
+		.addField("Email", email);
 
-		try {
-			message.author.send({embed: embed})
-		} catch (e) {
-			return message.channel.send(`❎ **| ${message.author}, your DM is locked!**`);
-		}
-		message.channel.send(`✅ **| ${message.author}, the user info has been sent to you!**`)
-	})
+	try {
+		message.author.send({embed: embed})
+	} catch (e) {
+		return message.channel.send(`❎ **| ${message.author}, your DM is locked!**`);
+	}
+	message.channel.send(`✅ **| ${message.author}, the user info has been sent to you!**`)
 };
 
 module.exports.config = {
 	name: "emailcheck",
-	description: "Retrieves the registered email of an osu!droid account.",
+	description: "Retrieves the registered email of an droid account.",
 	usage: "emailcheck <uid>",
 	detail: "`uid`: Uid to retrieve email from [Integer]`",
 	permission: "Specific person (<@132783516176875520> and <@386742340968120321>)"
