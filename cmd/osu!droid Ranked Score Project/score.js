46c6fa8106649ced7849e5a5e2cc7075469a0d48
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-24 19:51:37 +0700

    Fix res[0] detection

--- cmd/osu!droid Ranked Score Project/score.js
+++ cmd/osu!droid Ranked Score Project/score.js
@@ -192,7 +192,7 @@ module.exports.run = (client, message, args, maindb, alicedb) => {
                     let scorediff = score - prescore;
                     calculateLevel(Math.floor(currentlevel) - 1, score, level => {
                         let levelremain = (level - Math.floor(level)) * 100;
-                        embed.setDescription(`Ranked score: **${score.toLocaleString()}**\nScore gained: **${scorediff.toLocaleString()}**\nCurrent level: **${Math.floor(level)} (${levelremain.toFixed(2)}%)${Math.floor(level) > Math.floor(currentlevel)?"\n🆙 Level up!":""}**\nScore needed to level up: **${(scoreRequirement(Math.floor(level) + 1) - score).toLocaleString()}**${!res[0]?"\nHey, looks like you are new to the system! You can ask <@386742340968120321> to enter all of your previous scores, or ignore this message if you want to start new!":""}`);
+                        embed.setDescription(`Ranked score: **${score.toLocaleString()}**\nScore gained: **${scorediff.toLocaleString()}**\nCurrent level: **${Math.floor(level)} (${levelremain.toFixed(2)}%)${Math.floor(level) > Math.floor(currentlevel)?"\n🆙 Level up!":""}**\nScore needed to level up: **${(scoreRequirement(Math.floor(level) + 1) - score).toLocaleString()}**${!res ? "\nHey, looks like you are new to the system! You can ask <@386742340968120321> to enter all of your previous scores, or ignore this message if you want to start new!" : ""}`);
                         message.channel.send('✅ **| <@' + discordid + '>, successfully submitted your play(s). More info in embed.**', {embed: embed});
                         if (res[0]) {
                             let updateVal = {
