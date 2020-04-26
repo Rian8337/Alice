fec929bfe86b3634a94047b9199699b4044687be
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-04-26 13:37:17 +0700

    Change query

--- cmd/osu! and osu!droid/bindsearch.js
+++ cmd/osu! and osu!droid/bindsearch.js
@@ -1,44 +1,31 @@
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args, maindb) => {
-    let uid = args[0];
+    const uid = args[0];
     if (isNaN(uid)) return message.channel.send("❎ **| Hey, can you at least give me a valid uid?**");
-    let binddb = maindb.collection("userbind");
-    let query = {uid: uid};
+    const binddb = maindb.collection("userbind");
+    const query = {previous_bind: {$all: [uid.toString()]}};
     let rolecheck;
     try {
         rolecheck = message.member.roles.highest.hexColor
     } catch (e) {
         rolecheck = "#000000"
     }
-    let embed = new Discord.MessageEmbed()
+    const embed = new Discord.MessageEmbed()
         .setColor(rolecheck);
 
-    binddb.find(query).toArray((err, res) => {
+    binddb.findOne(query, (err, res) => {
         if (err) {
             console.log(err);
             return message.channel.send("❎ **| I'm not receiving any response from database. Perhaps try again?**")
         }
-        if (!res[0]) {
+        if (!res) {
             embed.setDescription(`**Uid ${uid} is not binded**`);
             return message.channel.send({embed: embed})
         }
-        let bind = `Uid ${uid} is binded to `;
-        let accounts = '';
-        let userid = '';
-        let acc = 0;
-        res.forEach(x => {
-            accounts += `<@${x.discordid}>`;
-            userid += `\`${x.discordid}\` `;
-            acc++;
-            if (acc == res.length) {
-                accounts = accounts.trimRight().split(" ").join(", ");
-                userid = `\nUser ID: ${userid.trimRight().split(" ").join(", ")}`;
-                bind += accounts + userid;
-                embed.setDescription(`**${bind}**`);
-                message.channel.send({embed: embed}).catch(console.error)
-            }
-        })
+        const bind = `Uid ${uid} is binded to <@${res.discordid}>.\nUser ID: ${res.discordid}`;
+        embed.setDescription(`**${bind}**`);
+        message.channel.send({embed: embed}).catch(console.error)
     })
 };
 
