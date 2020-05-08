605f01284312d6e7346ef92cf522d69f28ccfbbf
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-05-08 22:01:57 +0700

    Fix iteration

--- util/unverified.js
+++ util/unverified.js
@@ -5,21 +5,22 @@ module.exports.run = async (client, alicedb) => {
     let unverified = guild.members.cache.filter((member) => !member.roles.cache.find((r) => r.name === 'Member') && !member.user.bot && (member.joinedTimestamp != null || member.joinedAt != null) && curtime - member.joinedTimestamp > 43200000);
     if (unverified.size === 0) return;
     let count = 0;
+    
     for await (const [, member] of unverified.entries()) {
         unverifieddb.findOne({discordid: member.id}, (err, res) => {
-            if (err) continue;
-            count++;
-            if (!res) {
-                unverifieddb.insertOne({discordid: member.id}, err => {
-                    if (!err) member.send("❗**| Hey, you have 12 hours left to verify yourself in osu!droid International Discord server! Please head to <#360716684174032896> and read the channel's pinned message for how to verify.**").catch(console.error);
-                    continue
-                })
-            }
-            else if (curtime - member.joinedTimestamp > 86400000) {
-                unverifieddb.deleteOne({discordid: member.id}, err => {
-                    if (err) continue;
-                    member.kick(`Unverified prune (user joined at ${member.joinedAt.toUTCString()})`).catch(console.error);
-                })
+            if (!err) {
+                count++;
+
+                if (!res) {
+                    unverifieddb.insertOne({discordid: member.id}, err => {
+                        if (!err) member.send("❗**| Hey, you have 12 hours left to verify yourself in osu!droid International Discord server! Please head to <#360716684174032896> and read the channel's pinned message for how to verify.**").catch(console.error)
+                    })
+                }
+                else if (curtime - member.joinedTimestamp > 86400000) {
+                    unverifieddb.deleteOne({discordid: member.id}, err => {
+                        if (!err) member.kick(`Unverified prune (user joined at ${member.joinedAt.toUTCString()})`).catch(console.error);
+                    })
+                }
             }
         });
     }
