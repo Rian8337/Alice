47f8174d8416088993de7684ae105020a5a1258a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-05-08 14:20:13 +0700

    Log different channel if in DM

--- mybot.js
+++ mybot.js
@@ -313,7 +313,8 @@ client.on("message", message => {
 			}, 5000);
 			//if (cd.has(message.author.id)) return message.channel.send("❎ **| Hey, calm down with the command! I need to rest too, you know.**");
 			if (apidown && require_api.includes(cmd.config.name)) return message.channel.send("❎ **| I'm sorry, API is currently unstable or down, therefore you cannot use droid-related commands!**");
-			console.log(`${message.author.tag} (#${message.channel.name}): ${message.content}`);
+			if (!(message.channel instanceof Discord.DMChannel)) console.log(`${message.author.tag} (#${message.channel.name}): ${message.content}`);
+			else console.log(`${message.author.tag} (DM): ${message.content}`);
 			cmd.run(client, message, args, maindb, alicedb, current_map);
 			//cd.add(message.author.id);
 			//setTimeout(() => {
@@ -332,7 +333,8 @@ client.on("message", message => {
 			}, 5000);
 			//if (cd.has(message.author.id)) return message.channel.send("❎ **| Hey, calm down with the command! I need to rest too, you know.**");
 			if (apidown && require_api.includes(cmd.help.name)) return message.channel.send("❎ **| I'm sorry, API is currently unstable or down, therefore you cannot use droid-related commands!**");
-			console.log(`${message.author.tag} (#${message.channel.name}): ${message.content}`);
+			if (!(message.channel instanceof Discord.DMChannel)) console.log(`${message.author.tag} (#${message.channel.name}): ${message.content}`);
+			else console.log(`${message.author.tag} (DM): ${message.content}`);
 			cmd.run(client, message, args, maindb, alicedb, current_map);
 			//cd.add(message.author.id);
 			//setTimeout(() => {
