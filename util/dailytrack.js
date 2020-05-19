2fe3b4a92122258327c1c0a74737caca51e7a13a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-05-19 20:50:08 +0700

    Make OneDrive link optional

--- util/dailytrack.js
+++ util/dailytrack.js
@@ -141,7 +141,7 @@ module.exports.run = (client, message = "", args = {}, maindb, alicedb) => {
             .setColor(mapinfo.statusColor())
             .setFooter(`Alice Synthesis Thirty | Challenge ID: ${challengeid}`, footer[index])
             .setThumbnail(`https://b.ppy.sh/thumb/${mapinfo.beatmapset_id}l.jpg`)
-            .setDescription(`**[${mapinfo.showStatistics("", 0)}](https://osu.ppy.sh/b/${beatmapid})**${featured ? `\nFeatured by <@${featured}>` : ""}\nDownload: [Google Drive](${dailyres.link[0]}) - [OneDrive](${dailyres.link[1]})`)
+            .setDescription(`**[${mapinfo.showStatistics("", 0)}](https://osu.ppy.sh/b/${beatmapid})**${featured ? `\nFeatured by <@${featured}>` : ""}\nDownload: [Google Drive](${dailyres.link[0]})${dailyres[0].link[1] ? `- [OneDrive](${dailyres[0].link[1]})` : ""}`)
             .addField("**Map Info**", `${mapinfo.showStatistics("", 2)}\n${mapinfo.showStatistics("", 3)}\n${mapinfo.showStatistics("", 4)}\n${mapinfo.showStatistics("", 5)}`)
             .addField(`**Star Rating**\n${"★".repeat(Math.min(10, parseInt(star.droid_stars.toString())))} ${parseFloat(star.droid_stars.toString()).toFixed(2)} droid stars\n${"★".repeat(Math.min(10, parseInt(star.pc_stars.toString())))} ${parseFloat(star.pc_stars.toString()).toFixed(2)} PC stars`, `**${dailyres.points === 1?"Point":"Points"}**: ${dailyres.points} ${dailyres.points === 1?"point":"points"}\n**Pass Condition**: ${pass_string}\n**Constrain**: ${constrain_string}\n\n**Bonus**\n${bonus_string}`);
 
