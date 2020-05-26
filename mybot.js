c382195401b3a0a468a41a90880e5b33998c0a49
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-05-26 11:59:09 +0700

    Revert change

--- mybot.js
+++ mybot.js
@@ -363,7 +363,7 @@ client.on("guildMemberAdd", member => {
 client.on("guildMemberUpdate", (oldMember) => {
 	if (oldMember.user.bot) return;
 	let general = oldMember.guild.channels.cache.get("316545691545501706");
-	if (!general || oldMember.roles.cache.find(r => r.name === 'Member')) return;
+	if (!general || oldMember.roles.cache.find((r) => r.name === "Member") || oldMember.roles.cache.size == newMember.roles.cache.size) return;
 	fs.readFile("welcome.txt", 'utf8', (err, data) => {
 		if (err) return console.log(err);
 		let welcomeMessage = `Welcome to ${oldMember.guild.name}, <@${oldMember.id}>!`;
