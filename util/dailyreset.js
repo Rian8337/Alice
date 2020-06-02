dd32411f9652660e3cc034b3e3b88bfc51024a46
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-02 20:29:22 +0700

    Remove unnecessary condition check

--- util/dailyreset.js
+++ util/dailyreset.js
@@ -3,7 +3,7 @@ let reset_time = 0;
 module.exports.run = alicedb => {
     const pointdb = alicedb.collection("playerpoints");
     const time = Math.floor(Date.now() / 1000);
-    if (reset_time && reset_time > time) return;
+    if (reset_time > time) return;
     pointdb.findOne({discordid: "386742340968120321"}, (err, res) => {
         if (err) return console.log(err);
         const dailyreset = res.dailyreset;
@@ -41,7 +41,7 @@ module.exports.run = alicedb => {
                         if (err) return console.log(err)
                     })
                 })
-            });
+            })
         })
     })
 };
