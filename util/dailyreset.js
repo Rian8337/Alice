e2ebd2651603706d3d3deafe8b1a2b1170e148da
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-02 20:27:01 +0700

    Add local reset time to reduce database load

--- util/dailyreset.js
+++ util/dailyreset.js
@@ -1,9 +1,13 @@
+let reset_time = 0;
+
 module.exports.run = alicedb => {
     const pointdb = alicedb.collection("playerpoints");
     const time = Math.floor(Date.now() / 1000);
+    if (reset_time && reset_time > time) return;
     pointdb.findOne({discordid: "386742340968120321"}, (err, res) => {
         if (err) return console.log(err);
         const dailyreset = res.dailyreset;
+        if (!reset_time) reset_time = dailyreset;
         if (dailyreset > time) return;
         console.log("Resetting daily claim");
         let updateVal = {
