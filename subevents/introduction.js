41c10608cba8d03288b1fe8ac8d8013d11ecf68b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-03 23:34:12 +0700

    Fix welcome.txt not found

--- subevents/introduction.js
+++ subevents/introduction.js
@@ -4,7 +4,7 @@ module.exports.run = (oldMember, newMember) => {
     if (oldMember.user.bot) return;
 	let general = oldMember.guild.channels.cache.get("316545691545501706");
 	if (!general || oldMember.roles.cache.find((r) => r.name === "Member") || oldMember.roles.cache.size == newMember.roles.cache.size) return;
-	fs.readFile("../welcome.txt", 'utf8', (err, data) => {
+	fs.readFile(__dirname + "/../welcome.txt", 'utf8', (err, data) => {
 		if (err) return console.log(err);
 		let welcomeMessage = `Welcome to ${oldMember.guild.name}, <@${oldMember.id}>!`;
 		setTimeout(() => {
