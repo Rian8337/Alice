e0e3790b5f91c4a2a85b2c4d85e083d638ee161f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-07 14:01:46 +0700

    Change verification limit to 1 week

--- util/unverified.js
+++ util/unverified.js
@@ -2,7 +2,7 @@ module.exports.run = async (client, alicedb) => {
     let unverifieddb = alicedb.collection("unverified");
     let guild = client.guilds.cache.get("316545691545501706");
     let curtime = Date.now();
-    let unverified = guild.members.cache.filter((member) => !member.roles.cache.find((r) => r.name === 'Member') && !member.user.bot && (member.joinedTimestamp != null || member.joinedAt != null) && curtime - member.joinedTimestamp > 43200000);
+    let unverified = guild.members.cache.filter((member) => !member.roles.cache.find((r) => r.name === 'Member') && !member.user.bot && (member.joinedTimestamp != null || member.joinedAt != null) && curtime - member.joinedTimestamp > 302400000);
     if (unverified.size === 0) return;
     let count = 0;
     
@@ -13,10 +13,10 @@ module.exports.run = async (client, alicedb) => {
 
                 if (!res) {
                     unverifieddb.insertOne({discordid: member.id}, err => {
-                        if (!err) member.send("❗**| Hey, you have 12 hours left to verify yourself in osu!droid International Discord server! Please head to <#360716684174032896> and read the channel's pinned message for how to verify.**").catch(console.error)
-                    })
+                        if (!err) member.send("❗**| Hey, you have 84 hours left to verify yourself in osu!droid International Discord server! Please head to <#360716684174032896> and read the channel's pinned message for how to verify.**").catch(console.error)
+                    });
                 }
-                else if (curtime - member.joinedTimestamp > 86400000) {
+                else if (curtime - member.joinedTimestamp > 604800000) {
                     unverifieddb.deleteOne({discordid: member.id}, err => {
                         if (!err) member.kick(`Unverified prune (user joined at ${member.joinedAt.toUTCString()})`).catch(console.error);
                     })
