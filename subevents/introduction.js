9f6e0be89c0c25a7f080fac389f60f40a769f48b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-11 06:40:48 +0700

    Do not send message for not new members

--- subevents/introduction.js
+++ subevents/introduction.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports.run = (oldMember, newMember) => {
     if (oldMember.user.bot) return;
 	let general = oldMember.guild.channels.cache.get("316545691545501706");
-	if (!general || oldMember.roles.cache.find((r) => r.name === "Member") || oldMember.roles.cache.size == newMember.roles.cache.size) return;
+	if (!general || oldMember.roles.cache.size > 0 || oldMember.roles.cache.find((r) => r.name === "Member") || oldMember.roles.cache.size == newMember.roles.cache.size) return;
 	fs.readFile(__dirname + "/../welcome.txt", 'utf8', (err, data) => {
 		if (err) return console.log(err);
 		let welcomeMessage = `Welcome to ${oldMember.guild.name}, <@${oldMember.id}>!`;
