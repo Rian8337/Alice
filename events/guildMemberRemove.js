0a065d193cf9f20629f5f47ba48eafdadbf56dfd
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-16 13:22:02 +0700

    Disable bind wipe

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -6,7 +6,7 @@ module.exports.run = (client, member, maindb, alicedb) => {
     client.subevents.get("clanCheck").run(client, member, maindb)
 
     // Wipe bind
-    client.subevents.get("bindWipe").run(client, member.guild, member.user, maindb, alicedb)
+    // client.subevents.get("bindWipe").run(client, member.guild, member.user, maindb, alicedb)
 };
 
 module.exports.config = {
