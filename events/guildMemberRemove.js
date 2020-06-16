643b6bfcb391902ee9c508235c2e07737a2d631b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-16 12:01:37 +0700

    Do not wipe unbinded users

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,6 @@
 module.exports.run = (client, member, maindb, alicedb) => {
     if (member.guild.id !== '316545691545501706') return;
+    if (!member.roles.cache.find(r => r.name === "Member")) return;
 
     // Check if player is in clan
     client.subevents.get("clanCheck").run(client, member, maindb)
