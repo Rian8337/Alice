22b5091d994497b9686523403a838b951924b2fe
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-18 17:55:29 +0700

    Do not welcome old members

--- subevents/introduction.js
+++ subevents/introduction.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports.run = (oldMember, newMember) => {
     if (oldMember.user.bot) return;
 	let general = oldMember.guild.channels.cache.get("316545691545501706");
-	if (!general || oldMember.roles.cache.find((r) => r.name === "Member") || oldMember.roles.cache.size == newMember.roles.cache.size) return;
+	if (!general || oldMember.roles.cache.size > 0) return;
 	general.send(`Welcome to ${oldMember.guild.name}, <@${oldMember.id}>!`, {files: ["https://i.imgur.com/LLzteLz.jpg"]})
 };
 
