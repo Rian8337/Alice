37f87dc6638d52517775d2295e7a25f322e36d94
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-25 08:31:38 +0700

    Attempt to fix introduction message

--- subevents/introduction.js
+++ subevents/introduction.js
@@ -1,9 +1,7 @@
-const fs = require('fs');
-
 module.exports.run = (oldMember, newMember) => {
     if (oldMember.user.bot) return;
 	let general = oldMember.guild.channels.cache.get("316545691545501706");
-	if (!general || oldMember.roles.cache.size > 0) return;
+	if (!general || oldMember.roles.cache.has("353397345636974593")) return;
 	general.send(`Welcome to ${oldMember.guild.name}, <@${oldMember.id}>!`, {files: ["https://i.imgur.com/LLzteLz.jpg"]})
 };
 
