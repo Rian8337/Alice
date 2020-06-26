30fbd47c8890cd0d94d6b0a99a0364b5f38f11b0
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-06-26 14:21:17 +0700

    Reenable bind wipe

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,12 +1,12 @@
 module.exports.run = (client, member, maindb, alicedb) => {
     if (member.guild.id !== '316545691545501706') return;
-    if (!member.roles.cache.find(r => r.name === "Member")) return;
+    if (!member.roles.cache.has("353397345636974593")) return;
 
     // Check if player is in clan
     client.subevents.get("clanCheck").run(client, member, maindb)
 
     // Wipe bind
-    // client.subevents.get("bindWipe").run(client, member.guild, member.user, maindb, alicedb)
+    client.subevents.get("bindWipe").run(client, member.guild, member.user, maindb, alicedb)
 };
 
 module.exports.config = {
