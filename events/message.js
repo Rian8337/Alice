4f255ccc3f470418b957c1c4825796a2ac377833
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-08-12 10:18:09 +0700

    Add command disabling

--- events/message.js
+++ events/message.js
@@ -4,6 +4,7 @@ const config = require('../config.json');
 let maintenance = false;
 let maintenance_reason = '';
 const current_map = [];
+const disabled_commands = [];
 let command_cooldown = 0;
 
 module.exports.run = (client, message, maindb, alicedb) => {
@@ -72,26 +73,59 @@ module.exports.run = (client, message, maindb, alicedb) => {
 	}
 	
 	// commands
-	if (message.author.id === '386742340968120321' && command === 'a!maintenance') {
+	if (message.author.id === '386742340968120321' && command === config.prefix + 'maintenance') {
 		maintenance_reason = args.join(" ");
-		if (!maintenance_reason) maintenance_reason = 'Unknown';
+		if (!maintenance_reason) {
+			maintenance_reason = 'Unknown';
+		}
 		maintenance = !maintenance;
+		if (maintenance) {
+			client.user.setActivity("Maintenance mode").catch(console.error);
+		}
+		else {
+			client.user.setActivity(config.prefix + "help", {type: "LISTENING"}).catch(console.error);
+		}
+		this.maintenance = maintenance;
 		message.channel.send(`✅ **| Maintenance mode has been set to \`${maintenance}\` for \`${maintenance_reason}\`.**`).catch(console.error);
-		if (maintenance) client.user.setActivity("Maintenance mode").catch(console.error);
-		else client.user.setActivity("a!help").catch(console.error);
-		module.exports.maintenance = maintenance
 	}
 
-	if (message.author.id === '386742340968120321' && command === 'a!cooldown') {
+	if (message.author.id === '386742340968120321' && command === config.prefix + 'cooldown') {
 		const seconds = parseFloat(args[0]);
-		if (isNaN(seconds) || seconds < 0) return message.channel.send("❎ **| Hey, please enter a valid cooldown period!**");
+		if (isNaN(seconds) || seconds < 0) {
+			return message.channel.send("❎ **| Hey, please enter a valid cooldown period!**");
+		}
 		command_cooldown = seconds;
-		message.channel.send(`✅ **| Successfully set command cooldown to ${seconds} ${seconds === 1 ? "second" : "seconds"}.**`)
+		message.channel.send(`✅ **| Successfully set command cooldown to ${seconds} ${seconds === 1 ? "second" : "seconds"}.**`);
+	}
+
+	if (message.author.id === "386742340968120321" && (command === config.prefix + 'cmd' || command === config.prefix + 'command')) {
+		if (args[0] === "list") {
+			if (disabled_commands.length === 0) {
+				return message.channel.send("❎ **| I'm sorry, there are no disabled commands now!**");
+			}
+			let string = `✅ **| The current disabled ${disabled_commands.length === 1 ? "command is" : "commands are"} `;
+			for (const disabled of disabled_commands) {
+				string += `\`${disabled}\`, `;
+			}
+			string = string.substring(0, string.length - 2) + ".**";
+			return message.channel.send(string);
+		}
+		const cmd = client.commands.get(args[0]) || client.aliases.get(args[0]);
+		if (!cmd) {
+			return message.channel.send("❎ **| Hey, please enter a command to enable or disable!**");
+		}
+		const cmd_index = disabled_commands.findIndex(c => c === cmd.config.name);
+		if (cmd_index === -1) {
+			disabled_commands.push(cmd.config.name);
+		} else {
+			disabled_commands.splice(cmd_index, 1);
+		}
+		message.channel.send(`✅ **| Successfully ${cmd_index === -1 ? "disabled" : "enabled"} \`${cmd.config.name}\` command.**`);
 	}
 	
 	if (message.content.includes("m.mugzone.net/chart/")) {
 		let cmd = client.commands.get("malodychart");
-		cmd.run(client, message, args)
+		cmd.run(client, message, args);
 	}
 	
 	const obj = {
@@ -102,6 +136,7 @@ module.exports.run = (client, message, maindb, alicedb) => {
 		alicedb: alicedb,
 		command: command,
 		current_map: current_map,
+		disabled_commands: disabled_commands,
 		command_cooldown: command_cooldown,
 		maintenance: maintenance,
 		maintenance_reason: maintenance_reason,
