e509037acf8adceec19da192ee318fa31387b92a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-08-19 11:53:08 +0700

    Try to fix birthday tracking

--- util/birthdaytrack.js
+++ util/birthdaytrack.js
@@ -1,3 +1,11 @@
+const { Client } = require("discord.js");
+const { Db } = require("mongodb");
+
+/**
+ * @param {Client} client 
+ * @param {Db} maindb 
+ * @param {Db} alicedb 
+ */
 module.exports.run = (client, maindb, alicedb) => {
     let d = new Date();
     let date = d.getUTCDate();
@@ -5,11 +13,13 @@ module.exports.run = (client, maindb, alicedb) => {
 
     let guild = client.guilds.cache.get('316545691545501706');
     let role = guild.roles.cache.get("695201338182860860");
-    if (!role) return;
+    if (!role) {
+        return;
+    }
 
-    let binddb = maindb.collection("userbind");
-    let birthday = alicedb.collection("birthday");
-    let points = alicedb.collection("playerpoints");
+    const binddb = maindb.collection("userbind");
+    const birthday = alicedb.collection("birthday");
+    const points = alicedb.collection("playerpoints");
 
     const query = {
         $and: [
@@ -19,7 +29,9 @@ module.exports.run = (client, maindb, alicedb) => {
     };
 
     birthday.find(query).toArray((err, res) => {
-        if (err) return console.log(err);
+        if (err) {
+            return console.log(err);
+        }
         let current_birthday = [];
 
         for (let timezone = -12; timezone < 13; ++timezone) {
@@ -31,7 +43,7 @@ module.exports.run = (client, maindb, alicedb) => {
             if (d.getUTCFullYear() % 4 === 0 && month === 1 && date === 29) {
                 date = 1;
                 month = 2;
-                d.setUTCMonth(month, date)
+                d.setUTCMonth(month, date);
             }
 
             const entries = res.filter((entry) => entry.timezone === timezone);
@@ -40,32 +52,44 @@ module.exports.run = (client, maindb, alicedb) => {
             month = d.getUTCMonth();
             for (let i = 0; i < entries.length; ++i) {
                 let entry = entries[i];
-                if (entry.date !== date || entry.month !== month) continue;
-                current_birthday.push(entry)
+                if (entry.date !== date || entry.month !== month) {
+                    continue;
+                }
+                current_birthday.push(entry);
             }
         }
         if (current_birthday.length > 0) console.log(`Found ${current_birthday.length} valid birthday entry`);
 
         role.members.forEach((member) => {
             let index = current_birthday.findIndex((entry) => entry.discordid === member.id);
-            if (index === -1) member.roles.remove(role, "Not birthday anymore").catch(console.error);
+            if (index === -1) {
+                member.roles.remove(role, "Not birthday anymore").catch(console.error);
+            }
         });
 
         current_birthday.forEach((entry) => {
             let user = guild.member(entry.discordid);
-            if (!user) return;
+            if (!user) {
+                return;
+            }
             console.log(user.user.username);
             let roles = user.roles;
             let birthday_role = roles.cache.get(role.id);
-            if (birthday_role) return;
+            if (birthday_role) {
+                return;
+            }
 
             roles.add(role, "Happy birthday!").catch(console.error);
-            if (user.user.bot) return;
+            if (user.user.bot) {
+                return;
+            }
             user.send("🎂 **| Hey, I want to wish you a happy birthday! Hopefully you have a happy day with your family, friends, and relatives. Please accept this gift of `1,000` Alice coins and a temporary birthday role from me.**").catch(console.error);
             console.log(`Added 1000 coins to ${user.user.username} as birthday prize`);
 
             points.findOne({discordid: entry.discordid}, (err, userres) => {
-                if (err) return console.log(err);
+                if (err) {
+                    return console.log(err);
+                }
                 if (userres) {
                     let coins = userres.alicecoins;
                     coins += 1000;
@@ -75,8 +99,10 @@ module.exports.run = (client, maindb, alicedb) => {
                         }
                     };
                     points.updateOne({discordid: entry.discordid}, updateVal, err => {
-                        if (err) return console.log(err)
-                    })
+                        if (err) {
+                            return console.log(err);
+                        }
+                    });
                 } else {
                     binddb.findOne({discordid: entry.discordid}, (err, ures) => {
                         if (err) return console.log(err);
@@ -93,13 +119,15 @@ module.exports.run = (client, maindb, alicedb) => {
                         };
 
                         points.insertOne(insertVal, err => {
-                            if (err) return console.log(err)
-                        })
-                    })
+                            if (err) {
+                                return console.log(err);
+                            }
+                        });
+                    });
                 }
-            })
-        })
-    })
+            });
+        });
+    });
 };
 
 module.exports.config = {
