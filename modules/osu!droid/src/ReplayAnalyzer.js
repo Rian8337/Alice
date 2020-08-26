8648fcbb10616d172f1d4f02f6763c3af54f36c6
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-08-26 14:52:33 +0700

    Add support for old replay version

--- modules/osu!droid/src/ReplayAnalyzer.js
+++ modules/osu!droid/src/ReplayAnalyzer.js
@@ -108,19 +108,19 @@ class ReplayAnalyzer {
                         .on('entry', async entry => {
                             const fileName = entry.path;
                             if (fileName === 'data') {
-                                resolve(await entry.buffer())
+                                resolve(await entry.buffer());
                             } else {
-                                entry.autodrain()
+                                entry.autodrain();
                             }
                         })
                         .on('error', e => {
-                            setTimeout(() => reject(e), 2000)
-                        })
+                            setTimeout(() => reject(e), 2000);
+                        });
                 })
                 .on('error', e => {
-                    reject(e)
-                })
-        })
+                    reject(e);
+                });
+        });
     }
 
     /**
@@ -130,114 +130,114 @@ class ReplayAnalyzer {
     _parseReplay() {
         // javaDeserialization can only somewhat parse some string field
         // the rest will be a buffer that we need to manually parse
-        const raw_object = javaDeserialization.parse(this.fixed_odr);
+        const rawObject = javaDeserialization.parse(this.fixed_odr);
 
-        let result_object;
-        try {
-            result_object = {
-                replay_version: raw_object[0].version,
-                folder_name: raw_object[1],
-                file_name: raw_object[2],
-                hash: raw_object[3],
-                time: Number(raw_object[4].readBigUInt64BE(0)),
-                hit300k: raw_object[4].readInt32BE(8),
-                hit300: raw_object[4].readInt32BE(12),
-                hit100k: raw_object[4].readInt32BE(16),
-                hit100: raw_object[4].readInt32BE(20),
-                hit50: raw_object[4].readInt32BE(24),
-                hit0: raw_object[4].readInt32BE(28),
-                score: raw_object[4].readInt32BE(32),
-                max_combo: raw_object[4].readInt32BE(36),
-                accuracy: raw_object[4].readFloatBE(40),
-                is_full_combo: raw_object[4][44],
-                player_name: raw_object[5],
-                raw_mods: raw_object[6].elements,
-                droid_mods: this._convertDroidMods(raw_object[6].elements),
-                converted_mods: this._convertMods(raw_object[6].elements),
-                cursor_movement: [],
-                hit_object_data: []
-            };
-        } catch (e) {
-            return this;
-        }
+        const resultObject = rawObject[0].version >= 3 ? {
+            replay_version: rawObject[0].version,
+            folder_name: rawObject[1],
+            file_name: rawObject[2],
+            hash: rawObject[3],
+            time: Number(rawObject[4].readBigUInt64BE(0)),
+            hit300k: rawObject[4].readInt32BE(8),
+            hit300: rawObject[4].readInt32BE(12),
+            hit100k: rawObject[4].readInt32BE(16),
+            hit100: rawObject[4].readInt32BE(20),
+            hit50: rawObject[4].readInt32BE(24),
+            hit0: rawObject[4].readInt32BE(28),
+            score: rawObject[4].readInt32BE(32),
+            max_combo: rawObject[4].readInt32BE(36),
+            accuracy: rawObject[4].readFloatBE(40),
+            is_full_combo: rawObject[4][44],
+            player_name: rawObject[5],
+            raw_mods: rawObject[6].elements,
+            droid_mods: this._convertDroidMods(rawObject[6].elements),
+            converted_mods: this._convertMods(rawObject[6].elements),
+            cursor_movement: [],
+            hit_object_data: []
+        } : {
+            replay_version: rawObject[0].version,
+            folder_name: rawObject[1],
+            file_name: rawObject[2],
+            hash: rawObject[3],
+            cursor_movement: [],
+            hit_object_data: []
+        };
 
-        let replay_data_buffer_array = [];
-        for (let i = 7; i < raw_object.length; i++) replay_data_buffer_array.push(raw_object[i]);
-    
-        //merge all buffer section into one for better control when parsing
-        let replay_data_buffer = Buffer.concat(replay_data_buffer_array);
-        let buffer_counter = 0;
+        const replayDataBufferArray = [];
+        for (let i = resultObject.replay_version >= 3 ? 7 : 4; i < rawObject.length; i++) replayDataBufferArray.push(rawObject[i]);
 
-        let size = replay_data_buffer.readInt32BE(buffer_counter);
-        buffer_counter += INT_LENGTH;
+        //merge all cursor movement and hit object data section into one for better control when parsing
+        const replayDataBuffer = Buffer.concat(replayDataBufferArray);
+        let bufferCounter = 0;
+
+        const size = replayDataBuffer.readInt32BE(bufferCounter);
+        bufferCounter += INT_LENGTH;
 
         //parse movement data
         for (let x = 0; x < size; x++) {
-            let move_size = replay_data_buffer.readInt32BE(buffer_counter);
-            buffer_counter += INT_LENGTH;
-            let move_array = {
-                size: move_size,
+            const moveSize = replayDataBuffer.readInt32BE(bufferCounter);
+            bufferCounter += INT_LENGTH;
+            const moveArray = {
+                size: moveSize,
                 time: [],
                 x: [],
                 y: [],
                 id: []
             };
-            for (let i = 0; i < move_size; i++) {
-                move_array.time[i] = replay_data_buffer.readInt32BE(buffer_counter);
-                buffer_counter += INT_LENGTH;
-                move_array.id[i] = move_array.time[i] & 3;
-                move_array.time[i] >>= 2;
-                if (move_array.id[i] !== CURSOR_ID_UP) {
-                    move_array.x[i] = replay_data_buffer.readInt16BE(buffer_counter);
-                    buffer_counter += SHORT_LENGTH;
-                    move_array.y[i] = replay_data_buffer.readInt16BE(buffer_counter);
-                    buffer_counter += SHORT_LENGTH
+            for (let i = 0; i < moveSize; i++) {
+                moveArray.time[i] = replayDataBuffer.readInt32BE(bufferCounter);
+                bufferCounter += INT_LENGTH;
+                moveArray.id[i] = moveArray.time[i] & 3;
+                moveArray.time[i] >>= 2;
+                if (moveArray.id[i] !== movementType.UP) {
+                    moveArray.x[i] = replayDataBuffer.readInt16BE(bufferCounter);
+                    bufferCounter += SHORT_LENGTH;
+                    moveArray.y[i] = replayDataBuffer.readInt16BE(bufferCounter);
+                    bufferCounter += SHORT_LENGTH;
                 }
                 else {
-                    move_array.x[i] = -1;
-                    move_array.y[i] = -1
+                    moveArray.x[i] = -1;
+                    moveArray.y[i] = -1;
                 }
             }
-            result_object.cursor_movement.push(new CursorData(move_array))
+            resultObject.cursor_movement.push(new CursorData(moveArray));
         }
 
-        let replay_object_length = replay_data_buffer.readInt32BE(buffer_counter);
-        buffer_counter += INT_LENGTH;
+        const replayObjectLength = replayDataBuffer.readInt32BE(bufferCounter);
+        bufferCounter += INT_LENGTH;
 
         //parse result data
-        for (let i = 0; i < replay_object_length; i++) {
-            let replay_object_data = {
+        for (let i = 0; i < replayObjectLength; i++) {
+            const replayObjectData = {
                 accuracy: 0,
                 tickset: [],
                 result: 0
             };
 
-            replay_object_data.accuracy = replay_data_buffer.readInt16BE(buffer_counter);
-            buffer_counter += SHORT_LENGTH;
-            let len = replay_data_buffer.readInt8(buffer_counter);
-            buffer_counter += BYTE_LENGTH;
+            replayObjectData.accuracy = replayDataBuffer.readInt16BE(bufferCounter);
+            bufferCounter += SHORT_LENGTH;
+            const len = replayDataBuffer.readInt8(bufferCounter);
+            bufferCounter += BYTE_LENGTH;
 
             if (len > 0) {
                 let bytes = [];
 
                 for (let j = 0; j < len; j++) {
-                    bytes.push(replay_data_buffer.readInt8(buffer_counter));
-                    buffer_counter += BYTE_LENGTH
+                    bytes.push(replayDataBuffer.readInt8(bufferCounter));
+                    bufferCounter += BYTE_LENGTH;
                 }
 
-                for (let j = 0; j < len * 8; j++) replay_object_data.tickset[j] = (bytes[len - j / 8 - 1] & 1 << (j % 8)) !== 0
+                for (let j = 0; j < len * 8; j++) replayObjectData.tickset[j] = (bytes[len - j / 8 - 1] & 1 << (j % 8)) !== 0;
             }
 
-            if (result_object.replay_version >= 1) {
-                replay_object_data.result = replay_data_buffer.readInt8(buffer_counter);
-                buffer_counter += BYTE_LENGTH
+            if (resultObject.replay_version >= 1) {
+                replayObjectData.result = replayDataBuffer.readInt8(bufferCounter);
+                bufferCounter += BYTE_LENGTH;
             }
 
-            result_object.hit_object_data.push(new ReplayObjectData(replay_object_data))
+            resultObject.hit_object_data.push(new ReplayObjectData(replayObjectData));
         }
-
-        this.is3Finger = result_object.cursor_movement[3].size / result_object.cursor_movement[1].size > 0.01;
-        this.data = new ReplayData(result_object)
+        this.data = new ReplayData(resultObject);
     }
 
     /**
@@ -264,11 +264,11 @@ class ReplayAnalyzer {
                 if (!replay_mods_bitmask.hasOwnProperty(property)) continue;
                 if (!mod.includes(property)) continue;
                 modbits |= replay_mods_bitmask[property];
-                break
+                break;
             }
         }
 
-        return mods.modbits_to_string(modbits)
+        return mods.modbits_to_string(modbits);
     }
 
     /**
@@ -287,7 +287,7 @@ class ReplayAnalyzer {
             "DOUBLETIME": "d",
             "HALFTIME": "t",
             "NIGHTCORE": "c"
-        }
+        };
 
         let mod_string = '';
         for (const mod of replay_mods) {
@@ -295,11 +295,11 @@ class ReplayAnalyzer {
                 if (!replay_mods_constants.hasOwnProperty(property)) continue;
                 if (!mod.includes(property)) continue;
                 mod_string += replay_mods_constants[property];
-                break
+                break;
             }
         } 
 
-        return mod_string
+        return mod_string;
     }
 }
 
