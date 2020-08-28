6079c91ca41d177902a307226edf57195d532060
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-08-28 20:58:12 +0700

    Move object types to constants

---
+++ modules/osu!droid/src/constants/objectTypes.js
@@ -0,0 +1,24 @@
+/**
+ * Bitmask constant of object types. This is needed as osu! uses bits to determine object types.
+ */
+const objectTypes = {
+    /**
+     * @type {number}
+     * @description The bitwise constant of circle.
+     */
+    circle: 1<<0,
+
+    /**
+     * @type {number}
+     * @description The bitwise constant of slider.
+     */
+    slider: 1<<1,
+
+    /**
+     * @type {number}
+     * @description The bitwise constant of spinner.
+     */
+    spinner: 1<<3
+};
+
+module.exports = objectTypes;
