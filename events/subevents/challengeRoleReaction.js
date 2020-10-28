107606065076b7d04dc71f92e7ec48b770db1996
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-10-28 20:39:19 +0700

    Attempt to fix role reaction

--- events/subevents/challengeRoleReaction.js
+++ events/subevents/challengeRoleReaction.js
@@ -1,19 +1,25 @@
+const { Client } = require('discord.js');
+
+/**
+ * @param {Client} client 
+ */
 module.exports.run = client => {
-    let guild = client.guilds.cache.get("316545691545501706");
-	let channel = guild.channels.cache.get("669221772083724318");
+    const guild = client.guilds.cache.get("316545691545501706");
+	const channel = guild.channels.cache.get("669221772083724318");
+	const role = guild.roles.cache.get("674918022116278282");
 	channel.messages.fetch("674626850164703232").then(message => {
 		message.react("✅").catch(console.error);
-		let collector = message.createReactionCollector((reaction, user) => reaction.emoji.name === "✅" && user.id !== client.user.id);
-		collector.on("collect", () => {
-			message.reactions.cache.find((r) => r.emoji.name === "✅").users.fetch({limit: 10}).then(collection => {
-				let user = guild.member(collection.find((u) => u.id !== client.user.id).id);
-				let role = guild.roles.cache.get("674918022116278282");
-				if (!user.roles.cache.has(role.id)) user.roles.add(role, "Automatic role assignment").catch(console.error);
-				else user.roles.remove(role, "Automatic role assignment").catch(console.error);
-				message.reactions.cache.forEach((reaction) => reaction.users.remove(user.id).catch(console.error))
-			})
-		})
-	}).catch(console.error)
+		const collector = message.createReactionCollector((reaction, user) => reaction.emoji.name === "✅" && user.id !== client.user.id);
+		collector.on("collect", (reaction, user) => {
+			const guildUser = guild.member(user);
+			if (guildUser.roles.cache.has(role.id)) {
+				guildUser.roles.add(role, "Automatic role assignment").catch(console.error);
+			} else {
+				guildUser.roles.remove(role, "Automatic role assignment").catch(console.error);
+			}
+			message.reactions.cache.forEach(reaction => reaction.users.remove(user.id).catch(console.error));
+		});
+	}).catch(console.error);
 };
 
 module.exports.config = {
