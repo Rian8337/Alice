79692320ba96e15cafdadfe7c2490da33a0c3a83
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-12-02 06:22:37 +0700

    Move punishment log to proper channel

--- cmd/Administrator/ban.js
+++ cmd/Administrator/ban.js
@@ -1,36 +1,81 @@
 const Discord = require('discord.js');
+const { Db } = require('mongodb');
 const config = require('../../config.json');
 
-module.exports.run = async (client, message, args) => {
-    if (message.channel instanceof Discord.DMChannel) return message.channel.send("This command is not available in DMs");
-    if (!message.isOwner && !message.member.hasPermission("ADMINISTRATOR", {checkAdmin: true, checkOwner: true})) return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an Owner!**");
+/**
+ * @param {Discord.Client} client 
+ * @param {Discord.Message} message 
+ * @param {string[]} args 
+ * @param {Db} maindb 
+ * @param {Db} alicedb 
+ */
+module.exports.run = async (client, message, args, maindb, alicedb) => {
+    if (message.channel instanceof Discord.DMChannel) {
+        return message.channel.send("This command is not available in DMs");
+    }
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
+        return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this. Please ask an admin!**");
+    }
 
-    let logchannel = message.guild.channels.cache.find((c) => c.name === config.management_channel);
-    if (!logchannel) return message.channel.send(`"❎ **| Hey, please create ${config.management_channel} first!**`);
-
-    let userid = args[0];
-    if (!userid) return message.channel.send("❎ **| Hey, please specify the correct user to ban!**");
-    userid = userid.replace('<@!','').replace('<@','').replace('>','');
-
-    if (isNaN(parseInt(userid))) return message.channel.send("❎ **| Hey, please specify the correct user to ban!**");
-    if (userid == message.author.id) return message.channel.send("❎ **| Why would you ban yourself?**");
-
-    let toban = await client.users.fetch(userid);
-    if (!toban) return message.channel.send("❎ **| I'm sorry, I cannot find the user!**");
-    if (toban.bot) return message.channel.send("❎ **| I'm sorry, you cannot ban bots!**");
-    let reason = args.slice(1).join(" ");
-    if (!reason) return message.channel.send("❎ **| Hey, please enter your ban reason!**");
-
-    message.guild.members.ban(toban, {reason: reason + ` (banned by ${message.author.username})`}).then(() => {
-        if (message.attachments.size > 0) {
-            let attachments = [];
-            message.attachments.forEach((attachment) => {
-                attachments.push(attachment.proxyURL);
-                if (attachments.length === message.attachments.size) logchannel.send({files: attachments})
-            });
+    const user = await message.guild.members.fetch(message.mentions.users.first() || args[0]).catch(console.error);
+    if (!user) {
+        return message.channel.send("❎ **| Hey, please specify the correct user to ban!**");
+    }
+    if (user.id === message.author.id) {
+        return message.channel.send("❎ **| Why would you ban yourself?**");
+    }
+    if (user.user.bot) {
+        return message.channel.send("❎ **| I'm sorry, you cannot ban bots!**");
+    }
+    const reason = args.slice(1).join(" ");
+    if (!reason) {
+        return message.channel.send("❎ **| Hey, please enter your ban reason!**");
+    }
+    
+    const channelDb = alicedb.collection("mutelogchannel");
+    channelDb.findOne({guildID: message.guild.id}, (err, res) => {
+        if (err) {
+            console.log(err);
+            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**");
         }
-        message.author.lastMessage.delete();
-    }).catch(() => message.channel.send("❎ **| I'm sorry, looks like the user is already banned or cannot be banned!**"))
+        if (!res) {
+            return message.channel.send("❎ **| I'm sorry, this server doesn't have a mute log configured!**");
+        }
+        const channel = message.guild.channels.resolve(res.channelID);
+        if (!channel) {
+            return message.channel.send(`❎ **| I'm sorry, please ask server managers to create a ban log channel first!**`);
+        }
+        if (!(channel instanceof Discord.TextChannel)) {
+            return message.channel.send("❎ **| Hey, ban log channel must be a text channel!**");
+        }
+
+        user.ban({reason: reason + ` (banned by ${message.author.username})`}).then(() => {
+            const footer = config.avatar_list;
+            const index = Math.floor(Math.random() * footer.length);
+
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+                .setFooter("Alice Synthesis Thirty", footer[index])
+                .setTimestamp(new Date())
+                .setColor(message.member.roles.color.hexColor)
+                .setThumbnail(toban.avatarURL({dynamic: true}))
+                .setTitle("Ban executed")
+                .addField("Banned user: " + toban.username, "User ID: " + userid)
+                .addField("=========================", "Reason:\n" + reason);
+
+            if (message.attachments.size > 0) {
+                const attachments = [];
+                for (const [, attachment] of message.attachments.entries()) {
+                    attachments.push(attachment.url);
+                }
+                channel.send({embed: embed, files: attachments});
+            } else {   
+                channel.send({embed: embed});
+            }
+
+            message.author.lastMessage.delete();
+        }).catch(() => message.channel.send("❎ **| I'm sorry, looks like the user is already banned or cannot be banned!**"));
+    });
 };
 
 module.exports.config = {
--- cmd/Staff/tempmute.js
+++ cmd/Staff/tempmute.js
@@ -6,10 +6,15 @@ function isEligible(member) {
     let res = 0;
     let eligibleRoleList = config.mute_perm; //mute_permission
     for (const id of eligibleRoleList) {
-        if (res === -1) break;
+        if (res === -1) {
+            break;
+        }
         if (member.roles.cache.has(id[0])) {
-            if (id[1] === -1) res = id[1];
-            else res = Math.max(res, id[1]);
+            if (id[1] === -1) {
+                res = id[1];
+            } else {
+                res = Math.max(res, id[1]);
+            }
         }
     }
     return res;
@@ -19,8 +24,10 @@ function isImmuned(member) {
     let res = 0;
     let immunedRoleList = config.mute_immune;
     for (const id of immunedRoleList) {
-        if (member.roles.cache.has(id)) res = 1;
-        break;
+        if (member.roles.cache.has(id)) {
+            res = 1;
+            break;
+        }
     }
     return res;
 }
@@ -41,101 +48,140 @@ function timeConvert(num) {
  * @param {Db} alicedb 
  */
 module.exports.run = async (client, message, args, maindb, alicedb) => {
-    if (message.channel.type !== "text") return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this.**");
+    if (message.channel.type !== "text") {
+        return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this.**");
+    }
     let timeLimit = isEligible(message.member);
-    if (message.isOwner || message.author.bot) timeLimit = -1;
-    if (!timeLimit) return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this.**");
+    if (message.isOwner || message.author.bot) {
+        timeLimit = -1;
+    }
+    if (!timeLimit) {
+        return message.channel.send("❎ **| I'm sorry, you don't have the permission to use this.**");
+    }
 
     const tomute = await message.guild.members.fetch(message.mentions.users.first() || args[0]).catch(console.error);
-    if (!tomute) return message.channel.send("❎ **| Hey, please enter a valid user to mute!**");
-    if (!message.author.bot && !message.isOwner && (isImmuned(tomute) || tomute.user.bot)) return message.channel.send("❎ **| I'm sorry, this user cannot be muted.**");
+    if (!tomute) {
+        return message.channel.send("❎ **| Hey, please enter a valid user to mute!**");
+    }
+    if (!message.author.bot && !message.isOwner && (isImmuned(tomute) || tomute.user.bot)) {
+        return message.channel.send("❎ **| I'm sorry, this user cannot be muted.**");
+    }
 
     let reason = args.slice(2).join(" ");
-    if (reason.length > 1800) return message.channel.send("❎ **| I'm sorry, your mute reason must be less than or equal to 1800 characters!**");
+    if (reason.length > 1800) {
+        return message.channel.send("❎ **| I'm sorry, your mute reason must be less than or equal to 1800 characters!**");
+    }
 
     let mutetime = args[1];
-    if (!mutetime) return message.channel.send("❎ **| Hey, at least tell me how long do I need to mute this user!**");
-    if (isNaN(mutetime)) return message.channel.send("❎ **| I'm sorry, the time limit is not valid. Only send number of seconds.**");
-    if (mutetime < 1) return message.channel.send("❎ **| I'm sorry, you can only mute for at least 1 second.**");
-    if (mutetime === Infinity) return message.channel.send("❎ **| To infinity and beyond! Seriously though, please enter a valid mute time! You can use `a!mute` (Moderator only) to permanently mute someone instead.**");
-    if (!message.author.bot && !message.isOwner && timeLimit !== -1 && timeLimit < mutetime) return message.channel.send("❎ **| I'm sorry, you don't have enough permission to mute a user for longer than " + timeLimit + "seconds.**");
+    if (!mutetime) {
+        return message.channel.send("❎ **| Hey, at least tell me how long do I need to mute this user!**");
+    }
+    if (isNaN(mutetime)) {
+        return message.channel.send("❎ **| I'm sorry, the time limit is not valid. Only send number of seconds.**");
+    }
+    if (mutetime < 1) {
+        return message.channel.send("❎ **| I'm sorry, you can only mute for at least 1 second.**");
+    }
+    if (mutetime === Infinity) {
+        return message.channel.send("❎ **| To infinity and beyond! Seriously though, please enter a valid mute time! You can use `a!mute` (Moderator only) to permanently mute someone instead.**");
+    }
+    if (!message.author.bot && !message.isOwner && timeLimit !== -1 && timeLimit < mutetime) {
+        return message.channel.send("❎ **| I'm sorry, you don't have enough permission to mute a user for longer than " + timeLimit + "seconds.**");
+    }
 
-    if (!reason) return message.channel.send("❎ **| Hey, can you give me your reason for muting?**");
+    if (!reason) {
+        return message.channel.send("❎ **| Hey, can you give me your reason for muting?**");
+    }
 
-    let channel = message.guild.channels.cache.find((c) => c.name === config.management_channel);
-    if (!channel) return message.channel.send(`❎ **| I'm sorry, please ask server managers to create a mute log channel first!**`);
+    const channelDb = alicedb.collection("mutelogchannel");
+    channelDb.findOne({guildID: message.guild.id}, async (err, res) => {
+        if (err) {
+            console.log(err);
+            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**");
+        }
+        if (!res) {
+            return message.channel.send("❎ **| I'm sorry, this server doesn't have a mute log configured!**");
+        }
+        const channel = message.guild.channels.resolve(res.channelID);
+        if (!channel) {
+            return message.channel.send(`❎ **| I'm sorry, please ask server managers to create a mute log channel first!**`);
+        }
+        if (!(channel instanceof Discord.TextChannel)) {
+            return message.channel.send("❎ **| Hey, ban log channel must be a text channel!**");
+        }
 
-    let muterole = message.guild.roles.cache.find((r) => r.name === 'elaina-muted');
-    //start of create role
-    if (!muterole) {
-        try {
-            muterole = await message.guild.roles.create({data: {name: "elaina-muted", color: "#000000", permissions:[]}, reason: "No mute role"});
+        let muterole = message.guild.roles.cache.find((r) => r.name === 'elaina-muted');
+        //start of create role
+        if (!muterole) {
+            try {
+                muterole = await message.guild.roles.create({data: {name: "elaina-muted", color: "#000000", permissions:[]}, reason: "No mute role"});
+                message.guild.channels.cache.forEach((channel) => {
+                    channel.updateOverwrite(muterole, {"SEND_MESSAGES": false, "ADD_REACTIONS": false}, "Disallow muted members to talk").catch(console.error);
+                });
+            } catch(e) {
+                console.log(e.stack);
+            }
+        } else {
             message.guild.channels.cache.forEach((channel) => {
                 channel.updateOverwrite(muterole, {"SEND_MESSAGES": false, "ADD_REACTIONS": false}, "Disallow muted members to talk").catch(console.error);
             });
-        } catch(e) {
-            console.log(e.stack);
         }
-    } else {
-        message.guild.channels.cache.forEach((channel) => {
-            channel.updateOverwrite(muterole, {"SEND_MESSAGES": false, "ADD_REACTIONS": false}, "Disallow muted members to talk").catch(console.error);
+        //end of create role
+
+        message.delete().catch(O_o=>{});
+
+        let string = `**${tomute} in ${message.channel} for ${timeConvert(mutetime)} (${mutetime} ${mutetime === 1 ? "second" : "seconds"})**\nUser ID: ${tomute.id}\n\n=========================\n\n**Reason**:\n${reason}`;
+
+        let footer = config.avatar_list;
+        const index = Math.floor(Math.random() * footer.length);
+
+        const muteembed = new Discord.MessageEmbed()
+            .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+            .setTitle("Mute executed")
+            .setColor("#000000")
+            .setTimestamp(new Date())
+            .setFooter("User ID: " + tomute.id, footer[index])
+            .setDescription(string);
+
+        try {
+            await tomute.send(`❗**| Hey, you were muted for \`${mutetime}\` seconds for \`${reason}\`. Sorry!**`, {embed: muteembed});
+        } catch (e) {
+            message.channel.send(`❗**| A user has been muted... but their DMs are locked. The user will be muted for ${mutetime} second(s).**`);
+        }
+
+        if (mutetime >= 21600) {
+            const loungedb = alicedb.collection("loungelock");
+            loungedb.findOne({discordid: tomute.id}, (err, res) => {
+                if (err) {
+                    console.log(err);
+                    return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**");
+                }
+                if (!res) {
+                    loungedb.insertOne({discordid: tomute.id}, err => {
+                        if (err) {
+                            console.log(err);
+                            return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**");
+                        }
+                        channel.send("✅ **| Successfully locked user from lounge.**");
+                    });
+                }
+            });
+        }
+
+        if (message.attachments.size > 0) {
+            muteembed.attachFiles([message.attachments.first()]);
+        }
+
+        channel.send({embed: muteembed}).then(msg => {
+            tomute.roles.add(muterole.id)
+                .catch(console.error);
+
+            setTimeout(() => {
+                tomute.roles.remove(muterole.id);
+                muteembed.setFooter("User ID: " + tomute.id + " | User unmuted", footer[index]);
+                msg.edit({embed: muteembed});
+            }, mutetime * 1000);
         });
-    }
-    //end of create role
-
-    message.delete().catch(O_o=>{});
-
-    let string = `**${tomute} in ${message.channel} for ${timeConvert(mutetime)} (${mutetime} ${mutetime === 1 ? "second" : "seconds"})**\nUser ID: ${tomute.id}\n\n=========================\n\n**Reason**:\n${reason}`;
-
-    let footer = config.avatar_list;
-    const index = Math.floor(Math.random() * footer.length);
-
-    let muteembed = new Discord.MessageEmbed()
-        .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-        .setTitle("Mute executed")
-        .setColor("#000000")
-        .setTimestamp(new Date())
-        .setFooter("User ID: " + tomute.id, footer[index])
-        .setDescription(string);
-
-    try {
-        await tomute.send(`❗**| Hey, you were muted for \`${mutetime}\` seconds for \`${reason}\`. Sorry!**`, {embed: muteembed});
-    } catch (e) {
-        message.channel.send(`❗**| A user has been muted... but their DMs are locked. The user will be muted for ${mutetime} second(s).**`);
-    }
-
-    if (mutetime >= 21600) {
-        const loungedb = alicedb.collection("loungelock");
-        loungedb.findOne({discordid: tomute.id}, (err, res) => {
-            if (err) {
-                console.log(err);
-                return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**");
-            }
-            if (!res) {
-                loungedb.insertOne({discordid: tomute.id}, err => {
-                    if (err) {
-                        console.log(err);
-                        return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**");
-                    }
-                    channel.send("✅ **| Successfully locked user from lounge.**");
-                });
-            }
-        });
-    }
-
-    if (message.attachments.size > 0) {
-        muteembed.attachFiles([message.attachments.first()]);
-    }
-
-    channel.send({embed: muteembed}).then(msg => {
-        tomute.roles.add(muterole.id)
-            .catch(console.error);
-
-        setTimeout(() => {
-            tomute.roles.remove(muterole.id);
-            muteembed.setFooter("User ID: " + tomute.id + " | User unmuted", footer[index]);
-            msg.edit({embed: muteembed});
-        }, mutetime * 1000);
     });
 };
 
