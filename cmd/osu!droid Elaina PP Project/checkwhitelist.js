d28f886b0621a11805ae3f689a7f6a22890b51e2
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-02-27 23:08:35 +0700

    Allow to take maps from cache

--- cmd/osu!droid Elaina PP Project/checkwhitelist.js
+++ cmd/osu!droid Elaina PP Project/checkwhitelist.js
@@ -7,22 +7,35 @@ const { Db } = require('mongodb');
  * @param {Discord.Message} message 
  * @param {string[]} args 
  * @param {Db} maindb 
+ * @param {Db} alicedb
+ * @param {[string, string]} current_map
  */
-module.exports.run = async (client, message, args, maindb) => {
-    const beatmapLink = args[0];
+module.exports.run = async (client, message, args, maindb, alicedb, current_map) => {
+    let beatmapID = args[0];
+    let hash;
 
-    if (!beatmapLink) {
-        return message.channel.send("❎ **| Hey, please enter a beatmap link!**");
+    if (!beatmapID) {
+        const channelEntry = current_map.find(c => c[0] === message.channel.id);
+        if (!channelEntry) {
+            return message.channel.send("❎ **| I'm sorry, there is no map being talked in the channel!**");
+        }
+        hash = channelEntry[1];
+    } else {
+        const a = beatmapLink.split("/");
+        beatmapID = parseInt(a[a.length - 1]);
+
+        if (isNaN(beatmapID)) {
+            return message.channel.send("❎ **| I'm sorry, that's not a valid beatmap link or ID!**");
+        }
     }
 
-    const a = beatmapLink.split("/");
-    const beatmapID = parseInt(a[a.length - 1]);
-
-    if (isNaN(beatmapID)) {
-        return message.channel.send("❎ **| I'm sorry, that's not a valid beatmap link or ID!**");
+    let mapinfo;
+    if (beatmapID) {
+        mapinfo = await osudroid.MapInfo.getInformation({beatmapID, file: false});
+    } else {
+        mapinfo = await osudroid.MapInfo.getInformation({hash, file: false});
     }
 
-    const mapinfo = await osudroid.MapInfo.getInformation({beatmapID, file: false});
 
     if (mapinfo.error) {
 		return message.channel.send("❎ **| I'm sorry, I cannot fetch beatmap info from osu! API! Perhaps it is down?**");
@@ -38,6 +51,9 @@ module.exports.run = async (client, message, args, maindb) => {
         return message.channel.send("❎ **| Hey, this beatmap doesn't need to be whitelisted!**");
     }
 
+    beatmapID = mapinfo.beatmapID;
+    hash = mapinfo.hash;
+
     const whitelistDb = maindb.collection("mapwhitelist");
 
     whitelistDb.findOne({mapid: beatmapID}, (err, res) => {
@@ -50,7 +66,7 @@ module.exports.run = async (client, message, args, maindb) => {
             return message.channel.send(`✅ **| \`${mapinfo.fullTitle}\` is not whitelisted.**`);
         }
 
-        if (res.hashid === mapinfo.hash) {
+        if (res.hashid === hash) {
             message.channel.send(`✅ **| \`${mapinfo.fullTitle}\` is whitelisted and updated.**`);
         } else {
             message.channel.send(`✅ **| \`${mapinfo.fullTitle}\` is whitelisted, but not updated.**`);
@@ -59,5 +75,9 @@ module.exports.run = async (client, message, args, maindb) => {
 };
 
 module.exports.config = {
-    name: "checkwhitelist"
+    name: "checkwhitelist",
+    description: "Checks if a beatmap is whitelisted.",
+    usage: "checkwhitelist [beatmap link/ID]",
+    detail: "`beatmap link/ID`: The ID or link of the beatmap",
+    permission: "None"
 };
