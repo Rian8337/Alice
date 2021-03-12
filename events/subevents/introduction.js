44e52a0e83a8d9f80df3a870734b2eda23e69e83
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-03-12 21:03:31 +0700

    Fix role comparison logic

--- events/subevents/introduction.js
+++ events/subevents/introduction.js
@@ -1,9 +1,15 @@
-module.exports.run = (oldMember, newMember) => {
+const { GuildMember } = require('discord.js');
+
+/**
+ * @param {GuildMember} oldMember 
+ * @param {GuildMember} newMember 
+ */
+ module.exports.run = (oldMember, newMember) => {
     if (oldMember.user.bot) {
 		return;
 	}
 	const general = oldMember.guild.channels.cache.get("316545691545501706");
-	if (!general || oldMember.roles.cache.has("353397345636974593") || oldMember.roles.cache.size === newMember.roles.cache.size) {
+	if (!general || oldMember.roles.cache.find(r => r.name === "Member") || !newMember.roles.cache.find(r => r.name === "Member")) {
 		return;
 	}
 	general.send(`Welcome to ${oldMember.guild.name}, <@${oldMember.id}>!`, {files: ["https://i.imgur.com/LLzteLz.jpg"]});
