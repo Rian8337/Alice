b417962653c2fdcad9aa09e4f1570b7b6cfac061
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-03-12 21:36:17 +0700

    Refactor and allow in DM

--- events/subevents/8ball.js
+++ events/subevents/8ball.js
@@ -1,5 +1,4 @@
 module.exports.run = (client, message, msgArray, alicedb) => {
-    if (message.channel.type === "dm") return message.channel.send("I do not want to respond in DMs!");
     let args = msgArray.slice(0);
     let cmd = client.utils.get("response");
     cmd.run(message, args, alicedb)
--- util/response.js
+++ util/response.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const cd = new Set();
 const config = require('../config.json');
 
 function responseFactor(message, msg, like, hate, badword) {
@@ -22,19 +21,18 @@ function responseFactor(message, msg, like, hate, badword) {
 }
 
 module.exports.run = (message, args, alicedb) => {
-    if (cd.has(message.author.id)) return message.channel.send("❎ **| Hey, calm down with the question! I need to rest too, you know.**");
     if (!args[0]) return;
     let filterdb = alicedb.collection("responsefilter");
     let askdb = alicedb.collection("askcount");
-    filterdb.find({name: "response"}).toArray((err, res) => {
+    filterdb.findOne({name: "response"}, (err, res) => {
         if (err) {
             console.log(err);
             return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
         }
-        let like = res[0].like;
-        let hate = res[0].hate;
-        let badword = res[0].badwords;
-        let response = res[0].response;
+        let like = res.like;
+        let hate = res.hate;
+        let badword = res.badwords;
+        let response = res.response;
         const index = Math.max(1, Math.floor(Math.random() * response.length));
         let answer = response[index];
         let msg = args.join(" ");
@@ -44,13 +42,13 @@ module.exports.run = (message, args, alicedb) => {
         if (factor === 2) answer = "N... No! I would never think of that...";
         if (factor === 3) answer = "Um... Uh...";
         if (factor === 4) answer = "Uh, I don't think I want to answer that.";
-        askdb.find({discordid: message.author.id}).toArray((err, askres) => {
+        askdb.findOne({discordid: message.author.id}, (err, askres) => {
             if (err) {
                 console.log(err);
                 return message.channel.send("❎ **| I'm sorry, I'm having trouble receiving response from database. Please try again!**")
             }
             let count = 0;
-            if (askres[0]) count = askres[0].count;
+            if (askres) count = askres.count;
             let footer = config.avatar_list;
             const footerindex = Math.floor(Math.random() * footer.length);
             let rolecheck;
@@ -67,7 +65,7 @@ module.exports.run = (message, args, alicedb) => {
                 .setDescription(`**Q**: ${msg}\n**A**: ${answer}`);
 
             message.channel.send({embed: embed}).catch(console.error);
-            if (askres[0]) {
+            if (askres) {
                 let updateVal = {
                     $set: {
                         count: count + 1
@@ -75,7 +73,6 @@ module.exports.run = (message, args, alicedb) => {
                 };
                 askdb.updateOne({discordid: message.author.id}, updateVal, err => {
                     if (err) return console.log(err);
-                    console.log("Ask data updated")
                 })
             }
             else {
@@ -85,11 +82,10 @@ module.exports.run = (message, args, alicedb) => {
                 };
                 askdb.insertOne(insertVal, err => {
                     if (err) return console.log(err);
-                    console.log("Ask data updated")
-                })
+                });
             }
-        })
-    })
+        });
+    });
 };
 
 module.exports.config = {
