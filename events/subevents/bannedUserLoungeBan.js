6277773be9ae4dc31fcc7aa3362b5b350a868f83
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-03-27 01:58:55 +0700

    Wait for 1 second before attempting to send notification

--- events/subevents/bannedUserLoungeBan.js
+++ events/subevents/bannedUserLoungeBan.js
@@ -7,63 +7,65 @@ const config = require('../../config.json');
  * @param {Discord.User} user 
  * @param {Db} alicedb 
  */
-module.exports.run = async (guild, user, alicedb) => {
-	const auditLogEntries = await guild.fetchAuditLogs({user: user, limit: 1, type: "MEMBER_BAN_ADD"}).catch(console.error);
-	if (!auditLogEntries) {
-		return;
-	}
+module.exports.run = (guild, user, alicedb) => {
+	setTimeout(async () => {
+		const auditLogEntries = await guild.fetchAuditLogs({user: user, limit: 1, type: "MEMBER_BAN_ADD"}).catch(console.error);
+		if (!auditLogEntries) {
+			return;
+		}
 
-	const auditLog = auditLogEntries.entries.first();
-	const executor = auditLog.executor;
-	const reason = auditLog.reason ?? "Not specified.";
-	
-	const footer = config.avatar_list;
-	const index = Math.floor(Math.random() * footer.length);
-	const embed = new Discord.MessageEmbed()
-		.setTitle("Ban executed")
-		.setThumbnail(executor.avatarURL({dynamic: true}))
-		.setFooter("Alice Synthesis Thirty", footer[index])
-		.setTimestamp(new Date())
-		.addField(`Banned user: ${user.tag}`, `User ID: ${user.id}`)
-		.addField("=========================", `Reason: ${reason}`);
+		const auditLog = auditLogEntries.entries.first();
+		const executor = auditLog.executor;
+		const reason = auditLog.reason ?? "Not specified.";
 		
-	const loungeDb = alicedb.collection("loungelock");
-	const channelDb = alicedb.collection("punishmentconfig");
+		const footer = config.avatar_list;
+		const index = Math.floor(Math.random() * footer.length);
+		const embed = new Discord.MessageEmbed()
+			.setTitle("Ban executed")
+			.setThumbnail(executor.avatarURL({dynamic: true}))
+			.setFooter("Alice Synthesis Thirty", footer[index])
+			.setTimestamp(new Date())
+			.addField(`Banned user: ${user.tag}`, `User ID: ${user.id}`)
+			.addField("=========================", `Reason: ${reason}`);
+			
+		const loungeDb = alicedb.collection("loungelock");
+		const channelDb = alicedb.collection("punishmentconfig");
 
-	channelDb.findOne({guildID: guild.id}, (err, log) => {
-		if (err) {
-			console.log(err);
-			console.log("Unable to retrieve lounge ban data");
-		}
-		if (!log) {
-			return;
-		}
-		const channel = guild.channels.resolve(log.logChannel);
-		if (!channel) {
-			return;
-		}
-		channel.send(embed);
-		if (guild.id !== "316545691545501706") {
-			return;
-		}
-		loungeDb.findOne({discordid: user.id}, (err, res) => {
+		channelDb.findOne({guildID: guild.id}, (err, log) => {
 			if (err) {
 				console.log(err);
 				console.log("Unable to retrieve lounge ban data");
 			}
-			if (res) {
+			if (!log) {
 				return;
 			}
-	
-			loungeDb.insertOne({discordid: user.id}, err => {
+			const channel = guild.channels.resolve(log.logChannel);
+			if (!channel) {
+				return;
+			}
+			channel.send(embed);
+			if (guild.id !== "316545691545501706") {
+				return;
+			}
+			loungeDb.findOne({discordid: user.id}, (err, res) => {
 				if (err) {
 					console.log(err);
-					console.log("Unable to insert ban data");
+					console.log("Unable to retrieve lounge ban data");
 				}
-				channel.send("✅ **| Successfully locked user from lounge.**");
+				if (res) {
+					return;
+				}
+		
+				loungeDb.insertOne({discordid: user.id}, err => {
+					if (err) {
+						console.log(err);
+						console.log("Unable to insert ban data");
+					}
+					channel.send("✅ **| Successfully locked user from lounge.**");
+				});
 			});
 		});
-	});
+	}, 1000);
 };
 
 module.exports.config = {
--- events/subevents/unbanNotification.js
+++ events/subevents/unbanNotification.js
@@ -8,36 +8,38 @@ const config = require('../../config.json');
  * @param {Db} alicedb 
  */
 module.exports.run = (guild, user, alicedb) => {
-    alicedb.collection("punishmentconfig").findOne({guildID: guild.id}, async (err, res) => {
-        if (err || !res) {
-            return;
-        }
-        const channel = guild.channels.resolve(res.logChannel);
-        if (!(channel instanceof Discord.TextChannel)) {
-            return;
-        }
-
-        const auditLogEntries = await guild.fetchAuditLogs({user: user, limit: 1, type: "MEMBER_BAN_REMOVE"}).catch(console.error);
-        if (!auditLogEntries) {
-            return;
-        }
-        const auditLog = auditLogEntries.entries.first();
-        const executor = auditLog.executor;
-        const reason = auditLog.reason ?? "Not specified.";
-        
-        const footer = config.avatar_list;
-        const index = Math.floor(Math.random() * footer.length);
-        const embed = new Discord.MessageEmbed()
-            .setTitle("Unban executed")
-            .setAuthor(executor.tag, executor.avatarURL({dynamic: true}))
-            .setThumbnail(user.avatarURL({dynamic: true}))
-            .setFooter("Alice Synthesis Thirty", footer[index])
-            .setTimestamp(new Date())
-            .addField(`Unbanned user: ${user.tag}`, `User ID: ${user.id}`)
-            .addField("=========================", `Reason: ${reason}`);
+    setTimeout(() => {
+        alicedb.collection("punishmentconfig").findOne({guildID: guild.id}, async (err, res) => {
+            if (err || !res) {
+                return;
+            }
+            const channel = guild.channels.resolve(res.logChannel);
+            if (!(channel instanceof Discord.TextChannel)) {
+                return;
+            }
+    
+            const auditLogEntries = await guild.fetchAuditLogs({user: user, limit: 1, type: "MEMBER_BAN_REMOVE"}).catch(console.error);
+            if (!auditLogEntries) {
+                return;
+            }
+            const auditLog = auditLogEntries.entries.first();
+            const executor = auditLog.executor;
+            const reason = auditLog.reason ?? "Not specified.";
             
-        channel.send({embed: embed});
-    });
+            const footer = config.avatar_list;
+            const index = Math.floor(Math.random() * footer.length);
+            const embed = new Discord.MessageEmbed()
+                .setTitle("Unban executed")
+                .setAuthor(executor.tag, executor.avatarURL({dynamic: true}))
+                .setThumbnail(user.avatarURL({dynamic: true}))
+                .setFooter("Alice Synthesis Thirty", footer[index])
+                .setTimestamp(new Date())
+                .addField(`Unbanned user: ${user.tag}`, `User ID: ${user.id}`)
+                .addField("=========================", `Reason: ${reason}`);
+                
+            channel.send({embed: embed});
+        });
+    }, 1000);
 };
 
 module.exports.config = {
