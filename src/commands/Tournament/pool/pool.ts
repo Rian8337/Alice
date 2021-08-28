84bef1819f12805e0fee3d2d076a48967c1fb3f1
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-08-28 11:06:02 +0700

    Add pool command

---
+++ src/commands/Tournament/pool/pool.ts
@@ -0,0 +1,83 @@
+import { DatabaseManager } from "@alice-database/DatabaseManager";
+import { TournamentMapLengthInfo } from "@alice-database/utils/aliceDb/TournamentMapLengthInfo";
+import { TournamentMappool } from "@alice-database/utils/elainaDb/TournamentMappool";
+import { CommandArgumentType } from "@alice-enums/core/CommandArgumentType";
+import { CommandCategory } from "@alice-enums/core/CommandCategory";
+import { Command } from "@alice-interfaces/core/Command";
+import { OnButtonPageChange } from "@alice-interfaces/utils/OnButtonPageChange";
+import { EmbedCreator } from "@alice-utils/creators/EmbedCreator";
+import { MessageButtonCreator } from "@alice-utils/creators/MessageButtonCreator";
+import { MessageCreator } from "@alice-utils/creators/MessageCreator";
+import { DateTimeFormatHelper } from "@alice-utils/helpers/DateTimeFormatHelper";
+import { GuildMember, MessageEmbed } from "discord.js";
+import { poolStrings } from "./poolStrings";
+
+export const run: Command["run"] = async (_, interaction) => {
+    const id: string = interaction.options.getString("id", true);
+
+    const mappoolMainData: TournamentMappool | null =
+        await DatabaseManager.elainaDb.collections.tournamentMappool.getFromId(id);
+
+    if (!mappoolMainData) {
+        return interaction.editReply({
+            content: MessageCreator.createReject(poolStrings.poolNotFound)
+        });
+    }
+
+    const mappoolDurationData: TournamentMapLengthInfo | null =
+        await DatabaseManager.aliceDb.collections.tournamentMapLengthInfo.getFromId(id);
+
+    if (!mappoolDurationData) {
+        return interaction.editReply({
+            content: MessageCreator.createReject(poolStrings.poolNotFound)
+        });
+    }
+
+    const embed: MessageEmbed = EmbedCreator.createNormalEmbed(
+        { color: (<GuildMember | null> interaction.member)?.displayColor }
+    );
+
+    const onPageChange: OnButtonPageChange = async (_, page) => {
+        for (let i = 5 * (page - 1); i < Math.min(mappoolMainData.map.length, 5 + 5 * (page - 1)); ++i) {
+            embed.addField(mappoolMainData.map[i][1], `**Length**: ${DateTimeFormatHelper.secondsToDHMS(parseInt(<string >mappoolDurationData.map[i][1]))}`);
+        }
+    };
+
+    MessageButtonCreator.createLimitedButtonBasedPaging(
+        interaction,
+        { embeds: [ embed ] },
+        [interaction.user.id],
+        mappoolMainData.map,
+        5,
+        1,
+        60,
+        onPageChange
+    );
+};
+
+export const category: Command["category"] = CommandCategory.TOURNAMENT;
+
+export const config: Command["config"] = {
+    name: "pool",
+    description: "Retrieves a list of beatmaps from a registered tournament mappool.",
+    options: [
+        {
+            name: "id",
+            required: true,
+            type: CommandArgumentType.STRING,
+            description: "The ID of the mappool."
+        }
+    ],
+    example: [
+        {
+            command: "pool t11sf",
+            description: "will retrieve a list of beatmaps from tournament mappool \"t11sf\"."
+        },
+        {
+            command: "pool t8gf",
+            description: "will retrieve a list of beatmaps from tournament mappool \"t8gf\"."
+        }
+    ],
+    permissions: [],
+    scope: "ALL"
+};
---
+++ src/commands/Tournament/pool/poolStrings.ts
@@ -0,0 +1,6 @@
+/**
+ * Strings for the `pool` command.
+ */
+export enum poolStrings {
+    poolNotFound = "I'm sorry, I cannot find the mappool that you are looking for!"
+};
