cee552e72b5e03bbc2d772da996b547f8ef4ab4f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-08-28 21:51:14 +0700

    fix(mute): remove user mention

--- src/commands/Staff/mute/muteStrings.ts
+++ src/commands/Staff/mute/muteStrings.ts
@@ -5,6 +5,6 @@ export enum muteStrings {
     userToMuteNotFound = "Hey, please enter a valid user to mute!",
     infiniteMuteError = "To infinity and beyond! Seriously though, please enter a valid mute duration! You can use \`/mute\` to permanently mute someone instead.",
     muteFailed = "I'm sorry, I cannot mute the user: \`%s\`.",
-    temporaryMuteSuccess = "Successfully muted %s for %s.",
-    permanentMuteSuccess = "Successfully muted %s permanently."
+    temporaryMuteSuccess = "Successfully muted the user for %s.",
+    permanentMuteSuccess = "Successfully muted the user permanently."
 };
