95b4ded0346d7a5ae54035fa71198621fd6aadfd
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-08-29 00:45:18 +0700

    refactor(HelperFunctions): remove unused methods

--- src/utils/helpers/HelperFunctions.ts
+++ src/utils/helpers/HelperFunctions.ts
@@ -13,40 +13,4 @@ export abstract class HelperFunctions {
     static sleep(duration: number): Promise<void> {
         return new Promise(resolve => setTimeout(resolve, duration * 1000));
     }
-
-    /**
-     * Gets the fields of a class, stripping
-     * any methods away.
-     * 
-     * @param o The class to get the fields from.
-     * @returns An object with each class fields mapped respectively.
-     */
-    static getClassFields(o: any): any {
-        const names: string[] = Object.getOwnPropertyNames(o);
-        const values: any[] = Object.values(o);
-
-        const final: any = {};
-
-        for (let i = 0; i < names.length; ++i) {
-            Object.defineProperty(final, names[i], { value: values[i], configurable: true, enumerable: true, writable: true })
-        }
-
-        return final;
-    }
-
-    /**
-     * Converts a CSS RGB string to hex code.
-     * 
-     * @param rgb The CSS RGB string.
-     * @returns The hex code from the RGB string.
-     */
-    static rgbToHex(rgb: string): string {
-        const colors: string[] = rgb.split("(")[1].split(")")[0].split(",").map(v => {
-            const hex: string = parseInt(v).toString(16);
-
-            return hex.length === 1 ? `0${hex}` : hex;
-        });
-
-        return "#" + colors.join("");
-    }
 }
