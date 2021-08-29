0dd7d731e84197566ac08d2fff875a9bba422d9c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-08-29 18:54:39 +0700

    refactor(guildBanRemove): apply API changes

--- src/events/guildBanRemove/utils/notifyUnban.ts
+++ src/events/guildBanRemove/utils/notifyUnban.ts
@@ -1,17 +1,17 @@
-import { Guild, GuildChannel, MessageEmbed, TextChannel, User } from "discord.js";
+import { GuildBan, GuildChannel, MessageEmbed, TextChannel } from "discord.js";
 import { EventUtil } from "@alice-interfaces/core/EventUtil";
 import { EmbedCreator } from "@alice-utils/creators/EmbedCreator";
 import { GuildPunishmentConfig } from "@alice-database/utils/aliceDb/GuildPunishmentConfig";
 import { DatabaseManager } from "@alice-database/DatabaseManager";
 
-export const run: EventUtil["run"] = async (_, guild: Guild, user: User) => {
-    const guildConfig: GuildPunishmentConfig | null = await DatabaseManager.aliceDb.collections.guildPunishmentConfig.getGuildConfig(guild.id);
+export const run: EventUtil["run"] = async (_, guildBan: GuildBan) => {
+    const guildConfig: GuildPunishmentConfig | null = await DatabaseManager.aliceDb.collections.guildPunishmentConfig.getGuildConfig(guildBan.guild);
 
     if (!guildConfig) {
         return;
     }
 
-    const logChannel: GuildChannel | null = guildConfig.getGuildLogChannel(guild);
+    const logChannel: GuildChannel | null = guildConfig.getGuildLogChannel(guildBan.guild);
 
     if (!(logChannel instanceof TextChannel)) {
         return;
@@ -22,8 +22,9 @@ export const run: EventUtil["run"] = async (_, guild: Guild, user: User) => {
     );
 
     embed.setTitle("Unban Executed")
-        .setThumbnail(<string> user.avatarURL({ dynamic: true }))
-        .addField(`Unbanned user: ${user.tag}`, `User ID: ${user.id}`);
+        .setThumbnail(<string> guildBan.user.avatarURL({ dynamic: true }))
+        .addField(`Unbanned user: ${guildBan.user.tag}`, `User ID: ${guildBan.user.id}`)
+        .addField("=========================", `Reason: ${guildBan.reason ?? "Not specified."}`);
 
     logChannel.send({ embeds: [embed] });
 };
