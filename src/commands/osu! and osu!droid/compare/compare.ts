486f0567bcefbc5a0fe0ee023ac2f138a5a80d91
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-08-29 12:17:52 +0700

    enhancement(compare): move to command level

--- src/commands/osu! and osu!droid/compare/compare.ts
+++ src/commands/osu! and osu!droid/compare/compare.ts
@@ -1,12 +1,24 @@
+import { Constants } from "@alice-core/Constants";
+import { DatabaseManager } from "@alice-database/DatabaseManager";
+import { UserBindCollectionManager } from "@alice-database/managers/elainaDb/UserBindCollectionManager";
+import { UserBind } from "@alice-database/utils/elainaDb/UserBind";
 import { CommandArgumentType } from "@alice-enums/core/CommandArgumentType";
 import { CommandCategory } from "@alice-enums/core/CommandCategory";
 import { Command } from "@alice-interfaces/core/Command";
+import { EmbedCreator } from "@alice-utils/creators/EmbedCreator";
 import { MessageCreator } from "@alice-utils/creators/MessageCreator";
-import { CommandHelper } from "@alice-utils/helpers/CommandHelper";
 import { BeatmapManager } from "@alice-utils/managers/BeatmapManager";
+import { GuildMember, MessageEmbed, Snowflake } from "discord.js";
+import { Player, Score } from "osu-droid";
 import { compareStrings } from "./compareStrings";
 
 export const run: Command["run"] = async (_, interaction) => {
+    if (interaction.options.data.length > 1) {
+        return interaction.editReply({
+            content: MessageCreator.createReject(compareStrings.tooManyOptions)
+        });
+    }
+
     const cachedBeatmapHash: string | undefined = BeatmapManager.getChannelLatestBeatmap(interaction.channel!.id);
 
     if (!cachedBeatmapHash) {
@@ -15,75 +27,112 @@ export const run: Command["run"] = async (_, interaction) => {
         });
     }
 
-    CommandHelper.runSubcommandFromInteraction(interaction);
+    const discordid: Snowflake | undefined = interaction.options.getUser("user")?.id;
+    let uid: number | undefined | null = interaction.options.getInteger("uid");
+    const username: string | null = interaction.options.getString("username");
+
+    const dbManager: UserBindCollectionManager = DatabaseManager.elainaDb.collections.userBind;
+
+    let bindInfo: UserBind | null | undefined;
+
+    let player: Player | undefined;
+
+    switch (true) {
+        case !!uid:
+            player = await Player.getInformation({ uid: uid! });
+            uid = player.uid;
+            break;
+        case !!username:
+            player = await Player.getInformation({ username: username! });
+            uid = player.uid;
+            break;
+        case !!discordid:
+            bindInfo = await dbManager.getFromUser(discordid!);
+
+            if (!bindInfo) {
+                return interaction.editReply({
+                    content: MessageCreator.createReject(Constants.userNotBindedReject)
+                });
+            }
+
+            player = await Player.getInformation({ uid: bindInfo.uid });
+            break;
+        default:
+            // If no arguments are specified, default to self
+            bindInfo = await dbManager.getFromUser(interaction.user);
+
+            if (!bindInfo) {
+                return interaction.editReply({
+                    content: MessageCreator.createReject(Constants.selfNotBindedReject)
+                });
+            }
+
+            player = await Player.getInformation({ uid: bindInfo.uid });
+    }
+
+    if (!player.username) {
+        return interaction.editReply({
+            content: MessageCreator.createReject(compareStrings.playerNotFound)
+        });
+    }
+
+    const score: Score = await Score.getFromHash({ uid: player.uid, hash: cachedBeatmapHash });
+
+    if (!score.title) {
+        return interaction.editReply({
+            content: MessageCreator.createReject(compareStrings.scoreNotFound)
+        });
+    }
+
+    const embed: MessageEmbed = await EmbedCreator.createRecentPlayEmbed(
+        score,
+        player.avatarURL,
+        (<GuildMember | null> interaction.member)?.displayColor
+    );
+
+    interaction.editReply({
+        content: MessageCreator.createAccept(compareStrings.comparePlayDisplay, player.username),
+        embeds: [ embed ]
+    });
 };
 
 export const category: Command["category"] = CommandCategory.OSU;
 
 export const config: Command["config"] = {
     name: "compare",
-    description: "Compares a player's score among others.",
+    description: "Compares yours or a player's score among others.",
     options: [
         {
             name: "user",
-            type: CommandArgumentType.SUB_COMMAND,
-            description: "Compares a Discord user's score among others.",
-            options: [
-                {
-                    name: "user",
-                    required: true,
-                    type: CommandArgumentType.USER,
-                    description: "The Discord user to compare."
-                }
-            ]
+            type: CommandArgumentType.USER,
+            description: "The Discord user to compare."
         },
         {
             name: "uid",
-            type: CommandArgumentType.SUB_COMMAND,
-            description: "Compares an osu!droid account's score among others from its uid.",
-            options: [
-                {
-                    name: "uid",
-                    required: true,
-                    type: CommandArgumentType.INTEGER,
-                    description: "The uid of the osu!droid account."
-                }
-            ]
+            type: CommandArgumentType.INTEGER,
+            description: "The uid of the player."
         },
         {
             name: "username",
-            type: CommandArgumentType.SUB_COMMAND,
-            description: "Compares an osu!droid account's score among others from its username.",
-            options: [
-                {
-                    name: "username",
-                    required: true,
-                    type: CommandArgumentType.STRING,
-                    description: "The username of the osu!droid account."
-                }
-            ]
-        },
-        {
-            name: "self",
-            type: CommandArgumentType.SUB_COMMAND,
-            description: "Compares your score among others."
+            type: CommandArgumentType.STRING,
+            description: "The username of the player."
         }
     ],
     example: [
         {
-            command: "compare self",
+            command: "compare",
             description: "will compare your score among others."
         },
         {
-            command: "compare uid 51076",
+            command: "compare uid:51076",
             description: "will compare the score of an osu!droid account with uid 51076."
         },
         {
-            command: "compare username NeroYuki",
+            command: "compare username:NeroYuki",
             description: "will compare the score of an osu!droid account with username NeroYuki."
         },
         {
-            command: "compare user @Rian8337#0001",
+            command: "compare user:@Rian8337#0001",
             description: "will compare the score of Rian8337."
         }
     ],
--- src/commands/osu! and osu!droid/compare/compareStrings.ts
+++ src/commands/osu! and osu!droid/compare/compareStrings.ts
@@ -2,6 +2,7 @@
  * Strings for the `compare` command.
  */
 export enum compareStrings {
+    tooManyOptions = "I'm sorry, you can only either specify a uid, user, or username! You cannot mix them!",
     noCachedBeatmap = "I'm sorry, there is no beatmap being talked in the channel!",
     playerNotFound = "I'm sorry, I cannot find the player that you are looking for!",
     scoreNotFound = "I'm sorry, this player has not submitted any scores in the beatmap!",
