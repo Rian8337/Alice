f19637b3d1a6b8f1867402cfa5309a79e5702377
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-08-31 21:08:26 +0700

    enhancement(DatabaseUtilityConstructor): make data optional

--- src/types/database/DatabaseUtilityConstructor.ts
+++ src/types/database/DatabaseUtilityConstructor.ts
@@ -1,3 +1,3 @@
 import { Bot } from "@alice-core/Bot";
 
-export type DatabaseUtilityConstructor<T, C> = new (client: Bot, data: T) => C;
+export type DatabaseUtilityConstructor<T, C> = new (client: Bot, data?: T) => C;
