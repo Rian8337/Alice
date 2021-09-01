c3bbf7d918d9d409638d59579f36d3144d97b94a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-09-01 07:52:03 +0700

    enhancement: add automatic role assignment

---
+++ src/events/ready/utils/mudaeRoleReaction.ts
@@ -0,0 +1,20 @@
+import { EventUtil } from "@alice-interfaces/core/EventUtil";
+import { RoleReactionCreator } from "@alice-utils/creators/RoleReactionCreator";
+import { Guild, TextChannel, Role, Message } from "discord.js";
+
+export const run: EventUtil["run"] = async (client) => {
+    const guild: Guild = await client.guilds.fetch("635532651029332000");
+
+    const channel: TextChannel = <TextChannel> await guild.channels.fetch("640165306404438026");
+
+    const role: Role = (await guild.roles.fetch("674918022116278282"))!;
+
+    const message: Message = await channel.messages.fetch("657597328772956160");
+
+    RoleReactionCreator.createReactionBasedRoleAssignment(
+        guild,
+        message,
+        role,
+        "639481086425956382"
+    );
+};
---
+++ src/utils/creators/RoleReactionCreator.ts
@@ -0,0 +1,38 @@
+import { Symbols } from "@alice-enums/utils/Symbols";
+import { Manager } from "@alice-utils/base/Manager";
+import { Guild, GuildMember, Message, ReactionCollector, Role, Snowflake } from "discord.js";
+
+/**
+ * A utility to create reaction-based role assignment.
+ */
+export abstract class RoleReactionCreator extends Manager {
+    /**
+     * Creates a reaction-based role assignment.
+     * 
+     * @param guild The guild to create the reaction-based role assignment in.
+     * @param message The message to create the reaction-based role assignment on.
+     * @param role The role to assign.
+     * @param emoji The emoji to use in the reaction-based role assignment.
+     */
+    static async createReactionBasedRoleAssignment(guild: Guild, message: Message, role: Role, emoji: Symbols | Snowflake): Promise<ReactionCollector> {
+        await message.react(emoji);
+
+        const collector: ReactionCollector = message.createReactionCollector({
+            filter: (reaction, user) => (reaction.emoji.name === emoji || reaction.emoji.id === emoji) && user.id !== this.client.user?.id
+        });
+
+        collector.on("collect", async (_, user) => {
+            const guildMember: GuildMember = await guild.members.fetch(user);
+
+            if (guildMember.roles.cache.has(role.id)) {
+                guildMember.roles.remove(role, "Automatic role assignment");
+            } else {
+                guildMember.roles.add(role, "Automatic role assignment");
+            }
+
+            message.reactions.cache.forEach(reaction => reaction.users.remove(user.id));
+        });
+
+        return collector;
+    }
+}
