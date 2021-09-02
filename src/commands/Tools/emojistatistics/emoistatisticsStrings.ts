ff502ab88fb8ad8f1c0649708267a7520c6381ea
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-09-02 07:44:00 +0700

    enhancement: add emojistatistics command

---
+++ src/commands/Tools/emojistatistics/emoistatisticsStrings.ts
@@ -0,0 +1,7 @@
+/**
+ * Strings for the `emojistatistics` command.
+ */
+export enum emojistatisticsStrings {
+    serverHasNoData = "I'm sorry, this server has no emoji usage statistics!",
+    noValidEmojis = "I'm sorry, I couldn't find any valid emojis!"
+};
---
+++ src/commands/Tools/emojistatistics/emojistatistics.ts
@@ -0,0 +1,122 @@
+import { DatabaseManager } from "@alice-database/DatabaseManager";
+import { EmojiStatistics } from "@alice-database/utils/aliceDb/EmojiStatistics";
+import { CommandArgumentType } from "@alice-enums/core/CommandArgumentType";
+import { CommandCategory } from "@alice-enums/core/CommandCategory";
+import { Command } from "@alice-interfaces/core/Command";
+import { OnButtonPageChange } from "@alice-interfaces/utils/OnButtonPageChange";
+import { EmbedCreator } from "@alice-utils/creators/EmbedCreator";
+import { MessageButtonCreator } from "@alice-utils/creators/MessageButtonCreator";
+import { MessageCreator } from "@alice-utils/creators/MessageCreator";
+import { GuildEmoji, GuildMember, MessageEmbed } from "discord.js";
+import { emojistatisticsStrings } from "./emoistatisticsStrings";
+
+export const run: Command["run"] = async (_, interaction) => {    
+    const stats: EmojiStatistics | null = await DatabaseManager.aliceDb.collections.emojiStatistics.getGuildStatistics(interaction.guild!);
+
+    if (!stats) {
+        return interaction.editReply({
+            content: MessageCreator.createReject(emojistatisticsStrings.serverHasNoData)
+        });
+    }
+
+    const validEmojis: {
+        emoji: GuildEmoji;
+        count: number;
+        averagePerMonth: number;
+    }[] = [];
+
+    const currentDate: Date = new Date();
+
+    for await (const emoji of stats.emojiStats.values()) {
+        const actualEmoji: GuildEmoji | void = await interaction.guild!.emojis.fetch(emoji.id).catch(() => {});
+
+        if (!actualEmoji) {
+            continue;
+        }
+
+        const dateCreation: Date = actualEmoji.createdAt;
+        const months: number = Math.max(1, (currentDate.getUTCFullYear() - dateCreation.getUTCFullYear()) * 12 + currentDate.getUTCMonth() - dateCreation.getUTCMonth());
+
+        validEmojis.push({
+            emoji: actualEmoji,
+            count: emoji.count,
+            averagePerMonth: Math.round(emoji.count / months)
+        });
+    }
+
+    if (validEmojis.length === 0) {
+        return interaction.editReply({
+            content: MessageCreator.createReject(emojistatisticsStrings.noValidEmojis)
+        });
+    }
+
+    const sortOption: string = interaction.options.getString("sortoption") ?? "overall";
+
+    validEmojis.sort((a, b) => {
+        return sortOption === "overall" ?
+            b.count - a.count :
+            b.averagePerMonth - a.averagePerMonth;
+    });
+
+    const embed: MessageEmbed = EmbedCreator.createNormalEmbed(
+        { color: (<GuildMember> interaction.member).displayColor }
+    );
+
+    embed.setAuthor(`Emoji Statistics for ${interaction.guild!.name}`, interaction.guild!.iconURL({ dynamic: true })!)
+        .setDescription(`**Sort Mode**: ${sortOption === "overall" ? "Overall" : "Average per month"} usage`);
+
+    const onPageChange: OnButtonPageChange = async (_, page) => {
+        for (let i = 5 * (page - 1); i < Math.min(validEmojis.length, 5 + 5 * (page - 1)); ++i) {
+            embed.addField(
+                `${i + 1}. ${validEmojis[i].emoji.name}`,
+                `**Emoji**: ${validEmojis[i].emoji}\n` +
+                `**Date Creation**: ${validEmojis[i].emoji.createdAt.toUTCString()}\n` +
+                `**Overall Usage**: ${validEmojis[i].count.toLocaleString()}\n` +
+                `**Average Per Month Usage**: ${validEmojis[i].averagePerMonth.toLocaleString()}`
+            );
+        }
+    };
+
+    MessageButtonCreator.createLimitedButtonBasedPaging(
+        interaction,
+        { embeds: [ embed ] },
+        [interaction.user.id],
+        validEmojis,
+        5,
+        1,
+        120,
+        onPageChange
+    );
+};
+
+export const category: Command["category"] = CommandCategory.TOOLS;
+
+export const config: Command["config"] = {
+    name: "emojistatistics",
+    description: "Views statistics for emoji usage of the server.",
+    options: [
+        {
+            name: "sortoption",
+            type: CommandArgumentType.STRING,
+            description: "Whether to sort based on overall or average per month usage. Defaults to overall.",
+            choices: [
+                {
+                    name: "Average per month",
+                    value: "average"
+                },
+                {
+                    name: "Overall",
+                    value: "overall"
+                }
+            ]
+        }
+    ],
+    example: [
+        {
+            command: "emojistatistics average",
+            description: "will show emoji usage statistics in the server sorted by average usage per month."
+        }
+    ],
+    permissions: [],
+    scope: "GUILD_CHANNEL"
+};
