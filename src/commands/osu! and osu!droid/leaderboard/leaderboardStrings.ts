a81aef37e1da08c11cbedc87aadaf0ba8eb7b93c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-09-06 20:50:07 +0700

    refactor(leaderboardStrings): remove unused enum

--- src/commands/osu! and osu!droid/leaderboard/leaderboardStrings.ts
+++ src/commands/osu! and osu!droid/leaderboard/leaderboardStrings.ts
@@ -4,7 +4,6 @@
 export enum leaderboardStrings {
     invalidPage = "Hey, please enter a valid page!",
     dppLeaderboardClanNotFound = "I'm sorry, I cannot find the clan!",
-    pageNumberTooBig = "Nah, we don't have that much player. \:P",
-    noBeatmapFound = "Hey, please enter a valid beatmap link!",
+    noBeatmapFound = "Hey, please enter a valid beatmap link or ID!",
     beatmapHasNoScores = "I'm sorry, this beatmap doesn't have any scores submitted!"
 };
