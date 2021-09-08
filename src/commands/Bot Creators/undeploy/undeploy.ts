3dbe1af752ccb6bcd174896d07b43dc3b7298fc8
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-09-08 23:47:25 +0700

    fix(undeploy): try a different method of deleting a command

--- src/commands/Bot Creators/undeploy/undeploy.ts
+++ src/commands/Bot Creators/undeploy/undeploy.ts
@@ -2,18 +2,28 @@ import { CommandArgumentType } from "@alice-enums/core/CommandArgumentType";
 import { CommandCategory } from "@alice-enums/core/CommandCategory";
 import { Command } from "@alice-interfaces/core/Command";
 import { MessageCreator } from "@alice-utils/creators/MessageCreator";
+import { ApplicationCommand, ApplicationCommandManager } from "discord.js";
 import { undeployStrings } from "./undeployStrings";
 
 export const run: Command["run"] = async (client, interaction) => {
     const commandName: string = interaction.options.getString("command", true);
 
-    if (interaction.options.getBoolean("debug")) {
-        await interaction.guild!.commands.fetch();
+    const isDebug: boolean = interaction.options.getBoolean("debug") ?? false;
 
-        await interaction.guild!.commands.cache.find(v => v.name === commandName)?.delete();
-    } else {
-        await client.application!.commands.cache.find(v => v.name === commandName)?.delete();
+    if (isDebug) {
+        await interaction.guild!.commands.fetch();
     }
+
+    const command: ApplicationCommand | undefined =
+        (isDebug ? interaction.guild! : client.application!).commands.cache.find(v => v.name === commandName);
+
+    if (!command) {
+        return interaction.editReply({
+            content: MessageCreator.createReject(undeployStrings.commandNotFound)
+        });
+    }
+
+    await (isDebug ? interaction.guild! : client.application!).commands.delete(command);
 
     interaction.editReply({
         content: MessageCreator.createAccept(undeployStrings.commandUndeploySuccessful, commandName)
