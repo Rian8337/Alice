7eeac3644a50c5b7053f061eb1dd1ba710b33701
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-09-15 21:38:29 +0700

    fix(daily-manualsubmit): port daily-submit response data

--- src/commands/osu! and osu!droid/daily/subcommands/daily-manualsubmit.ts
+++ src/commands/osu! and osu!droid/daily/subcommands/daily-manualsubmit.ts
@@ -3,6 +3,7 @@ import { DatabaseManager } from "@alice-database/DatabaseManager";
 import { PlayerInfoCollectionManager } from "@alice-database/managers/aliceDb/PlayerInfoCollectionManager";
 import { Challenge } from "@alice-database/utils/aliceDb/Challenge";
 import { PlayerInfo } from "@alice-database/utils/aliceDb/PlayerInfo";
+import { Clan } from "@alice-database/utils/elainaDb/Clan";
 import { UserBind } from "@alice-database/utils/elainaDb/UserBind";
 import { ChallengeCompletionData } from "@alice-interfaces/challenge/ChallengeCompletionData";
 import { OperationResult } from "@alice-interfaces/core/OperationResult";
@@ -14,7 +15,7 @@ import { PermissionHelper } from "@alice-utils/helpers/PermissionHelper";
 import { StringHelper } from "@alice-utils/helpers/StringHelper";
 import { RESTManager } from "@alice-utils/managers/RESTManager";
 import { Collection, GuildEmoji, GuildMember, MessageEmbed, Snowflake } from "discord.js";
-import { ReplayAnalyzer, ReplayData, RequestResponse } from "osu-droid";
+import { Player, ReplayAnalyzer, ReplayData, RequestResponse } from "osu-droid";
 import { dailyStrings } from "../dailyStrings";
 
 export const run: Subcommand["run"] = async (client, interaction) => {
@@ -97,9 +98,6 @@ export const run: Subcommand["run"] = async (client, interaction) => {
     const playerInfo: PlayerInfo | null =
         await playerInfoDbManager.getFromUser(interaction.user);
 
-    // Keep track of how many levels are gained
-    let levelGained: number = bonusLevel;
-
     // Ask for verification from staff
     const staffMembers: Collection<Snowflake, GuildMember> = await PermissionHelper.getMainGuildStaffMembers(client);
 
@@ -118,7 +116,7 @@ export const run: Subcommand["run"] = async (client, interaction) => {
             `**Hit good (100)**: ${data.accuracy.n100} (${data.hit100k} katu)\n` +
             `**Hit meh (50)**: ${data.accuracy.n50}\n` +
             `**Misses**: ${data.accuracy.nmiss}\n\n` +
-            `**Bonus Level Reached**: ${levelGained}`
+            `**Bonus Level Reached**: ${bonusLevel}`
         );
 
     const confirmation: boolean = await MessageButtonCreator.createConfirmation(
@@ -135,39 +133,44 @@ export const run: Subcommand["run"] = async (client, interaction) => {
         return;
     }
 
+    // Keep track of how many points are gained
+    let pointsGained: number = bonusLevel * 2 + challenge.points;
+
     if (playerInfo) {
 
         const challengeData: ChallengeCompletionData | undefined = playerInfo.challenges.get(challenge.challengeid);
 
         if (challengeData) {
-            levelGained -= challengeData.highestLevel;
+            // Player has completed challenge. Subtract the challenge's original points
+            // and difference from highest challenge level
+            pointsGained -= challenge.points + (challengeData.highestLevel - Math.max(0, bonusLevel - challengeData.highestLevel)) * 2;
 
-            challengeData.highestLevel = bonusLevel;
+            challengeData.highestLevel = Math.max(bonusLevel, challengeData.highestLevel);
         } else {
             playerInfo.challenges.set(challenge.challengeid, { id: challenge.challengeid, highestLevel: bonusLevel });
         }
 
-        if (levelGained) {
-            await playerInfoDbManager.update(
-                { discordid: interaction.user.id },
-                {
-                    $set: {
-                        challenges: [...playerInfo.challenges.values()]
-                    },
-                    $inc: {
-                        alicecoins: (levelGained + (challengeData ? challenge.points : 0)) * 2,
-                        points: levelGained + (challengeData ? challenge.points : 0)
-                    }
+        await playerInfoDbManager.update(
+            { discordid: interaction.user.id },
+            {
+                $set: {
+                    challenges: [...playerInfo.challenges.values()]
+                },
+                $inc: {
+                    alicecoins: pointsGained * 2,
+                    points: pointsGained
                 }
-            );
-        }
+            }
+        );
     } else {
+        const player: Player = await Player.getInformation({ uid: bindInfo.uid });
+
         await playerInfoDbManager.insert({
-            uid: bindInfo.uid,
-            username: bindInfo.username,
+            uid: player.uid,
+            username: player.username,
             discordid: interaction.user.id,
-            points: levelGained,
-            alicecoins: levelGained * 2,
+            points: pointsGained,
+            alicecoins: pointsGained * 2,
             challenges: [
                 {
                     id: challenge.challengeid,
@@ -177,21 +180,25 @@ export const run: Subcommand["run"] = async (client, interaction) => {
         });
     }
 
-    const coin: GuildEmoji = client.emojis.resolve(Constants.aliceCoinEmote)!;
+    if (bindInfo.clan) {
+        const clan: Clan = (await DatabaseManager.elainaDb.collections.clan.getFromName(bindInfo.clan))!;
+
+        clan.incrementPower(pointsGained);
+
+        await clan.updateClan();
+    }
 
     interaction.editReply({
         content: MessageCreator.createAccept(
             dailyStrings.challengeCompleted,
             challenge.challengeid,
-            bonusLevel.toString(),
-            levelGained.toString(),
-            levelGained === 1 ? "" : "s",
-            coin.toString(),
-            (levelGained * 2).toString(),
-            (playerInfo?.points ?? 0 + levelGained).toString(),
-            (playerInfo?.points ?? 0 + levelGained) === 1 ? "" : "s",
-            coin.toString(),
-            (playerInfo?.alicecoins ?? 0 + levelGained * 2).toString()
+            bonusLevel.toLocaleString(),
+            pointsGained.toLocaleString(),
+            pointsGained === 1 ? "" : "s",
+            (pointsGained * 2).toLocaleString(),
+            ((playerInfo?.points ?? 0) + pointsGained).toLocaleString(),
+            ((playerInfo?.points ?? 0) + pointsGained) === 1 ? "" : "s",
+            ((playerInfo?.alicecoins ?? 0) + pointsGained * 2).toLocaleString()
         )
     });
 };
