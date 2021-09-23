15d6c78ad9112f28e55dfdc979314b990309b546
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-09-23 07:33:44 +0700

    Increase FL small distance nerf
    Because FL circle radius in droid is bigger.

--- modules/osu-droid/src/difficulty/skills/DroidFlashlight.ts
+++ modules/osu-droid/src/difficulty/skills/DroidFlashlight.ts
@@ -26,21 +26,23 @@ export class DroidFlashlight extends DroidSkill {
         for (let i = 0; i < this.previous.length; ++i) {
             const previous: DifficultyHitObject = this.previous[i];
 
-            if (!(previous.object instanceof Spinner)) {
-                const jumpDistance: number = current.object.stackedPosition.subtract(previous.object.endPosition).length;
-
-                cumulativeStrainTime += previous.strainTime;
-
-                // We want to nerf objects that can be easily seen within the Flashlight circle radius.
-                if (i === 0) {
-                    smallDistNerf = Math.min(1, jumpDistance / 75);
-                }
-
-                // We also want to nerf stacks so that only the first object of the stack is accounted for.
-                const stackNerf: number = Math.min(1, previous.jumpDistance / scalingFactor / 25);
-
-                result += Math.pow(0.8, i) * stackNerf * scalingFactor * jumpDistance / cumulativeStrainTime;
+            if (previous.object instanceof Spinner) {
+                continue;
             }
+
+            const jumpDistance: number = current.object.stackedPosition.subtract(previous.object.endPosition).length;
+
+            cumulativeStrainTime += previous.strainTime;
+
+            // We want to nerf objects that can be easily seen within the Flashlight circle radius.
+            if (i === 0) {
+                smallDistNerf = Math.min(1, jumpDistance / 125);
+            }
+
+            // We also want to nerf stacks so that only the first object of the stack is accounted for.
+            const stackNerf: number = Math.min(1, previous.jumpDistance / scalingFactor / 25);
+
+            result += Math.pow(0.8, i) * stackNerf * scalingFactor * jumpDistance / cumulativeStrainTime;
         }
 
         return Math.pow(smallDistNerf * result, 2) * this.skillMultiplier;
