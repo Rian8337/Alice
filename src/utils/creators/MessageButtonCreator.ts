ddfaff1c4c772e3f3c7d6c2404c3a5c6601b962e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-09-26 16:26:39 +0700

    refactor(MessageButtonCreator): remove unused import

--- src/utils/creators/MessageButtonCreator.ts
+++ src/utils/creators/MessageButtonCreator.ts
@@ -1,7 +1,6 @@
 import { Symbols } from "@alice-enums/utils/Symbols";
 import { OnButtonPageChange } from "@alice-interfaces/utils/OnButtonPageChange";
 import { InteractionCollectorCreator } from "@alice-utils/base/InteractionCollectorCreator";
-import { MessageHelper } from "@alice-utils/helpers/MessageHelper";
 import { CommandInteraction, InteractionCollector, Message, MessageActionRow, MessageButton, MessageComponentInteraction, MessageEmbed, MessageOptions, Snowflake } from "discord.js";
 import { MessageCreator } from "./MessageCreator";
 
