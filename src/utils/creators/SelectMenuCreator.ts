7758b9fae8340896711804474dc832fab053ab8f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-09-26 16:26:25 +0700

    feat(SelectMenuCreator): support paging

--- src/utils/creators/SelectMenuCreator.ts
+++ src/utils/creators/SelectMenuCreator.ts
@@ -1,6 +1,8 @@
 import { CommandInteraction, InteractionCollector, Message, MessageActionRow, MessageComponentInteraction, MessageSelectMenu, MessageSelectOptionData, SelectMenuInteraction, Snowflake } from "discord.js";
 import { InteractionCollectorCreator } from "@alice-utils/base/InteractionCollectorCreator";
 import { MessageCreator } from "./MessageCreator";
+import { OnButtonPageChange } from "@alice-interfaces/utils/OnButtonPageChange";
+import { MessageButtonCreator } from "./MessageButtonCreator";
 
 /**
  * A utility to create message select menus.
@@ -21,20 +23,36 @@ export abstract class SelectMenuCreator extends InteractionCollectorCreator {
             const selectMenu: MessageSelectMenu = new MessageSelectMenu()
                 .setCustomId("whatever")
                 .setPlaceholder(placeholder)
-                .addOptions(choices);
+                .addOptions(choices.slice(0, 25));
 
             const component: MessageActionRow = new MessageActionRow()
                 .addComponents(selectMenu);
 
-            const message: Message = <Message> await interaction.editReply({
-                content: MessageCreator.createWarn("A select menu has appeared..."),
-                components: [component]
-            });
+            const onPageChange: OnButtonPageChange = async (_, page) => {
+                selectMenu.spliceOptions(0, selectMenu.options.length)
+                    .addOptions(choices.slice(25 * (page - 1), 25 + 25 * (page - 1)));
+            };
+
+            const message: Message = await MessageButtonCreator.createLimitedButtonBasedPaging(
+                interaction,
+                {
+                    content: MessageCreator.createWarn("A select menu has appeared..."),
+                    components: [ component ]
+                },
+                [interaction.user.id],
+                choices,
+                25,
+                1,
+                duration,
+                onPageChange
+            );
 
             const collector: InteractionCollector<MessageComponentInteraction> =
                 this.createSelectMenuCollector(message, users, duration);
 
-            collector.on("collect", () => {
+            collector.on("collect", async i => {
+                await i.deferUpdate();
+
                 collector.stop();
             });
 
@@ -50,7 +68,7 @@ export abstract class SelectMenuCreator extends InteractionCollectorCreator {
                             content: MessageCreator.createReject("Timed out."),
                             components: []
                         });
-    
+
                         setTimeout(() => {
                             interaction.deleteReply();
                         }, 5 * 1000);
