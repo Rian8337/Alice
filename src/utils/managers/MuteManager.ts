070ba4429ce3c854481cecd35660ecf160257650
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-10-04 07:45:17 +0700

    feat(MuteManager): add function to check if a user is muted

--- src/utils/managers/MuteManager.ts
+++ src/utils/managers/MuteManager.ts
@@ -118,7 +118,7 @@ export abstract class MuteManager extends PunishmentManager {
         });
 
         // Check if the user is already muted via role
-        if (member.roles.cache.has(muteRole.id)) {
+        if (this.isUserMuted(member)) {
             return this.createOperationResult(false, "The user is already muted");
         }
 
@@ -221,8 +221,8 @@ export abstract class MuteManager extends PunishmentManager {
             return this.createOperationResult(false, "Unable to find mute role in the server");
         }
 
-        if (!member.roles.cache.has(muteRole.id)) {
-            return this.createOperationResult(false, "The user is already muted");
+        if (!this.isUserMuted(member)) {
+            return this.createOperationResult(false, "The user is not muted");
         }
 
         const logChannel: GuildChannel | ThreadChannel | null = member.guild.channels.resolve(muteInformation.logChannelID);
@@ -383,6 +383,22 @@ export abstract class MuteManager extends PunishmentManager {
     }
 
     /**
+     * Checks if a guild member is muted.
+     * 
+     * @param member The member.
+     * @returns Whether the guild member is muted.
+     */
+    static isUserMuted(member: GuildMember): boolean {
+        const muteRole: Role | undefined = this.getGuildMuteRole(member.guild);
+
+        if (!muteRole) {
+            return false;
+        }
+
+        return member.roles.cache.has(muteRole.id);
+    }
+
+    /**
      * Continues a mute.
      * 
      * @param client The instance of the bot.
@@ -396,7 +412,7 @@ export abstract class MuteManager extends PunishmentManager {
             return;
         }
 
-        if (!member.roles.cache.has(muteRole.id)) {
+        if (!this.isUserMuted(member)) {
             await member.roles.add(muteRole, "Mute continuation");
         }
 
