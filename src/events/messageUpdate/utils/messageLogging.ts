b1abd40042bc2d5b120b5324f5dd7590c710a2ed
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-10-06 19:37:12 +0700

    fix(messageLogging): fetch before accessing properties

--- src/events/messageUpdate/utils/messageLogging.ts
+++ src/events/messageUpdate/utils/messageLogging.ts
@@ -3,9 +3,13 @@ import { EventUtil } from "@alice-interfaces/core/EventUtil";
 import { EmbedCreator } from "@alice-utils/creators/EmbedCreator";
 
 export const run: EventUtil["run"] = async (_, oldMessage: Message, newMessage: Message) => {
-    if (newMessage.author?.bot) {
+    await oldMessage.fetch();
+
+    if (oldMessage.author.bot) {
         return;
     }
+
+    await newMessage.fetch();
 
     const logChannel: GuildChannel | ThreadChannel | undefined = newMessage.guild?.channels.cache.find(c => c.id === "643770576238018570");
 
