0c8f0e7010fa0abd4ab0d09515280c2e7b5631b4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-10-18 21:56:33 +0700

    feat: use memory optimization as recommended by Heroku

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     name: "Bot",
     script: "./dist/main.js",
+    args: "--trace-warnings --optimize_for_size",
     instances: 1,
     env: {
       NODE_ENV: "development"
