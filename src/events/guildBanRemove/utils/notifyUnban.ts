528bff2ca5198d85b83bf5d064b5bb3101938682
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-11-04 18:41:52 +0700

    feat(notifyUnban): add "unbanner"

--- src/events/guildBanRemove/utils/notifyUnban.ts
+++ src/events/guildBanRemove/utils/notifyUnban.ts
@@ -1,10 +1,26 @@
-import { GuildBan, GuildChannel, MessageEmbed, TextChannel } from "discord.js";
+import { GuildAuditLogs, GuildAuditLogsEntry, GuildBan, GuildChannel, MessageEmbed, TextChannel, User } from "discord.js";
 import { EventUtil } from "@alice-interfaces/core/EventUtil";
 import { EmbedCreator } from "@alice-utils/creators/EmbedCreator";
 import { GuildPunishmentConfig } from "@alice-database/utils/aliceDb/GuildPunishmentConfig";
 import { DatabaseManager } from "@alice-database/DatabaseManager";
 
 export const run: EventUtil["run"] = async (_, guildBan: GuildBan) => {
+    const auditLogEntries: GuildAuditLogs = await guildBan.guild.fetchAuditLogs(
+        { limit: 1, type: "MEMBER_BAN_REMOVE" }
+    );
+
+    const unbanLog: GuildAuditLogsEntry | undefined = auditLogEntries.entries.first();
+
+    if (!unbanLog) {
+        return;
+    }
+
+    const user: User = <User> unbanLog.target;
+
+    if (user.id !== guildBan.user.id) {
+        return;
+    }
+
     const guildConfig: GuildPunishmentConfig | null = await DatabaseManager.aliceDb.collections.guildPunishmentConfig.getGuildConfig(guildBan.guild);
 
     if (!guildConfig) {
@@ -25,6 +41,10 @@ export const run: EventUtil["run"] = async (_, guildBan: GuildBan) => {
         .setThumbnail(<string> guildBan.user.avatarURL({ dynamic: true }))
         .addField(`Unbanned user: ${guildBan.user.tag}`, `User ID: ${guildBan.user.id}`)
         .addField("=========================", `Reason: ${guildBan.reason ?? "Not specified."}`);
+
+    if (unbanLog.executor) {
+        embed.setAuthor(unbanLog.executor.tag, unbanLog.executor.avatarURL({ dynamic: true })!);
+    }
 
     logChannel.send({ embeds: [embed] });
 };
