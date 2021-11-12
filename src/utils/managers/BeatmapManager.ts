3eb0bb38cc944e778e24c52253f1406e1829c34e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-11-12 12:50:17 +0700

    feat(BeatmapManager): update difficulty color spectrum in line with new changes

--- src/utils/managers/BeatmapManager.ts
+++ src/utils/managers/BeatmapManager.ts
@@ -17,9 +17,9 @@ export abstract class BeatmapManager extends Manager {
      */
     private static readonly difficultyColorSpectrum: d3.ScaleLinear<string, string, never> =
         d3.scaleLinear<string>()
-            .domain([1.5, 2, 2.5, 3.25, 4.5, 6, 7, 8])
+            .domain([0.1, 1.25, 2, 2.5, 3.3, 4.2, 4.9, 5.8, 6.7, 7.7, 9])
             .clamp(true)
-            .range(["#4FC0FF", "#4FFFD5", "#7CFF4F", "#F6F05C", "#FF8068", "#FF3C71", "#6563DE", "#18158E"])
+            .range(["#4290FB", "#4FC0FF", "#4FFFD5", "#7CFF4F|", "#F6F05C", "#FF8068", "#FF4E6F", "#C645B8", "#6563DE", "#18158E", "#000000"])
             .interpolate(d3.interpolateRgb.gamma(2.2));
 
     /**
@@ -246,7 +246,14 @@ export abstract class BeatmapManager extends Manager {
      * @returns The color in hex code.
      */
     static getBeatmapDifficultyColor(rating: number): string {
-        return rating >= 8 ? "#000000" : HelperFunctions.rgbToHex(this.difficultyColorSpectrum(rating));
+        switch (true) {
+            case rating < 0.1:
+                return "#AAAAAA";
+            case rating >= 9:
+                return "#000000";
+            default:
+                return HelperFunctions.rgbToHex(this.difficultyColorSpectrum(rating));
+        }
     }
 
     /**
