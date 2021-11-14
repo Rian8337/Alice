c9ae033b709daed494804f610c129f5d0bcb6546
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-11-14 13:25:55 +0700

    feat: add translated languages

---
+++ src/enums/events/VerifyLanguage.ts
@@ -0,0 +1,11 @@
+/**
+ * Available languages for verification.
+ */
+export enum VerifyLanguage {
+    chinese_simplified = "Chinese (Simplified)",
+    chinese_traditional = "Chinese (Traditional)",
+    korean = "Korean",
+    russian = "Russian",
+    thai = "Thai",
+    vietnamese = "Vietnamese"
+};
