50f8ea7cadb26443b729b80f7b7bf755b4f8fdf6
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-12-09 15:48:26 +0700

    feat: add temporary auto recalculation utility

---
+++ src/events/ready/utils/automateRecalculation.ts
@@ -0,0 +1,61 @@
+import { recalcStrings } from "@alice-commands/osu!droid Elaina PP Project and Ranked Score Project/recalc/recalcStrings";
+import { Config } from "@alice-core/Config";
+import { DatabaseManager } from "@alice-database/DatabaseManager";
+import { UserBindCollectionManager } from "@alice-database/managers/elainaDb/UserBindCollectionManager";
+import { UserBind } from "@alice-database/utils/elainaDb/UserBind";
+import { EventUtil } from "@alice-interfaces/core/EventUtil";
+import { MessageCreator } from "@alice-utils/creators/MessageCreator";
+import { Message, TextChannel } from "discord.js";
+
+export const run: EventUtil["run"] = async (client) => {
+    const channel: TextChannel = <TextChannel>(
+        await client.channels.fetch("546135349533868072")
+    );
+
+    const message: Message = await channel.messages.fetch("905354500380954644");
+
+    const dbManager: UserBindCollectionManager =
+        DatabaseManager.elainaDb.collections.userBind;
+
+    let calculatedCount: number =
+        await dbManager.getRecalcCalculatedPlayerCount();
+
+    const uncalculatedCount: number =
+        await dbManager.getRecalcUncalculatedPlayerCount();
+
+    const total: number = calculatedCount + uncalculatedCount;
+
+    let player: UserBind | undefined;
+
+    while ((player = (await dbManager.getRecalcUnscannedPlayers(1)).first())) {
+        client.logger.info(`Now calculating ID ${player.discordid}`);
+
+        await player.recalculateAllScores(false, true);
+
+        client.logger.info(`${++calculatedCount} players recalculated`);
+
+        await message.edit({
+            content: MessageCreator.createWarn(
+                recalcStrings.fullRecalcTrackProgress,
+                calculatedCount.toLocaleString(),
+                total.toLocaleString(),
+                ((calculatedCount * 100) / total).toFixed(2)
+            ),
+        });
+    }
+
+    await dbManager.update({}, { $unset: { dppScanComplete: "" } });
+
+    channel.send({
+        content: MessageCreator.createAccept(
+            recalcStrings.fullRecalcSuccess,
+            `<@${Config.botOwners[1]}>`
+        ),
+    });
+};
+
+export const config: EventUtil["config"] = {
+    description: "Responsible for resuming ongoing calculation.",
+    togglePermissions: ["BOT_OWNER"],
+    toggleScope: ["GLOBAL"],
+};
