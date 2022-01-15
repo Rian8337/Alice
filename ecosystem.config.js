6d4122e00e372cd65a2a4a749590f3d65e2c7b12
Rian <52914632+Rian8337@users.noreply.github.com>
2022-01-15 20:52:25 +0700

    chore: revert to not use ts-node

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-    apps: [
-        {
-            name: "Bot",
-            script: "./src/main.ts",
-            args: "--trace-warnings -r ts-node/register",
-            instances: 1,
-            env: {
-                NODE_ENV: "development",
-            },
-            env_production: {
-                NODE_ENV: "production",
-            },
-        },
-    ],
+  apps : [{
+    name: "Bot",
+    script: "./dist/main.js",
+    args: "--trace-warnings",
+    instances: 1,
+    env: {
+      NODE_ENV: "development"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }]
 };
