82938931996e137cc3b30cf8e9862cecfb5627cc
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-02-21 20:38:25 +0700

    feat: remove ban and unban command

--- src/commands/Staff/unban/unbanStrings.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Strings for the `unban` command.
- */
-export enum unbanStrings {
-    userToUnbanNotFound = "Hey, please specify a correct user to unban!",
-    selfUnbanError = "How can you unban yourself if you're banned in the server?",
-    unbanFailed = "I'm sorry, I cannot unban the user: `%s`.",
-    unbanSuccessful = "Successfully unbanned the user.",
-}
