173eb6ed1db6da196e55d09814dfd96338b3a3ac
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-30 17:37:11 +0700

    fix(multiplayer-close): retrieve `channelId`

--- src/interactions/commands/osu! and osu!droid/multiplayer/subcommands/multiplayer-close.ts
+++ src/interactions/commands/osu! and osu!droid/multiplayer/subcommands/multiplayer-close.ts
@@ -18,6 +18,7 @@ export const run: SlashSubcommand<true>["run"] = async (_, interaction) => {
             {
                 projection: {
                     _id: 0,
+                    channelId: 1,
                     "status.isPlaying": 1,
                     "settings.roomHost": 1,
                     "settings.forcedAR": 1,
