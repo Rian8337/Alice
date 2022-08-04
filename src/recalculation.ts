05e03646a7d47aad3cfc1d4e1a8cc8e0a7461136
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-08-04 22:09:01 +0700

    feat: add separate process for recalculations

---
+++ src/recalculation.ts
@@ -0,0 +1,40 @@
+import "module-alias/register";
+import { config } from "dotenv";
+import { DatabaseManager } from "@alice-database/DatabaseManager";
+import { UserBindCollectionManager } from "@alice-database/managers/elainaDb/UserBindCollectionManager";
+import { UserBind } from "@alice-database/utils/elainaDb/UserBind";
+import consola from "consola";
+process.env.UV_THREADPOOL_SIZE = "128";
+
+config();
+
+DatabaseManager.init().then(async () => {
+    const dbManager: UserBindCollectionManager =
+        DatabaseManager.elainaDb.collections.userBind;
+
+    const uncalculatedCount: number =
+        await dbManager.getRecalcUncalculatedPlayerCount();
+
+    if (uncalculatedCount === 0) {
+        process.exit(0);
+    }
+
+    let calculatedCount: number =
+        await dbManager.getRecalcCalculatedPlayerCount();
+
+    const total: number = calculatedCount + uncalculatedCount;
+
+    let player: UserBind | null;
+
+    while ((player = await dbManager.getRecalcUnscannedPlayers(1))) {
+        consola.info(`Now calculating ID ${player.discordid}`);
+
+        await player.recalculateAllScores(false, true);
+
+        consola.info(`${++calculatedCount}/${total} players recalculated (${(calculatedCount * 100 / total).toFixed(2)}%)`);
+    }
+
+    consola.info("Recalculation done");
+
+    process.exit(0);
+});
