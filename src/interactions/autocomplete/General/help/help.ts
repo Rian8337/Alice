fdfc2dd1c308248403e57ed89d1f18f7e814a18f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-09-02 10:39:54 +0700

    feat: use regex to filter command names instead

--- src/interactions/autocomplete/General/help/help.ts
+++ src/interactions/autocomplete/General/help/help.ts
@@ -3,9 +3,11 @@ import { AutocompleteHandler } from "@alice-structures/core/AutocompleteHandler"
 export const run: AutocompleteHandler["run"] = async (client, interaction) => {
     const focusedValue: string = interaction.options.getFocused();
 
+    const regExp: RegExp = new RegExp(focusedValue, "i");
+
     interaction.respond(
         [...client.interactions.chatInput.keys()]
-            .filter((v) => v.startsWith(focusedValue))
+            .filter((v) => regExp.test(v))
             .slice(0, 25)
             .map((v) => {
                 return {
