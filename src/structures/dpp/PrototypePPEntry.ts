40f11efaa37fa19df85ed46a4e9ea0ce62ee3512
Rian <52914632+Rian8337@users.noreply.github.com>
2022-10-02 20:59:31 +0700

    Remove two hand cursor index

--- src/structures/dpp/PrototypePPEntry.ts
+++ src/structures/dpp/PrototypePPEntry.ts
@@ -5,9 +5,4 @@ export interface PrototypePPEntry extends PPEntry {
      * The pp before the score was recalculated.
      */
     prevPP: number;
-
-    /**
-     * The cursor indexes of each hitobject.
-     */
-    cursorIndexes: number[];
 }
