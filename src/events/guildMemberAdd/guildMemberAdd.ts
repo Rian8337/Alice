1f5fdd8efd42d1e726d245294ee185d02546cffd
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-12-25 10:24:47 +0800

    feat: remove verification gate for main server

---
+++ src/events/guildMemberAdd/guildMemberAdd.ts
@@ -0,0 +1,13 @@
+import { GuildMember } from "discord.js";
+import { Event } from "structures/core/Event";
+import { EventHelper } from "@alice-utils/helpers/EventHelper";
+
+export const run: Event["run"] = async (client, member: GuildMember) => {
+    EventHelper.runUtilities(
+        client,
+        __dirname,
+        member.guild,
+        undefined,
+        member
+    ).catch((e: Error) => client.emit("error", e));
+};
---
+++ src/events/guildMemberAdd/utils/welcomeMessage.ts
@@ -0,0 +1,33 @@
+import { GuildBasedChannel, GuildMember } from "discord.js";
+import { EventUtil } from "structures/core/EventUtil";
+import { Constants } from "@alice-core/Constants";
+import { DatabaseManager } from "@alice-database/DatabaseManager";
+
+export const run: EventUtil["run"] = async (_, member: GuildMember) => {
+    if (member.guild.id !== Constants.mainServer) {
+        return;
+    }
+
+    const general: GuildBasedChannel | null = await member.guild.channels.fetch(Constants.mainServer);
+
+    if (general?.isTextBased()){
+        general.send({
+            content: `Welcome ${
+                (await DatabaseManager.elainaDb.collections.userBind.isUserBinded(
+                    member.id
+                ))
+                    ? "back "
+                    : ""
+            }to ${member.guild.name}, ${member}!`,
+            files: [Constants.welcomeImageLink],
+        });
+    }
+
+};
+
+export const config: EventUtil["config"] = {
+    description:
+        "Responsible for greeting new users to the server.",
+    togglePermissions: ["BotOwner"],
+    toggleScope: ["GLOBAL"],
+};
