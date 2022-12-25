b45c0fd2fe7c27ccaf2e235a8ce70775e3bd0350
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-12-25 11:21:43 +0800

    feat(dailyCycle): remove unverified prune

--- src/events/ready/utils/dailyCycle.ts
+++ src/events/ready/utils/dailyCycle.ts
@@ -1,12 +1,9 @@
-import { Bot } from "@alice-core/Bot";
 import { Config } from "@alice-core/Config";
-import { Constants } from "@alice-core/Constants";
 import { DatabaseManager } from "@alice-database/DatabaseManager";
 import { PlayerInfo } from "@alice-database/utils/aliceDb/PlayerInfo";
 import { EventUtil } from "structures/core/EventUtil";
 import { MessageAnalyticsHelper } from "@alice-utils/helpers/MessageAnalyticsHelper";
 import { CommandUtilManager } from "@alice-utils/managers/CommandUtilManager";
-import consola from "consola";
 
 async function resetDailyCoinsAndMapShare(): Promise<void> {
     await DatabaseManager.aliceDb.collections.playerInfo.updateOne(
@@ -26,40 +23,7 @@ async function resetDailyCoinsAndMapShare(): Promise<void> {
     );
 }
 
-async function kickUnverifiedMembers(client: Bot): Promise<void> {
-    consola.info("Checking for unverified members to prune");
-
-    const guild = await client.guilds.fetch(Constants.mainServer);
-
-    const members = await guild.members.fetch({ force: true });
-
-    const role = guild.roles.cache.find((r) => r.name === "Member")!;
-
-    const currentDate = Date.now();
-
-    const unverifiedMembers = members.filter(
-        (v) =>
-            !v.user.bot &&
-            !v.roles.cache.has(role.id) &&
-            currentDate - v.joinedTimestamp! >= 86400 * 7 * 1000
-    );
-
-    const totalKick = unverifiedMembers.size;
-
-    let kickedCount = 0;
-
-    consola.info("Kicking", totalKick, "members");
-
-    for (const [, member] of unverifiedMembers) {
-        await member.kick("Unverified prune");
-
-        consola.info(`${++kickedCount}/${totalKick} members kicked`);
-    }
-
-    consola.info("Pruning done");
-}
-
-export const run: EventUtil["run"] = async (client) => {
+export const run: EventUtil["run"] = async () => {
     const playerInfo: PlayerInfo =
         (await DatabaseManager.aliceDb.collections.playerInfo.getFromUser(
             "386742340968120321",
@@ -88,7 +52,6 @@ export const run: EventUtil["run"] = async (client) => {
 
         resetDailyCoinsAndMapShare();
         MessageAnalyticsHelper.fetchDaily((resetTime - 86400) * 1000);
-        kickUnverifiedMembers(client);
     }, 15 * 1000);
 };
 
