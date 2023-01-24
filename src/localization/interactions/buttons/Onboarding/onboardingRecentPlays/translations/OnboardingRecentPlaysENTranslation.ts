56e49f13d831d7b8062142f7718ee66d62bd2687
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-01-24 18:41:02 +0800

    feat(OnboardingRecentPlaysENTranslation): add note about context menu command

--- src/localization/interactions/buttons/Onboarding/onboardingRecentPlays/translations/OnboardingRecentPlaysENTranslation.ts
+++ src/localization/interactions/buttons/Onboarding/onboardingRecentPlays/translations/OnboardingRecentPlaysENTranslation.ts
@@ -8,7 +8,7 @@ export class OnboardingRecentPlaysENTranslation extends Translation<OnboardingRe
     override readonly translations: OnboardingRecentPlaysStrings = {
         embedTitle: "Display Recent Plays",
         recentPlaysIntroduction:
-            "One of the most commonly used feature of mine is showing a player's recent plays. This is accessible via the </recent:881019169603223584> and </recent5:881019186137169970> commands. These commands can only display plays that were submitted to the server, which requires you to have an osu!droid account.",
+            'One of the most commonly used feature of mine is showing a player\'s recent plays. This is accessible via the </recent:881019169603223584> and </recent5:881019186137169970> commands as well as the "View Recent Plays" button accessible in the apps menu. These commands can only display plays that were submitted to the server, which requires you to have an osu!droid account.',
         accountRegistrationQuote:
             "Don't have a registered osu!droid account yet? Register one [here](https://osudroid.moe/user/?action=register).",
         recentCommandExplanation:
@@ -16,7 +16,7 @@ export class OnboardingRecentPlaysENTranslation extends Translation<OnboardingRe
         recent5CommandExplanation:
             "The </recent5:881019186137169970> command will show your 50 most recent plays in a paginated way, each page containing 5 plays. Unlike the previous command, this command displays less information to keep the response short and concise.",
         accountBindConvenienceQuote:
-            'Both commands are more convenient to use if you bind your osu!droid account to Discord account. For more information, please refer to the "Bind osu!droid account" button in the first introduction menu.',
+            'These features are more convenient to use if you bind your osu!droid account to Discord account. For more information, please refer to the "Bind osu!droid account" button in the first introduction menu.',
         tryCommandsForBindedAccount:
             "If you have binded your osu!droid account, feel free to try both commands with the buttons below!",
         showMostRecentPlay: "Show most recent play",
