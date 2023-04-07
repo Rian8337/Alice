3f5dbad2e2c8f1ed2eb422b3e1924b1080067033
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-04-07 19:10:20 +0800

    Use `setInterval` for name change requests

--- src/events/ready/utils/acceptNameChange.ts
+++ src/events/ready/utils/acceptNameChange.ts
@@ -1,17 +1,22 @@
 import { DatabaseManager } from "@alice-database/DatabaseManager";
+import consola from "consola";
 import { EventUtil } from "structures/core/EventUtil";
 
 export const run: EventUtil["run"] = async () => {
-    const nameChanges =
-        await DatabaseManager.aliceDb.collections.nameChange.getActiveNameChangeRequests();
+    setInterval(async () => {
+        consola.info("Checking name changes");
 
-    for (const nameChange of nameChanges.values()) {
-        await nameChange.accept();
+        const nameChanges =
+            await DatabaseManager.aliceDb.collections.nameChange.getActiveNameChangeRequests();
 
-        console.log(`Processed uid ${nameChange.uid}`);
-    }
+        for (const nameChange of nameChanges.values()) {
+            await nameChange.accept();
 
-    console.log("Done");
+            consola.info(`Processed uid ${nameChange.uid}`);
+        }
+
+        consola.success("Done");
+    }, 60 * 10 * 1000);
 };
 
 export const config: EventUtil["config"] = {
