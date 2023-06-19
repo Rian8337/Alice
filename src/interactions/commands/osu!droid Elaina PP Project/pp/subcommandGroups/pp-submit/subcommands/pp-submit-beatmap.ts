e3504b7a47438d50cb35ca6188ba79ff39cfab3c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-06-19 13:21:35 +0800

    Reenable manual pp submission

--- src/interactions/commands/osu!droid Elaina PP Project/pp/subcommandGroups/pp-submit/subcommands/pp-submit-beatmap.ts
+++ src/interactions/commands/osu!droid Elaina PP Project/pp/subcommandGroups/pp-submit/subcommands/pp-submit-beatmap.ts
@@ -18,24 +18,11 @@ import { Symbols } from "@alice-enums/utils/Symbols";
 import { NumberHelper } from "@alice-utils/helpers/NumberHelper";
 import { PPSubmissionOperationResult } from "@alice-structures/dpp/PPSubmissionOperationResult";
 import { LocaleHelper } from "@alice-utils/helpers/LocaleHelper";
-import { Config } from "@alice-core/Config";
 
 export const run: SlashSubcommand<true>["run"] = async (_, interaction) => {
     const localization: PPLocalization = new PPLocalization(
         await CommandHelper.getLocale(interaction)
     );
-
-    if (!Config.isDebug) {
-        interaction.ephemeral = true;
-
-        return InteractionHelper.reply(interaction, {
-            content: MessageCreator.createReject(
-                new ConstantsLocalization(localization.language).getTranslation(
-                    Constants.notAvailableInChannelReject
-                )
-            ),
-        });
-    }
 
     await InteractionHelper.deferReply(interaction);
 
