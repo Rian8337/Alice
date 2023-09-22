80dec95b6f9575b6d730f7968dbf8deb8458d9eb
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-09-22 20:55:40 +0700

    Reenable manual pp submission

--- src/interactions/commands/osu!droid Elaina PP Project/pp/subcommandGroups/pp-submit/pp-submit.ts
+++ src/interactions/commands/osu!droid Elaina PP Project/pp/subcommandGroups/pp-submit/pp-submit.ts
@@ -6,14 +6,6 @@ import { CommandHelper } from "@alice-utils/helpers/CommandHelper";
 import { InteractionHelper } from "@alice-utils/helpers/InteractionHelper";
 
 export const run: SlashSubcommandGroup["run"] = async (_, interaction) => {
-    if (!Config.isDebug) {
-        return InteractionHelper.reply(interaction, {
-            content: MessageCreator.createReject(
-                "Command temporarily disabled while recalculation is in progress.",
-            ),
-        });
-    }
-
     if (
         !CommandHelper.isExecutedByBotOwner(interaction) &&
         !Config.ppChannel.includes(interaction.channelId)
