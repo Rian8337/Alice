d7a4f7ea11a5eca33babae984bb5e5ed69626ba9
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-01-28 21:36:15 +0800

    Refactor `Constants` to use `satisfies` keyword

--- src/core/Constants.ts
+++ src/core/Constants.ts
@@ -8,91 +8,94 @@ export class Constants {
     /**
      * Default message to send when a user doesn't meet required permissions to use a command.
      */
-    static readonly noPermissionReject: keyof ConstantsStrings =
-        "noPermissionToExecuteCommand";
+    static readonly noPermissionReject =
+        "noPermissionToExecuteCommand" satisfies keyof ConstantsStrings;
 
     /**
      * Default message to send when a Discord user doesn't have a bound osu!droid account.
      */
-    static readonly selfNotBindedReject: keyof ConstantsStrings =
-        "selfAccountNotBinded";
+    static readonly selfNotBindedReject =
+        "selfAccountNotBinded" satisfies keyof ConstantsStrings;
 
     /**
      * Default message to send when a command is not available in a server.
      */
-    static readonly notAvailableInServerReject: keyof ConstantsStrings =
-        "commandNotAvailableInServer";
+    static readonly notAvailableInServerReject =
+        "commandNotAvailableInServer" satisfies keyof ConstantsStrings;
 
     /**
      * Default message to send when a command is not available in a channel.
      */
-    static readonly notAvailableInChannelReject: keyof ConstantsStrings =
-        "commandNotAvailableInChannel";
+    static readonly notAvailableInChannelReject =
+        "commandNotAvailableInChannel" satisfies keyof ConstantsStrings;
 
     /**
      * Default message to send when a user (third-person) doesn't have a bound osu!droid account.
      */
-    static readonly userNotBindedReject: keyof ConstantsStrings =
-        "userAccountNotBinded";
+    static readonly userNotBindedReject =
+        "userAccountNotBinded" satisfies keyof ConstantsStrings;
 
     /**
      * The ID of main guild.
      */
-    static readonly mainServer: Snowflake = "316545691545501706";
+    static readonly mainServer = "316545691545501706" satisfies Snowflake;
 
     /**
      * The ID of testing guild.
      */
-    static readonly testingServer: Snowflake = "528941000555757598";
+    static readonly testingServer = "528941000555757598" satisfies Snowflake;
 
     /**
      * The ID of the lounge channel in the main server.
      */
-    static readonly loungeChannel: Snowflake = "927204556683771945";
+    static readonly loungeChannel = "927204556683771945" satisfies Snowflake;
 
     /**
      * The ID of the lounge role in the main server.
      */
-    static readonly loungeRole: Snowflake = "667403004118433793";
+    static readonly loungeRole = "667403004118433793" satisfies Snowflake;
 
     /**
      * The link to welcome image (used to welcome new members to the server).
      */
-    static readonly welcomeImageLink: string =
+    static readonly welcomeImageLink =
         "https://cdn.discordapp.com/attachments/316545691545501706/1101147416410984478/LLzteLz.jpg";
 
     /**
      * The uid limit that is used to check if a uid from a user's input is too small.
      */
-    static readonly uidMinLimit: number = 2417;
+    static readonly uidMinLimit = 2417;
 
     /**
      * The uid limit that is used to check if a uid from a user's input is too big.
      */
-    static readonly uidMaxLimit: number = 500000;
+    static readonly uidMaxLimit = 500000;
 
     /**
      * The ID of the Alice coins emote.
      */
-    static readonly aliceCoinEmote: Snowflake = "669532330980802561";
+    static readonly aliceCoinEmote = "669532330980802561" satisfies Snowflake;
 
     /**
      * The ID of the channel that is storing tag attachments.
      */
-    static readonly tagAttachmentChannel: Snowflake = "695521921441333308";
+    static readonly tagAttachmentChannel =
+        "695521921441333308" satisfies Snowflake;
 
     /**
      * The ID of the channel for managing map share submissions.
      */
-    static readonly mapShareChannel: Snowflake = "715423228461449297";
+    static readonly mapShareChannel = "715423228461449297" satisfies Snowflake;
 
     /**
      * The ID of the channel that is storing skin previews.
      */
-    static readonly skinPreviewChannel: Snowflake = "999480010459070505";
+    static readonly skinPreviewChannel =
+        "999480010459070505" satisfies Snowflake;
 
     /**
      * The ID of the linked role for displaying dpp statistics in user profiles.
      */
-    static readonly dppProfileDisplayerRole: Snowflake = "1082254268691644446";
+    static readonly dppProfileDisplayerRole =
+        "1082254268691644446" satisfies Snowflake;
 }
