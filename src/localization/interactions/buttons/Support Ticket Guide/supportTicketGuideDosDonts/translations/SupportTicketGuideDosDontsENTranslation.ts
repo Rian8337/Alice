42eb1c309f9ccc5b713d3cf8845c78fe18481f53
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-02-08 10:52:05 +0800

    Add specifier for private information

--- src/localization/interactions/buttons/Support Ticket Guide/supportTicketGuideDosDonts/translations/SupportTicketGuideDosDontsENTranslation.ts
+++ src/localization/interactions/buttons/Support Ticket Guide/supportTicketGuideDosDonts/translations/SupportTicketGuideDosDontsENTranslation.ts
@@ -15,7 +15,7 @@ export class SupportTicketGuideDosDontsENTranslation extends Translation<Support
         createTicketDos2:
             "Provide screenshots when necessary. This can be done after creating a ticket.",
         createTicketDontsHeader: "DON'Ts when creating a ticket:",
-        createTicketDonts1: `Including your private information. Apart from ${userMention(Config.botOwners[1])}, ${userMention(Config.botOwners[0])}, and ${userMention("210769870848000001")}, no one in the staff team has access to your private information (excluding your password). Some ticket presets will ask your private information such as email, but these information can only be seen by ${userMention(Config.botOwners[1])} and ${userMention("210769870848000001")}. Should you require private information to be submitted, please move to the Direct Messages of one of those people instead.`,
+        createTicketDonts1: `Including your private information. Apart from ${userMention(Config.botOwners[1])}, ${userMention(Config.botOwners[0])}, and ${userMention("210769870848000001")}, no one in the staff team has access to the private information of your osu!droid account (excluding your password). Some ticket presets will ask your private information such as email, but these information can only be seen by ${userMention(Config.botOwners[1])} and ${userMention("210769870848000001")}. Should you require private information to be submitted, please move to the Direct Messages of one of those people instead.`,
         createTicketDonts2: `Creating a ticket for game-related questions or bug reports. These matters should be posted in ${channelMention("1006369245577347082")} instead.`,
     };
 }
