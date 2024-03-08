44eccce231747020ee0e905d111f94e24f770ae8
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-03-08 23:19:23 +0700

    Disable pp submission for the time being

--- src/interactions/commands/osu!droid Elaina PP Project/pp/subcommandGroups/pp-submit/pp-submit.ts
+++ src/interactions/commands/osu!droid Elaina PP Project/pp/subcommandGroups/pp-submit/pp-submit.ts
@@ -6,6 +6,16 @@ import { CommandHelper } from "@alice-utils/helpers/CommandHelper";
 import { InteractionHelper } from "@alice-utils/helpers/InteractionHelper";
 
 export const run: SlashSubcommandGroup["run"] = async (_, interaction) => {
+    if (!Config.isDebug) {
+        interaction.ephemeral = true;
+
+        return InteractionHelper.reply(interaction, {
+            content: MessageCreator.createReject(
+                "This command is currently disabled while recalculation is progressing. Please wait patiently. Any submitted scores will still submit to the dpp system via the automatic submission system.",
+            ),
+        });
+    }
+
     if (
         !CommandHelper.isExecutedByBotOwner(interaction) &&
         !Config.ppChannel.includes(interaction.channelId)
