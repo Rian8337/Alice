2ddaa26205e326ffb4ca2c69679a8c935594200a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-05-17 11:00:23 +0800

    Fix accuracy in `ProfileCardCreator`

--- src/utils/creators/ProfileCardCreator.ts
+++ src/utils/creators/ProfileCardCreator.ts
@@ -402,7 +402,10 @@ export class ProfileCardCreator {
         if (this.bindInfo) {
             this.context.fillText(
                 `${this.localization.getTranslation("accuracy")}: ${
-                    this.player.accuracy
+                    this.player.accuracy /
+                    (this.player instanceof Player
+                        ? 1
+                        : this.player.playcount * 1000)
                 }% | ${this.bindInfo.weightedAccuracy.toFixed(2)}%`,
                 x,
                 y + yOffset,
@@ -410,7 +413,10 @@ export class ProfileCardCreator {
         } else {
             this.context.fillText(
                 `${this.localization.getTranslation("accuracy")}: ${
-                    this.player.accuracy
+                    this.player.accuracy /
+                    (this.player instanceof Player
+                        ? 1
+                        : this.player.playcount * 1000)
                 }%`,
                 x,
                 y + yOffset,
