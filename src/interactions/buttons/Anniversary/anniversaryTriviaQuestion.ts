7d71aefaf3f67b7e71fdaa789122caeb60e23844
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-05-23 01:36:00 +0800

    Don't allow previous question navigation after submission

--- src/interactions/buttons/Anniversary/anniversaryTriviaQuestion.ts
+++ src/interactions/buttons/Anniversary/anniversaryTriviaQuestion.ts
@@ -20,6 +20,10 @@ export const run: ButtonCommand["run"] = async (_, interaction) => {
     const questionId = parseInt(split[1]);
     const attemptIndex = split[2] ? parseInt(split[2]) : undefined;
 
+    if (attemptIndex !== undefined && player.currentAttempt === undefined) {
+        return;
+    }
+
     const question = CacheManager.anniversaryTriviaQuestions.get(questionId)!;
     const language = CommandHelper.getLocale(interaction);
 
