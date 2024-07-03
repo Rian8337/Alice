a4d0a71d06ac26adac4feb75be53136e0b24a6fd
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-07-03 21:54:09 +0800

    Add env file reloader

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import "module-alias/register";
 import { config } from "dotenv";
 import { Bot } from "@alice-core/Bot";
+import { watch } from "chokidar";
+import { join } from "path";
+import consola from "consola";
 process.env.UV_THREADPOOL_SIZE = "128";
 
 config();
 
-const bot: Bot = new Bot();
+// Reload .env file when it changes.
+watch(join(process.cwd(), ".env"), {
+    awaitWriteFinish: true,
+    ignoreInitial: true,
+}).on("change", () => {
+    config();
+
+    consola.info("Reloaded .env file.");
+});
+
+const bot = new Bot();
 
 bot.start();
