a7b3af4aa343877c28318221f41987c9ac34ccf1
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-07-14 03:24:17 +0800

    Remove automatic .env loader

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
 import "module-alias/register";
 import { config } from "dotenv";
 import { Bot } from "@alice-core/Bot";
-import { watch } from "chokidar";
-import { join } from "path";
-import consola from "consola";
 process.env.UV_THREADPOOL_SIZE = "128";
 
 config();
-
-// Reload .env file when it changes.
-watch(join(process.cwd(), ".env"), {
-    awaitWriteFinish: true,
-    ignoreInitial: true,
-}).on("change", () => {
-    config();
-
-    consola.info("Reloaded .env file.");
-});
 
 const bot = new Bot();
 
