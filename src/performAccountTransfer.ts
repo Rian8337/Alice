d2a18d5000fa793b527967f0f925740b63d49871
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-10-17 08:05:34 +0800

    Use try-catch in account transfer script

--- src/performAccountTransfer.ts
+++ src/performAccountTransfer.ts
@@ -29,70 +29,83 @@ Promise.all([DatabaseManager.init(), officialPool.connect()]).then(async () => {
     for (const transfer of transfers.values()) {
         const connection = await officialPool.getConnection();
 
-        await connection.beginTransaction();
+        try {
+            await connection.beginTransaction();
 
-        for (const uidToTransfer of transfer.transferList) {
-            // Mark the uid as archived.
+            for (const uidToTransfer of transfer.transferList) {
+                // Mark the uid as archived.
+                await connection.query(
+                    `UPDATE ${userTable} SET archived = 1 WHERE id = ?`,
+                    [uidToTransfer],
+                );
+
+                // Transfer the scores.
+                for (const table of scoreTables) {
+                    await connection.query(
+                        `UPDATE ${table} SET uid = ? WHERE uid = ?`,
+                        [transfer.transferUid, uidToTransfer],
+                    );
+                }
+            }
+
+            const scoreTable = scoreTables[0];
+            const valuesArr = [transfer.transferUid, transfer.transferUid];
+
             await connection.query(
-                `UPDATE ${userTable} SET archived = 1 WHERE id = ?`,
-                [uidToTransfer],
+                `UPDATE ${userTable} SET score = (SELECT SUM(score) FROM ${scoreTable} WHERE uid = ?) WHERE id = ?`,
+                valuesArr,
             );
 
-            // Transfer the scores.
-            for (const table of scoreTables) {
-                await connection.query(
-                    `UPDATE ${table} SET uid = ? WHERE uid = ?`,
-                    [transfer.transferUid, uidToTransfer],
+            const topScores = await connection
+                .query<
+                    RowDataPacket[]
+                >(`SELECT accuracy FROM ${scoreTable} WHERE uid = ? AND score > 0 ORDER BY pp DESC LIMIT 100`, [transfer.transferUid])
+                .then(
+                    (res) =>
+                        res[0] as Pick<OfficialDatabaseScore, "accuracy">[],
                 );
+
+            let accuracy = 0;
+            let accuracyWeight = 0;
+
+            for (let i = 0; i < topScores.length; ++i) {
+                const weight = Math.pow(0.95, i);
+
+                accuracy += topScores[i].accuracy * weight;
+                accuracyWeight += weight;
             }
+
+            accuracy /= accuracyWeight;
+
+            await connection.query(
+                `UPDATE ${userTable} SET accuracy = ${accuracy} WHERE id = ?`,
+                valuesArr,
+            );
+
+            await connection.query(
+                `UPDATE ${userTable} SET playcount = (SELECT COUNT(*) FROM ${scoreTable} WHERE uid = ? AND score > 0) WHERE id = ?`,
+                valuesArr,
+            );
+
+            await connection.commit();
+
+            console.log(
+                `Transfer for ${transfer.discordId} has been completed.`,
+            );
+        } catch (e) {
+            console.error(
+                `Failed to transfer account for ${transfer.discordId}: ${e}`,
+            );
+
+            await connection.rollback();
+        } finally {
+            connection.release();
         }
-
-        const scoreTable = scoreTables[0];
-        const valuesArr = [transfer.transferUid, transfer.transferUid];
-
-        await connection.query(
-            `UPDATE ${userTable} SET score = (SELECT SUM(score) FROM ${scoreTable} WHERE uid = ?) WHERE id = ?`,
-            valuesArr,
-        );
-
-        const topScores = await connection
-            .query<
-                RowDataPacket[]
-            >(`SELECT accuracy FROM ${scoreTable} WHERE uid = ? AND score > 0 ORDER BY pp DESC LIMIT 100`, [transfer.transferUid])
-            .then((res) => res[0] as Pick<OfficialDatabaseScore, "accuracy">[]);
-
-        let accuracy = 0;
-        let accuracyWeight = 0;
-
-        for (let i = 0; i < topScores.length; ++i) {
-            const weight = Math.pow(0.95, i);
-
-            accuracy += topScores[i].accuracy * weight;
-            accuracyWeight += weight;
-        }
-
-        accuracy /= accuracyWeight;
-
-        await connection.query(
-            `UPDATE ${userTable} SET accuracy = ${accuracy} WHERE id = ?`,
-            valuesArr,
-        );
-
-        await connection.query(
-            `UPDATE ${userTable} SET playcount = (SELECT COUNT(*) FROM ${scoreTable} WHERE uid = ? AND score > 0) WHERE id = ?`,
-            valuesArr,
-        );
-
-        await connection.commit();
-
-        connection.release();
 
         await dbManager.updateOne(
             { discordId: transfer.discordId },
             { $set: { transferDone: true } },
         );
-
-        console.log(`Transfer for ${transfer.discordId} has been completed.`);
     }
 
     console.log("All transfers have been completed.");
